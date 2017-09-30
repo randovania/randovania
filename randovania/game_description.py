08e91b459391e1108e056d3e4703ce86a57b3bd9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-09-30 20:22:13 -0300

    Adding world parser.

---
+++ randovania/game_description.py
@@ -0,0 +1,89 @@
+from typing import NamedTuple, List, Dict, Union
+
+
+class RequirementInfo(NamedTuple):
+    index: int
+    long_name: str
+    short_name: str
+
+
+class DamageReduction(NamedTuple):
+    inventory_index: int
+    damage_multiplier: float
+
+
+class DamageRequirementInfo(NamedTuple):
+    index: int
+    long_name: str
+    short_name: str
+    reductions: List[DamageReduction]
+
+
+class IndividualRequirement(NamedTuple):
+    requirement_type: int
+    requirement_index: int
+    amount: int
+    negate: bool
+
+
+class RequirementSet(NamedTuple):
+    alternatives: List[IndividualRequirement]
+
+
+class DockWeakness(NamedTuple):
+    index: int
+    name: str
+    is_blast_shield: bool
+    requirements: RequirementSet
+
+
+class GenericNode(NamedTuple):
+    name: str
+    heal: bool
+
+
+class DockNode(NamedTuple):
+    name: str
+    heal: bool
+    dock_index: int
+    connected_area_asset_id: int
+    connected_dock_index: int
+    dock_type: int
+    dock_weakness_index: int
+
+
+class PickupNode(NamedTuple):
+    name: str
+    heal: bool
+    pickup_index: int
+
+
+class TeleporterNode(NamedTuple):
+    name: str
+    heal: bool
+    destination_world_asset_id: int
+    destination_area_asset_id: int
+    teleporter_instance_id: int
+
+
+class EventNode(NamedTuple):
+    name: str
+    heal: bool
+    event_index: int
+
+
+Node = Union[GenericNode, DockNode, PickupNode, TeleporterNode, EventNode]
+
+
+class Area(NamedTuple):
+    name: str
+    area_asset_id: int
+    default_node_index: int
+    nodes: List[Node]
+    connections: Dict[int, Dict[int, RequirementSet]]
+
+
+class World(NamedTuple):
+    name: str
+    world_asset_id: int
+    areas: List[Area]
