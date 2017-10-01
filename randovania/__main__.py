107647a0f08d57d50b7015e0ec7532f5dcb5afa3
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-01 13:40:52 -0300

    Fixes

--- randovania/__main__.py
+++ randovania/__main__.py
@@ -1,8 +1,9 @@
 import argparse
-
 import os
+import pprint
 
-from randovania import log_parser, impossible_check, datareader, resolver
+from randovania import log_parser, datareader, resolver
+from randovania.game_description import consistency_check
 from randovania.log_parser import Game
 
 
@@ -18,6 +19,8 @@ def main():
     if game == Game.PRIME2:
         game_description = datareader.read(os.path.join(os.path.dirname(__file__), "data", "prime2.bin"),
                                            log.pickup_entries)
+
+        # pprint.pprint(consistency_check(game_description))
         resolver.resolve(game_description)
 
 
