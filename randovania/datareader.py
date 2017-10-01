e0661030c8761681793f338528b61a5a64a025f4
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-09-30 23:20:45 -0300

    Add BinarySource

--- randovania/datareader.py
+++ randovania/datareader.py
@@ -3,111 +3,83 @@ import struct
 from functools import partial
 from typing import List
 
+from randovania.binary_file_reader import BinarySource
 from randovania.game_description import DamageReduction, SimpleResourceInfo, DamageResourceInfo, \
     IndividualRequirement, \
     DockWeakness, RequirementSet, World, Area, Node, GenericNode, DockNode, PickupNode, TeleporterNode, EventNode, \
     RandomizerFileData, ResourceType, ResourceDatabase, DockType, DockWeaknessDatabase
 
 
-def read_byte(file) -> int:
-    return struct.unpack("!B", file.read(1))[0]
-
-
-def read_short(file) -> int:
-    return struct.unpack("!H", file.read(2))[0]
-
-
-def read_uint(file) -> int:
-    return struct.unpack("!I", file.read(4))[0]
-
-
-def read_float(file) -> float:
-    return struct.unpack("!f", file.read(4))[0]
-
-
-def read_bool(file) -> bool:
-    return struct.unpack("!?", file.read(1))[0]
-
-
-def read_string(file) -> str:
-    chars = []
-    while True:
-        c = file.read(1)
-        if c[0] == 0:
-            return b"".join(chars).decode("UTF-8")
-        chars.append(c)
-
-
-def read_array(x, item_reader):
-    count = read_byte(x)
+def read_array(source: BinarySource, item_reader):
+    count = source.read_byte()
     return [
-        item_reader(x)
+        item_reader(source)
         for _ in range(count)
     ]
 
 
-def read_damage_reduction(x) -> DamageReduction:
-    index = read_byte(x)
-    multiplier = read_float(x)
+def read_damage_reduction(source: BinarySource) -> DamageReduction:
+    index = source.read_byte()
+    multiplier = source.read_float()
     return DamageReduction(index, multiplier)
 
 
-def read_damage_reductions(x) -> List[DamageReduction]:
-    return read_array(x, read_damage_reduction)
+def read_damage_reductions(source: BinarySource) -> List[DamageReduction]:
+    return read_array(source, read_damage_reduction)
 
 
-def read_resource_info(x) -> SimpleResourceInfo:
-    index = read_byte(x)
-    long_name = read_string(x)
-    short_name = read_string(x)
+def read_resource_info(source: BinarySource) -> SimpleResourceInfo:
+    index = source.read_byte()
+    long_name = source.read_string()
+    short_name = source.read_string()
     return SimpleResourceInfo(index, long_name, short_name)
 
 
-def read_resource_info_array(x) -> List[SimpleResourceInfo]:
-    return read_array(x, read_resource_info)
+def read_resource_info_array(source: BinarySource) -> List[SimpleResourceInfo]:
+    return read_array(source, read_resource_info)
 
 
-def read_damage_resource_info(x) -> DamageResourceInfo:
-    index = read_byte(x)
-    long_name = read_string(x)
-    short_name = read_string(x)
-    return DamageResourceInfo(index, long_name, short_name, read_damage_reductions(x))
+def read_damage_resource_info(source: BinarySource) -> DamageResourceInfo:
+    index = source.read_byte()
+    long_name = source.read_string()
+    short_name = source.read_string()
+    return DamageResourceInfo(index, long_name, short_name, read_damage_reductions(source))
 
 
-def read_damage_resource_info_array(x) -> List[DamageResourceInfo]:
-    return read_array(x, read_damage_resource_info)
+def read_damage_resource_info_array(source: BinarySource) -> List[DamageResourceInfo]:
+    return read_array(source, read_damage_resource_info)
 
 
 # Requirement
 
-def read_individual_requirement(x, resource_database: ResourceDatabase) -> IndividualRequirement:
-    requirement_type = ResourceType(read_byte(x))
-    requirement_index = read_byte(x)
-    amount = read_short(x)
-    negate = read_bool(x)
+def read_individual_requirement(source: BinarySource, resource_database: ResourceDatabase) -> IndividualRequirement:
+    requirement_type = ResourceType(source.read_byte())
+    requirement_index = source.read_byte()
+    amount = source.read_short()
+    negate = source.read_bool()
     return IndividualRequirement.with_data(resource_database, requirement_type, requirement_index, amount,
                                            negate)
 
 
-def read_requirement_list(x, resource_database: ResourceDatabase) -> List[IndividualRequirement]:
-    return read_array(x, partial(read_individual_requirement, resource_database=resource_database))
+def read_requirement_list(source: BinarySource, resource_database: ResourceDatabase) -> List[IndividualRequirement]:
+    return read_array(source, partial(read_individual_requirement, resource_database=resource_database))
 
 
-def read_requirement_set(x, resource_database: ResourceDatabase) -> RequirementSet:
-    return RequirementSet(read_array(x, partial(read_requirement_list, resource_database=resource_database)))
+def read_requirement_set(source: BinarySource, resource_database: ResourceDatabase) -> RequirementSet:
+    return RequirementSet(read_array(source, partial(read_requirement_list, resource_database=resource_database)))
 
 
 # Dock Weakness
 
-def read_dock_weakness_database(x, resource_database: ResourceDatabase) -> DockWeaknessDatabase:
-    def read_dock_weakness(x) -> DockWeakness:
-        index = read_byte(x)
-        name = read_string(x)
-        requirement_set = read_requirement_set(x, resource_database)
+def read_dock_weakness_database(source: BinarySource, resource_database: ResourceDatabase) -> DockWeaknessDatabase:
+    def read_dock_weakness(source: BinarySource) -> DockWeakness:
+        index = source.read_byte()
+        name = source.read_string()
+        requirement_set = read_requirement_set(source, resource_database)
         return DockWeakness(index, name, False, requirement_set)
 
-    door_types = read_array(x, read_dock_weakness)
-    portal_types = read_array(x, read_dock_weakness)
+    door_types = read_array(source, read_dock_weakness)
+    portal_types = read_array(source, read_dock_weakness)
     return DockWeaknessDatabase(
         door=door_types,
         morph_ball=[DockWeakness(0, "Morph Ball Door", False, RequirementSet([[]]))],
@@ -124,104 +96,106 @@ class WorldReader:
         self.resource_database = resource_database
         self.dock_weakness_database = dock_weakness_database
 
-    def read_node(self, x) -> Node:
-        name = read_string(x)
-        heal = read_bool(x)
-        node_type = read_byte(x)
+    def read_node(self, source: BinarySource) -> Node:
+        name = source.read_string()
+        heal = source.read_bool()
+        node_type = source.read_byte()
 
         if node_type == 0:
-            x.read(2)  # Throw 2 bytes away
+            source.skip(2)
             return GenericNode(name, heal)
 
         elif node_type == 1:
-            dock_index = read_byte(x)
-            connected_area_asset_id = read_uint(x)
-            connected_dock_index = read_byte(x)
-            dock_type = DockType(read_byte(x))
-            dock_weakness_index = read_byte(x)
-            x.read(5)  # Throw 5 bytes away
+            dock_index = source.read_byte()
+            connected_area_asset_id = source.read_uint()
+            connected_dock_index = source.read_byte()
+            dock_type = DockType(source.read_byte())
+            dock_weakness_index = source.read_byte()
+            source.skip(5)
             return DockNode(name, heal, dock_index, connected_area_asset_id, connected_dock_index,
                             self.dock_weakness_database.get_by_type_and_index(dock_type, dock_weakness_index))
 
         elif node_type == 2:
-            pickup_index = read_byte(x)
-            x.read(2)  # Throw 2 bytes away
+            pickup_index = source.read_byte()
+            source.skip(2)
             return PickupNode(name, heal, pickup_index)
 
         elif node_type == 3:
-            destination_world_asset_id = read_uint(x)
-            destination_area_asset_id = read_uint(x)
-            teleporter_instance_id = read_uint(x)
-            x.read(2)  # Throw 2 bytes away
+            destination_world_asset_id = source.read_uint()
+            destination_area_asset_id = source.read_uint()
+            teleporter_instance_id = source.read_uint()
+            source.skip(2)
             return TeleporterNode(name, heal, destination_world_asset_id, destination_area_asset_id,
                                   teleporter_instance_id)
 
         elif node_type == 4:
-            event_index = read_byte(x)
-            x.read(2)  # Throw 2 bytes away
+            event_index = source.read_byte()
+            source.skip(2)
             return EventNode(name, heal, event_index)
 
         else:
             raise Exception("Unknown node type: {}".format(node_type))
 
-    def read_area(self, x) -> Area:
-        name = read_string(x)
-        asset_id = read_uint(x)
-        node_count = read_byte(x)
-        default_node_index = read_byte(x)
+    def read_area(self, source: BinarySource) -> Area:
+        name = source.read_string()
+        asset_id = source.read_uint()
+        node_count = source.read_byte()
+        default_node_index = source.read_byte()
         nodes = [
-            self.read_node(x)
+            self.read_node(source)
             for _ in range(node_count)
         ]
         connections = {
-            source: {
-                target: read_requirement_set(x, self.resource_database)
+            origin: {
+                target: read_requirement_set(source, self.resource_database)
                 for target in range(node_count)
-                if source != target
+                if origin != target
             }
-            for source in range(node_count)
+            for origin in range(node_count)
         }
         return Area(name, asset_id, default_node_index, nodes, connections)
 
-    def read_area_list(self, x) -> List[Area]:
-        return read_array(x, self.read_area)
+    def read_area_list(self, source: BinarySource) -> List[Area]:
+        return read_array(source, self.read_area)
 
-    def read_world(self, x) -> World:
-        name = read_string(x)
-        asset_id = read_uint(x)
-        areas = self.read_area_list(x)
+    def read_world(self, source: BinarySource) -> World:
+        name = source.read_string()
+        asset_id = source.read_uint()
+        areas = self.read_area_list(source)
         return World(name, asset_id, areas)
 
-    def read_world_list(self, x) -> List[World]:
-        return read_array(x, self.read_world)
+    def read_world_list(self, source: BinarySource) -> List[World]:
+        return read_array(source, self.read_world)
 
 
 def parse_file(x) -> RandomizerFileData:
     if x.read(4) != b"Req.":
         raise Exception("Invalid file format.")
+    
+    source = BinarySource(x)
 
-    format_version = read_uint(x)
+    format_version = source.read_uint()
     if format_version != 5:
         raise Exception("Unsupported format version: {}, expected 5".format(format_version))
 
-    game = read_byte(x)
-    game_name = read_string(x)
+    game = source.read_byte()
+    game_name = source.read_string()
 
-    items = read_resource_info_array(x)
-    events = read_resource_info_array(x)
-    tricks = read_resource_info_array(x)
-    damage = read_damage_resource_info_array(x)
-    misc = read_resource_info_array(x)
+    items = read_resource_info_array(source)
+    events = read_resource_info_array(source)
+    tricks = read_resource_info_array(source)
+    damage = read_damage_resource_info_array(source)
+    misc = read_resource_info_array(source)
     # File seems to have a mistake here: no count for difficulty.
-    difficulty = [SimpleResourceInfo(read_byte(x), read_string(x), read_string(x))]
-    versions = read_resource_info_array(x)
+    difficulty = [SimpleResourceInfo(source.read_byte(), source.read_string(), source.read_string())]
+    versions = read_resource_info_array(source)
 
     resource_database = ResourceDatabase(item=items, event=events, trick=tricks, damage=damage, version=versions,
                                          misc=misc, difficulty=difficulty)
 
-    dock_weakness_database = read_dock_weakness_database(x, resource_database)
+    dock_weakness_database = read_dock_weakness_database(source, resource_database)
     world_reader = WorldReader(resource_database, dock_weakness_database)
-    worlds = world_reader.read_world_list(x)
+    worlds = world_reader.read_world_list(source)
 
     return RandomizerFileData(
         game=game,
