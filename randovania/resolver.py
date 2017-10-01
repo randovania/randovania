9dd3a49ee4df580fb91e0853d4b75fb436cf448e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-01 18:08:14 -0300

    Add state, and implement collect_pickup function.

--- randovania/resolver.py
+++ randovania/resolver.py
@@ -2,8 +2,10 @@ import copy
 from typing import Set, Iterator, Tuple, List
 
 from randovania.game_description import GameDescription, ResourceType, Node, CurrentResources, DockNode, TeleporterNode, \
-    RequirementSet, Area, EventNode, resolve_dock_node, resolve_teleporter_node, PickupNode, ResourceInfo
+    RequirementSet, Area, EventNode, resolve_dock_node, resolve_teleporter_node, PickupNode, ResourceInfo, \
+    ResourceDatabase
 from randovania.log_parser import PickupEntry
+from randovania.pickup_database import pickup_name_to_resource_gain
 
 default_items = {
     (ResourceType.MISC, 0): 1,  # "No Requirements"
@@ -34,20 +36,52 @@ class State:
     resources: CurrentResources
     pickups: Set[PickupEntry]
 
+    def __init__(self, resources: CurrentResources, pickups: Set[PickupEntry]):
+        self.resources = resources
+        self.pickups = pickups
+
     def has_pickup(self, pickup: PickupEntry) -> bool:
         return pickup in self.pickups
 
     def event_triggered(self, event: ResourceInfo) -> bool:
         return self.resources.get(event, 0) > 0
 
+    def collect_pickup(self, pickup: PickupEntry, resource_database: ResourceDatabase) -> "State":
+        if pickup in self.pickups:
+            raise ValueError("Trying to collect an already collected pickup '{}'".format(pickup))
+
+        new_resources = copy.copy(self.resources)
+        new_pickups = copy.copy(self.pickups)
+
+        new_pickups.add(pickup)
+        for resource, quantity in pickup_name_to_resource_gain(pickup.item, resource_database):
+            new_resources[resource] = new_resources.get(resource, 0)
+            new_resources[resource] += quantity
+
+        return State(new_resources, new_pickups)
+
+    def trigger_event(self, event: ResourceInfo) -> "State":
+        if self.event_triggered(event):
+            raise ValueError("Trying to trigger already triggered event '{}'".format(event))
+
+        new_resources = copy.copy(self.resources)
+        new_pickups = copy.copy(self.pickups)
+
+        new_resources[event] = 1
+        return State(new_resources, new_pickups)
+
 
 def potential_nodes_from(node: Node,
                          game_description: GameDescription,
-                         current_resources: CurrentResources) -> Iterator[Tuple[Node, RequirementSet]]:
-
+                         current_state: State) -> Iterator[Tuple[Node, RequirementSet]]:
     if isinstance(node, EventNode):
         # You can't walk through an event node until you've already triggered it
-        if current_resources.get(node.event, 0) == 0:
+        if not current_state.event_triggered(node.event):
+            return
+
+    if isinstance(node, PickupNode):
+        # You need to get the pickup to pass by a pickup node
+        if not current_state.has_pickup(node.pickup):
             return
 
     if isinstance(node, DockNode):
@@ -73,7 +107,7 @@ def potential_nodes_from(node: Node,
 
 
 def calculate_reach(current_reach: Reach, game_description: GameDescription,
-                    current_resources: CurrentResources) -> Reach:
+                    current_state: State) -> Reach:
     new_reach = set()
 
     checked_nodes = set()
@@ -86,12 +120,12 @@ def calculate_reach(current_reach: Reach, game_description: GameDescription,
 
         print("> Checking paths from {}".format(_n(node)))
 
-        for target_node, requirements in potential_nodes_from(node, game_description, current_resources):
+        for target_node, requirements in potential_nodes_from(node, game_description, current_state):
             if target_node in checked_nodes or target_node in nodes_to_check:
                 print("Not checking {} again.".format(_n(target_node)))
                 continue
 
-            if requirements.satisfied(current_resources):
+            if requirements.satisfied(current_state.resources):
                 print("Requirements for {} satisfied.".format(_n(target_node)))
                 nodes_to_check.append(target_node)
             else:
@@ -118,7 +152,7 @@ def pretty_print_area(area: Area):
     print(area.name)
     for node in area.nodes:
         print(">", node.name, type(node))
-        for target_node, requirements in potential_nodes_from(node, _gd, {}):
+        for target_node, requirements in potential_nodes_from(node, _gd, State({}, set())):
             print(" >", _n(target_node))
             for r in requirements.alternatives:
                 print("  ", ", ".join(map(str, r)))
@@ -130,30 +164,31 @@ def resolve(game_description: GameDescription):
     starting_area_asset_id = 1655756413
     item_loss_skip = True
 
+    # global state for easy printing functions
     global _gd
     _gd = game_description
 
     starting_world = game_description.world_by_asset_id(starting_world_asset_id)
     starting_area = starting_world.area_by_asset_id(starting_area_asset_id)
     starting_node = starting_area.nodes[starting_area.default_node_index]
+    starting_state = State({
+        # "No Requirements"
+        game_description.resource_database.get_by_type_and_index(ResourceType.MISC, 0): 1
+    }, set())
 
-    starting_items = copy.copy(default_items)
+    current_state = starting_state.collect_pickup(PickupEntry(None, None, "_StartingItems"),
+                                                  game_description.resource_database)
     if item_loss_skip:
-        starting_items.update(items_lost_to_item_loss)
+        current_state = starting_state.collect_pickup(PickupEntry(None, None, "_ItemLossItems"),
+                                                      game_description.resource_database)
 
-    current_resources = {
-        game_description.resource_database.get_by_type_and_index(resource_type, index): quantity
-        for ((resource_type, index), quantity) in starting_items.items()
-    }
     reach = {
         starting_node
     }
-    new_reach = calculate_reach(reach, game_description, current_resources)
+    new_reach = calculate_reach(reach, game_description, current_state)
     #
     # print("====")
     # for node in new_reach:
     #     print(_n(node))
     # print("====")
     # pretty_print_area(starting_world.areas[2])
-
-
