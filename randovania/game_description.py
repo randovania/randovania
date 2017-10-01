f69720b5328f18c2f3015af6d3e3518bd580512d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-09-30 22:53:39 -0300

    Storing DockWeakness in DockNode.

--- randovania/game_description.py
+++ randovania/game_description.py
@@ -114,6 +114,16 @@ class DockWeakness(NamedTuple):
     is_blast_shield: bool
     requirements: RequirementSet
 
+    def __repr__(self):
+        return self.name
+
+
+def _find_dock_weakness_with_id(info_list: List[DockWeakness], index: int) -> DockWeakness:
+    for info in info_list:
+        if info.index == index:
+            return info
+    raise ValueError("Dock weakness with index {} not found in {}".format(index, info_list))
+
 
 @unique
 class DockType(Enum):
@@ -130,16 +140,19 @@ class DockWeaknessDatabase(NamedTuple):
     portal: List[DockWeakness]
 
     def get_by_type(self, dock_type: DockType) -> List[DockWeakness]:
-        if dock_type == ResourceType.ITEM:
+        if dock_type == DockType.DOOR:
             return self.door
-        elif dock_type == ResourceType.EVENT:
+        elif dock_type == DockType.MORPH_BALL_DOOR:
             return self.morph_ball
-        elif dock_type == ResourceType.TRICK:
+        elif dock_type == DockType.OTHER:
             return self.other
-        elif dock_type == ResourceType.DAMAGE:
+        elif dock_type == DockType.PORTAL:
             return self.portal
         else:
             raise ValueError("Invalid dock_type: {}".format(dock_type))
+
+    def get_by_type_and_index(self, dock_type: DockType, weakness_index: int) -> DockWeakness:
+        return _find_dock_weakness_with_id(self.get_by_type(dock_type), weakness_index)
 
 
 class GenericNode(NamedTuple):
@@ -153,8 +166,7 @@ class DockNode(NamedTuple):
     dock_index: int
     connected_area_asset_id: int
     connected_dock_index: int
-    dock_type: DockType
-    dock_weakness_index: int
+    dock_weakness: DockWeakness
 
 
 class PickupNode(NamedTuple):
