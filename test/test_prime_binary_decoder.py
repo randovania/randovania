0750418d750b745e40fcc8ce43dce8b03f1fa048
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-02 21:43:08 -0300

    Assert some round trips

--- test/test_prime_binary_decoder.py
+++ test/test_prime_binary_decoder.py
@@ -7,7 +7,32 @@ from randovania import prime_binary_decoder
 
 def test_simple_round_trip():
     sample_data = {
+        "game": 2,
+        "game_name": "Metroid Prime 2: Echoes",
+        "resource_database": {
+            "items": [],
+            "events": [],
+            "tricks": [],
+            "damage": [],
+            "versions": [],
+            "misc": [],
+            "difficulty": [],
+        },
+        "dock_weakness_database": {
+            "door": [],
+            "portal": []
+        },
+        "worlds": [],
     }
+
+    b = io.BytesIO()
+    b_io = b  # type: BinaryIO
+    prime_binary_decoder.encode(sample_data, b_io)
+
+    b.seek(0)
+    decoded = prime_binary_decoder.decode(b_io)
+
+    assert sample_data == decoded
 
 
 def test_complex_encode(test_files_dir):
@@ -18,3 +43,8 @@ def test_complex_encode(test_files_dir):
 
     # Run
     prime_binary_decoder.encode(data, b_io)
+
+    # Assert
+    assert test_files_dir.join("prime_data_as_binary.bin").read_binary() == b.getvalue()
+
+
