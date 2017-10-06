26f1c1c9e5f4cbaa0357d1793f1ab8df520c86f8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-05 23:24:06 -0300

    Make empty game description impossible

--- test/test_game_description.py
+++ test/test_game_description.py
@@ -2,4 +2,4 @@ from randovania.game_description import RequirementSet
 
 
 def test_empty_requirement_set():
-    assert RequirementSet.empty().satisfied({})
+    assert not RequirementSet([]).satisfied({})
