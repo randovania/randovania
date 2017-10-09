edc7e8fcd87066e56990953ae3aba5dc8b0a0634
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-09 00:31:46 -0300

    `Fix missing integer overflow in RNG

---
+++ test/test_random.py
@@ -0,0 +1,11 @@
+import pprint
+
+import randovania.games.prime.random as rng
+
+
+def test_random_a():
+    r = rng.Random(1965278358)
+    for i in range(20):
+        arg = 119 - i
+        num = r.next_with_max(arg)
+        assert num <= arg
