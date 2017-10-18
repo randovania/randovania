031513ee33f52004ba74a42d4ab161e38884eaf6
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-17 23:02:14 -0200

    0.7.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='randovania',
-    version='0.7.0',
+    version='0.7.1',
     author='Henrique Gemignani',
     url='https://github.com/henriquegemignani/randovania',
     description='A randomizer validator for the Metroid Prime series.',
