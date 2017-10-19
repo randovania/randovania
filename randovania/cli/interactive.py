396e3b3cc9477fe648bff9138efea4d998b9e75d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2017-10-19 02:27:25 -0200

    Making more user friendly

--- randovania/cli/interactive.py
+++ randovania/cli/interactive.py
@@ -2,6 +2,7 @@ import json
 import os
 import subprocess
 from argparse import ArgumentParser
+from collections import OrderedDict
 from typing import BinaryIO, List
 
 import py
@@ -10,11 +11,13 @@ from randovania import get_data_path
 from randovania.games.prime import binary_data
 from randovania.resolver.echoes import search_seed, RandomizerConfiguration, ResolverConfiguration
 
+MAX_DIFFICULTY = 5
+
 
 def load_options_to(current_options):
     try:
         with open(os.path.expanduser("~/.config/randovania.json")) as options_file:
-            new_options = json.load(options_file)
+            new_options = json.load(options_file)["options"]
 
         for option_name in current_options.keys():
             if option_name in new_options:
@@ -28,7 +31,10 @@ def save_options(options):
     config_folder = py.path.local(os.path.expanduser("~/.config"))
     config_folder.ensure_dir()
     with config_folder.join("randovania.json").open("w") as options_file:
-        json.dump(options, options_file)
+        json.dump({
+            "version": 1,
+            "options": options
+        }, options_file)
 
 
 def value_parser_int(s: str) -> int:
@@ -99,13 +105,64 @@ def apply_seed(randomizer_config: RandomizerConfiguration,
     subprocess.run(args, check=True)
 
 
+def validate_difficulty(difficulty: int):
+    if difficulty < 0:
+        raise ValueError("Difficulty should be at least 0")
+    if difficulty > MAX_DIFFICULTY:
+        raise ValueError("Difficulty should be at most {}".format(MAX_DIFFICULTY))
+
+
+def validate_min_difficulty(difficulty: int, current_options):
+    validate_difficulty(difficulty)
+    if difficulty > current_options["max_difficulty"]:
+        raise ValueError("Min difficulty should be the same or smaller"
+                         " than min difficulty ({})".format(current_options["min_difficulty"]))
+
+
+def validate_max_difficulty(difficulty: int, current_options):
+    validate_difficulty(difficulty)
+    if difficulty < current_options["min_difficulty"]:
+        raise ValueError("Max difficulty should be the same or higher"
+                         " than min difficulty ({})".format(current_options["min_difficulty"]))
+
+
+def validate_pickup_list(pickup_list: List[int], current_options):
+    pass
+
+
+def validate_iso_path(iso_path: str, options):
+    try:
+        with open(iso_path, "rb"):
+            pass
+    except FileNotFoundError as x:
+        raise ValueError(str(x))
+
+    if not iso_path.lower().endswith(".iso"):
+        raise ValueError("Does not end in '.iso'")
+
+
+def validate_game_files_path(game_files_path: str, options):
+    if not os.path.isdir(game_files_path):
+        raise ValueError("Not a directory")
+
+    required_files = ["default.dol", "FrontEnd.pak", "Metroid1.pak", "Metroid2.pak"]
+    missing_files = [os.path.isfile(os.path.join(game_files_path, required_file))
+                     for required_file in required_files]
+
+    if not all(missing_files):
+        raise ValueError("Is not a valid game folder. Missing files: {}".format([
+            filename for filename, exists in zip(required_files, missing_files)
+            if not exists
+        ]))
+
+
 def interactive_shell(args):
     data_file_path = os.path.join(get_data_path(), "prime2.bin")
     with open(data_file_path, "rb") as x:  # type: BinaryIO
         data = binary_data.decode(x)
 
     print("""
- _______  _______  _        ______   _______           _______  _       _________ _______ 
+ _______  _______  _        ______   _______           _______  _       _________ _______
 (  ____ )(  ___  )( (    /|(  __  \ (  ___  )|\     /|(  ___  )( (    /|\__   __/(  ___  )
 | (    )|| (   ) ||  \  ( || (  \  )| (   ) || )   ( || (   ) ||  \  ( |   ) (   | (   ) |
 | (____)|| (___) ||   \ | || |   ) || |   | || |   | || (___) ||   \ | |   | |   | (___) |
@@ -115,46 +172,78 @@ def interactive_shell(args):
 |/   \__/|/     \||/    )_)(______/ (_______)   \_/   |/     \||/    )_)\_______/|/     \|
 
     ===  Welcome to the randovania interactive shell!
-    
+
 """)
 
     parsing_commands = True
-    options = {
-        "max_difficulty": 3,
-        "min_difficulty": 0,
-        "item_loss_enabled": False,
-        "tricks_enabled": True,
-        "excluded_pickups": [],
-        "randomize_elevators": False,
-        "hud_memo_popup_removal": True,
-        "game_files": "",
-    }
+    options = OrderedDict()
+    options["max_difficulty"] = 3
+    options["min_difficulty"] = 0
+    options["item_loss_enabled"] = False
+    options["tricks_enabled"] = True
+    options["excluded_pickups"] = []
+    options["randomize_elevators"] = False
+    options["hud_memo_popup_removal"] = True
+    # options["iso_path"] = ""
+    options["game_files_path"] = ""
+    options["seed"] = 0
     load_options_to(options)
 
+    options_validation = {
+        "max_difficulty": validate_max_difficulty,
+        "min_difficulty": validate_min_difficulty,
+        "excluded_pickups": validate_pickup_list,
+        "iso_path": validate_iso_path,
+        "game_files_path": validate_game_files_path,
+    }
+
     def print_config():
-        print("        === Current configuration ===\n"
-              "         max_difficulty  {max_difficulty}\n"
-              "         min_difficulty  {min_difficulty}\n"
-              "      item_loss_enabled  {item_loss_enabled}\n"
-              "         tricks_enabled  {tricks_enabled}\n"
-              "       excluded_pickups  {excluded_pickups}\n"
-              "    randomize_elevators  {randomize_elevators}\n"
-              " hud_memo_popup_removal  {hud_memo_popup_removal}\n"
-              "        game_files_path  {game_files_path}\n"
-              "".format(**options))
+        print("        === Current configuration ===")
+        spacing: int = max(len(option_name) for option_name in options.keys())
+        for option_name, value in options.items():
+            print("{0:>{spacing}}  {1}".format(option_name, value, spacing=spacing))
+        print()
 
     def quit_shell():
         raise SystemExit(0)
 
-    def stop_parsing_commands():
-        nonlocal parsing_commands
-        parsing_commands = False
+    def generate():
+        randomizer_config = RandomizerConfiguration(options["excluded_pickups"], options["randomize_elevators"])
+        resolver_config = ResolverConfiguration(options["max_difficulty"],
+                                                options["min_difficulty"],
+                                                options["tricks_enabled"],
+                                                options["item_loss_enabled"])
+
+        print("\n== Will now search for a seed!")
+        print_config()
+        print("\n* This may take a while, and your computer may not respond correctly while running.")
+
+        seed, seed_count = search_seed(data, randomizer_config, resolver_config)
+        print("A seed was found with the given configuration after {} attempts.".format(seed_count))
+        print("\n=== Seed: {}".format(seed))
+        options["seed"] = seed
+        print("Try the 'randomize' command to use this seed.")
+
+    def randomize():
+        if not has_randomizer_binary():
+            print("== WARNING ==\n"
+                  "Randomizer.exe not found in the current directory, cannot randomize the game files.\n")
+            return
+
+        if not options["game_files_path"]:
+            print("game_files_path is not set, no game to randomize.")
+            return
+
+        randomizer_config = RandomizerConfiguration(options["excluded_pickups"], options["randomize_elevators"])
+        apply_seed(randomizer_config, options["seed"],
+                   options["item_loss"], options["hud_memo_popup_removal"], options["game_files_path"])
 
     commands = {
         "view_config": print_config,
         "quit": quit_shell,
         "exit": quit_shell,
-        "generate": stop_parsing_commands,
+        "generate": generate,
+        "randomize": randomize,
     }
 
     def print_help():
@@ -176,10 +265,6 @@ def interactive_shell(args):
     commands["help"] = print_help
     print_config()
 
-    if not has_randomizer_binary():
-        print("== WARNING ==\n"
-              "Randomizer.exe not found in the current directory, cannot automatically apply the seed.\n")
-
     print("Please enter your commands. If in doubt, type 'help'.\n")
 
     while parsing_commands:
@@ -191,10 +276,13 @@ def interactive_shell(args):
 
         if first_part in options:
             try:
-                options[first_part] = value_parsers[type(options[first_part])](command[1])
+                the_value = value_parsers[type(options[first_part])](command[1])
+                if first_part in options_validation:
+                    options_validation[first_part](the_value, options)
+                options[first_part] = the_value
                 save_options(options)
             except ValueError as e:
-                print("Invalid value for '{}': {}".format(first_part, e))
+                print("'{}' is an invalid value for '{}': {}".format(command[1], first_part, e))
             except IndexError:
                 print("Missing value!")
 
@@ -206,24 +294,6 @@ def interactive_shell(args):
 
         elif first_part:
             print("Unknown command '{}'. If in doubt, type 'help'.".format(first_part))
-
-    randomizer_config = RandomizerConfiguration(options["excluded_pickups"], options["randomize_elevators"])
-    resolver_config = ResolverConfiguration(options["max_difficulty"],
-                                            options["min_difficulty"],
-                                            options["tricks_enabled"],
-                                            options["item_loss_enabled"])
-
-    print("\n== Will now search for a seed!")
-    print_config()
-    print("\n* This may take a while, and your computer may not respond correctly while running.")
-
-    seed, seed_count = search_seed(data, randomizer_config, resolver_config)
-    print("A seed was found with the given configuration after {} attempts.".format(seed_count))
-    print("\n=== Seed: {}".format(seed))
-
-    if has_randomizer_binary():
-        apply_seed(randomizer_config, seed,
-                   options["item_loss"], options["hud_memo_popup_removal"], options["game_files"])
 
     input("Press anything to exit.")
 
