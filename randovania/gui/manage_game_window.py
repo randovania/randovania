4a87d45cce928099dcca04ea97c53a1759a049b7
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-05-20 16:57:02 -0300

    Implement full seed generation on GUI

--- randovania/gui/manage_game_window.py
+++ randovania/gui/manage_game_window.py
@@ -1,14 +1,23 @@
-from typing import Optional
+import threading
+from typing import Optional, BinaryIO
 
 import multiprocessing
 
+import os
 from PyQt5 import QtCore
 from PyQt5.QtGui import QIntValidator
 from PyQt5.QtWidgets import QMainWindow, QFileDialog
 
+from randovania import get_data_path
+from randovania.games.prime import binary_data
 from randovania.gui import application_options
 from randovania.gui.manage_game_window_ui import Ui_ManageGameWindow
 from randovania.interface_common.options import CpuUsage
+from randovania.resolver.echoes import RandomizerConfiguration, ResolverConfiguration, search_seed
+
+
+class AbortGeneration(Exception):
+    pass
 
 
 class ManageGameWindow(QMainWindow, Ui_ManageGameWindow):
@@ -17,9 +26,8 @@ class ManageGameWindow(QMainWindow, Ui_ManageGameWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.loadIsoButton.clicked.connect(self.load_iso)
-        self.packageIsoButton.clicked.connect(self.package_iso)
-        self.progressBar.setHidden(True)
+        self.seed_search_thread = None
+        self.abort_seed_generation_requested = None
 
         options = application_options()
 
@@ -48,12 +56,12 @@ class ManageGameWindow(QMainWindow, Ui_ManageGameWindow):
         self.currentSeedEdit.setValidator(QIntValidator(0, 2147483647))
         self.applySeedButton.clicked.connect(self.apply_seed)
 
-    def load_iso(self):
-        QFileDialog.getOpenFileName(self, filter="*.iso")
+        # ISO Packing
+        self.loadIsoButton.clicked.connect(self.load_iso)
+        self.packageIsoButton.clicked.connect(self.package_iso)
+        self.progressBar.setHidden(True)
 
-    def package_iso(self):
-        self.progressBar.setHidden(False)
-
+    # File Location
     def prompt_new_files_location(self):
         result = QFileDialog.getExistingDirectory()
         if result:
@@ -68,6 +76,7 @@ class ManageGameWindow(QMainWindow, Ui_ManageGameWindow):
     def reset_files_location(self):
         self.set_current_files_location(None)
 
+    # Seed Generation
     def update_num_seeds(self, seed_count: int):
         self.generationStatusLabel.setText(QtCore.QCoreApplication.translate(
             "ManageGameWindow", "Generating %n seed(s) so far...", n=seed_count
@@ -76,10 +85,48 @@ class ManageGameWindow(QMainWindow, Ui_ManageGameWindow):
     def generate_new_seed(self):
         self.generateSeedButton.setEnabled(False)
         self.abortGenerateButton.setEnabled(True)
+        self.abort_seed_generation_requested = None
+
+        options = application_options()
+        randomizer_config = RandomizerConfiguration.from_options(options)
+        resolver_config = ResolverConfiguration.from_options(options)
+        cpu_count = options.cpu_usage.num_cpu_for_count(multiprocessing.cpu_count())
+
+        def seed_report(seed_count: int):
+            self.update_num_seeds(seed_count)
+            if self.abort_seed_generation_requested:
+                raise AbortGeneration()
+
+        def gui_seed_searcher():
+            data_file_path = os.path.join(get_data_path(), "prime2.bin")
+            with open(data_file_path, "rb") as x:  # type: BinaryIO
+                data = binary_data.decode(x)
+
+            try:
+                seed, seed_count = search_seed(data=data,
+                                               randomizer_config=randomizer_config,
+                                               resolver_config=resolver_config,
+                                               cpu_count=cpu_count,
+                                               seed_report=seed_report)
+
+                self.generationStatusLabel.setText(QtCore.QCoreApplication.translate(
+                    "ManageGameWindow", "Seed '{}' found after %n seed(s).", n=seed_count
+                ).format(seed))
+                self.currentSeedEdit.setText(str(seed))
+
+            except AbortGeneration:
+                self.generationStatusLabel.setText(QtCore.QCoreApplication.translate(
+                    "ManageGameWindow", "Seed generation aborted."))
+
+            self.generateSeedButton.setEnabled(True)
+            self.abortGenerateButton.setEnabled(False)
+
+        self.seed_search_thread = threading.Thread(target=gui_seed_searcher)
+        seed_report(0)
+        self.seed_search_thread.start()
 
     def abort_seed_generation(self):
-        self.generateSeedButton.setEnabled(True)
-        self.abortGenerateButton.setEnabled(False)
+        self.abort_seed_generation_requested = True
 
     # CPU Usage
     def enable_cpu_options_by_cpu_count(self):
@@ -102,5 +149,13 @@ class ManageGameWindow(QMainWindow, Ui_ManageGameWindow):
         options.cpu_usage = cpu_usage
         options.save_to_disk()
 
+    # Seed
     def apply_seed(self):
         pass
+
+    # ISO Packing
+    def load_iso(self):
+        QFileDialog.getOpenFileName(self, filter="*.iso")
+
+    def package_iso(self):
+        self.progressBar.setHidden(False)
