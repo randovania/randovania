61d09a75f1bed581e238f3feb70ad35cab34c934
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-05-27 03:43:10 -0300

    Add DataEditorWindow as a preview (unfinished) feature

--- randovania/gui/qt.py
+++ randovania/gui/qt.py
@@ -4,6 +4,7 @@ from argparse import ArgumentParser
 from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
 
+from randovania.gui.data_editor import DataEditorWindow
 from randovania.gui.mainwindow_ui import Ui_MainWindow
 from randovania.gui.manage_game_window import ManageGameWindow
 from randovania.gui.randomize_window import RandomizeWindow
@@ -11,12 +12,13 @@ from randovania.interface_common.options import Options
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
+    def __init__(self, preview: bool):
         super().__init__()
         self.setupUi(self)
 
         self.manage_game_window = ManageGameWindow()
         self.randomize_window = RandomizeWindow()
+        self.data_editor_window = DataEditorWindow()
 
         _translate = QtCore.QCoreApplication.translate
 
@@ -25,6 +27,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         self.configurationTab = self.randomize_window.centralWidget
         self.tabWidget.insertTab(1, self.configurationTab, _translate("MainWindow", "Configuration"))
+
+        if preview:
+            self.dataEditorTab = self.data_editor_window.centralWidget
+            self.tabWidget.insertTab(3, self.dataEditorTab, _translate("MainWindow", "Data Editor"))
 
         self.tabWidget.setCurrentIndex(0)
 
@@ -52,7 +58,7 @@ def run(args):
     app.options = Options()
     app.options.load_from_disk()
 
-    main_window = MainWindow()
+    main_window = MainWindow(args.preview)
     app.main_window = main_window
     main_window.show()
     sys.exit(app.exec_())
@@ -63,4 +69,5 @@ def create_subparsers(sub_parsers):
         "gui",
         help="Run the Graphical User Interface"
     )
+    parser.add_argument("--preview", action="store_true", help="Activates preview features")
     parser.set_defaults(func=run)
