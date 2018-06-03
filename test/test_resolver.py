c8b3c1b0c67254b9bfabc03df484e64a7cbcd2b1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-06-02 23:18:41 -0300

    Add testing for actions_with_reach

---
+++ test/test_resolver.py
@@ -0,0 +1,39 @@
+from unittest.mock import MagicMock
+
+from randovania.resolver import resolver
+from randovania.resolver.game_description import EventNode
+
+
+def test_actions_with_reach_empty():
+    state = MagicMock()
+    game = MagicMock()
+
+    options = list(resolver.actions_with_reach([], state, game))
+
+    assert options == []
+
+
+def test_actions_with_reach_no_resources():
+    state = MagicMock()
+    game = MagicMock()
+
+    options = list(resolver.actions_with_reach([MagicMock(), MagicMock()], state, game))
+
+    assert options == []
+
+
+def test_actions_with_reach_with_event():
+    state = MagicMock()
+    game = MagicMock()
+    event = MagicMock(spec=EventNode)
+    state.has_resource.return_value = False
+
+    # Run
+    options = list(resolver.actions_with_reach([event], state, game))
+
+    # Assert
+    assert options == [event]
+    event.resource.assert_called_once_with(game.resource_database, game.pickup_database)
+    state.has_resource.assert_called_once_with(event.resource.return_value)
+    game.get_additional_requirements.assert_called_once_with(event)
+    game.get_additional_requirements.return_value.satisfied.assert_called_once_with(state.resources)
