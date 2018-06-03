407bf6314bedbbf9ba37817eca57fb9fd787c07c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-06-03 00:14:55 -0300

    Add tests for search_seed

--- test/resolver/test_echoes.py
+++ test/resolver/test_echoes.py
@@ -131,3 +131,64 @@ def test_generate_and_validate(mock_generate_log: MagicMock,
                       resolver_config=resolver_config,
                       verbose=False)
     output_queue.put.assert_called_once_with((seed, mock_run_resolver.return_value))
+
+
+@pytest.mark.parametrize("starting_seed", range(10000, 30000, 10000))
+@pytest.mark.parametrize("seeds_to_generate", range(5, 10))
+@pytest.mark.parametrize("cpu_count", range(1, 4))
+@patch("multiprocessing.Process", autospec=True)
+@patch("multiprocessing.Queue", autospec=True)
+def test_search_seed(mock_queue: MagicMock,
+                     mock_process: MagicMock,
+                     starting_seed: int,
+                     seeds_to_generate: int,
+                     cpu_count: int):
+    data = MagicMock(spec=RandomizerConfiguration)
+    randomizer_config = MagicMock(spec=RandomizerConfiguration)
+    resolver_config = MagicMock(spec=ResolverConfiguration)
+    seed_report = MagicMock()
+
+    input_queue = MagicMock()
+    output_queue = MagicMock()
+    mock_queue.side_effect = [input_queue, output_queue]
+
+    output_queue.get.side_effect = [
+                                       (starting_seed + i, False)
+                                       for i in range(seeds_to_generate - 1)
+                                   ] + [(starting_seed + seeds_to_generate, True), None, None]
+
+    processes = [MagicMock() for _ in range(cpu_count)]
+    mock_process.side_effect = processes
+
+    # Call
+    final_seed, seed_count = echoes.search_seed(
+        data=data,
+        randomizer_config=randomizer_config,
+        resolver_config=resolver_config,
+        cpu_count=cpu_count,
+        seed_report=seed_report,
+        start_on_seed=starting_seed)
+
+    # Assertions
+    assert final_seed == starting_seed + seeds_to_generate
+    assert seed_count == seeds_to_generate
+
+    input_queue.put_nowait.assert_has_calls([
+        call(starting_seed + i)
+        for i in range(seeds_to_generate)
+    ])
+    seed_report.assert_has_calls([
+        call(i + 1)
+        for i in range(seeds_to_generate - 1)
+    ])
+
+    mock_process.assert_has_calls([
+        call(
+            target=echoes.generate_and_validate,
+            args=(data, randomizer_config, resolver_config, input_queue, output_queue)
+        )
+        for _ in range(cpu_count)
+    ])
+    for process in processes:
+        process.start.assert_called_once_with()
+        process.terminate.assert_called_once_with()
