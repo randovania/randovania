491e725f64ad75fc74dae975e12852f39571f9cd
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-06-04 23:26:32 -0300

    Split additional_requirements from GameDescription

--- randovania/resolver/resolver.py
+++ randovania/resolver/resolver.py
@@ -3,25 +3,31 @@ from typing import Set, Optional
 from randovania.resolver import debug
 from randovania.resolver.game_description import GameDescription, RequirementSet, CurrentResources
 from randovania.resolver.logic import calculate_reach, calculate_satisfiable_actions, build_static_resources, \
-    calculate_starting_state
+    calculate_starting_state, LogicMemory
 from randovania.resolver.state import State
 
 
-def advance_depth(state: State, game: GameDescription) -> Optional[State]:
+def advance_depth(state: State,
+                  memory: LogicMemory,
+                  game: GameDescription) -> Optional[State]:
     if game.victory_condition.satisfied(state.resources):
         return state
 
-    reach, satisfiable_requirements = calculate_reach(state, game)
+    reach, satisfiable_requirements = calculate_reach(state, memory, game)
     debug.log_new_advance(state, reach)
 
-    for action in calculate_satisfiable_actions(state, reach, satisfiable_requirements, game):
+    for action in calculate_satisfiable_actions(reach, satisfiable_requirements, state, memory, game):
         new_state = advance_depth(
-            state.act_on_node(action, game.resource_database, game.pickup_database), game)
+            state=state.act_on_node(action, game.resource_database, game.pickup_database),
+            memory=memory,
+            game=game)
+
+        # We got a positive result. Send it back up
         if new_state:
             return new_state
 
     debug.log_rollback(state)
-    game.additional_requirements[state.node] = RequirementSet(satisfiable_requirements)
+    memory.additional_requirements[state.node] = RequirementSet(satisfiable_requirements)
     # print("> Rollback finished.")
     return None
 
@@ -46,4 +52,6 @@ def resolve(difficulty_level: int,
     starting_state = calculate_starting_state(item_loss, game)
     simplify_connections(game, starting_state.resources)
 
-    return advance_depth(starting_state, game)
+    memory = LogicMemory()
+
+    return advance_depth(starting_state, memory, game)
