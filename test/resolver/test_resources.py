4243b2e946b08c63937413d56b167f31ab41474f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-06-05 23:27:07 -0300

    Add merge_resources

---
+++ test/resolver/test_resources.py
@@ -0,0 +1,11 @@
+import pytest
+
+from randovania.resolver.resources import merge_resources
+
+
+@pytest.mark.parametrize(["a", "b", "result"], [
+    ({"a": 5}, {"b": 6}, {"a": 5, "b": 6}),
+    ({"a": 5}, {"a": 6}, {"a": 11}),
+])
+def test_merge_resources(a, b, result):
+    assert merge_resources(a, b) == result
