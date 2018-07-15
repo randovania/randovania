6fdf432f2a8f22b92752be309f25700950e57853
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-07-15 15:20:16 -0300

    Add extra test case

--- test/resolver/test_generator.py
+++ test/resolver/test_generator.py
@@ -35,7 +35,23 @@ _test_descriptions = [
                         65, 12, 21, 96, 28, 105, 13, 87, 15, 115, 42, 49, 11, 112, 45, 113, 92, 10, 53, 62, 69, 24, 17,
                         77, 46, 83, 75, 108, 90, 98, 22, 29, 118, 47, 109, 39, 93, 20, 48, 81, 5, 78, 114, 85, 111, 35,
                         41),
-    )
+    ),
+    _create_test_layout_description(
+        configuration=LayoutConfiguration(seed_number=50000,
+                                          logic=LayoutLogic.NO_GLITCHES,
+                                          mode=LayoutMode.STANDARD,
+                                          sky_temple_keys=LayoutRandomizedFlag.RANDOMIZED,
+                                          item_loss=LayoutEnabledFlag.ENABLED,
+                                          elevators=LayoutRandomizedFlag.VANILLA,
+                                          hundo_guaranteed=LayoutEnabledFlag.DISABLED,
+                                          difficulty=LayoutDifficulty.NORMAL),
+        pickup_mapping=(94, 118, 55, 80, 98, 41, 60, 107, 1, 26, 3, 92, 44, 88, 35, 32, 61, 66, 114, 73, 23, 82, 75, 46,
+                        99, 10, 7, 78, 97, 37, 104, 17, 62, 28, 85, 112, 36, 72, 57, 65, 93, 115, 14, 81, 6, 13, 24, 76,
+                        38, 45, 96, 74, 52, 47, 102, 16, 111, 68, 105, 84, 21, 109, 70, 49, 100, 34, 43, 51, 40, 67,
+                        116, 71, 2, 83, 53, 90, 106, 91, 89, 50, 31, 30, 63, 56, 54, 103, 8, 48, 0, 5, 117, 58, 113,
+                        110, 39, 69, 108, 20, 79, 4, 15, 29, 12, 33, 59, 11, 18, 95, 77, 9, 64, 101, 19, 86, 22, 27, 25,
+                        42, 87),
+    ),
 ]
 
 
