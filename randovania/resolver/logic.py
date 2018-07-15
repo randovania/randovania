75a5409d110d4e5eeec9ff5730f04b608e43820a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-07-14 23:26:32 -0300

    Progress to Major Mode items

--- randovania/resolver/logic.py
+++ randovania/resolver/logic.py
@@ -3,6 +3,7 @@ from typing import Dict, Set
 
 from randovania.resolver.game_description import GameDescription
 from randovania.resolver.game_patches import GamePatches
+from randovania.resolver.layout_configuration import LayoutConfiguration
 from randovania.resolver.node import Node
 from randovania.resolver.requirements import RequirementSet
 from randovania.resolver.resources import CurrentResources, ResourceDatabase
@@ -12,12 +13,14 @@ class Logic:
     """Extra information that persists even after a backtrack, to prevent irrelevant backtracking."""
 
     game: GameDescription
+    configuration: LayoutConfiguration
     patches: GamePatches
     additional_requirements: Dict[Node, RequirementSet]
     node_sightings: Dict[Node, int]
 
-    def __init__(self, game: GameDescription, patches: GamePatches):
+    def __init__(self, game: GameDescription, configuration: LayoutConfiguration, patches: GamePatches):
         self.game = game
+        self.configuration = configuration
         self.patches = patches
         self.additional_requirements = {}
         self.node_sightings = collections.defaultdict(int)
