873482d716e5152f81d7b25ede8279c3d9bb9d17
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-07-30 02:25:44 -0300

    Add --version and improve appveyor testing, fix release binary

--- randovania/cli/__init__.py
+++ randovania/cli/__init__.py
@@ -1,5 +1,7 @@
 import argparse
+import os
 
+import randovania
 from randovania.cli import echoes, interactive
 from randovania.gui import qt
 
@@ -13,11 +15,19 @@ def create_subparsers(root_parser):
     qt.create_subparsers(root_parser)
 
 
+def _print_version(args):
+    print("Randovania {} from {}".format(
+        randovania.VERSION,
+        os.path.dirname(randovania.__file__)))
+
+
 def run_cli():
     parser = argparse.ArgumentParser()
     create_subparsers(parser.add_subparsers(dest="game"))
+    parser.add_argument("--version", action="store_const",
+                        const=_print_version, dest="func")
 
     args = parser.parse_args()
-    if args.game is None:
+    if getattr(args, "func", None) is None:
         args.func = qt.run
     args.func(args)
