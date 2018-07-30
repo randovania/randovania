6917b84fc401696c81d4323c4366b0f1882b8569
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-07-30 01:04:55 -0300

    Update tests to use 0.11.0

--- test/resolver/test_generator.py
+++ test/resolver/test_generator.py
@@ -14,7 +14,7 @@ def _create_test_layout_description(
         pickup_mapping):
     return LayoutDescription(
         configuration=configuration,
-        version='0.10.0',
+        version='0.11.0',
         pickup_mapping=pickup_mapping,
         solver_path=())
 
