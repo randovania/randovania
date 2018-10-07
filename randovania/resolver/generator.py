89ddca012435afe8cc9db4b9a77f7bb77692a769
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-10-07 16:41:26 -0300

    More code refactor

--- randovania/resolver/generator.py
+++ randovania/resolver/generator.py
@@ -1,8 +1,7 @@
 import collections
 import copy
-import itertools
 from random import Random
-from typing import List, Tuple, Iterator, Optional, FrozenSet, Callable, TypeVar, Dict, Set
+from typing import List, Tuple, Iterator, Optional, FrozenSet, Callable, Dict
 
 from randovania import VERSION
 from randovania.games.prime import echoes_elevator
@@ -10,6 +9,7 @@ from randovania.resolver import debug, resolver, data_reader
 from randovania.resolver.bootstrap import logic_bootstrap
 from randovania.resolver.game_description import GameDescription, calculate_interesting_resources
 from randovania.resolver.game_patches import GamePatches
+from randovania.resolver.item_pool import calculate_item_pool, calculate_available_pickups
 from randovania.resolver.layout_configuration import LayoutConfiguration, LayoutEnabledFlag, LayoutMode, \
     LayoutRandomizedFlag, LayoutLogic
 from randovania.resolver.layout_description import LayoutDescription, SolverPath
@@ -60,18 +60,11 @@ def _state_to_solver_path(final_state: State,
     )
 
 
-def calculate_available_pickups(remaining_items: Iterator[PickupEntry], categories: Set[str]) -> Iterator[PickupEntry]:
-    for pickup in remaining_items:
-        if pickup.item_category in categories:
-            yield pickup
-
-
 def generate_list(data: Dict,
                   seed_number: int,
                   configuration: LayoutConfiguration,
                   status_update: Callable[[str], None]
                   ) -> LayoutDescription:
-
     elevators = echoes_elevator.elevator_list_for_configuration(configuration, seed_number)
     new_patches = _create_patches(
         seed_number=seed_number,
@@ -106,49 +99,6 @@ def generate_list(data: Dict,
     )
 
 
-def _calculate_item_pool(configuration: LayoutConfiguration,
-                         game: GameDescription,
-                         ) -> List[PickupEntry]:
-    split_pickups = game.resource_database.pickups_split_by_name()
-    useless_item = split_pickups["Energy Transfer Module"][0]
-
-    pickups_with_configured_quantity = configuration.pickups_with_configured_quantity
-
-    for pickup_name, pickup_list in split_pickups.items():
-        if pickup_name in pickups_with_configured_quantity:
-            configured_quantity = configuration.quantity_for_pickup(pickup_name)
-            pickups_with_configured_quantity.remove(pickup_name)
-        else:
-            configured_quantity = len(pickup_list)
-
-        quantity_delta = configured_quantity - len(pickup_list)
-
-        if quantity_delta > 0:
-            # We need more: copy the last element
-            pickup_list.extend(pickup_list[-1:] * quantity_delta)
-
-        elif quantity_delta < 0:
-            # We need less: drop the end of the list
-            # Yes, the index of the following slice should be negative
-            del pickup_list[quantity_delta:]
-
-    if pickups_with_configured_quantity:
-        raise GenerationFailure("Configuration has custom quantity for unknown pickups: {}".format(
-            pickups_with_configured_quantity))
-
-    item_pool = list(itertools.chain.from_iterable(split_pickups.values()))
-    quantity_delta = len(item_pool) - len(game.resource_database.pickups)
-    if quantity_delta > 0:
-        raise GenerationFailure("Invalid configuration: requested {} more items than available slots ({}).".format(
-            quantity_delta, len(game.resource_database.pickups)
-        ))
-
-    elif quantity_delta < 0:
-        item_pool.extend([useless_item] * -quantity_delta)
-
-    return item_pool
-
-
 def _create_patches(
         seed_number: int,
         configuration: LayoutConfiguration,
@@ -171,7 +121,7 @@ def _create_patches(
     else:
         categories.add("sky_temple_key")
 
-    item_pool = _calculate_item_pool(configuration, game)
+    item_pool = calculate_item_pool(configuration, game)
     available_pickups = tuple(shuffle(rng, sorted(calculate_available_pickups(item_pool, categories))))
     remaining_items = [
         pickup for pickup in sorted(item_pool)
@@ -295,14 +245,6 @@ def _add_item_to_node(item: PickupEntry,
 
 def _num_items_in_patches(patches: GamePatches) -> int:
     return len([x for x in patches.pickup_mapping if x is not None])
-
-
-def _calculate_distance(source: State, target: State):
-    result = 0
-    while target != source:
-        result += 1
-        target = target.previous_state
-    return result
 
 
 def _iterate_with_weights(potential_pickup_nodes: List[PickupNode],
