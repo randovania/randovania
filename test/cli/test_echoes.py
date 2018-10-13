5bf98aff9cc74e3a659c3e500a749f5d121f1c49
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-10-12 23:49:26 -0300

    Adding test for cli/echoes.py

---
+++ test/cli/test_echoes.py
@@ -0,0 +1,43 @@
+from unittest.mock import patch, MagicMock, ANY
+
+from randovania.cli import echoes
+from randovania.resolver.layout_configuration import LayoutConfiguration, LayoutLogic, LayoutMode, LayoutRandomizedFlag, \
+    LayoutEnabledFlag, LayoutDifficulty
+
+
+@patch("randovania.cli.prime_database.decode_data_file", autospec=True)
+@patch("randovania.resolver.generator.generate_list", autospec=True)
+def test_distribute_command_logic(mock_generate_list: MagicMock,
+                                  mock_decode_data_file: MagicMock,
+                                  ):
+    # Setup
+    args = MagicMock()
+    args.logic = "hard"
+    args.major_items_mode = False
+    args.vanilla_sky_temple_keys = False
+    args.skip_item_loss = True
+    args.seed = 15000
+
+    # Run
+    echoes.distribute_command_logic(args)
+
+    # Assert
+    mock_decode_data_file.assert_called_once_with(args)
+    mock_generate_list.assert_called_once_with(
+        data=mock_decode_data_file.return_value,
+        seed_number=args.seed,
+        configuration=LayoutConfiguration(
+            logic=LayoutLogic.HARD,
+            mode=LayoutMode.STANDARD,
+            sky_temple_keys=LayoutRandomizedFlag.RANDOMIZED,
+            item_loss=LayoutEnabledFlag.ENABLED,
+            elevators=LayoutRandomizedFlag.VANILLA,
+            hundo_guaranteed=LayoutEnabledFlag.DISABLED,
+            difficulty=LayoutDifficulty.NORMAL,
+            pickup_quantities={}
+        ),
+        status_update=ANY
+    )
+
+    save_file_mock: MagicMock = mock_generate_list.return_value.save_to_file
+    save_file_mock.assert_called_once_with(args.output_file)
