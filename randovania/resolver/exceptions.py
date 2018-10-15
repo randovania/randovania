0eb163280d38dab02847e5b86c0e247ba168d32f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-10-14 22:04:51 -0300

    Improve error message for GenerationFailure

--- randovania/resolver/exceptions.py
+++ randovania/resolver/exceptions.py
@@ -6,3 +6,10 @@ class GenerationFailure(Exception):
         super().__init__(reason)
         self.configuration = configuration
         self.seed_number = seed_number
+
+    def __str__(self) -> str:
+        return "Generation Failure: {} received for {} with config {}".format(
+            super().__str__(),
+            self.seed_number,
+            self.configuration.as_str
+        )
