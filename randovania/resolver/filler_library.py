ef8779f58760e369351dcff77c6bd32d67f1e996
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-11-18 00:01:36 -0200

    Refactoring

---
+++ randovania/resolver/filler_library.py
@@ -0,0 +1,12 @@
+from typing import Iterator
+
+from randovania.game_description.node import Node, PickupNode
+from randovania.resolver.game_patches import PickupAssignment
+
+
+def filter_unassigned_pickup_nodes(nodes: Iterator[Node],
+                                   pickup_assignment: PickupAssignment,
+                                   ) -> Iterator[PickupNode]:
+    for node in nodes:
+        if isinstance(node, PickupNode) and node.pickup_index not in pickup_assignment:
+            yield node
