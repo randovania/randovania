170f6fc862dd524827bfef396bc554beb9cbae55
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-11-19 01:05:24 -0200

    Keep debug level when running in another process

--- randovania/interface_common/echoes.py
+++ randovania/interface_common/echoes.py
@@ -7,7 +7,7 @@ from randovania.game_description.data_reader import read_resource_database, read
 from randovania.game_description.resources import ResourceDatabase, PickupDatabase
 from randovania.games.prime import binary_data
 
-from randovania.resolver import generator
+from randovania.resolver import generator, debug
 from randovania.resolver.layout_configuration import LayoutConfiguration
 from randovania.resolver.layout_description import LayoutDescription
 
@@ -15,11 +15,13 @@ from randovania.resolver.layout_description import LayoutDescription
 def _generate_layout_worker(output_pipe,
                             data: Dict,
                             seed_number: int,
-                            configuration: LayoutConfiguration):
+                            configuration: LayoutConfiguration,
+                            debug_level: int):
     try:
         def status_update(message: str):
             output_pipe.send(message)
 
+        debug._DEBUG_LEVEL = debug_level
         layout_description = generator.generate_list(data,
                                                      seed_number,
                                                      configuration,
@@ -39,7 +41,7 @@ def generate_layout(data: Dict,
 
     process = multiprocessing.Process(
         target=_generate_layout_worker,
-        args=(output_pipe, data, seed_number, configuration)
+        args=(output_pipe, data, seed_number, configuration, debug._DEBUG_LEVEL)
     )
     process.start()
     try:
