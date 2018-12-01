c46b8c79e55255929734cbdd92835dd6bf85beef
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-12-01 15:30:02 -0200

    Rename to _read_from_persisted in update_checker

--- test/interface_common/test_update_checker.py
+++ test/interface_common/test_update_checker.py
@@ -3,13 +3,14 @@ from unittest.mock import MagicMock, patch
 from randovania.interface_common import update_checker
 
 
-@patch("randovania.interface_common.update_checker._read_from_db", autospec=True)
-def test_get_latest_version_work(mock_read_from_db: MagicMock):
+@patch("randovania.interface_common.update_checker._read_from_persisted", autospec=True)
+def test_get_latest_version_work(mock_read_from_persisted: MagicMock,
+                                 ):
     on_result = MagicMock()
 
     update_checker._get_latest_version_work(on_result)
 
     on_result.assert_called_once_with(
-        mock_read_from_db.return_value.tag_name,
-        mock_read_from_db.return_value.html_url
+        mock_read_from_persisted.return_value.tag_name,
+        mock_read_from_persisted.return_value.html_url
     )
