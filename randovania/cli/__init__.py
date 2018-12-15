e3cc554585fbd7c1647a39c76dfc121a83aa6041
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-12-15 17:34:16 -0200

    Add tests for cli.__init__.py

--- randovania/cli/__init__.py
+++ randovania/cli/__init__.py
@@ -20,13 +20,19 @@ def _print_version(args):
         os.path.dirname(randovania.__file__)))
 
 
-def run_cli():
+def _create_parser():
     parser = argparse.ArgumentParser()
     create_subparsers(parser.add_subparsers(dest="game"))
     parser.add_argument("--version", action="store_const",
                         const=_print_version, dest="func")
+    return parser
 
-    args = parser.parse_args()
+
+def _run_args(args):
     if getattr(args, "func", None) is None:
         args.func = qt.run
     args.func(args)
+
+
+def run_cli():
+    _run_args(_create_parser().parse_args())
---
+++ test/cli/test_init.py
@@ -0,0 +1,70 @@
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from randovania import cli
+
+
+@patch("randovania.cli.games", autospec=True)
+@patch("randovania.cli.qt.create_subparsers", autospec=True)
+def test_create_subparsers(mock_qt_create_subparsers: MagicMock,
+                           mock_games: MagicMock,
+                           ):
+    # Setup
+    root_parser = MagicMock()
+    games = [MagicMock(), MagicMock()]
+    mock_games.__iter__.return_value = games
+
+    # Run
+    cli.create_subparsers(root_parser)
+
+    # Assert
+    for game in games:
+        game.create_subparsers.assert_called_once_with(root_parser)
+    mock_qt_create_subparsers.assert_called_once_with(root_parser)
+
+
+@pytest.mark.parametrize("args", [
+    [],
+    ["--version"],
+])
+def test_create_parser(args):
+    # Run
+    cli._create_parser().parse_args(args)
+
+
+@patch("randovania.cli.qt.run", autospec=True)
+def test_run_args_no_option(mock_qt_run: MagicMock,
+                            ):
+    # Setup
+    args = MagicMock()
+    args.func = None
+
+    # Run
+    cli._run_args(args)
+
+    # Assert
+    mock_qt_run.assert_called_once_with(args)
+
+
+def test_run_args_with_func():
+    # Setup
+    args = MagicMock()
+
+    # Run
+    cli._run_args(args)
+
+    # Assert
+    args.func.assert_called_once_with(args)
+
+
+@patch("randovania.cli._run_args", autospec=True)
+@patch("randovania.cli._create_parser", autospec=True)
+def test_run_cli(mock_create_parser: MagicMock,
+                 mock_run_args: MagicMock,
+                 ):
+    # Run
+    cli.run_cli()
+
+    # Assert
+    mock_run_args.assert_called_once_with(mock_create_parser.return_value.parse_args.return_value)
