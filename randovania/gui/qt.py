c747da091d7a21fa4436470bd294fc9eadac96c8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-12-25 12:02:17 -0200

    Improving the DataEditorWindow

--- randovania/gui/qt.py
+++ randovania/gui/qt.py
@@ -17,12 +17,7 @@ def catch_exceptions(t, val, tb):
 old_hook = sys.excepthook
 
 
-def run(args):
-    QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
-    app = QApplication(sys.argv)
-
-    sys.excepthook = catch_exceptions
-
+def show_main_window(app: QApplication, args):
     options = Options.with_default_data_dir()
     options.load_from_disk()
 
@@ -30,6 +25,27 @@ def run(args):
     main_window = MainWindow(options, getattr(args, "preview", False))
     app.main_window = main_window
     main_window.show()
+
+
+def show_data_editor(app: QApplication, args):
+    from randovania.games.prime import default_data
+    from randovania.gui.data_editor import DataEditorWindow
+
+    app.data_visualizer = DataEditorWindow(default_data.decode_default_prime2())
+    app.data_visualizer.show()
+
+
+def run(args):
+    QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+    app = QApplication(sys.argv)
+
+    sys.excepthook = catch_exceptions
+
+    if args.window == "data-editor":
+        show_data_editor(app, args)
+    else:
+        show_main_window(app, args)
+
     sys.exit(app.exec_())
 
 
@@ -39,4 +55,6 @@ def create_subparsers(sub_parsers):
         help="Run the Graphical User Interface"
     )
     parser.add_argument("--preview", action="store_true", help="Activates preview features")
+    parser.add_argument("--data-editor", action="store_const", dest="window", const="data-editor",
+                        help="Displays the data editor window")
     parser.set_defaults(func=run)
