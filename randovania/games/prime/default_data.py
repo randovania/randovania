3b5e6c28668466846013e34b95f88fa6a1bdc2dc
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2018-12-28 18:09:58 -0200

    Move binary data to another directory

--- randovania/games/prime/default_data.py
+++ randovania/games/prime/default_data.py
@@ -8,6 +8,6 @@ from randovania.games.prime.binary_data import decode_file_path
 @functools.lru_cache()
 def decode_default_prime2() -> dict:
     return decode_file_path(
-        get_data_path().joinpath("prime2.bin"),
-        get_data_path().joinpath("prime2_extra.json")
+        get_data_path().joinpath("binary_data", "prime2.bin"),
+        get_data_path().joinpath("binary_data", "prime2_extra.json")
     )
