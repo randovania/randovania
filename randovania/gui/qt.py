075c9644602078f893f4d8b953ceb3a02a2a91f9
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-01-13 17:40:39 -0200

    Add `--tracker` to cli

--- randovania/gui/qt.py
+++ randovania/gui/qt.py
@@ -35,14 +35,27 @@ def show_data_editor(app: QApplication, args):
     app.data_visualizer.show()
 
 
+def show_tracker(app: QApplication, args):
+    from randovania.gui.tracker_window import TrackerWindow
+
+    options = Options.with_default_data_dir()
+    options.load_from_disk()
+
+    app.tracker = TrackerWindow(options.layout_configuration)
+    app.tracker.show()
+
+
 def run(args):
     QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
     app = QApplication(sys.argv)
 
     sys.excepthook = catch_exceptions
 
-    if getattr(args, "window", None) == "data-editor":
+    target_window = getattr(args, "window", None)
+    if target_window == "data-editor":
         show_data_editor(app, args)
+    elif target_window == "tracker":
+        show_tracker(app, args)
     else:
         show_main_window(app, args)
 
@@ -55,6 +68,9 @@ def create_subparsers(sub_parsers):
         help="Run the Graphical User Interface"
     )
     parser.add_argument("--preview", action="store_true", help="Activates preview features")
-    parser.add_argument("--data-editor", action="store_const", dest="window", const="data-editor",
-                        help="Displays the data editor window")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--data-editor", action="store_const", dest="window", const="data-editor",
+                        help="Opens only data editor window")
+    group.add_argument("--tracker", action="store_const", dest="window", const="tracker",
+                        help="Opens only the tracker window")
     parser.set_defaults(func=run)
