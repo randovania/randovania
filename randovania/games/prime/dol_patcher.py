40d3b6b6a7d623706b90447d37db4e53af712430
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-01-19 13:30:46 -0200

    Also patch the initial area in the dol

--- randovania/games/prime/dol_patcher.py
+++ randovania/games/prime/dol_patcher.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Tuple, Dict, BinaryIO
+from typing import Tuple, Dict, BinaryIO, Iterator
 
+from randovania.game_description.area import Area
 from randovania.game_description.world import World
 
 GameVersion = str
-
 
 # TODO: these maybe also include the target register
 _instructions = {
@@ -41,8 +41,22 @@ class FullLoadInstruction:
         dol.write(low_value.to_bytes(2, "big"))
 
 
-_initial_load_offsets: Dict[GameVersion, Tuple[FullLoadInstruction, ...]] = {
-    "v1.028": (FullLoadInstruction(0x00140378, 0x00140380), FullLoadInstruction(0x00140388, 0x00140380))
+@dataclass
+class EchoesInitialLocationInstructions:
+    world_load: Tuple[FullLoadInstruction, FullLoadInstruction]
+    area_load: FullLoadInstruction
+
+    @property
+    def all_instructions(self) -> Iterator[FullLoadInstruction]:
+        yield from self.world_load
+        yield self.area_load
+
+
+_initial_location_instructions_per_version: Dict[GameVersion, EchoesInitialLocationInstructions] = {
+    "v1.028": EchoesInitialLocationInstructions(
+        (FullLoadInstruction(0x00140378, 0x00140380), FullLoadInstruction(0x00140388, 0x00140390)),
+        FullLoadInstruction(0x00140398, 0x0014039c),
+    )
 }
 
 
@@ -61,15 +75,16 @@ def _check_instruction_at(dol: BinaryIO, offset: int, instruction: str, game_ver
             ))
 
 
-def change_starting_world(game_root: Path, new_starting_world: World):
+def change_starting_spawn(game_root: Path, new_starting_world: World, new_starting_area: Area):
     dol_path = game_root.joinpath("sys", "main.dol")
 
     version = "v1.028"
-    offsets = _initial_load_offsets[version]
+    location_instructions = _initial_location_instructions_per_version[version]
 
     with dol_path.open("r+b") as dol:
-        for offset in offsets:
+        for offset in location_instructions.all_instructions:
             offset.check_has_instructions(dol, version)
 
-        for offset in offsets:
+        for offset in location_instructions.world_load:
             offset.write_value(dol, new_starting_world.world_asset_id)
+        location_instructions.area_load.write_value(dol, new_starting_area.area_asset_id)
