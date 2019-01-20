79b372db1191e23c126fbe025dfce60d793c2a2b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-01-20 14:03:17 -0200

    Adding LayoutStartingGear

--- randovania/resolver/layout_configuration.py
+++ randovania/resolver/layout_configuration.py
@@ -57,7 +57,24 @@ class LayoutStartingLocation(BitPackValue):
 
     @classmethod
     def bit_pack_unpack(cls, decoder: BitPackDecoder):
-        return LayoutStartingLocation()
+        return cls()
+
+
+@dataclass(frozen=True)
+class LayoutStartingGear(BitPackValue):
+    has_scan_visor: bool
+    has_charge_beam: bool
+    has_morph_ball: bool
+
+    def bit_pack_format(self) -> Iterator[int]:
+        yield from []
+
+    def bit_pack_arguments(self) -> Iterator[int]:
+        yield from []
+
+    @classmethod
+    def bit_pack_unpack(cls, decoder: BitPackDecoder):
+        return cls()
 
 
 @dataclass(frozen=True)
