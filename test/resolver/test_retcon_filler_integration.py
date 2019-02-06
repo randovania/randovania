854e8946d38340a99dff84bf6b1f473ce0258ae8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-02-05 23:45:11 -0200

    Add test for retcon_playthrough_filler directly

---
+++ test/resolver/test_retcon_filler_integration.py
@@ -0,0 +1,35 @@
+from random import Random
+from unittest.mock import MagicMock
+
+import pytest
+
+from randovania.game_description import data_reader
+from randovania.game_description.game_patches import GamePatches
+from randovania.layout.layout_configuration import LayoutConfiguration
+from randovania.resolver.bootstrap import logic_bootstrap
+from randovania.resolver.filler import retcon
+
+
+skip_generation_tests = pytest.mark.skipif(
+    pytest.config.option.skip_generation_tests,
+    reason="skipped due to --skip-generation-tests")
+
+
+@skip_generation_tests
+@pytest.mark.skip
+def test_retcon_filler_integration():
+    layout_configuration = LayoutConfiguration.default()
+
+    rng = Random("fixed-seed!")
+    status_update = MagicMock()
+
+    game = data_reader.decode_data(layout_configuration.game_data)
+    patches = GamePatches.with_game(game)
+    available_pickups = game.pickup_database.all_useful_pickups
+
+    logic, state = logic_bootstrap(layout_configuration, game, patches)
+    logic.game.simplify_connections(state.resources)
+
+    filler_patches = retcon.retcon_playthrough_filler(logic, state, tuple(available_pickups), rng, status_update)
+    assert filler_patches == patches
+
