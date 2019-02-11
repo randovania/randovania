4868db70393980d2c10bfaa3498aa81a85e17418
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-02-10 23:21:30 -0200

    Add roundtrip tests for PickupQuantities

--- test/layout/test_pickup_quantities.py
+++ test/layout/test_pickup_quantities.py
@@ -1,24 +1,53 @@
-from typing import Dict
-
 import pytest
 
-from randovania.bitpacking.bitpacking import pack_value, BitPackDecoder
+from randovania.bitpacking import bitpacking
+from randovania.bitpacking.bitpacking import BitPackDecoder
 from randovania.layout.pickup_quantities import PickupQuantities
 
 
-@pytest.fixture(params=[{"Missile Expansion": 0, "Light Suit": 0, "Super Missile": 0},
-                        {"Missile Expansion": 0, "Light Suit": 0, "Super Missile": 1},
-                        {"Missile Expansion": 10, "Light Suit": 0, "Super Missile": 0},
-                        {"Missile Expansion": 10, "Light Suit": 2, "Super Missile": 1},
-                        {"Missile Expansion": 10, "Super Missile": 1},
-                        ], name="pickup_quantities")
-def _pickup_quantities(request):
-    yield request.param
+@pytest.fixture(
+    params=[
+        {"encoded": b'\x00',
+         "json": {}},
+
+        {"encoded": b'\x87S,X\x88\xe6\xc4\x04\x10',
+         "json": {"Missile Expansion": 0, "Light Suit": 0, "Super Missile": 0}},
+
+        {"encoded": b'\x85[,"9\xb1\x01\x04',
+         "json": {"Missile Expansion": 0, "Light Suit": 0, "Super Missile": 1}},
+
+        {"encoded": b'\x85S\x16\xa29qM\x88\x08 ',
+         "json": {"Missile Expansion": 10, "Light Suit": 0, "Super Missile": 0}},
+
+        {"encoded": b'\x81Z#\x98\x14\xe0\x80\x82,\x08',
+         "json": {"Missile Expansion": 10, "Light Suit": 2, "Super Missile": 1}},
+
+        {"encoded": b'\x81b#\x98\x14\xe0\x80\x82\x00',
+         "json": {"Missile Expansion": 10, "Super Missile": 1}},
+    ],
+    name="resources_with_data")
+def _resources_with_data(request):
+    return request.param["encoded"], PickupQuantities.from_params(request.param["json"])
 
 
-def test_compare_encode_round_trip(pickup_quantities: Dict[str, int]):
-    original = PickupQuantities.from_params(pickup_quantities)
-    encoded = pack_value(original)
+def test_decode(resources_with_data):
+    # Setup
+    data, expected = resources_with_data
 
-    decoded = PickupQuantities.bit_pack_unpack(BitPackDecoder(encoded))
-    assert original == decoded
+    # Run
+    decoder = BitPackDecoder(data)
+    result = PickupQuantities.bit_pack_unpack(decoder)
+
+    # Assert
+    assert result == expected
+
+
+def test_encode(resources_with_data):
+    # Setup
+    expected, value = resources_with_data
+
+    # Run
+    result = bitpacking.pack_value(value)
+
+    # Assert
+    assert result == expected
