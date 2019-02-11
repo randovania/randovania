8fcb87876a32f63382a97c6bbf146b593bbc74ea
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-02-10 22:58:56 -0200

    Add roundtrip test for StartingLocation

--- test/layout/test_starting_location.py
+++ test/layout/test_starting_location.py
@@ -1,6 +1,7 @@
 import pytest
 
 from randovania.bitpacking import bitpacking
+from randovania.bitpacking.bitpacking import BitPackDecoder
 from randovania.game_description.area_location import AreaLocation
 from randovania.layout.starting_location import StartingLocationConfiguration, StartingLocation
 
@@ -70,3 +71,38 @@ def test_invalid_constructor_location_should_be_missing():
 
     assert str(err.value) == "custom_location set to world 0/area 0, " \
                              "but configuration is StartingLocationConfiguration.SHIP instead of CUSTOM"
+
+
+@pytest.fixture(
+    params=[
+        {"encoded": b'\x00', "json": "ship"},
+        {"encoded": b'@', "json": "random-save-station"},
+        {"encoded": b'\x82@', "json": {"world_asset_id": 1006255871, "area_asset_id": 341957679}},
+        {"encoded": b'\x92\xc0', "json": {"world_asset_id": 1039999561, "area_asset_id": 3548128276}},
+    ],
+    name="location_with_data")
+def _location_with_data(request):
+    return request.param["encoded"], StartingLocation.from_json(request.param["json"])
+
+
+def test_decode(location_with_data):
+    # Setup
+    data, expected = location_with_data
+
+    # Run
+    decoder = BitPackDecoder(data)
+    result = StartingLocation.bit_pack_unpack(decoder)
+
+    # Assert
+    assert result == expected
+
+
+def test_encode(location_with_data):
+    # Setup
+    expected, value = location_with_data
+
+    # Run
+    result = bitpacking.pack_value(value)
+
+    # Assert
+    assert result == expected
