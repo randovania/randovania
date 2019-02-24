60833d67527d5c5e5493a5df7ca4d0f23b4a4c15
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-02-24 14:08:26 -0300

    Rework how MajorItemState works

--- randovania/game_description/item/major_item.py
+++ randovania/game_description/item/major_item.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Optional, List, Tuple
 
+from randovania.game_description.resources import PickupIndex
+
 
 class MajorItemCategory(Enum):
     VISOR = "visor"
@@ -22,7 +24,7 @@ class MajorItem:
     item: int
     ammo: Tuple[int, ...]
     required: bool
-    original_index: Optional[int]
+    original_index: Optional[PickupIndex]
     probability_offset: int
 
     @classmethod
@@ -34,18 +36,20 @@ class MajorItem:
             item=value["item"],
             ammo=tuple(value.get("ammo", [])),
             required=value.get("required", False),
-            original_index=value.get("original_index"),
+            original_index=PickupIndex(value["original_index"]) if "original_index" in value else None,
             probability_offset=value["probability_offset"],
         )
 
     @property
     def as_json(self) -> dict:
-        return {
+        result = {
             "item_category": self.item_category.value,
             "model_index": self.model_index,
             "item": self.item,
             "ammo": list(self.ammo),
             "required": self.required,
-            "original_index": self.original_index,
             "probability_offset": self.probability_offset,
         }
+        if self.original_index is not None:
+            result["original_index"] = self.original_index.index
+        return result
--- randovania/layout/major_items_configuration.py
+++ randovania/layout/major_items_configuration.py
@@ -9,19 +9,14 @@ from randovania.layout.major_item_state import MajorItemState
 @dataclass(frozen=True)
 class MajorItemsConfiguration:
     items_state: Dict[MajorItem, MajorItemState]
-    ammo_count_for_item: Dict[MajorItem, int]
     progressive_suit: bool
 
     @property
     def as_json(self) -> dict:
         return {
             "items_state": {
-                major_item.name: state.value
+                major_item.name: state.as_json
                 for major_item, state in self.items_state.items()
-            },
-            "ammo_count": {
-                major_item.name: count
-                for major_item, count in self.ammo_count_for_item.items()
             },
             "progressive_suit": self.progressive_suit,
         }
@@ -30,20 +25,8 @@ class MajorItemsConfiguration:
     def from_json(cls, value: dict, item_database: ItemDatabase) -> "MajorItemsConfiguration":
         return cls(
             items_state={
-                item_database.major_items[name]: MajorItemState(state)
-                for name, state in value["items_state"].items()
-            },
-            ammo_count_for_item={
-                item_database.major_items[name]: count
-                for name, count in value["ammo_count"].items()
+                item_database.major_items[name]: MajorItemState.from_json(state_data)
+                for name, state_data in value["items_state"].items()
             },
             progressive_suit=value["progressive_suit"]
-        )
-
-    @classmethod
-    def default(cls) -> "MajorItemsConfiguration":
-        return cls(
-            items_state={},
-            ammo_count_for_item={},
-            progressive_suit=True
         )
