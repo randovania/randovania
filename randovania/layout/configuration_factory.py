784e3b3576ffc39a4bab14c4cca636c3fe380b63
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-02-24 17:07:11 -0300

    Split configuration jsons per item type

--- randovania/layout/configuration_factory.py
+++ randovania/layout/configuration_factory.py
@@ -10,16 +10,17 @@ from randovania.layout.major_items_configuration import MajorItemsConfiguration
 def get_major_items_configurations_for(mode: str) -> MajorItemsConfiguration:
     item_database = default_prime2_item_database()
 
-    with get_data_path().joinpath("json_data", "configurations", "vanilla.json").open() as open_file:
+    with get_data_path().joinpath("json_data", "configurations", "major_items",
+                                  "{}.json".format(mode)).open() as open_file:
         data = json.load(open_file)
 
-    return MajorItemsConfiguration.from_json(data["major_items"], item_database)
+    return MajorItemsConfiguration.from_json(data, item_database)
 
 
 def get_ammo_configurations_for(mode: str) -> AmmoConfiguration:
     item_database = default_prime2_item_database()
 
-    with get_data_path().joinpath("json_data", "configurations", "vanilla.json").open() as open_file:
+    with get_data_path().joinpath("json_data", "configurations", "ammo", "{}.json".format(mode)).open() as open_file:
         data = json.load(open_file)
 
-    return AmmoConfiguration.from_json(data["ammo"], item_database)
+    return AmmoConfiguration.from_json(data, item_database)
