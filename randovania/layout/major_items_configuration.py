c66bb625c413751ebf4321a1d05015613e2dafbf
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-02-24 15:52:49 -0300

    Remove Progressive Suit bool

--- randovania/layout/major_items_configuration.py
+++ randovania/layout/major_items_configuration.py
@@ -9,7 +9,6 @@ from randovania.layout.major_item_state import MajorItemState
 @dataclass(frozen=True)
 class MajorItemsConfiguration:
     items_state: Dict[MajorItem, MajorItemState]
-    progressive_suit: bool
 
     @property
     def as_json(self) -> dict:
@@ -17,8 +16,7 @@ class MajorItemsConfiguration:
             "items_state": {
                 major_item.name: state.as_json
                 for major_item, state in self.items_state.items()
-            },
-            "progressive_suit": self.progressive_suit,
+            }
         }
 
     @classmethod
@@ -27,6 +25,5 @@ class MajorItemsConfiguration:
             items_state={
                 item_database.major_items[name]: MajorItemState.from_json(state_data)
                 for name, state_data in value["items_state"].items()
-            },
-            progressive_suit=value["progressive_suit"]
+            }
         )
