8b438332268e67e3cc32f4ba67606478b95c4e25
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-03-10 18:23:03 +0100

    Add AmmoConfiguration to MainRulesWindow, split ammo maximum to be per resource

--- randovania/gui/main_rules.py
+++ randovania/gui/main_rules.py
@@ -1,15 +1,21 @@
+import dataclasses
 from functools import partial
 
 from PySide2.QtCore import QRect, Qt
-from PySide2.QtWidgets import QMainWindow, QLabel, QGroupBox, QGridLayout, QToolButton, QSizePolicy, QDialog
+from PySide2.QtWidgets import QMainWindow, QLabel, QGroupBox, QGridLayout, QToolButton, QSizePolicy, QDialog, QSpinBox
 
-from randovania.game_description.default_database import default_prime2_item_database
+from randovania.game_description.default_database import default_prime2_item_database, default_prime2_resource_database
+from randovania.game_description.item.ammo import Ammo
+from randovania.game_description.item.item_database import ItemDatabase
 from randovania.game_description.item.major_item import MajorItemCategory, MajorItem
+from randovania.game_description.resource_type import ResourceType
 from randovania.gui.background_task_mixin import BackgroundTaskMixin
 from randovania.gui.item_configuration_popup import ItemConfigurationPopup
 from randovania.gui.main_rules_ui import Ui_MainRules
 from randovania.gui.tab_service import TabService
 from randovania.interface_common.options import Options
+from randovania.layout.ammo_configuration import AmmoConfiguration
+from randovania.layout.ammo_state import AmmoState
 
 
 def _toggle_category_visibility(category_button: QToolButton, category_box: QGroupBox):
@@ -28,11 +34,20 @@ class MainRulesWindow(QMainWindow, Ui_MainRules):
         self.gridLayout.setAlignment(Qt.AlignTop)
 
         # Create Stuff
+        item_database = default_prime2_item_database()
         self._create_categories_boxes(size_policy)
-        self._create_major_item_boxes()
+        self._create_major_item_boxes(item_database)
+        self._create_ammo_maximum_boxes(item_database)
+        self._create_ammo_pickup_boxes(item_database)
 
     def on_options_changed(self):
-        pass
+        ammo_configuration = self._options.ammo_configuration
+
+        for ammo_item, maximum in ammo_configuration.maximum_ammo.items():
+            self._ammo_maximum_spinboxes[ammo_item].setValue(maximum)
+
+        for ammo, state in ammo_configuration.items_state.items():
+            self._ammo_pickup_spinboxes[ammo].setValue(state.pickup_count)
 
     def _create_categories_boxes(self, size_policy):
         self._boxes_for_category = {}
@@ -61,9 +76,7 @@ class MainRulesWindow(QMainWindow, Ui_MainRules):
             category_button.clicked.connect(partial(_toggle_category_visibility, category_button, category_box))
             category_box.setVisible(False)
 
-    def _create_major_item_boxes(self):
-        item_database = default_prime2_item_database()
-
+    def _create_major_item_boxes(self, item_database: ItemDatabase):
         for major_item in item_database.major_items.values():
             category_box, category_layout, elements = self._boxes_for_category[major_item.item_category]
 
@@ -82,6 +95,11 @@ class MainRulesWindow(QMainWindow, Ui_MainRules):
             item_button.clicked.connect(partial(self.show_item_popup, major_item))
 
     def show_item_popup(self, item: MajorItem):
+        """
+        Shows the ItemConfigurationPopup for the given MajorItem
+        :param item:
+        :return:
+        """
         major_items_configuration = self._options.major_items_configuration
 
         popup = ItemConfigurationPopup(self, item, major_items_configuration.items_state[item])
@@ -92,3 +110,66 @@ class MainRulesWindow(QMainWindow, Ui_MainRules):
                 self._options.major_items_configuration = major_items_configuration.replace_state_for_item(
                     item, popup.state
                 )
+
+    def _create_ammo_maximum_boxes(self, item_database: ItemDatabase):
+        """
+        Creates the GroupBox with SpinBoxes for selecting the maximum value of all the ammo items
+        :param item_database:
+        :return:
+        """
+        resource_database = default_prime2_resource_database()
+        ammo_items = {}
+
+        self._ammo_maximum_spinboxes = {}
+
+        for ammo in item_database.ammo.values():
+            for ammo_item in ammo.items:
+                ammo_items[ammo_item] = max(ammo_items.get(ammo_item, 0), ammo.maximum)
+
+        for i, (ammo_item, maximum) in enumerate(ammo_items.items()):
+            maximum_name_label = QLabel(self.maximum_count_box)
+            maximum_spinbox = QSpinBox(self.maximum_count_box)
+
+            item = resource_database.get_by_type_and_index(ResourceType.ITEM, ammo_item)
+            maximum_name_label.setText(item.long_name)
+            maximum_spinbox.setMaximum(maximum)
+            maximum_spinbox.valueChanged.connect(partial(self._on_update_ammo_maximum_spinbox, ammo_item))
+
+            self.maximum_count_layout.addWidget(maximum_name_label, i, 0)
+            self.maximum_count_layout.addWidget(maximum_spinbox, i, 1)
+
+            self._ammo_maximum_spinboxes[ammo_item] = maximum_spinbox
+
+    def _on_update_ammo_maximum_spinbox(self, ammo_int: int, value: int):
+        with self._options as options:
+            options.ammo_configuration = options.ammo_configuration.replace_maximum_for_item(
+                ammo_int, value
+            )
+
+    def _create_ammo_pickup_boxes(self, item_database: ItemDatabase):
+        """
+        Creates the GroupBox with SpinBoxes for selecting the pickup count of all the ammo
+        :param item_database:
+        :return:
+        """
+        self._ammo_pickup_spinboxes = {}
+
+        for i, ammo in enumerate(item_database.ammo.values()):
+            pickup_name_label = QLabel(self.pickup_count_box)
+            pickup_name_label.setText(ammo.name)
+
+            pickup_spinbox = QSpinBox(self.pickup_count_box)
+            pickup_spinbox.valueChanged.connect(partial(self._on_update_ammo_pickup_spinbox, ammo))
+
+            self.pickup_count_layout.addWidget(pickup_name_label, i, 0)
+            self.pickup_count_layout.addWidget(pickup_spinbox, i, 1)
+
+            self._ammo_pickup_spinboxes[ammo] = pickup_spinbox
+
+    def _on_update_ammo_pickup_spinbox(self, ammo: Ammo, value: int):
+        with self._options as options:
+            ammo_configuration = options.ammo_configuration
+            options.ammo_configuration = ammo_configuration.replace_state_for_ammo(
+                ammo,
+                dataclasses.replace(ammo_configuration.items_state[ammo], pickup_count=value)
+            )
--- randovania/layout/ammo_state.py
+++ randovania/layout/ammo_state.py
@@ -3,14 +3,12 @@ from dataclasses import dataclass
 
 @dataclass(frozen=True)
 class AmmoState:
-    total_count: int
     variance: int
     pickup_count: int
 
     @property
     def as_json(self) -> dict:
         return {
-            "total_count": self.total_count,
             "variance": self.variance,
             "pickup_count": self.pickup_count,
         }
@@ -18,7 +16,6 @@ class AmmoState:
     @classmethod
     def from_json(cls, value: dict) -> "AmmoState":
         return cls(
-            total_count=value["total_count"],
             variance=value["variance"],
             pickup_count=value["pickup_count"],
         )
