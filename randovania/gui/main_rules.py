e2ca1d9cbdf0dadefd102b1a148cc80d48bc6be8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-03-17 23:31:54 +0100

    Don't add required items to the item list

--- randovania/gui/main_rules.py
+++ randovania/gui/main_rules.py
@@ -299,7 +299,7 @@ class MainRulesWindow(QMainWindow, Ui_MainRules):
 
     def _create_major_item_boxes(self, item_database: ItemDatabase):
         for major_item in item_database.major_items.values():
-            if major_item.item_category == MajorItemCategory.ENERGY_TANK:
+            if major_item.required or major_item.item_category == MajorItemCategory.ENERGY_TANK:
                 continue
             category_box, category_layout, elements = self._boxes_for_category[major_item.item_category]
 
