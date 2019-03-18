aa89709b5f9957fca8317baa8cfe79c9829dbb8a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-03-19 00:29:24 +0100

    Fixed elevators in the seed log

--- test/layout/test_game_patches_serializer.py
+++ test/layout/test_game_patches_serializer.py
@@ -1,8 +1,11 @@
+import dataclasses
+
 import pytest
 
 from randovania.game_description import data_reader
 from randovania.game_description.game_patches import GamePatches
 from randovania.game_description.node import PickupNode
+from randovania.game_description.resources import find_resource_info_with_long_name
 from randovania.layout import game_patches_serializer
 from randovania.layout.layout_configuration import LayoutConfiguration
 
@@ -10,6 +13,8 @@ from randovania.layout.layout_configuration import LayoutConfiguration
 @pytest.fixture(
     params=[
         {},
+        {"starting_item": "Morph Ball"},
+        {"elevator": [1572998, "Temple Grounds/Transport to Agon Wastes"]}
     ],
     name="patches_with_data")
 def _patches_with_data(request, echoes_game_data):
@@ -28,8 +33,23 @@ def _patches_with_data(request, echoes_game_data):
             for world in sorted(game.world_list.worlds, key=lambda w: w.name)
         },
     }
+    patches = GamePatches.with_game(game)
 
-    return data, GamePatches.with_game(game)
+    if request.param.get("starting_item"):
+        item_name = request.param.get("starting_item")
+        patches = patches.assign_extra_initial_items([
+            (find_resource_info_with_long_name(game.resource_database.item, item_name), 1)
+        ])
+        data["starting_items"][item_name] = 1
+
+    if request.param.get("elevator"):
+        elevator_id, elevator_source = request.param.get("elevator")
+        patches = dataclasses.replace(patches, elevator_connection={
+            elevator_id: game.starting_location,
+        })
+        data["elevators"][elevator_source] = "Temple Grounds/Landing Site"
+
+    return data, patches
 
 
 def test_encode(patches_with_data, echoes_game_data):
