d2f14bec6d9b7e5d56d2f3a3109fc6f0da27b4f2
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-03-24 02:42:59 +0100

    Using construct to encode/decode the binary data

--- randovania/games/prime/binary_data.py
+++ randovania/games/prime/binary_data.py
@@ -5,6 +5,9 @@ import operator
 from pathlib import Path
 from typing import List, Callable, TypeVar, BinaryIO, Dict, TextIO
 
+from construct import Struct, Int32ub, Const, CString, Byte, Rebuild, Embedded, Float32b, Flag, \
+    Short, PrefixedArray, Array, Switch, LazyStruct
+
 from randovania.binary_file import BinarySource, BinaryWriter
 
 X = TypeVar('X')
@@ -131,6 +134,7 @@ def read_node(source: BinarySource) -> Dict:
         node["connected_dock_index"] = source.read_byte()
         node["dock_type"] = source.read_byte()
         node["dock_weakness_index"] = source.read_byte()
+
         source.skip(3)
 
     elif node_type == 2:
@@ -369,3 +373,104 @@ def encode(original_data: Dict, x: BinaryIO) -> dict:
     write_array(writer, data.pop("worlds"), write_world)
 
     return data
+
+
+ConstructResourceInfo = Struct(
+    index=Byte,
+    long_name=CString("utf8"),
+    short_name=CString("utf8"),
+)
+
+ConstructIndividualRequirement = Struct(
+    requirement_type=Byte,
+    requirement_index=Byte,
+    amount=Short,
+    negate=Flag,
+)
+ConstructRequirementList = PrefixedArray(Byte, ConstructIndividualRequirement)
+ConstructRequirementSet = PrefixedArray(Byte, ConstructRequirementList)
+
+ConstructDockWeakness = Struct(
+    index=Byte,
+    name=CString("utf8"),
+    is_blast_door=Flag,
+    requirement_set=ConstructRequirementSet,
+)
+
+ConstructResourceDatabase = Struct(
+    items=PrefixedArray(Byte, ConstructResourceInfo),
+    events=PrefixedArray(Byte, ConstructResourceInfo),
+    tricks=PrefixedArray(Byte, ConstructResourceInfo),
+    damage=PrefixedArray(Byte, Struct(
+        Embedded(ConstructResourceInfo),
+        reductions=PrefixedArray(Byte, Struct(
+            index=Byte,
+            multiplier=Float32b,
+        )),
+    )),
+    versions=PrefixedArray(Byte, ConstructResourceInfo),
+    misc=PrefixedArray(Byte, ConstructResourceInfo),
+    difficulty=PrefixedArray(Byte, ConstructResourceInfo),
+)
+
+ConstructNode = Struct(
+    name=CString("utf8"),
+    heal=Flag,
+    node_type=Byte,
+    data=Switch(
+        lambda this: this.node_type,
+        {
+            1: Struct(
+                dock_index=Byte,
+                connected_area_asset_id=Int32ub,
+                connected_dock_index=Byte,
+                dock_type=Byte,
+                dock_weakness_index=Byte,
+                _=Const(b"\x00\x00\x00"),
+            ),
+            2: Struct(
+                pickup_index=Byte,
+            ),
+            3: Struct(
+                destination_world_asset_id=Int32ub,
+                destination_area_asset_id=Int32ub,
+                teleporter_instance_id=Int32ub,
+            ),
+            4: Struct(
+                event_index=Byte,
+            ),
+        }
+    )
+)
+
+
+ConstructArea = Struct(
+    name=CString("utf8"),
+    asset_id=Int32ub,
+    node_count=Rebuild(Byte, lambda this: len(this.nodes)),
+    default_node_index=Byte,
+    nodes=Array(lambda this: this.node_count, ConstructNode),
+    connections=Array(
+        lambda this: this.node_count,
+        Array(lambda this: this.node_count - 1, ConstructRequirementSet)
+    )
+)
+
+ConstructWorld = Struct(
+    name=CString("utf8"),
+    asset_id=Int32ub,
+    areas=PrefixedArray(Byte, ConstructArea),
+)
+
+ConstructGame = Struct(
+    magic_number=Const(b"Req."),
+    format_version=Int32ub,
+    game=Byte,
+    game_name=CString("utf8"),
+    resource_database=ConstructResourceDatabase,
+    dock_weakness_database=Struct(
+        door=PrefixedArray(Byte, ConstructDockWeakness),
+        portal=PrefixedArray(Byte, ConstructDockWeakness),
+    ),
+    worlds=PrefixedArray(Byte, ConstructWorld),
+)
