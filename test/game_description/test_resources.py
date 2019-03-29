ba30db2a1c6a62408dbe111e2374710736ec71f5
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-03-29 20:08:20 +0100

    Add convert_resources to PickupEntry

--- test/game_description/test_resources.py
+++ test/game_description/test_resources.py
@@ -1,6 +1,9 @@
 import pytest
 
-from randovania.game_description.resources import merge_resources, PickupIndex
+from randovania.game_description.item.item_category import ItemCategory
+from randovania.game_description.resource_type import ResourceType
+from randovania.game_description.resources import merge_resources, PickupIndex, PickupEntry, ConditionalResources, \
+    ResourceConversion, SimpleResourceInfo, add_resource_gain_to_current_resources
 
 
 @pytest.mark.parametrize(["a", "b", "result"], [
@@ -18,3 +21,27 @@ def test_pickup_index_equality():
 def test_pickup_index_has():
     d = {PickupIndex(1): True}
     assert PickupIndex(1) in d
+
+
+def test_add_resource_gain_to_current_resources_convert():
+    # Setup
+    resource_a = SimpleResourceInfo(1, "A", "A", ResourceType.ITEM)
+    resource_b = SimpleResourceInfo(2, "B", "B", ResourceType.ITEM)
+
+    pickup = PickupEntry(
+        name="P1", model_index=1, item_category=ItemCategory.SUIT,
+        resources=(ConditionalResources(None, None, ()),),
+        convert_resources=(ResourceConversion(resource_a, resource_b),)
+    )
+    current_resources = {
+        resource_a: 5
+    }
+
+    # Run
+    add_resource_gain_to_current_resources(pickup.resource_gain(current_resources), current_resources)
+
+    # Assert
+    assert current_resources == {
+        resource_a: 0,
+        resource_b: 5
+    }
