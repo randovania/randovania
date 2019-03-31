fdd08e34605392deabf996ea30f0a77dd7faa3e5
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-03-31 15:05:44 +0200

    Workaround something setting debug level to a magic mock

--- test/resolver/test_resolver.py
+++ test/resolver/test_resolver.py
@@ -2,8 +2,7 @@ import pytest
 
 from randovania.game_description import data_reader
 from randovania.layout.layout_description import LayoutDescription
-from randovania.resolver import resolver
-
+from randovania.resolver import resolver, debug
 
 skip_resolver_tests = pytest.mark.skipif(
     pytest.config.option.skip_resolver_tests,
@@ -13,6 +12,8 @@ skip_resolver_tests = pytest.mark.skipif(
 @skip_resolver_tests
 def test_resolver_with_log_file(test_files_dir):
     # Setup
+    debug._DEBUG_LEVEL = 0
+
     description = LayoutDescription.from_file(test_files_dir.joinpath("log_files", "seed_a.json"))
     configuration = description.permalink.layout_configuration
     game = data_reader.decode_data(configuration.game_data)
