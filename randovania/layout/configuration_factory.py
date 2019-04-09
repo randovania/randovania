073e1607ff0971fd61abb2e5ff8dc4293f87f724
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-04-10 01:06:18 +0200

    Adding vanilla translator options

--- randovania/layout/configuration_factory.py
+++ randovania/layout/configuration_factory.py
@@ -1,4 +1,5 @@
 import json
+from typing import Dict
 
 from randovania import get_data_path
 from randovania.game_description.default_database import default_prime2_item_database
@@ -6,7 +7,7 @@ from randovania.game_description.resources.translator_gate import TranslatorGate
 from randovania.layout.ammo_configuration import AmmoConfiguration
 from randovania.layout.major_item_state import MajorItemState
 from randovania.layout.major_items_configuration import MajorItemsConfiguration
-from randovania.layout.translator_configuration import TranslatorConfiguration, LayoutTranslatorRequirement
+from randovania.layout.translator_configuration import LayoutTranslatorRequirement
 
 
 def get_default_major_items_configurations() -> MajorItemsConfiguration:
@@ -32,16 +33,14 @@ def get_default_ammo_configurations() -> AmmoConfiguration:
     return AmmoConfiguration.from_json(data, item_database)
 
 
-def _raw_translator_configurations(data: dict) -> TranslatorConfiguration:
-    return TranslatorConfiguration(
-        {
-            TranslatorGate(int(key)): LayoutTranslatorRequirement(item)
-            for key, item in data["translator_requirement"].items()
-        }
-    )
+def _raw_translator_configurations(data: dict) -> Dict[TranslatorGate, LayoutTranslatorRequirement]:
+    return {
+        TranslatorGate(int(key)): LayoutTranslatorRequirement(item)
+        for key, item in data["translator_requirement"].items()
+    }
 
 
-def get_vanilla_actual_translator_configurations() -> TranslatorConfiguration:
+def get_vanilla_actual_translator_configurations() -> Dict[TranslatorGate, LayoutTranslatorRequirement]:
     with get_data_path().joinpath("item_database", "default_state",
                                   "translator_vanilla_actual.json").open() as open_file:
         data = json.load(open_file)
@@ -49,7 +48,7 @@ def get_vanilla_actual_translator_configurations() -> TranslatorConfiguration:
     return _raw_translator_configurations(data)
 
 
-def get_vanilla_colors_translator_configurations() -> TranslatorConfiguration:
+def get_vanilla_colors_translator_configurations() -> Dict[TranslatorGate, LayoutTranslatorRequirement]:
     with get_data_path().joinpath("item_database", "default_state",
                                   "translator_vanilla_colors.json").open() as open_file:
         data = json.load(open_file)
