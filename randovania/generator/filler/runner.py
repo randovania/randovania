4456ad298c125723537d5e899cd79a997d5cbf66
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-04-24 22:00:28 +0200

    Set the precision for hints after the fact

--- randovania/generator/filler/runner.py
+++ randovania/generator/filler/runner.py
@@ -1,8 +1,10 @@
+import dataclasses
 from random import Random
 from typing import List, Tuple, Callable
 
 from randovania.game_description.game_description import GameDescription
 from randovania.game_description.game_patches import GamePatches
+from randovania.game_description.hint import HintItemPrecision, HintLocationPrecision
 from randovania.game_description.item.item_category import ItemCategory
 from randovania.game_description.resources.pickup_entry import PickupEntry
 from randovania.generator.filler.retcon import retcon_playthrough_filler
@@ -26,6 +28,55 @@ def _split_expansions(item_pool: List[PickupEntry]) -> Tuple[List[PickupEntry], 
             major_items.append(pickup)
 
     return major_items, expansions
+
+
+def add_hints_precision(configuration: LayoutConfiguration,
+                        patches: GamePatches,
+                        rng: Random,
+                        ) -> GamePatches:
+    """
+    Adds precision to all hints that are missing one.
+    :param configuration:
+    :param patches:
+    :param rng:
+    :return:
+    """
+
+    hints_to_replace = {
+        asset: hint
+        for asset, hint in patches.hints.items()
+        if hint.location_precision is None or hint.item_precision is None
+    }
+
+    asset_ids = list(hints_to_replace.keys())
+
+    # Add random item precisions
+    rng.shuffle(asset_ids)
+    item_precisions = []
+    for asset_id in asset_ids:
+        if not item_precisions:
+            item_precisions = HintItemPrecision.weighted_list()
+            rng.shuffle(item_precisions)
+
+        hints_to_replace[asset_id] = dataclasses.replace(hints_to_replace[asset_id],
+                                                         item_precision=item_precisions.pop())
+
+    # Add random location precisions
+    rng.shuffle(asset_ids)
+    location_precisions = []
+    for asset_id in asset_ids:
+        if not location_precisions:
+            location_precisions = HintLocationPrecision.weighted_list()
+            rng.shuffle(location_precisions)
+
+        hints_to_replace[asset_id] = dataclasses.replace(hints_to_replace[asset_id],
+                                                         location_precision=location_precisions.pop())
+
+    # Replace the hints the in the patches
+    return dataclasses.replace(patches, hints={
+        asset: hints_to_replace.get(asset, hint)
+        for asset, hint in patches.hints.items()
+    })
 
 
 def run_filler(configuration: LayoutConfiguration,
@@ -60,4 +111,6 @@ def run_filler(configuration: LayoutConfiguration,
         maximum_random_starting_items=major_configuration.maximum_random_starting_items,
         status_update=status_update)
 
-    return filler_patches, major_items + expansions
+    result = add_hints_precision(configuration, filler_patches, rng)
+
+    return result, major_items + expansions
