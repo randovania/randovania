4bffcb86312820775b1a75cd63ab8bd2d9179a00
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-05-01 18:08:55 +0200

    Add tests for item_hints

--- randovania/games/prime/patcher_file_lib/item_hints.py
+++ randovania/games/prime/patcher_file_lib/item_hints.py
@@ -4,36 +4,12 @@ from typing import Dict, Tuple, List
 from randovania.game_description.assignment import PickupAssignment
 from randovania.game_description.game_patches import GamePatches
 from randovania.game_description.hint import HintType, HintLocationPrecision, HintItemPrecision, Hint
+from randovania.game_description.node import LogbookNode
 from randovania.game_description.resources.pickup_entry import PickupEntry
 from randovania.game_description.world_list import WorldList
 from randovania.games.prime.patcher_file_lib.hint_name_creator import LocationHintCreator, create_simple_logbook_hint, \
     color_as_joke, color_text_as_red
 
-_LORE_SCANS = {
-    0x987884FB: "Luminoth Lore translated. (Age of Anxiety)",  # Meeting Grounds
-    0x3E0F8F4F: "Luminoth Lore translated. (Agon Falls)",  # Sanctuary Energy Controller
-    0x5324575E: "Luminoth Lore translated. (Cataclysm)",  # E3BEF27F
-
-    0xD25C0D22: "Luminoth Lore translated. (Dark Aether)",  # 133BF5B8
-    0x692E362E: "Luminoth Lore translated. (Light of Aether)",  # 2BCD44A7
-    0x49CD4F34: "Luminoth Lore translated. (New Weapons)",  # 3501473A
-    0xC3576EA5: "Luminoth Lore translated. (Origins)",  # 02A01334
-    0x39E3A79D: "Luminoth Lore translated. (Our Heritage)",  # 62FF94EE
-    0x54C87F8C: "Luminoth Lore translated. (Our War Begins)",  # 80E67F90
-    0x24E69725: "Luminoth Lore translated. (Paradise)",  # 427FAD9A
-    0x9F94AC29: "Luminoth Lore translated. (Recovering Energy)",  # 4BB5AE60
-    0x742B0696: "Luminoth Lore translated. (Sanctuary Falls)",  # A2406387
-    0xEFBA4480: "Luminoth Lore translated. (Saving Aether)",  # 02FC3717
-    0xF5535CEA: "Luminoth Lore translated. (Shattered Hope)",  # 73342A54
-    0x0405EE3F: "Luminoth Lore translated. (The Final Crusade)",  # 5571E89E
-    0x45C31C0B: "Luminoth Lore translated. (The Ing Attack)",  # 7448931C
-    0x82919C91: "Luminoth Lore translated. (The New Terror)",  # FB628DCB
-    0xCF593D9A: "Luminoth Lore translated. (The Sky Temple)",  # 70E5E6DD
-    0xA272E58B: "Luminoth Lore translated. (The Stellar Object)",  # DB7B2CED
-    0x8E9FCFAE: "Luminoth Lore translated. (The World Warped)",  # EE4732BE
-    0xBF77D533: "Luminoth Lore translated. (Torvus Falls)",  # 914F1381
-    0xF2BF7438: "Luminoth Lore translated. (Twilight)",  # 47265C0B
-}
 
 _PRIME_1_ITEMS = [
     "Varia Suit",
@@ -129,8 +105,8 @@ def create_hints(patches: GamePatches,
     """
 
     hint_name_creator = LocationHintCreator(world_list)
-    joke_items = list(set(_PRIME_1_ITEMS) | set(_PRIME_3_ITEMS))
-    joke_locations = list(set(_PRIME_3_LOCATIONS))
+    joke_items = list(sorted(set(_PRIME_1_ITEMS) | set(_PRIME_3_ITEMS)))
+    joke_locations = list(sorted(set(_PRIME_3_LOCATIONS)))
 
     rng.shuffle(joke_items)
     rng.shuffle(joke_locations)
@@ -176,9 +152,11 @@ def create_hints(patches: GamePatches,
 
     return [
         create_simple_logbook_hint(
-            asset_id,
-            hints_for_asset.get(asset_id, "Someone forgot to leave a message."))
-        for asset_id in _LORE_SCANS
+            logbook_node.string_asset_id,
+            hints_for_asset.get(logbook_node.string_asset_id, "Someone forgot to leave a message."))
+
+        for logbook_node in world_list.all_nodes
+        if isinstance(logbook_node, LogbookNode)
     ]
 
 
@@ -221,7 +199,7 @@ def _calculate_determiner(pickup_assignment: PickupAssignment,
     return determiner
 
 
-def hide_hints() -> list:
+def hide_hints(world_list: WorldList) -> list:
     """
     Creates the string patches entries that changes the Lore scans in the game
     completely useless text.
@@ -229,6 +207,8 @@ def hide_hints() -> list:
     """
 
     return [
-        create_simple_logbook_hint(asset_id, "Some item was placed somewhere.")
-        for asset_id in _LORE_SCANS.keys()
+        create_simple_logbook_hint(logbook_node.string_asset_id, "Some item was placed somewhere.")
+
+        for logbook_node in world_list.all_nodes
+        if isinstance(logbook_node, LogbookNode)
     ]
---
+++ test/games/prime/patcher_file_lib/test_item_hints.py
@@ -0,0 +1,113 @@
+import dataclasses
+from unittest.mock import MagicMock
+
+import pytest
+
+from randovania.game_description.area import Area
+from randovania.game_description.hint import Hint, HintType, HintLocationPrecision, HintItemPrecision
+from randovania.game_description.item.item_category import ItemCategory
+from randovania.game_description.node import LogbookNode, PickupNode
+from randovania.game_description.resources.pickup_entry import PickupEntry, ConditionalResources
+from randovania.game_description.resources.pickup_index import PickupIndex
+from randovania.game_description.world import World
+from randovania.game_description.world_list import WorldList
+from randovania.games.prime.patcher_file_lib import item_hints
+
+
+@pytest.fixture(name="pickup")
+def _pickup() -> PickupEntry:
+    return PickupEntry(
+        name="Pickup",
+        model_index=0,
+        item_category=ItemCategory.MOVEMENT,
+        resources=(
+            ConditionalResources(None, None, ()),
+        ),
+    )
+
+
+def _create_world_list(asset_id: int, pickup_index: PickupIndex):
+    logbook_node = LogbookNode("Logbook A", True, asset_id, None, None, None, None)
+    pickup_node = PickupNode("Pickup Node", True, pickup_index)
+
+    world_list = WorldList([
+        World("World", 5000, [
+            Area("Area", 10000, 0, [logbook_node, pickup_node], {}),
+        ]),
+    ])
+
+    return logbook_node, pickup_node, world_list
+
+
+def test_create_hints_nothing(empty_patches):
+    # Setup
+    asset_id = 1000
+    pickup_index = PickupIndex(50)
+
+    logbook_node, pickup_node, world_list = _create_world_list(asset_id, pickup_index)
+
+    patches = dataclasses.replace(
+        empty_patches,
+        hints={
+            logbook_node.resource(): Hint(HintType.LOCATION,
+                                          HintLocationPrecision.DETAILED,
+                                          HintItemPrecision.DETAILED,
+                                          pickup_index)
+        })
+    rng = MagicMock()
+
+    # Run
+    result = item_hints.create_hints(patches, world_list, rng)
+
+    # Assert
+    message = ("An &push;&main-color=#a84343;Energy Transfer Module&pop; can be found at "
+               "&push;&main-color=#a84343;World - Area&pop;.")
+    assert result == [
+        {'asset_id': asset_id, 'strings': [message, '', message]}
+    ]
+
+
+@pytest.mark.parametrize("item", [
+    (HintItemPrecision.DETAILED, "The &push;&main-color=#a84343;Pickup&pop;"),
+    (HintItemPrecision.PRECISE_CATEGORY, "A &push;&main-color=#a84343;movement system&pop;"),
+    (HintItemPrecision.GENERAL_CATEGORY, "A &push;&main-color=#a84343;major item&pop;"),
+    (HintItemPrecision.WRONG_GAME, "The &push;&main-color=#45f731;X-Ray Visor&pop;"),
+])
+@pytest.mark.parametrize("location", [
+    (HintLocationPrecision.DETAILED, "&push;&main-color=#a84343;World - Area&pop;"),
+    (HintLocationPrecision.WORLD_ONLY, "&push;&main-color=#a84343;World&pop;"),
+    (HintLocationPrecision.WRONG_GAME, "&push;&main-color=#45f731;Temple of Bryyo&pop;"),
+])
+def test_create_hints_item_detailed(empty_patches, pickup,
+                                    item, location):
+    # Setup
+    asset_id = 1000
+    pickup_index = PickupIndex(50)
+
+    logbook_node, pickup_node, world_list = _create_world_list(asset_id, pickup_index)
+
+    patches = dataclasses.replace(
+        empty_patches,
+        pickup_assignment={
+            pickup_index: pickup,
+        },
+        hints={
+            logbook_node.resource(): Hint(HintType.LOCATION,
+                                          location[0],
+                                          item[0],
+                                          pickup_index)
+        })
+    rng = MagicMock()
+    rng.choice.side_effect = lambda x: x[0]
+
+    # Run
+    result = item_hints.create_hints(patches, world_list, rng)
+
+    # Assert
+    if location[0] == HintLocationPrecision.WRONG_GAME and item[0] == HintItemPrecision.WRONG_GAME:
+        message = "&push;&main-color=#45f731;Did you remember to check Trial Tunnel?&pop;"
+    else:
+        message = "{0} can be found at {1}.".format(item[1], location[1])
+    assert result == [
+        {'asset_id': asset_id, 'strings': [message, '', message]}
+    ]
