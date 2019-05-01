d5799e12742b3a51e85305a756ee5fb76025d2c5
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-05-01 12:39:00 +0200

    Implement option to disable item hints

--- randovania/generator/filler/runner.py
+++ randovania/generator/filler/runner.py
@@ -1,6 +1,6 @@
 import dataclasses
 from random import Random
-from typing import List, Tuple, Callable
+from typing import List, Tuple, Callable, TypeVar
 
 from randovania.game_description.game_description import GameDescription
 from randovania.game_description.game_patches import GamePatches
@@ -10,6 +10,8 @@ from randovania.game_description.resources.pickup_entry import PickupEntry
 from randovania.generator.filler.retcon import retcon_playthrough_filler
 from randovania.layout.layout_configuration import LayoutConfiguration
 from randovania.resolver import bootstrap
+
+T = TypeVar("T")
 
 
 def _split_expansions(item_pool: List[PickupEntry]) -> Tuple[List[PickupEntry], List[PickupEntry]]:
@@ -30,13 +32,29 @@ def _split_expansions(item_pool: List[PickupEntry]) -> Tuple[List[PickupEntry], 
     return major_items, expansions
 
 
-def add_hints_precision(configuration: LayoutConfiguration,
-                        patches: GamePatches,
+def _create_weighted_list(rng: Random,
+                          current: List[T],
+                          factory: Callable[[], List[T]],
+                          ) -> List[T]:
+    """
+    Ensures we always have a non-empty list.
+    :param rng:
+    :param current:
+    :param factory:
+    :return:
+    """
+    if not current:
+        current = factory()
+        rng.shuffle(current)
+
+    return current
+
+
+def add_hints_precision(patches: GamePatches,
                         rng: Random,
                         ) -> GamePatches:
     """
     Adds precision to all hints that are missing one.
-    :param configuration:
     :param patches:
     :param rng:
     :return:
@@ -54,25 +72,45 @@ def add_hints_precision(configuration: LayoutConfiguration,
     rng.shuffle(asset_ids)
     item_precisions = []
     for asset_id in asset_ids:
-        if not item_precisions:
-            item_precisions = HintItemPrecision.weighted_list()
-            rng.shuffle(item_precisions)
+        item_precisions = _create_weighted_list(rng, item_precisions,
+                                                HintItemPrecision.weighted_list)
+        precision = item_precisions.pop()
 
-        hints_to_replace[asset_id] = dataclasses.replace(hints_to_replace[asset_id],
-                                                         item_precision=item_precisions.pop())
+        hints_to_replace[asset_id] = dataclasses.replace(hints_to_replace[asset_id], item_precision=precision)
 
     # Add random location precisions
     rng.shuffle(asset_ids)
     location_precisions = []
     for asset_id in asset_ids:
-        if not location_precisions:
-            location_precisions = HintLocationPrecision.weighted_list()
-            rng.shuffle(location_precisions)
+        location_precisions = _create_weighted_list(rng, location_precisions,
+                                                    HintLocationPrecision.weighted_list)
+        precision = location_precisions.pop()
 
-        hints_to_replace[asset_id] = dataclasses.replace(hints_to_replace[asset_id],
-                                                         location_precision=location_precisions.pop())
+        hints_to_replace[asset_id] = dataclasses.replace(hints_to_replace[asset_id], location_precision=precision)
 
     # Replace the hints the in the patches
+    return dataclasses.replace(patches, hints={
+        asset: hints_to_replace.get(asset, hint)
+        for asset, hint in patches.hints.items()
+    })
+
+
+def replace_hints_without_precision_with_jokes(patches: GamePatches,
+                                               ) -> GamePatches:
+    """
+    Adds WRONG_GAME precision to all hints that are missing one precision.
+    :param patches:
+    :return:
+    """
+
+    hints_to_replace = {
+        asset: dataclasses.replace(hint,
+                                   item_precision=HintItemPrecision.WRONG_GAME,
+                                   location_precision=HintLocationPrecision.WRONG_GAME)
+        for asset, hint in patches.hints.items()
+        if hint.location_precision is None or hint.item_precision is None
+    }
+
     return dataclasses.replace(patches, hints={
         asset: hints_to_replace.get(asset, hint)
         for asset, hint in patches.hints.items()
@@ -111,6 +149,9 @@ def run_filler(configuration: LayoutConfiguration,
         maximum_random_starting_items=major_configuration.maximum_random_starting_items,
         status_update=status_update)
 
-    result = add_hints_precision(configuration, filler_patches, rng)
+    if configuration.hints.item_hints:
+        result = add_hints_precision(filler_patches, rng)
+    else:
+        result = replace_hints_without_precision_with_jokes(filler_patches)
 
     return result, major_items + expansions
