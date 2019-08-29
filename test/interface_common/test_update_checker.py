f3d66e07b9b489d9276abb75386f8ce77b8c093a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2019-08-29 23:38:06 +0200

    Update tests for versions_to_display_for_releases

--- test/interface_common/test_update_checker.py
+++ test/interface_common/test_update_checker.py
@@ -1,9 +1,18 @@
+import copy
 from distutils.version import StrictVersion
 
 import pytest
 
 from randovania.interface_common import update_checker
 from randovania.interface_common.update_checker import VersionDescription
+
+_CUSTOM_CHANGE_LOGS = {
+    4: "\n- *Major* - Foo\n",
+    5: "\n- *Major* - Foo\n- Something\n",
+    6: "\n- Bar\n",
+}
+_CUSTOM_EXPECTED_LOG = copy.copy(_CUSTOM_CHANGE_LOGS)
+_CUSTOM_EXPECTED_LOG[5] = "\n- *Major* - Foo\n\nFor more details, check the Change Log tab."
 
 
 @pytest.mark.parametrize(["current_version", "last_changelog_version", "expected_display"], [
@@ -17,7 +26,9 @@ def test_versions_to_display_for_releases(current_version, last_changelog_versio
                                           ):
     # Setup
     releases = [
-        {"tag_name": "v0.{}.0".format(i), "body": "Changelog v0.{}.0".format(i), "html_url": "url"}
+        {"tag_name": "v0.{}.0".format(i),
+         "body": "Changelog v0.{}.0{}".format(i, _CUSTOM_CHANGE_LOGS.get(i, "")),
+         "html_url": "url"}
         for i in reversed(range(1, 11))
     ]
 
@@ -35,10 +46,10 @@ def test_versions_to_display_for_releases(current_version, last_changelog_versio
                                                         "Changelog v0.{}.0".format(expected_display),
                                                         "url")
     assert change_logs == [
-        "## v0.{0}.0\n\nChangelog v0.{0}.0".format(i)
+        "## v0.{0}.0\n\nChangelog v0.{0}.0{1}".format(i, _CUSTOM_EXPECTED_LOG.get(i, ""))
         for i in reversed(range(last_changelog_version + 1, current_version + 1))
     ]
     assert all_change_logs == [
-        "## v0.{0}.0\n\nChangelog v0.{0}.0".format(i)
+        "## v0.{0}.0\n\nChangelog v0.{0}.0{1}".format(i, _CUSTOM_CHANGE_LOGS.get(i, ""))
         for i in reversed(range(1, current_version + 1))
     ]
