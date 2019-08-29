5b58cba921854ccf6ce2344e905a61b56ce3c522
SpaghettiToastBook <spaghettitoastbook@pm.me>
2019-08-29 11:30:30 -0400

    Remove I/E/T prefixes in data visualizer

--- randovania/game_description/resources/simple_resource_info.py
+++ randovania/game_description/resources/simple_resource_info.py
@@ -10,8 +10,4 @@ class SimpleResourceInfo(NamedTuple):
     resource_type: ResourceType
 
     def __str__(self):
-        short_resource_name = self.resource_type.short_name
-        if short_resource_name is not None:
-            return "{}: {}".format(short_resource_name, self.long_name)
-        else:
-            return self.long_name
+        return self.long_name
