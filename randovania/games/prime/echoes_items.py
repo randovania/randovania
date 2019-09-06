1585d5e898fcd1eb93518d2157bb3bee7335c8c4
SpaghettiToastBook <spaghettitoastbook@pm.me>
2019-09-05 20:58:22 -0400

    Clean up echoes_items.py and add tuple for bad charge combos

--- randovania/games/prime/echoes_items.py
+++ randovania/games/prime/echoes_items.py
@@ -3,18 +3,19 @@ Some constants related to specific Echoes items that the logic needs to deal wit
 """
 
 DARK_TEMPLE_KEY_MODEL = 37
-DARK_TEMPLE_KEY_NAMES = [
-    "Dark Agon Key {0}",
-    "Dark Torvus Key {0}",
-    "Ing Hive Key {0}"
-]
-DARK_TEMPLE_KEY_ITEMS = [
-    [32, 33, 34, ],
-    [35, 36, 37, ],
-    [38, 39, 40, ],
-]
+DARK_TEMPLE_KEY_NAMES = (
+    "Dark Agon Key {}",
+    "Dark Torvus Key {}",
+    "Ing Hive Key {}"
+)
+DARK_TEMPLE_KEY_ITEMS = (
+    (32, 33, 34), # Dark Agon
+    (35, 36, 37), # Dark Torvus
+    (38, 39, 40), # Ing Hive
+)
+
 SKY_TEMPLE_KEY_MODEL = 38
-SKY_TEMPLE_KEY_ITEMS = [
+SKY_TEMPLE_KEY_ITEMS = (
     29,
     30,
     31,
@@ -24,6 +25,9 @@ SKY_TEMPLE_KEY_ITEMS = [
     104,
     105,
     106,
-]
+)
+
+BAD_CHARGE_COMBO_ITEMS = (5, 6, 7)
+
 USELESS_PICKUP_MODEL = 30
 USELESS_PICKUP_ITEM = 107
