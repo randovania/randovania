ef24f8e3550dfb646d7a42ffa9aad01af185ae83
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-01-15 00:24:06 +0100

    Update files and tests to damage strictness

--- test/layout/test_permalink.py
+++ test/layout/test_permalink.py
@@ -25,7 +25,7 @@ def test_encode(mock_dictionary_byte_hash: MagicMock):
 
     # Assert
     mock_dictionary_byte_hash.assert_called_once_with(link.layout_configuration.game_data)
-    assert encoded == "gAAAfRePAAACwMAAADmg/A=="
+    assert encoded == "gAAAfRePAAABsDAAAA5o+A=="
 
 
 @pytest.mark.parametrize("invalid", [
@@ -94,7 +94,7 @@ def test_decode(mock_dictionary_byte_hash: MagicMock):
 
     # This test should break whenever we change how permalinks are created
     # When this happens, we must bump the permalink version and change the tests
-    encoded = "gAAAfReLCAACyMAAADmg/w=="
+    encoded = "gAAAfReLCAABsjAAAA5oLg=="
 
     expected = Permalink(
         seed_number=1000,
