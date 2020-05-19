0f93e7a7c5476b09cecf5eecad48223d9f516c24
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-05-19 23:15:31 +0200

    Automatic address->offset conversion for DOLs

--- test/games/prime/test_dol_patcher.py
+++ test/games/prime/test_dol_patcher.py
@@ -1,35 +1,40 @@
 from pathlib import Path
 from unittest.mock import patch, MagicMock, ANY
 
+import pytest
+
 from randovania.games.prime import dol_patcher
 from randovania.interface_common.echoes_user_preferences import EchoesUserPreferences
 
 
 @patch("randovania.games.prime.dol_patcher._apply_game_options_patch", autospec=True)
 @patch("randovania.games.prime.dol_patcher._apply_string_display_patch", autospec=True)
+@patch("randovania.games.prime.dol_patcher.DolFile")
 @patch("randovania.games.prime.dol_patcher._get_dol_path", autospec=True)
 @patch("randovania.games.prime.dol_patcher._read_binary_version", autospec=True)
 def test_apply_patches(mock_read_binary_version: MagicMock,
                        mock_get_dol_path: MagicMock,
+                       mock_dol_file_constructor: MagicMock,
                        mock_apply_string_display_patch: MagicMock,
                        mock_apply_game_options_patch: MagicMock,
                        ):
     # Setup
     game_root = MagicMock()
     cosmetic_patches = MagicMock()
-    mock_read_binary_version.return_value = dol_patcher._GC_NTSC_DOL_VERSION
-    version_patches = dol_patcher._ALL_VERSIONS_PATCHES[dol_patcher._GC_NTSC_DOL_VERSION]
-    dol_file = mock_get_dol_path.return_value.open.return_value.__enter__.return_value
+    version_patches = dol_patcher._ALL_VERSIONS_PATCHES[0]
+    mock_read_binary_version.return_value = version_patches
+    dol_file = mock_dol_file_constructor.return_value
 
     # Run
     dol_patcher.apply_patches(game_root, cosmetic_patches)
 
     # Assert
-    mock_read_binary_version.assert_called_once_with(game_root)
+    mock_read_binary_version.assert_called_once_with(dol_file)
     mock_get_dol_path.assert_called_once_with(game_root)
+    mock_dol_file_constructor.assert_called_once_with(mock_get_dol_path.return_value)
     mock_apply_string_display_patch.assert_called_once_with(version_patches.string_display, dol_file)
     mock_apply_game_options_patch.assert_called_once_with(
-        version_patches.game_options_file_offset,
+        version_patches.game_options_constructor_address,
         cosmetic_patches.user_preferences, dol_file
     )
 
@@ -38,22 +43,28 @@ def test_get_dol_path():
     assert dol_patcher._get_dol_path(Path("foo")) == Path("foo", "sys", "main.dol")
 
 
-def test_read_binary_version(tmpdir):
-    tmpdir.join("sys").mkdir()
-    tmpdir.join("sys", "main.dol").write_binary(b"123456789012345678901234567\x00\x3A\x22\xA0")
-    assert dol_patcher._read_binary_version(Path(tmpdir)) == dol_patcher._GC_NTSC_DOL_VERSION
+@pytest.mark.parametrize("version", dol_patcher._ALL_VERSIONS_PATCHES)
+def test_read_binary_version(version):
+    dol_file = MagicMock()
+    dol_file.read.return_value = version.build_string
+
+    # Run
+    result = dol_patcher._read_binary_version(dol_file)
+
+    # Assert
+    dol_file.set_editable.assert_called_once_with(False)
+    assert result == version
 
 
 def test_apply_string_display_patch():
-    offsets = dol_patcher.StringDisplayPatchOffsets(0x15, 0, 0, 0)
+    offsets = dol_patcher.StringDisplayPatchAddresses(0x15, 0, 0, 0, 0)
     dol_file = MagicMock()
 
     # Run
     dol_patcher._apply_string_display_patch(offsets, dol_file)
 
     # Assert
-    dol_file.seek.assert_called_once_with(0x15)
-    dol_file.write.assert_called_once_with(ANY)
+    dol_file.write.assert_called_once_with(0x15, ANY)
 
 
 def test_apply_game_options_patch():
@@ -65,5 +76,4 @@ def test_apply_game_options_patch():
     dol_patcher._apply_game_options_patch(offset, user_preferences, dol_file)
 
     # Assert
-    dol_file.seek.assert_called_once_with(0x15)
-    dol_file.write.assert_called_once_with(ANY)
+    dol_file.write.assert_called_once_with(53, ANY)
