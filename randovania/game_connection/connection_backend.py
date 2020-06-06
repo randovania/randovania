ff7d7af5869051621de5f8df0072a8a4992f4c6a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-06-06 15:09:24 +0300

    Add GameConnection and the backends

---
+++ randovania/game_connection/connection_backend.py
@@ -0,0 +1,39 @@
+from typing import List
+
+from PySide2.QtCore import Signal, QObject
+from _nod import Enum
+
+from randovania.game_description.resources.pickup_entry import PickupEntry
+
+
+class ConnectionStatus(Enum):
+    Disconnected = "Disconnected"
+    WrongGame = "Wrong Game"
+    NotInGame = "Not In-Game"
+    InGame = "In-Game"
+
+
+class ConnectionBase(QObject):
+    LocationCollected = Signal(int)
+
+    @property
+    def current_status(self) -> ConnectionStatus:
+        raise NotImplementedError()
+
+    async def display_message(self, message: str):
+        raise NotImplementedError()
+
+    def send_pickup(self, pickup: PickupEntry):
+        raise NotImplementedError()
+
+    def set_permanent_pickups(self, pickups: List[PickupEntry]):
+        raise NotImplementedError()
+
+
+class ConnectionBackend(ConnectionBase):
+    @property
+    def name(self) -> str:
+        raise NotImplementedError()
+
+    async def update(self, dt: float):
+        raise NotImplementedError()
