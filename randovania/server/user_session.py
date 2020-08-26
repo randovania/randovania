db1075f5ac676fa6f96f427e1d0a6df8ca5b0df8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-08-27 02:10:03 +0300

    Rename SocketWrapper to ServerApp, add user_session tests

--- randovania/server/user_session.py
+++ randovania/server/user_session.py
@@ -2,99 +2,99 @@ import base64
 import json
 from typing import Optional
 
-import cryptography.fernet
 import flask
 import peewee
-from flask_discord import DiscordOAuth2Session
 from requests_oauthlib import OAuth2Session
 
 from randovania.network_common.error import InvalidSession
 from randovania.server.database import User, GameSessionMembership
 from randovania.server.lib import logger
-from randovania.server.socket_wrapper import SocketWrapper
+from randovania.server.server_app import ServerApp
 
 
-def setup_app(app: flask.Flask, sio: SocketWrapper):
-    discord = DiscordOAuth2Session(app)
-    fernet_encrypt = cryptography.fernet.Fernet(b'doCWpQK1NhGTC8HBfVIVAa1fqkaOCipT7z6kYJcOgjI=')
+def _create_client_side_session(sio: ServerApp, user: Optional[User]):
+    """
 
-    def _create_client_side_session(user: Optional[User] = None):
-        """
+    :param user: If the session's user was already retrieved, pass it along to avoid an extra query.
+    :return:
+    """
+    session = sio.get_session()
+    encrypted_session = sio.fernet_encrypt.encrypt(json.dumps(session).encode("utf-8"))
+    if user is None:
+        user = User.get_by_id(session["user-id"])
+    elif user.id != session["user-id"]:
+        raise RuntimeError(f"Provided user does not match the session's user")
 
-        :param user: If the session's user was already retrieved, pass it along to avoid an extra query.
-        :return:
-        """
-        session = sio.get_session()
-        encrypted_session = fernet_encrypt.encrypt(json.dumps(session).encode("utf-8"))
-        if user is None:
-            user = User.get_by_id(session["user-id"])
-        elif user.id != session["user-id"]:
-            raise RuntimeError(f"Provided user does not match the session's user")
+    return {
+        "user": user.as_json,
+        "sessions": [
+            membership.session.create_list_entry()
+            for membership in GameSessionMembership.select().where(GameSessionMembership.user == user)
+        ],
+        "encoded_session_b85": base64.b85encode(encrypted_session),
+    }
 
-        return {
-            "user": user.as_json,
-            "sessions": [
-                membership.session.create_list_entry()
-                for membership in GameSessionMembership.select().where(GameSessionMembership.user == user)
-            ],
-            "encoded_session_b85": base64.b85encode(encrypted_session),
-        }
 
-    @sio.exception_on("login_with_discord")
-    def login_with_discord(code: str):
-        oauth = OAuth2Session(
-            client_id=flask.current_app.config["DISCORD_CLIENT_ID"],
-            scope=["identify"],
-            redirect_uri=flask.current_app.config["DISCORD_REDIRECT_URI"],
-        )
-        access_token = oauth.fetch_token(
-            "https://discord.com/api/oauth2/token",
-            code=code,
-            client_secret=flask.current_app.config["DISCORD_CLIENT_SECRET"],
-        )
+def login_with_discord(sio: ServerApp, code: str):
+    oauth = OAuth2Session(
+        client_id=sio.app.config["DISCORD_CLIENT_ID"],
+        scope=["identify"],
+        redirect_uri=sio.app.config["DISCORD_REDIRECT_URI"],
+    )
+    access_token = oauth.fetch_token(
+        "https://discord.com/api/oauth2/token",
+        code=code,
+        client_secret=sio.app.config["DISCORD_CLIENT_SECRET"],
+    )
 
-        flask.session["DISCORD_OAUTH2_TOKEN"] = access_token
-        discord_user = discord.fetch_user()
+    flask.session["DISCORD_OAUTH2_TOKEN"] = access_token
+    discord_user = sio.discord.fetch_user()
 
-        user: User = User.get_or_create(discord_id=discord_user.id,
-                                        defaults={"name": discord_user.name})[0]
+    user: User = User.get_or_create(discord_id=discord_user.id,
+                                    defaults={"name": discord_user.name})[0]
 
-        with sio.session() as session:
-            session["user-id"] = user.id
-            session["discord-access-token"] = access_token
+    with sio.session() as session:
+        session["user-id"] = user.id
+        session["discord-access-token"] = access_token
 
-        return _create_client_side_session(user)
+    return _create_client_side_session(sio, user)
 
-    @sio.exception_on("login_with_guest")
-    def login_with_guest():
-        user: User = User.create(name="Guest")
 
-        with sio.session() as session:
-            session["user-id"] = user.id
+def login_with_guest(sio: ServerApp):
+    user: User = User.create(name="Guest")
 
-        return _create_client_side_session(user)
+    with sio.session() as session:
+        session["user-id"] = user.id
 
-    @sio.exception_on("restore_user_session")
-    def restore_user_session(encrypted_session: bytes, session_id: Optional[int]):
-        try:
-            decrypted_session: bytes = fernet_encrypt.decrypt(encrypted_session)
-            session = json.loads(decrypted_session.decode("utf-8"))
+    return _create_client_side_session(sio, user)
 
-            user = User.get_by_id(session["user-id"])
 
-            if "discord-access-token" in session:
-                # TODO: test if the discord access token is still valid
-                flask.session["DISCORD_OAUTH2_TOKEN"] = session["discord-access-token"]
-            sio.get_server().save_session(flask.request.sid, session)
+def restore_user_session(sio: ServerApp, encrypted_session: bytes, session_id: Optional[int]):
+    try:
+        decrypted_session: bytes = sio.fernet_encrypt.decrypt(encrypted_session)
+        session = json.loads(decrypted_session.decode("utf-8"))
 
-            if session_id is not None:
-                sio.join_session_via_sio(GameSessionMembership.get_by_ids(user.id, session_id))
+        user = User.get_by_id(session["user-id"])
 
-            return _create_client_side_session(user)
+        if "discord-access-token" in session:
+            # TODO: test if the discord access token is still valid
+            flask.session["DISCORD_OAUTH2_TOKEN"] = session["discord-access-token"]
+        sio.get_server().save_session(flask.request.sid, session)
 
-        except (KeyError, peewee.DoesNotExist, json.JSONDecodeError):
-            raise InvalidSession()
+        if session_id is not None:
+            sio.join_session(GameSessionMembership.get_by_ids(user.id, session_id))
 
-        except Exception:
-            logger().exception("Error decoding user session")
-            raise InvalidSession()
+        return _create_client_side_session(sio, user)
+
+    except (KeyError, peewee.DoesNotExist, json.JSONDecodeError):
+        raise InvalidSession()
+
+    except Exception:
+        logger().exception("Error decoding user session")
+        raise InvalidSession()
+
+
+def setup_app(sio: ServerApp):
+    sio.on("login_with_discord", login_with_discord)
+    sio.on("login_with_guest", login_with_guest)
+    sio.on("restore_user_session", restore_user_session)
--- test/server/test_user_session.py
+++ test/server/test_user_session.py
@@ -1,7 +1,90 @@
-from unittest.mock import MagicMock
+import json
+from unittest.mock import MagicMock, patch, ANY
 
+import flask
+import pytest
+
+from randovania.network_common.error import InvalidSession
 from randovania.server import user_session
+from randovania.server.database import User
 
 
 def test_setup_app():
-    user_session.setup_app(MagicMock(), MagicMock())
+    user_session.setup_app(MagicMock())
+
+
+@patch("requests_oauthlib.OAuth2Session.fetch_token", autospec=True)
+def test_login_with_discord(mock_fetch_token: MagicMock, clean_database, flask_app):
+    # Setup
+    sio = MagicMock()
+    session = {}
+    sio.session.return_value.__enter__.return_value = session
+    sio.get_session.return_value = session
+    mock_fetch_token.return_value = "access_token"
+    sio.fernet_encrypt.encrypt.return_value = b"encrypted"
+
+    discord_user = sio.discord.fetch_user.return_value
+    discord_user.id = 1234
+    discord_user.name = "A Name"
+
+    # Run
+    with flask_app.test_request_context():
+        result = user_session.login_with_discord(sio, "code")
+
+    # Assert
+    mock_fetch_token.assert_called_once_with(
+        ANY,
+        "https://discord.com/api/oauth2/token",
+        code="code",
+        client_secret=sio.app.config["DISCORD_CLIENT_SECRET"],
+    )
+    user = User.get(User.discord_id == 1234)
+    assert user.name == "A Name"
+
+    assert session == {
+        "user-id": user.id,
+        "discord-access-token": "access_token",
+    }
+    assert result == {
+        "user": user.as_json,
+        "sessions": [],
+        "encoded_session_b85": b'Wo~0~d2n=PWB',
+    }
+
+
+@patch("randovania.server.user_session._create_client_side_session", autospec=True)
+def test_restore_user_session_with_discord(mock_create_session: MagicMock,
+                                           flask_app, fernet, clean_database):
+    sio = MagicMock()
+    sio.fernet_encrypt = fernet
+
+    user: User = User.create(
+        id=1234,
+        discord_id=5678,
+        name="The Name"
+    )
+
+    session = {
+        "user-id": 1234,
+        "discord-access-token": "access-token",
+    }
+    enc_session = fernet.encrypt(json.dumps(session).encode("utf-8"))
+
+    # Run
+    with flask_app.test_request_context():
+        flask.request.sid = 7890
+        result = user_session.restore_user_session(sio, enc_session, None)
+
+    # Assert
+    sio.get_server.return_value.save_session.assert_called_once_with(7890, session)
+    mock_create_session.assert_called_once_with(sio, user)
+    assert result is mock_create_session.return_value
+
+
+def test_restore_user_session_invalid_key(flask_app, fernet):
+    sio = MagicMock()
+    sio.fernet_encrypt = fernet
+
+    with pytest.raises(InvalidSession):
+        user_session.restore_user_session(sio, b"", None)
+        pass
