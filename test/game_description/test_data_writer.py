b5a0b1131e701502587b2a00bd209c63f3aeaf2d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-09-02 00:04:17 +0300

    Fix use of templates not being combined with resource requirements
    Fixes #866.

--- test/game_description/test_data_writer.py
+++ test/game_description/test_data_writer.py
@@ -1,10 +1,12 @@
 import io
 import json
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 import randovania
 from randovania.game_description import data_reader, data_writer
+from randovania.game_description.requirements import RequirementAnd, ResourceRequirement, RequirementTemplate
 from randovania.games.prime import default_data
 
 
@@ -34,3 +36,35 @@ def test_commited_human_readable_description(echoes_game_description):
     data_writer.write_human_readable_world_list(echoes_game_description, buffer)
 
     assert default_data.prime2_human_readable_path().read_text("utf-8") == buffer.getvalue()
+
+
+@patch("randovania.game_description.data_writer.pretty_print_requirement")
+def test_pretty_print_requirement_array_one_item(mock_print_requirement: MagicMock):
+    mock_print_requirement.return_value = ["a", "b"]
+
+    req = MagicMock()
+    array = RequirementAnd([req])
+
+    # Run
+    result = list(data_writer.pretty_print_requirement_array(array, 3))
+
+    # Assert
+    assert result == ["a", "b"]
+    mock_print_requirement.assert_called_once_with(req, 3)
+
+
+@patch("randovania.game_description.data_writer.pretty_print_requirement")
+def test_pretty_print_requirement_array_combinable(mock_print_requirement: MagicMock, echoes_resource_database):
+    mock_print_requirement.return_value = ["a", "b"]
+
+    array = RequirementAnd([
+        ResourceRequirement(echoes_resource_database.item[0], 1, False),
+        RequirementTemplate(echoes_resource_database, "Shoot Sunburst"),
+    ])
+
+    # Run
+    result = list(data_writer.pretty_print_requirement_array(array, 3))
+
+    # Assert
+    assert result == [(3, "Power Beam and Shoot Sunburst")]
+    mock_print_requirement.assert_not_called()
