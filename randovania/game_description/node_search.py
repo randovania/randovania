53c1a23c7457e2f3aba9ffcc5d4b88cdc67a7770
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-10-10 17:58:23 +0300

    Balancing relative hints

--- randovania/game_description/node_search.py
+++ randovania/game_description/node_search.py
@@ -3,21 +3,31 @@ from typing import Dict, Optional
 import networkx
 
 from randovania.game_description.area import Area
+from randovania.game_description.game_patches import GamePatches
 from randovania.game_description.node import Node, DockNode, TeleporterNode, PickupNode
 from randovania.game_description.resources.pickup_index import PickupIndex
 from randovania.game_description.world_list import WorldList
 
 
 def distances_to_node(world_list: WorldList, starting_node: Node,
-                      cutoff: Optional[int] = None) -> Dict[Area, int]:
+                      *,
+                      ignore_elevators: bool = True,
+                      cutoff: Optional[int] = None,
+                      patches: Optional[GamePatches] = None,
+                      ) -> Dict[Area, int]:
     """
     Compute the shortest distance from a node to all reachable areas.
     :param world_list:
     :param starting_node:
+    :param ignore_elevators:
     :param cutoff: Exclude areas with a length longer that cutoff.
+    :param patches:
     :return: Dict keyed by area to shortest distance to starting_node.
     """
     g = networkx.DiGraph()
+
+    dock_connections = patches.dock_connection if patches is not None else {}
+    elevator_connections = patches.elevator_connection if patches is not None else {}
 
     for area in world_list.all_areas:
         g.add_node(area)
@@ -27,9 +37,11 @@ def distances_to_node(world_list: WorldList, starting_node: Node,
             new_areas = set()
             for node in area.nodes:
                 if isinstance(node, DockNode):
-                    new_areas.add(world.area_by_asset_id(node.default_connection.area_asset_id))
-                elif isinstance(node, TeleporterNode):
-                    new_areas.add(world_list.area_by_area_location(node.default_connection))
+                    connection = dock_connections.get((area.area_asset_id, node.dock_index), node.default_connection)
+                    new_areas.add(world.area_by_asset_id(connection.area_asset_id))
+                elif isinstance(node, TeleporterNode) and not ignore_elevators:
+                    connection = elevator_connections.get(node.teleporter_instance_id, node.default_connection)
+                    new_areas.add(world_list.area_by_area_location(connection))
 
             for next_area in new_areas:
                 g.add_edge(area, next_area)
