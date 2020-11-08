c312b1befe4511a680356bad9a5a548dcd60a0b0
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-11-08 03:23:21 +0200

    Add RandovaniaGame to LayoutConfiguration

--- randovania/games/game.py
+++ randovania/games/game.py
@@ -1,7 +1,9 @@
 from enum import Enum
 
+from randovania.bitpacking.bitpacking import BitPackEnum
 
-class RandovaniaGame(Enum):
+
+class RandovaniaGame(BitPackEnum, Enum):
     PRIME1 = "prime1"
     PRIME2 = "prime2"
     PRIME3 = "prime3"
