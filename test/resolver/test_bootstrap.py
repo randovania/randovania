c4f0a5c1096fa91fda12fa4d040396b9707be60d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-11-11 23:40:12 +0200

    Ensure all misc resources are present.
    * 1 for the Enable Vanilla X
    * 1 or 0 for the Vanilla Great Temple Emerald Translator Gate
    * 0 for everything else
    * Also add all non-event/damage/item resources

--- test/resolver/test_bootstrap.py
+++ test/resolver/test_bootstrap.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 import pytest
 
 from randovania.game_description.resources.resource_type import ResourceType
@@ -6,20 +8,33 @@ from randovania.resolver import bootstrap
 
 
 @pytest.mark.parametrize("elevators", [LayoutElevators.VANILLA, LayoutElevators.TWO_WAY_RANDOMIZED])
-def test_create_vanilla_translator_resources(echoes_resource_database,
-                                             elevators: LayoutElevators,
-                                             ):
+def test_misc_resources_for_configuration(echoes_resource_database,
+                                          elevators: LayoutElevators,
+                                          ):
     # Setup
+    configuration = MagicMock()
+    configuration.elevators = elevators
     gfmc_resource = echoes_resource_database.get_by_type_and_index(ResourceType.MISC, 16)
     torvus_resource = echoes_resource_database.get_by_type_and_index(ResourceType.MISC, 17)
     great_resource = echoes_resource_database.get_by_type_and_index(ResourceType.MISC, 18)
 
     # Run
-    result = bootstrap._create_vanilla_translator_resources(echoes_resource_database, elevators)
+    result = bootstrap.misc_resources_for_configuration(configuration, echoes_resource_database)
+    relevant_tricks = {
+        trick: result[trick]
+        for trick in [gfmc_resource, torvus_resource, great_resource]
+    }
 
     # Assert
-    assert result == {
+    assert relevant_tricks == {
         gfmc_resource: 0,
         torvus_resource: 0,
         great_resource: 0 if elevators != LayoutElevators.VANILLA else 1,
     }
+
+
+def test_logic_bootstrap(default_preset, echoes_game_description):
+    new_game, state = bootstrap.logic_bootstrap(default_preset.layout_configuration, echoes_game_description,
+                                                echoes_game_description.create_game_patches())
+
+    
