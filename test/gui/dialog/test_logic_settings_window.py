1a157976c2712ffa17ff62176d4c87f62457d6fa
Ethan Armbrust <ethanarm@buffalo.edu>
2020-11-17 17:20:50 -0500

    Added tests for tristate checkboxes

--- test/gui/dialog/test_logic_settings_window.py
+++ test/gui/dialog/test_logic_settings_window.py
@@ -1,5 +1,6 @@
 from randovania.gui.dialog.logic_settings_window import LogicSettingsWindow
 from randovania.interface_common.preset_editor import PresetEditor
+from PySide2.QtCore import Qt
 
 
 def test_on_preset_changed(skip_qtbot, default_preset):
@@ -9,3 +10,27 @@ def test_on_preset_changed(skip_qtbot, default_preset):
 
     # Run
     window.on_preset_changed(editor.create_custom_preset_with())
+
+
+def test_starting_location_world_select(skip_qtbot, default_preset):
+    # Setup
+    editor = PresetEditor(default_preset)
+    window = LogicSettingsWindow(None, editor)
+    skip_qtbot.addWidget(window)
+
+    # Run
+    checkbox_list = window._starting_location_for_world
+    window.on_preset_changed(editor.create_custom_preset_with())
+    assert len(checkbox_list) == 10
+    temple_grounds_checkbox = checkbox_list["Temple Grounds"]
+    assert temple_grounds_checkbox.checkState() == Qt.PartiallyChecked
+    skip_qtbot.mouseClick(temple_grounds_checkbox, Qt.LeftButton)
+    assert temple_grounds_checkbox.checkState() == Qt.Checked
+    assert len(editor.layout_configuration.starting_location.locations) == 40
+    skip_qtbot.mouseClick(temple_grounds_checkbox, Qt.LeftButton)
+    assert temple_grounds_checkbox.checkState() == Qt.Unchecked
+    assert len(editor.layout_configuration.starting_location.locations) == 0
+    skip_qtbot.mouseClick(temple_grounds_checkbox, Qt.LeftButton)
+    window.on_preset_changed(editor.create_custom_preset_with())
+    assert temple_grounds_checkbox.checkState() == Qt.Checked
+    assert len(editor.layout_configuration.starting_location.locations) == 40
