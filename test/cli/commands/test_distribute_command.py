1f948015976fd35e05adbfe07928b6ba24bbb004
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-11-22 14:49:01 +0200

    Make number of generation retries configurable

--- test/cli/commands/test_distribute_command.py
+++ test/cli/commands/test_distribute_command.py
@@ -1,17 +1,23 @@
 from pathlib import Path
 from unittest.mock import patch, MagicMock, ANY
 
+import pytest
+
 import randovania.cli.commands.distribute
 
 
-@patch("randovania.layout.permalink.Permalink.from_str")
-@patch("randovania.generator.generator.generate_description", autospec=True)
-def test_distribute_command_logic(mock_generate_description: MagicMock,
-                                  mock_from_str: MagicMock,
-                                  ):
+@pytest.mark.parametrize("no_retry", [False, True])
+def test_distribute_command_logic(no_retry: bool, mocker):
     # Setup
+    mock_generate: MagicMock = mocker.patch("randovania.generator.generator.generate_description", autospec=True)
+    mock_from_str: MagicMock = mocker.patch("randovania.layout.permalink.Permalink.from_str", autospec=True)
+
     args = MagicMock()
     args.output_file = Path("asdfasdf/qwerqwerqwer/zxcvzxcv.json")
+    args.no_retry = no_retry
+    extra_args = {}
+    if no_retry:
+        extra_args["attempts"] = 0
 
     # Run
     randovania.cli.commands.distribute.distribute_command_logic(args)
@@ -19,12 +25,13 @@ def test_distribute_command_logic(mock_generate_description: MagicMock,
     # Assert
     mock_from_str.assert_called_once_with(args.permalink)
 
-    mock_generate_description.assert_called_once_with(
+    mock_generate.assert_called_once_with(
         permalink=mock_from_str.return_value,
         status_update=ANY,
         validate_after_generation=args.validate,
         timeout=None,
+        **extra_args,
     )
 
-    save_file_mock: MagicMock = mock_generate_description.return_value.save_to_file
+    save_file_mock: MagicMock = mock_generate.return_value.save_to_file
     save_file_mock.assert_called_once_with(args.output_file)
