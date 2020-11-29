11545f07ad1f898095ef3e404e8588dfea95d4cb
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-11-29 19:23:35 +0200

    Add tests

--- test/gui/test_generate_seed_tab.py
+++ test/gui/test_generate_seed_tab.py
@@ -2,6 +2,7 @@ import pytest
 from PySide2 import QtWidgets
 from mock import MagicMock, AsyncMock
 
+from randovania.games.game import RandovaniaGame
 from randovania.gui.generate_seed_tab import GenerateSeedTab
 from randovania.gui.generated.main_window_ui import Ui_MainWindow
 
@@ -51,3 +52,30 @@ async def test_on_customize_button(tab, mocker, has_existing_window):
         mock_settings_window.assert_called_once()
         mock_execute_dialog.assert_awaited_once_with(mock_settings_window.return_value)
         tab._add_new_preset.assert_called_once()
+
+
+@pytest.mark.parametrize("game", [RandovaniaGame.PRIME2, RandovaniaGame.PRIME3])
+def test_on_options_changed_select_preset(tab, preset_manager, game):
+    preset = preset_manager.default_preset_for_game(game)
+
+    tab._window_manager.preset_manager = preset_manager
+    tab.setup_ui()
+
+    tab._options.selected_preset_name = preset.name
+
+    # Run
+    tab.on_options_changed(tab._options)
+
+    # Assert
+    assert tab._current_preset_data == preset
+
+
+def test_select_game(tab, preset_manager):
+    tab._window_manager.preset_manager = preset_manager
+    tab.setup_ui()
+
+    tab.select_game(RandovaniaGame.PRIME3)
+    assert tab._current_preset_data.game == RandovaniaGame.PRIME3
+
+    tab.select_game(RandovaniaGame.PRIME2)
+    assert tab._current_preset_data.game == RandovaniaGame.PRIME2
