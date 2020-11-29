b4970babb06a3f2426a16aaf0ab68ba58c5d1338
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-11-29 22:34:58 +0200

    Check if can also leave in all reachable pickups

--- test/generator/test_generator_reach.py
+++ test/generator/test_generator_reach.py
@@ -12,6 +12,7 @@ from randovania.game_description.echoes_game_specific import EchoesGameSpecific
 from randovania.game_description.game_description import GameDescription
 from randovania.game_description.node import ResourceNode, GenericNode, TranslatorGateNode
 from randovania.game_description.requirements import Requirement
+from randovania.game_description.resources.pickup_index import PickupIndex
 from randovania.game_description.resources.resource_info import add_resources_into_another
 from randovania.game_description.resources.translator_gate import TranslatorGate
 from randovania.game_description.world import World
@@ -89,11 +90,16 @@ def test_all_pickups_locations_reachable_with_all_pickups_for_preset(preset_name
     first_reach, second_reach = _create_reaches_and_compare(game, state)
     first_actions, second_actions = _compare_actions(first_reach, second_reach)
 
-    found_pickups = set(filter_pickup_nodes(filter_reachable(second_reach.nodes, first_reach)))
     all_pickups = set(filter_pickup_nodes(game.world_list.all_nodes))
 
+    collected_indices = {
+        resource
+        for resource, quantity in second_reach.state.resources.items()
+        if quantity > 0 and isinstance(resource, PickupIndex)
+    }
+
     pprint.pprint(first_actions)
-    assert sorted(all_pickups, key=lambda it: it.pickup_index) == sorted(found_pickups, key=lambda it: it.pickup_index)
+    assert sorted(it.pickup_index for it in all_pickups) == sorted(collected_indices)
 
 
 @pytest.mark.parametrize("has_translator", [False, True])
