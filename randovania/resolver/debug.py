67fb2329710f3b386225f65dabd92b0303ff9e61
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2020-12-28 14:04:38 +0200

    Fix resolver debug printing.

--- randovania/resolver/debug.py
+++ randovania/resolver/debug.py
@@ -11,13 +11,12 @@ from randovania.resolver.logic import Logic
 
 _DEBUG_LEVEL = 0
 count = 0
-_gd: GameDescription = None
 _current_indent = 0
-_last_printed_additional = None
+_last_printed_additional: dict = None
 
 
-def n(node: Node, with_world=False) -> str:
-    return _gd.world_list.node_name(node, with_world) if node is not None else "None"
+def n(node: Node, world_list, with_world=False) -> str:
+    return world_list.node_name(node, with_world) if node is not None else "None"
 
 
 def sorted_requirementset_print(new_requirements: Set[RequirementList]):
@@ -54,6 +53,8 @@ def log_new_advance(state: "State", reach: "ResolverReach"):
     increment_attempts()
     _current_indent += 1
     if _DEBUG_LEVEL > 0:
+        world_list = state.world_list
+
         if hasattr(state.node, "resource"):
             resource = state.node.resource()
             if isinstance(resource, PickupIndex):
@@ -63,10 +64,10 @@ def log_new_advance(state: "State", reach: "ResolverReach"):
         else:
             resource = None
 
-        print("{}> {} for {}".format(_indent(1), n(state.node), resource))
+        print("{}> {} for {}".format(_indent(1), n(state.node, world_list=world_list), resource))
         if _DEBUG_LEVEL >= 3:
             for node in reach.nodes:
-                print("{}: {}".format(_indent(), n(node)))
+                print("{}: {}".format(_indent(), n(node, world_list=world_list)))
 
 
 def log_checking_satisfiable_actions():
@@ -74,20 +75,22 @@ def log_checking_satisfiable_actions():
         print("{}# Satisfiable Actions".format(_indent()))
 
 
-def log_rollback(state, has_action, possible_action: bool):
+def log_rollback(state: "State", has_action, possible_action: bool):
     global _current_indent
     if _DEBUG_LEVEL > 1:
-        print("{}* Rollback {}; Had action? {}; Possible Action? {}".format(_indent(),
-                                                                            n(state.node), has_action, possible_action))
+        print("{}* Rollback {}; Had action? {}; Possible Action? {}".format(
+            _indent(),
+            n(state.node, world_list=state.world_list),
+            has_action, possible_action))
     _current_indent -= 1
 
 
 def log_skip_action_missing_requirement(node: Node, game: GameDescription, requirement_set: RequirementSet):
     if _DEBUG_LEVEL > 1:
         if node in _last_printed_additional and _last_printed_additional[node] == requirement_set:
-            print("{}* Skip {}, same additional".format(_indent(), n(node)))
+            print("{}* Skip {}, same additional".format(_indent(), n(node, world_list=game.world_list)))
         else:
-            print("{}* Skip {}, missing additional:".format(_indent(), n(node)))
+            print("{}* Skip {}, missing additional:".format(_indent(), n(node, world_list=game.world_list)))
             requirement_set.pretty_print(_indent(-1))
             _last_printed_additional[node] = requirement_set
 
@@ -99,31 +102,14 @@ def print_distribute_one_item_detail(potential_pickup_nodes, start_time):
         ))
 
 
-def print_distribute_one_item(state, available_item_pickups):
+def print_distribute_one_item(state: "State", available_item_pickups):
     if _DEBUG_LEVEL > 0:
         print("\n> Distribute starting at {} with {} resources and {} pickups left.".format(
-            n(state.node),
+            n(state.node, world_list=state.world_list),
             len(state.resources),
             len(available_item_pickups)
         ))
         return time.perf_counter()
-
-
-def print_distribute_one_item_rollback(state):
-    if _DEBUG_LEVEL > 0:
-        print(": Rollback at {}.".format(n(state.node)))
-
-
-def print_distribute_fill_pickup_index(pickup_index: PickupIndex, action: PickupEntry, logic: Logic):
-    target_node = None
-    for node in logic.game.all_nodes:
-        if isinstance(node, PickupNode) and node.pickup_index == pickup_index:
-            target_node = node
-
-    if _DEBUG_LEVEL > 1:
-        print("Placed {} at {}".format(
-            action,
-            n(target_node, with_world=True)))
 
 
 def print_actions_of_reach(reach: GeneratorReach):
