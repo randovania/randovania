1a2ab5a0641143d95e6b206efe37beec01f887d1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-04-30 23:35:24 +0300

    Add player name for multiworld hints

--- test/generator/filler/test_runner.py
+++ test/generator/filler/test_runner.py
@@ -103,10 +103,12 @@ def test_add_hints_precision(empty_patches, mocker):
     relative_hint_provider.assert_called_once_with(player_state, initial_patches, rng, PickupIndex(3))
     assert result.hints == {
         LogbookAsset(0): Hint(HintType.LOCATION, PrecisionPair(HintLocationPrecision.DETAILED,
-                                                               HintItemPrecision.DETAILED, include_owner=False),
+                                                               HintItemPrecision.DETAILED,
+                                                               include_owner=False),
                               PickupIndex(1)),
         LogbookAsset(1): Hint(HintType.LOCATION, PrecisionPair(HintLocationPrecision.WORLD_ONLY,
-                                                               HintItemPrecision.PRECISE_CATEGORY, include_owner=False),
+                                                               HintItemPrecision.PRECISE_CATEGORY,
+                                                               include_owner=True),
                               PickupIndex(2)),
         LogbookAsset(2): relative_hint_provider.return_value,
     }
