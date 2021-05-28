c6fe87acccf91b2ba1d90c5d16479969196901af
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-05-29 01:19:11 +0300

    Add custom damage reductions
    * Fixes Varia-only heat reduction
    * Fixes prime 1 damage reduction, as well as progressive
    * Makes custom heat/dark aether damage customization logical

--- randovania/game_description/resources/damage_resource_info.py
+++ randovania/game_description/resources/damage_resource_info.py
@@ -1,8 +1,8 @@
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 from randovania.game_description.resources.item_resource_info import ItemResourceInfo
 
 
 class DamageReduction(NamedTuple):
-    inventory_item: ItemResourceInfo
+    inventory_item: Optional[ItemResourceInfo]
     damage_multiplier: float
