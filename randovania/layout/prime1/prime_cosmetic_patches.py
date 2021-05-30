a01bbc9e9b6fbc7ce79a75feed3a89b9582761ad
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-05-31 01:38:04 +0300

    Add debug mode for randomprime that prints indices

--- randovania/layout/prime1/prime_cosmetic_patches.py
+++ randovania/layout/prime1/prime_cosmetic_patches.py
@@ -8,6 +8,7 @@ from randovania.layout.base.cosmetic_patches import BaseCosmeticPatches
 class PrimeCosmeticPatches(BaseCosmeticPatches):
     qol_cosmetic: bool = True
     open_map: bool = True
+    debug_pickups: bool = False
 
     @classmethod
     def default(cls) -> "PrimeCosmeticPatches":
