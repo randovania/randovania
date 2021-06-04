47376f3fecb1dbb364f06da25642dd5fffd29a8c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-06-04 22:55:35 +0300

    GameInputDialog: start in the paths of the input fields

--- test/gui/dialog/test_game_input_dialog.py
+++ test/gui/dialog/test_game_input_dialog.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from unittest.mock import MagicMock
 
+import pytest
 from PySide2 import QtCore
 
 from randovania.gui.dialog.game_input_dialog import GameInputDialog
@@ -8,13 +9,23 @@ from randovania.games.game import RandovaniaGame
 from randovania.interface_common.options import Options
 
 
-def test_on_output_file_button_exists(skip_qtbot, tmp_path, mocker):
+@pytest.mark.parametrize("has_output_dir", [False, True])
+def test_on_output_file_button_exists(skip_qtbot, tmp_path, mocker, has_output_dir):
     # Setup
     mock_prompt = mocker.patch("randovania.gui.lib.common_qt_lib.prompt_user_for_output_file", autospec=True)
 
+    if has_output_dir:
+        output_directory = tmp_path.joinpath("output_path")
+        expected_default_name = str(tmp_path.joinpath("output_path", "MyHashGame.iso"))
+        output_directory.mkdir()
+    else:
+        output_directory = None
+        expected_default_name = "MyHashGame.iso"
+
     patcher = MagicMock()
+    patcher.default_output_file.return_value = "MyHashGame.iso"
     options = MagicMock()
-    options.output_directory = None
+    options.options_for_game.return_value.output_directory = output_directory
     window = GameInputDialog(options, patcher, "MyHash", True, RandovaniaGame.PRIME2)
     mock_prompt.return_value = tmp_path.joinpath("foo", "game.iso")
 
@@ -23,7 +34,7 @@ def test_on_output_file_button_exists(skip_qtbot, tmp_path, mocker):
 
     # Assert
     patcher.default_output_file.assert_called_once_with("MyHash")
-    mock_prompt.assert_called_once_with(window, patcher.default_output_file.return_value,
+    mock_prompt.assert_called_once_with(window, expected_default_name,
                                         patcher.valid_output_file_types)
     assert window.output_file_edit.text() == str(tmp_path.joinpath("foo", "game.iso"))
     assert tmp_path.joinpath("foo").is_dir()
