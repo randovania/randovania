358711ceb1047406f29a3a4cbab5ab18a2c58ab1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-06-28 20:35:59 +0300

    Combine Major/Minor cutscene settings to a single combo

--- randovania/gui/preset_settings/prime_patches_tab.py
+++ randovania/gui/preset_settings/prime_patches_tab.py
@@ -7,6 +7,7 @@ from randovania.gui.lib import signal_handling
 from randovania.gui.preset_settings.preset_tab import PresetTab
 from randovania.interface_common.preset_editor import PresetEditor
 from randovania.layout.preset import Preset
+from randovania.layout.prime1.prime_configuration import LayoutCutsceneMode
 
 _FIELDS = [
     "main_plaza_door",
@@ -16,8 +17,6 @@ _FIELDS = [
     "backwards_lower_mines",
     "phazon_elite_without_dynamo",
     "qol_game_breaking",
-    "qol_minor_cutscenes",
-    "qol_major_cutscenes",
     "small_samus",
 ]
 
@@ -33,6 +32,10 @@ class PresetPrimePatches(PresetTab, Ui_PresetPrimePatches):
         self.description_label.setText(self.description_label.text().replace("color:#0000ff;", ""))
 
         # Signals
+        self.cutscene_combo.setItemData(0, LayoutCutsceneMode.ORIGINAL)
+        self.cutscene_combo.setItemData(1, LayoutCutsceneMode.MINOR)
+        self.cutscene_combo.setItemData(2, LayoutCutsceneMode.MAJOR)
+        signal_handling.on_combo(self.cutscene_combo, self._on_cutscene_changed)
         for f in _FIELDS:
             self._add_persist_option(getattr(self, f"{f}_check"), f)
 
@@ -47,7 +50,12 @@ class PresetPrimePatches(PresetTab, Ui_PresetPrimePatches):
 
         signal_handling.on_checked(check, persist)
 
+    def _on_cutscene_changed(self, value: LayoutCutsceneMode):
+        with self._editor as editor:
+            editor.set_configuration_field("qol_cutscenes", value)
+
     def on_preset_changed(self, preset: Preset):
         config = preset.configuration
         for f in _FIELDS:
             typing.cast(QtWidgets.QCheckBox, getattr(self, f"{f}_check")).setChecked(getattr(config, f))
+        signal_handling.combo_set_to_value(self.cutscene_combo, config.qol_cutscenes)
