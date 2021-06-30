20ea21e2c2bfe93bae9701a609562cb35e2c4548
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-06-30 21:02:18 +0300

    Remove the debug_pickups option

--- randovania/layout/prime1/prime_cosmetic_patches.py
+++ randovania/layout/prime1/prime_cosmetic_patches.py
@@ -8,7 +8,6 @@ from randovania.layout.base.cosmetic_patches import BaseCosmeticPatches
 class PrimeCosmeticPatches(BaseCosmeticPatches):
     qol_cosmetic: bool = True
     open_map: bool = True
-    debug_pickups: bool = False
 
     @classmethod
     def default(cls) -> "PrimeCosmeticPatches":
