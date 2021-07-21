7b5f382e4b2413a006dac2d63a3236a11d262aa0
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-07-21 03:19:49 +0300

    Update py-randomprime to 0.3.1

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         'aiohttp',
         'aiofiles',
         'dulwich>=0.20',
-        'py_randomprime>=0.2.0',
+        'py_randomprime>=0.3.1',
     ],
     extras_require={
         "gui": [
