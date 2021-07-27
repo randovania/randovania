ea23e6f6adfb8b6cb3585f55913378b293a8bc1a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-07-27 22:44:42 +0300

    Use limited PPC interpreter to fix DebugExecutorWindow

--- test/gui/test_debug_backend_window.py
+++ test/gui/test_debug_backend_window.py
@@ -5,9 +5,10 @@ from PySide2.QtCore import Qt
 from mock import patch, MagicMock, AsyncMock
 
 from randovania.game_connection.connection_base import InventoryItem
-from randovania.game_connection.connector.prime_remote_connector import PrimeRemoteConnector
+from randovania.game_connection.connector.prime_remote_connector import PrimeRemoteConnector, DolRemotePatch
 from randovania.game_connection.executor.memory_operation import MemoryOperation
-from randovania.gui.debug_backend_window import DebugBackendWindow
+from randovania.games.prime import all_prime_dol_patches
+from randovania.gui.debug_backend_window import DebugExecutorWindow
 
 
 def _echoes_powerup_offset(item_index: int) -> int:
@@ -17,56 +18,66 @@ def _echoes_powerup_offset(item_index: int) -> int:
     return (powerups_offset + vector_data_offset) + (item_index * powerup_size)
 
 
-@pytest.fixture(name="backend")
-def debug_backend_window(skip_qtbot):
-    return DebugBackendWindow()
+@pytest.fixture(name="executor")
+def debug_executor_window(skip_qtbot):
+    return DebugExecutorWindow()
 
 
 @pytest.mark.asyncio
-async def test_display_message(backend: DebugBackendWindow):
-    connector = PrimeRemoteConnector(backend._used_version)
-
-    message = "Foo"
-    await backend.perform_single_memory_operation(connector._write_string_to_game_buffer(message))
-    await backend.perform_single_memory_operation(
-        MemoryOperation(backend._used_version.cstate_manager_global + 0x2, write_bytes=b"\x01"))
-
-    backend._read_message_from_game()
-    assert backend.messages_list.findItems(message, Qt.MatchFlag.MatchExactly)
-
-
-@pytest.mark.asyncio
-async def test_update_inventory_label(backend: DebugBackendWindow, echoes_resource_database):
+async def test_display_message(executor: DebugExecutorWindow):
     # Setup
-    offset_func = _echoes_powerup_offset
-    player_state_pointer = backend._used_version.cstate_manager_global + 0x150c
-    item_inv = {
-        echoes_resource_database.energy_tank: InventoryItem(4, 4),
-        echoes_resource_database.multiworld_magic_item: InventoryItem(0, 2),
-    }
-
-    memory_ops = [
-        MemoryOperation(
-            address=player_state_pointer,
-            offset=offset_func(item.index),
-            write_bytes=struct.pack(">II", *item_inv[item])
-        )
-        for item in [echoes_resource_database.energy_tank, echoes_resource_database.multiworld_magic_item]
-    ]
-    await backend.perform_memory_operations(memory_ops)
+    await executor._ensure_initialized_game_memory()
+    connector = PrimeRemoteConnector(executor._used_version)
+    message = "Foo Bar"
 
     # Run
-    await backend._update_inventory_label()
+    await connector.execute_remote_patches(
+        executor,
+        [connector._dol_patch_for_hud_message(message)],
+    )
+    executor._handle_remote_execution()
 
     # Assert
-    assert "Energy Tank x 4/4" in backend.inventory_label.text()
-    assert "Multiworld Magic Identifier x 0/2" in backend.inventory_label.text()
+    assert executor.messages_list.findItems(message, Qt.MatchFlag.MatchExactly)
+
+
+@pytest.mark.asyncio
+async def test_update_inventory_label(executor: DebugExecutorWindow, echoes_resource_database):
+    # Setup
+    await executor._ensure_initialized_game_memory()
+    connector = PrimeRemoteConnector(executor._used_version)
+
+    await connector.execute_remote_patches(
+        executor,
+        [
+            DolRemotePatch([], all_prime_dol_patches.increment_item_capacity_patch(
+                executor._used_version.powerup_functions,
+                echoes_resource_database.game_enum,
+                echoes_resource_database.multiworld_magic_item.index,
+                delta=2,
+            )),
+            DolRemotePatch([], all_prime_dol_patches.adjust_item_amount_and_capacity_patch(
+                executor._used_version.powerup_functions,
+                echoes_resource_database.game_enum,
+                echoes_resource_database.energy_tank.index,
+                delta=4,
+            )),
+        ],
+    )
+    executor._handle_remote_execution()
+
+    # Run
+    await executor._update_inventory_label()
+
+    # Assert
+    assert "Energy Tank x 4/4" in executor.inventory_label.text()
+    assert "Multiworld Magic Identifier x 0/2" in executor.inventory_label.text()
 
 
 @pytest.mark.asyncio
 @patch("randovania.gui.lib.common_qt_lib.get_network_client", autospec=True)
 async def test_setup_locations_combo(mock_get_network_client: MagicMock,
-                                     backend: DebugBackendWindow):
+                                     executor: DebugExecutorWindow):
     # Setup
     patcher_data = {
         "pickups": [
@@ -75,17 +86,17 @@ async def test_setup_locations_combo(mock_get_network_client: MagicMock,
         ]
     }
     mock_get_network_client.return_value.session_admin_player = AsyncMock(return_value=patcher_data)
-    backend._used_version = backend._used_version
+    executor._used_version = executor._used_version
 
     # Run
-    await backend._setup_locations_combo()
+    await executor._setup_locations_combo()
 
     # Assert
     mock_get_network_client.assert_called_once_with()
-    assert backend.collect_location_combo.count() > 100
+    assert executor.collect_location_combo.count() > 100
 
 
-def test_clear(backend: DebugBackendWindow):
-    backend.clear()
-    assert backend.messages_list.count() == 0
-    assert backend.inventory_label.text() == ""
+def test_clear(executor: DebugExecutorWindow):
+    executor.clear()
+    assert executor.messages_list.count() == 0
+    assert executor.inventory_label.text() == ""
