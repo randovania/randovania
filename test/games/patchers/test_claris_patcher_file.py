26587ed4bff5a494e1fd55382c5f33e9227098c8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-08-15 00:08:55 +0300

    Echoes: delay consulting RandomizerData.json until patching the ISO

--- test/games/patchers/test_claris_patcher_file.py
+++ test/games/patchers/test_claris_patcher_file.py
@@ -31,3 +31,30 @@ def test_create_starting_popup_items(default_echoes_preset, echoes_resource_data
         'Extra starting items:',
         'Dark Beam, 3 Energy Tank, 15 Missiles, Screw Attack'
     ]
+
+
+def test_adjust_model_name(randomizer_data):
+    # Setup
+    patcher_data = {
+        "pickups": [
+            {"model_name": "DarkVisor"},
+            {"model_name": "SkyTempleKey"},
+            {"model_name": "MissileExpansion"},
+            {"model_name": "prime1_Boost Ball"},
+            {"model_name": "prime1_Plasma Beam"},
+        ]
+    }
+
+    # Run
+    claris_patcher_file.adjust_model_name(patcher_data, randomizer_data)
+
+    # Assert
+    assert patcher_data == {
+        "pickups": [
+            {"model_index": 11, "sound_index": 0, "jingle_index": 1},
+            {"model_index": 38, "sound_index": 1, "jingle_index": 2},
+            {"model_index": 22, "sound_index": 0, "jingle_index": 0},
+            {"model_index": 18, "sound_index": 0, "jingle_index": 1},
+            {"model_index": 30, "sound_index": 0, "jingle_index": 1},
+        ]
+    }
