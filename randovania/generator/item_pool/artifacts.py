b33e550b12b8c00f5e267f62e3a8a353eacd1dd8
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-08-18 01:33:22 +0300

    Setting for requiring a number of actions before artifacts are placed

--- randovania/generator/item_pool/artifacts.py
+++ randovania/generator/item_pool/artifacts.py
@@ -10,10 +10,12 @@ from randovania.layout.prime1.artifact_mode import LayoutArtifactMode
 
 def add_artifacts(resource_database: ResourceDatabase,
                   mode: LayoutArtifactMode,
+                  artifact_minimum_progression: int,
                   ) -> PoolResults:
     """
     :param resource_database:
     :param mode
+    :param artifact_minimum_progression
     :return:
     """
     item_pool: List[PickupEntry] = []
@@ -22,13 +24,14 @@ def add_artifacts(resource_database: ResourceDatabase,
     artifacts_to_place = mode.value
 
     for i in range(artifacts_to_place):
-        item_pool.append(pickup_creator.create_artifact(i, resource_database))
+        item_pool.append(pickup_creator.create_artifact(i, artifact_minimum_progression, resource_database))
 
     first_automatic_artifact = artifacts_to_place
 
     for automatic_artifact in range(first_automatic_artifact, 12):
         resource_info.add_resource_gain_to_current_resources(
-            pickup_creator.create_artifact(automatic_artifact, resource_database).all_resources,
+            pickup_creator.create_artifact(automatic_artifact, artifact_minimum_progression,
+                                           resource_database).all_resources,
             initial_resources
         )
 
