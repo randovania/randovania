db69b2f3861f04b8ae6bfd57d6e1e2c8ad3228cb
duncathan <dunc@duncathan.com>
2021-08-23 23:09:24 -0600

    fixes #392
    echoes uses a different game ID (GM2E1R) when saving ISOs with menu mod enabled, so that different save file formats aren't shared

--- randovania/games/patchers/gamecube/banner_patcher.py
+++ randovania/games/patchers/gamecube/banner_patcher.py
@@ -1,14 +1,19 @@
 from pathlib import Path
 
+from construct.core import Byte
 
-def patch_game_name_and_id(game_files_path: Path, new_name: str):
+
+def patch_game_name_and_id(game_files_path: Path, new_name: str, game_id_replacement: str):
     b = new_name.encode("ASCII")
     if len(b) > 40:
         raise ValueError("Name '{}' is bigger than 40 bytes".format(new_name))
+    
+    gid = game_id_replacement.encode("ASCII")
+    if len(gid) != 6:
+        raise ValueError("Game ID '{}' is not exactly 6 bytes".format(game_id_replacement))
 
     with game_files_path.joinpath("sys", "boot.bin").open("r+b") as boot_bin:
-        boot_bin.seek(0x5)
-        boot_bin.write(b"R")
+        boot_bin.write(gid)
         boot_bin.seek(0x20)
         boot_bin.write(b)
 
