ee7bef403a53192b464fc51358aee9dd43f04d75
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-08-30 02:35:11 +0300

    Fix tests

--- test/server/discord/test_preset_lookup.py
+++ test/server/discord/test_preset_lookup.py
@@ -8,7 +8,8 @@ from randovania.server.discord import preset_lookup
 
 @pytest.mark.asyncio
 async def test_on_message_from_bot(mocker):
-    mock_look_for: AsyncMock = mocker.patch("randovania.server.bot.look_for_permalinks", new_callable=AsyncMock)
+    mock_look_for: AsyncMock = mocker.patch("randovania.server.discord.preset_lookup.look_for_permalinks",
+                                            new_callable=AsyncMock)
     client = MagicMock()
     cog = preset_lookup.PermalinkLookupCog(None, client)
 
@@ -30,7 +31,8 @@ async def test_on_message_from_bot(mocker):
 ])
 @pytest.mark.asyncio
 async def test_on_message_wrong_place(mocker, name_filter: str, guild: int, expected):
-    mock_look_for: AsyncMock = mocker.patch("randovania.server.bot.look_for_permalinks", new_callable=AsyncMock)
+    mock_look_for: AsyncMock = mocker.patch("randovania.server.discord.preset_lookup.look_for_permalinks",
+                                            new_callable=AsyncMock)
     cog = preset_lookup.PermalinkLookupCog({"channel_name_filter": name_filter, "guild": guild}, MagicMock())
 
     message = MagicMock()
@@ -42,63 +44,78 @@ async def test_on_message_wrong_place(mocker, name_filter: str, guild: int, expe
 
     # Assert
     if expected:
-        mock_look_for.assert_awaited_once_with(message.content, message.channel)
+        mock_look_for.assert_awaited_once_with(message)
     else:
         mock_look_for.assert_not_awaited()
 
 
+@pytest.mark.parametrize("is_solo", [False, True])
+@pytest.mark.parametrize("has_multiple", [False, True])
 @pytest.mark.asyncio
-async def test_look_for_permalinks(mocker):
-    permalink_1 = MagicMock()
-    permalink_1.player_count = 2
-
+async def test_look_for_permalinks(mocker, is_solo, has_multiple):
     preset = MagicMock()
     preset.game = RandovaniaGame.METROID_PRIME_ECHOES
+    permalink_1 = MagicMock()
+    permalink_1.player_count = 1 if is_solo else 2
+    permalink_1.get_preset.return_value = preset
+
     permalink_2 = MagicMock()
-    permalink_2.player_count = 1
-    permalink_2.get_preset.return_value = preset
+    embed = MagicMock()
 
-    embed_1 = MagicMock()
-    embed_2 = MagicMock()
-
-    mock_embed: MagicMock = mocker.patch("discord.Embed", side_effect=[embed_1, embed_2])
+    mock_embed: MagicMock = mocker.patch("discord.Embed", side_effect=[embed])
 
     mock_describe: MagicMock = mocker.patch("randovania.gui.lib.preset_describer.describe",
                                             return_value=[
                                                 ("General", ["Foo", "Bar"]),
                                                 ("Other", ["X", "Y"]),
                                             ])
-    mock_from_str: MagicMock = mocker.patch("randovania.layout.permalink.Permalink.from_str",
-                                            side_effect=[permalink_1, permalink_2])
+    mock_from_str: MagicMock = mocker.patch(
+        "randovania.layout.permalink.Permalink.from_str",
+        side_effect=[permalink_1, permalink_2] if has_multiple else [permalink_1],
+    )
 
-    channel = AsyncMock()
-    message = "yu4abbceWfLI- `yua73123yWdLI-` foo???"
+    message = AsyncMock()
+    if has_multiple:
+        message.content = "yu4abbceWfLI- `yua73123yWdLI-` foo???"
+    else:
+        message.content = "yu4abbceWfLI- foo???"
 
     # Run
-    await preset_lookup.look_for_permalinks(message, channel)
+    await preset_lookup.look_for_permalinks(message)
 
     # Assert
-    mock_from_str.assert_has_calls([
-        call("yu4abbceWfLI-"),
-        call("yua73123yWdLI-"),
-    ])
-    permalink_2.get_preset.assert_called_once_with(0)
-    mock_describe.assert_called_once_with(preset)
+    if has_multiple:
+        mock_from_str.assert_has_calls([
+            call("yu4abbceWfLI-"),
+            call("yua73123yWdLI-"),
+        ])
+    else:
+        mock_from_str.assert_called_once_with("yu4abbceWfLI-")
 
-    mock_embed.assert_has_calls([
-        call(title="`yu4abbceWfLI-`", description="2 player multiworld permalink"),
-        call(title="`yua73123yWdLI-`", description="1 player multiworld permalink"),
-    ])
-    embed_1.add_field.assert_not_called()
-    assert embed_2.description == "Metroid Prime 2: Echoes permalink for Randovania {}".format(randovania.VERSION)
-    embed_2.add_field.assert_has_calls([
-        call(name="General", value="Foo\nBar", inline=True),
-        call(name="Other", value="X\nY", inline=True),
-    ])
-    channel.send.assert_has_awaits([
-        call(embed=embed_1),
-        call(embed=embed_2),
-    ])
+    if is_solo:
+        permalink_1.get_preset.assert_called_once_with(0)
+        mock_describe.assert_called_once_with(preset)
+    else:
+        permalink_1.get_preset.assert_not_called()
+        mock_describe.assert_not_called()
+
+    mock_embed.assert_called_once_with(
+        title="`yu4abbceWfLI-`", description=f"{permalink_1.player_count} player multiworld permalink",
+    )
+    if is_solo:
+        assert embed.description == "Metroid Prime 2: Echoes permalink for Randovania {}".format(randovania.VERSION)
+        embed.add_field.assert_has_calls([
+            call(name="General", value="Foo\nBar", inline=True),
+            call(name="Other", value="X\nY", inline=True),
+        ])
+
+    content = None
+    if has_multiple:
+        content = "Multiple permalinks found, using only the first."
+    message.reply.assert_awaited_once_with(
+        content=content,
+        embed=embed,
+    )
 
 
 @pytest.mark.asyncio
