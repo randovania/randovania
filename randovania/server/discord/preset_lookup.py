01dd51d11ba2eff55070102861273bcd62f775aa
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-08-30 02:01:12 +0300

    The discord bot now replies to attached LayoutDescription

--- randovania/server/discord/preset_lookup.py
+++ randovania/server/discord/preset_lookup.py
@@ -5,22 +5,28 @@ import re
 import discord
 from discord.ext import commands
 
-from randovania.server.discord.bot import RandovaniaBot
-
-
 import randovania
 from randovania.gui.lib import preset_describer
+from randovania.layout.layout_description import LayoutDescription
 from randovania.layout.permalink import Permalink
 from randovania.layout.preset_migration import VersionedPreset
+from randovania.server.discord.bot import RandovaniaBot
 
 possible_links_re = re.compile(r'([A-Za-z0-9-_]{8,})')
 
 
-async def look_for_permalinks(message: str, channel: discord.TextChannel):
-    for word in possible_links_re.finditer(message):
+async def look_for_permalinks(message: discord.Message):
+    embed = None
+    multiple_permalinks = False
+
+    for word in possible_links_re.finditer(message.content):
         try:
             permalink = Permalink.from_str(word.group(1))
         except ValueError:
+            continue
+
+        if embed is not None:
+            multiple_permalinks = True
             continue
 
         embed = discord.Embed(title=f"`{word.group(1)}`",
@@ -33,7 +39,11 @@ async def look_for_permalinks(message: str, channel: discord.TextChannel):
             for category, items in preset_describer.describe(preset):
                 embed.add_field(name=category, value="\n".join(items), inline=True)
 
-        await channel.send(embed=embed)
+    if embed is not None:
+        content = None
+        if multiple_permalinks:
+            content = "Multiple permalinks found, using only the first."
+        await message.reply(content=content, embed=embed)
 
 
 async def reply_for_preset(message: discord.Message, versioned_preset: VersionedPreset):
@@ -49,6 +59,34 @@ async def reply_for_preset(message: discord.Message, versioned_preset: Versioned
                           description=preset.description)
     for category, items in preset_describer.describe(preset):
         embed.add_field(name=category, value="\n".join(items), inline=True)
+    await message.reply(embed=embed)
+
+
+async def reply_for_layout_description(message: discord.Message, description: LayoutDescription):
+    embed = discord.Embed(
+        title="Spoiler file for Randovania {}".format(description.version),
+    )
+
+    if description.permalink.player_count == 1:
+        preset = description.get_preset(0)
+        embed.description = "{}, with preset {}".format(preset.game.long_name, preset.name)
+        for category, items in preset_describer.describe(preset):
+            embed.add_field(name=category, value="\n".join(items), inline=True)
+    else:
+        games = {preset.game.long_name for preset in description.permalink.presets.values()}
+        game_names = sorted(games)
+
+        last_game = game_names.pop()
+        games_text = ", ".join(game_names)
+        if games_text:
+            games_text += " and "
+        games_text += last_game
+
+        embed.description = "{} player multiworld for {}".format(
+            description.permalink.player_count,
+            games_text,
+        )
+
     await message.reply(embed=embed)
 
 
@@ -72,9 +110,14 @@ class PermalinkLookupCog(commands.Cog):
                 versioned_preset = VersionedPreset(json.loads(data.decode("utf-8")))
                 await reply_for_preset(message, versioned_preset)
 
+            elif filename.endswith(LayoutDescription.file_extension()):
+                data = await attachment.read()
+                description = LayoutDescription.from_json_dict(json.loads(data.decode("utf-8")))
+                await reply_for_layout_description(message, description)
+
         channel: discord.TextChannel = message.channel
         if self.configuration["channel_name_filter"] in channel.name:
-            await look_for_permalinks(message.content, channel)
+            await look_for_permalinks(message)
 
 
 def setup(bot: RandovaniaBot):
