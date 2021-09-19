23cefef02ce6481baaa5b965754ecfe915812485
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-09-19 15:22:01 +0300

    In `stable`, bump patch version.

--- setup.py
+++ setup.py
@@ -17,11 +17,17 @@ def version_scheme(version):
     import setuptools_scm.version
     if version.exact:
         return setuptools_scm.version.guess_next_simple_semver(
-            version.tag, retain=setuptools_scm.version.SEMVER_LEN, increment=False)
-    else:
-        return version.format_next_version(
-            setuptools_scm.version.guess_next_simple_semver, retain=setuptools_scm.version.SEMVER_MINOR
+            version.tag, retain=setuptools_scm.version.SEMVER_LEN, increment=False
         )
+    else:
+        if version.branch != "stable":
+            return version.format_next_version(
+                setuptools_scm.version.guess_next_simple_semver, retain=setuptools_scm.version.SEMVER_MINOR
+            )
+        else:
+            return version.format_next_version(
+                setuptools_scm.version.guess_next_simple_semver, retain=setuptools_scm.version.SEMVER_PATCH
+            )
 
 
 setup(
