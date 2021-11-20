357c7ddda662e2b8c2f333835fe94f6bf158b5dc
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-20 22:26:50 +0200

    Add canvas editor tests

---
+++ test/gui/lib/test_data_editor_canvas.py
@@ -0,0 +1,54 @@
+from unittest.mock import MagicMock, call, ANY
+
+import pytest
+from PySide2.QtCore import QPoint
+
+from randovania.gui.lib.data_editor_canvas import DataEditorCanvas
+
+
+@pytest.fixture(name="canvas")
+def _canvas(skip_qtbot, dread_game_description):
+    canvas = DataEditorCanvas()
+    skip_qtbot.addWidget(canvas)
+
+    for world in dread_game_description.world_list.worlds:
+        canvas.select_world(world)
+        for area in world.areas:
+            canvas.select_area(area)
+            for node in area.nodes:
+                canvas.highlight_node(node)
+                break
+            break
+        break
+    return canvas
+
+
+def test_paintEvent(skip_qtbot, canvas, mocker):
+    event = MagicMock()
+    mock_painter: MagicMock = mocker.patch("PySide2.QtGui.QPainter")
+
+    # Run
+    canvas.paintEvent(event)
+
+    # Assert
+    mock_painter.assert_called_once_with(canvas)
+    draw_ellipse: MagicMock = mock_painter.return_value.drawEllipse
+    draw_ellipse.assert_any_call(ANY, 5, 5)
+    draw_ellipse.assert_any_call(ANY, 7, 7)
+    mock_painter.return_value.drawText.assert_called()
+
+
+def test_contextMenuEvent(skip_qtbot, canvas, mocker):
+    mock_qmenu = mocker.patch("PySide2.QtWidgets.QMenu")
+
+    event = MagicMock()
+    event.globalPos.return_value = QPoint(100, 200)
+    canvas._update_scale_variables()
+
+    # Run
+    canvas.contextMenuEvent(event)
+
+    # Assert
+    mock_qmenu.assert_called_once_with(canvas)
+    event.globalPos.assert_has_calls([call(), call()])
+    mock_qmenu.return_value.exec_.assert_called_once_with(QPoint(100, 200))
