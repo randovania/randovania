bf4d10337650ebba2b48f8a77ee9fcf4687b7882
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-26 03:21:26 +0200

    Transform DockType into a db-provided class

--- test/game_description/world/test_world_list.py
+++ test/game_description/world/test_world_list.py
@@ -1,3 +1,5 @@
+from frozendict import frozendict
+
 from randovania.game_description.requirements import ResourceRequirement, RequirementAnd
 from randovania.game_description.resources.resource_type import ResourceType
 from randovania.game_description.resources.simple_resource_info import SimpleResourceInfo
@@ -13,14 +15,17 @@ def test_connections_from_dock_blast_shield(empty_patches):
     # Setup
     req_1 = ResourceRequirement(SimpleResourceInfo("Ev1", "Ev1", ResourceType.EVENT), 1, False)
     req_2 = ResourceRequirement(SimpleResourceInfo("Ev2", "Ev2", ResourceType.EVENT), 1, False)
-    weak_1 = DockWeakness(1, "Weak 1", DockLockType.FRONT_ALWAYS_BACK_FREE, req_1, DockType.DOOR)
-    weak_2 = DockWeakness(2, "Weak 2", DockLockType.FRONT_BLAST_BACK_BLAST, req_2, DockType.DOOR)
+    dock_type = DockType("Type", "Type", frozendict())
+    weak_1 = DockWeakness("Weak 1", DockLockType.FRONT_ALWAYS_BACK_FREE, frozendict(), req_1)
+    weak_2 = DockWeakness("Weak 2", DockLockType.FRONT_BLAST_BACK_BLAST, frozendict(), req_2)
 
-    node_1 = DockNode("Node 1", False, None, "", {}, 0, NodeIdentifier.create("W", "Area 2", "Node 2"), weak_1)
-    node_2 = DockNode("Node 2", False, None, "", {}, 1, NodeIdentifier.create("W", "Area 1", "Node 1"), weak_2)
+    node_1 = DockNode("Node 1", False, None, "", {}, 0,
+                      NodeIdentifier.create("W", "Area 2", "Node 2"), dock_type, weak_1)
+    node_2 = DockNode("Node 2", False, None, "", {}, 1,
+                      NodeIdentifier.create("W", "Area 1", "Node 1"), dock_type, weak_2)
 
-    area_1 = Area("Area 1", 0, True, [node_1], {}, {})
-    area_2 = Area("Area 2", 0, True, [node_2], {}, {})
+    area_1 = Area("Area 1", None, True, [node_1], {}, {})
+    area_2 = Area("Area 2", None, True, [node_2], {}, {})
 
     world = World("W", [area_1, area_2], {})
     world_list = WorldList([world])
