479d1b38e2b189270e420395bde1ab87b7c04e51
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-11-30 20:37:08 +0200

    Create description_migration

--- randovania/network_common/__init__.py
+++ randovania/network_common/__init__.py
@@ -4,12 +4,11 @@ SERVER_API_VERSION = 4
 
 
 def connection_headers():
-    from randovania.layout.layout_description import CURRENT_DESCRIPTION_SCHEMA_VERSION
-    from randovania.layout import preset_migration, permalink
+    from randovania.layout import preset_migration, permalink, description_migration
     return {
         "X-Randovania-Version": randovania.VERSION,
         "X-Randovania-API-Version": str(SERVER_API_VERSION),
         "X-Randovania-Preset-Version": str(preset_migration.CURRENT_VERSION),
         "X-Randovania-Permalink-Version": str(permalink.Permalink.current_version()),
-        "X-Randovania-Description-Version": str(CURRENT_DESCRIPTION_SCHEMA_VERSION),
+        "X-Randovania-Description-Version": str(description_migration.CURRENT_VERSION),
     }
