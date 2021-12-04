43c431f8bf7e85bbb8b6e50e3a34f4b2c8b2d41b
duncathan <dunc@duncathan.com>
2021-12-03 23:41:44 -0600

    set explosive to MALCO for bad ending

--- randovania/games/cave_story/gui/preset_settings/cs_item_pool_tab.py
+++ randovania/games/cave_story/gui/preset_settings/cs_item_pool_tab.py
@@ -1,7 +1,9 @@
 import dataclasses
 from PySide2 import QtWidgets
+from randovania.games.cave_story.layout.cs_configuration import CSObjective
 from randovania.gui.preset_settings.item_pool_tab import PresetItemPool
 from randovania.interface_common.preset_editor import PresetEditor
+from randovania.layout.base.major_item_state import MajorItemState
 from randovania.layout.preset import Preset
 
 
@@ -11,11 +13,17 @@ class CSPresetItemPool(PresetItemPool):
     def __init__(self, editor: PresetEditor):
         super().__init__(editor)
         self._create_puppy_checkbox()
+        self.previousObj = CSObjective.NORMAL_ENDING
     
     def on_preset_changed(self, preset: Preset):
         super().on_preset_changed(preset)
 
         self._puppy_widget.setChecked(preset.configuration.puppies_anywhere)
+        
+        if self.previousObj != preset.configuration.objective:
+            if self.previousObj == CSObjective.BAD_ENDING or preset.configuration.objective == CSObjective.BAD_ENDING:
+                self._update_explosive(preset.configuration.objective == CSObjective.BAD_ENDING)
+            self.previousObj = preset.configuration.objective
     
     def _create_puppy_checkbox(self):
         parent, layout, _ = self._boxes_for_category["puppies"]
@@ -38,3 +46,14 @@ class CSPresetItemPool(PresetItemPool):
         with self._editor as editor:
             editor.set_configuration_field("puppies_anywhere", anywhere)
     
+    def _update_explosive(self, bad_ending: bool):
+        items = self._boxes_for_category["items"][2]
+        explosive = next(item for item in items.keys() if item.name == "Explosive")
+        explosive_box = items[explosive]
+
+        if bad_ending:
+            explosive_box.setVisible(False)
+            explosive_box._update_for_state(MajorItemState(True, 0, 0))
+        else:
+            explosive_box.setVisible(True)
+            explosive_box._update_for_state(MajorItemState(False, 1, 0))
