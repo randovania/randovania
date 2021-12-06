ea73e6c1b79151c80e79b3c264bb960fc33acf58
duncathan <dunc@duncathan.com>
2021-12-05 21:14:36 -0600

    refactor hints in base_patches_factory

--- randovania/games/cave_story/generator/base_patches_factory.py
+++ randovania/games/cave_story/generator/base_patches_factory.py
@@ -1,5 +1,34 @@
-from randovania.generator.base_patches_factory import BasePatchesFactory
+from random import Random
+from randovania.game_description.game_description import GameDescription
+from randovania.game_description.game_patches import GamePatches
+from randovania.game_description.hint import Hint, HintItemPrecision, HintLocationPrecision, HintType, PrecisionPair
+from randovania.game_description.resources.pickup_index import PickupIndex
+from randovania.game_description.world.node import LogbookNode, LoreType, PickupNode
+from randovania.game_description.world.world_list import WorldList
+from randovania.games.cave_story.layout.cs_configuration import CSConfiguration, CSObjective
+from randovania.generator.base_patches_factory import BasePatchesFactory, HintTargetPrecision
 
 
 class CSBasePatchesFactory(BasePatchesFactory):
-    pass
+    def indices_with_hint(self, configuration: CSConfiguration, game: GameDescription, rng: Random, patches: GamePatches, world_list: WorldList) -> list[HintTargetPrecision]:
+        return []
+        
+        # TODO: assign base hints *after* generation?
+        items_with_hint = []
+        if configuration.objective == CSObjective.BAD_ENDING:
+            items_with_hint.append("Rusty Key")
+        else:
+            items_with_hint.append("ID Card")
+        if patches.starting_location.area_name != "Start Point":
+            items_with_hint.append("Arthur's Key")
+        
+        already_hinted_indices = [hint.target for hint in patches.hints.values() if hint.target is not None]
+        indices_with_hint = [
+            (node.pickup_index, HintLocationPrecision.DETAILED, HintItemPrecision.DETAILED)
+            for node in world_list.all_nodes
+            if isinstance(node, PickupNode)
+            and node.pickup_index not in already_hinted_indices
+            and patches.pickup_assignment[node.pickup_index].pickup.name in items_with_hint
+        ]
+        return indices_with_hint
+
