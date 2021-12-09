047a4284dc9728681fc0739e8f77340eefe25825
duncathan <dunc@duncathan.com>
2021-12-08 21:00:29 -0600

    add guaranteed pickups in camp; initialize HP counter properly

--- randovania/games/cave_story/generator/pool_creator.py
+++ randovania/games/cave_story/generator/pool_creator.py
@@ -13,17 +13,24 @@ def pool_creator(results: PoolResults, configuration: CSConfiguration, db: Resou
     if base_patches is None or rng is None:
         return
     
+    def get_valid_indices(indices):
+        return [p for p in indices if p not in results.assignment.keys()]
+
+    def get_valid_pickups(pickups):
+        pickup_iter = lambda w: (p for p in results.pickups if p.name == w)
+        return [next(pickup_iter(w)) for w in pickups if next(pickup_iter(w), None) is not None]
+
     # puppies
     if not configuration.puppies_anywhere:
-        puppy_indices = [p for p in PUPPY_INDICES if p not in results.assignment.keys()]
+        puppy_indices = get_valid_indices(PUPPY_INDICES)
         rng.shuffle(puppy_indices)
         puppies: list[PickupEntry] = list(filter(lambda p: p.item_category.name == "puppies", results.pickups))
         for p in puppies:
             results.pickups.remove(p)
             results.assignment[puppy_indices.pop()] = p
-    
+
     # weapon to break blocks in first cave (do it this way to ensure a particular distribution chance)
-    if base_patches.starting_location.area_name == "Start Point":
+    if base_patches.starting_location.area_name in {"Start Point", "First Cave", "Hermit Gunsmith"}:
         sn_weapons = list(SN_WEAPONS)
 
         bubble_sn = db.get_by_type_and_index(ResourceType.TRICK, "SNBubbler")
@@ -36,16 +43,28 @@ def pool_creator(results: PoolResults, configuration: CSConfiguration, db: Resou
             sn_weapons.add("Super Missile Launcher")
             sn_weapons.add("Progressive Missile Launcher")
         
-        weapon_iter = lambda w: (p for p in results.pickups if p.name == w)
-        sn_weapons = [next(weapon_iter(w)) for w in sn_weapons if next(weapon_iter(w), None) is not None]
+        sn_weapons = get_valid_pickups(sn_weapons)
         
-        first_cave_indices = [i for i in FIRST_CAVE_INDICES if i not in results.assignment.keys()]
+        first_cave_indices = get_valid_indices(FIRST_CAVE_INDICES)
         if first_cave_indices and sn_weapons:
             index = rng.choice(first_cave_indices)
             weapon = rng.choice(sn_weapons)
 
             results.pickups.remove(weapon)
             results.assignment[index] = weapon
+    
+    # strong weapon and life capsule in camp
+    if base_patches.starting_location.area_name == "Camp":
+        strong_weapons = get_valid_pickups(STRONG_WEAPONS)
+        life_capsules = get_valid_pickups(["5HP Life Capsule"])
+        camp_indices = get_valid_indices(CAMP_INDICES)
+
+        rng.shuffle(camp_indices)
+
+        if camp_indices and strong_weapons:
+            results.assignment[camp_indices.pop()] = rng.choice(strong_weapons)
+        if camp_indices and life_capsules:
+            results.assignment[camp_indices.pop()] = rng.choice(life_capsules)
 
 
 # skip 65 and 67 since they both require all 5 puppies
@@ -53,10 +72,19 @@ PUPPY_INDICES = [PickupIndex(i) for i in range(57, 67) if i != 65]
 
 FIRST_CAVE_INDICES = [PickupIndex(32), PickupIndex(33)]
 
+CAMP_INDICES = [PickupIndex(26), PickupIndex(27)]
+
 SN_WEAPONS = [
     "Polar Star",
     "Progressive Polar Star",
     "Machine Gun",
     "Blade",
-    "Nemesis"
+    "Nemesis",
+]
+
+STRONG_WEAPONS = [
+    "Blade",
+    "Nemesis",
+    "Machine Gun",
+    "Spur",
 ]
