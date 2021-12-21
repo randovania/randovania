638e9160b555eb98e9b553ce6f87cd11b9779991
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2021-12-21 22:11:26 +0000

    Run coverage for server tests, run all tests

--- tools/server_image_test.sh
+++ tools/server_image_test.sh
@@ -2,5 +2,5 @@
 
 set -e -x -v
 
-pip install --no-cache-dir -e .[test] -c requirements.txt
-python -m pytest test/server -p no:pytest-qt --skip-gui-tests --skip-echo-tool
+pip install --no-cache-dir -e ".[test]" -c requirements.txt
+python -m pytest test -p no:pytest-qt --skip-gui-tests --skip-echo-tool "$@"
