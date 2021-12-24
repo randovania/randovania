cd2a458402b80d246429e65899d4040711ff9547
toasterparty <toasterparty@derpymail.org>
2021-12-24 11:33:57 -0800

    Prime1 - Default In-Game Settings (#2485)
    * custom github actions
    
    * oops
    
    * undo debug workflow
    
    * Added gui for setting default in-game options
    
    * update randomprime test
    
    * changelog
    
    Co-authored-by: dyceron <38679103+dyceron@users.noreply.github.com>

--- randovania/games/prime1/gui/dialog/prime_cosmetic_patches_dialog.py
+++ randovania/games/prime1/gui/dialog/prime_cosmetic_patches_dialog.py
@@ -1,12 +1,15 @@
 import dataclasses
+from functools import partial
+
 from PySide2.QtCore import QSize
 from PySide2.QtGui import QColor
 
-from PySide2.QtWidgets import QColorDialog, QFrame, QLayout, QMessageBox, QSizePolicy, QWidget
+from PySide2.QtWidgets import QColorDialog, QFrame, QLayout, QMessageBox, QSizePolicy, QWidget, QLabel, QSlider, QCheckBox
 
 from randovania.gui.dialog.base_cosmetic_patches_dialog import BaseCosmeticPatchesDialog
 from randovania.gui.generated.prime_cosmetic_patches_dialog_ui import Ui_PrimeCosmeticPatchesDialog
 from randovania.games.prime1.layout.prime_cosmetic_patches import PrimeCosmeticPatches
+from randovania.games.prime1.layout.prime_user_preferences import PrimeUserPreferences, SoundMode
 
 SUIT_DEFAULT_COLORS = [
     [ (255, 173, 50), (220, 25, 45), (132, 240, 60) ], # Power
@@ -14,6 +17,14 @@ SUIT_DEFAULT_COLORS = [
     [ (170, 170, 145), (70, 25, 50), (40, 20, 90), (140, 240, 240) ], # Gravity
     [ (50, 50, 50), (20, 20, 20), (230, 50, 62) ] # Phazon
 ]
+
+def update_label_with_slider(label: QLabel, slider: QSlider):
+    if label.display_as_percentage:
+        min_value = slider.minimum()
+        percentage = (slider.value() - min_value) / (slider.maximum() - min_value)
+        label.setText(f"{percentage * 100: 3.0f}%")
+    else:
+        label.setText(str(slider.value()))
 
 def hue_rotate_color(original_color: tuple[int,int,int], rotation: int):
     color = QColor.fromRgb(*original_color)
@@ -30,12 +41,33 @@ def hue_rotate_color(original_color: tuple[int,int,int], rotation: int):
 
 
 class PrimeCosmeticPatchesDialog(BaseCosmeticPatchesDialog, Ui_PrimeCosmeticPatchesDialog):
+    preferences: PrimeUserPreferences
     _cosmetic_patches: PrimeCosmeticPatches
 
     def __init__(self, parent: QWidget, current: PrimeCosmeticPatches):
         super().__init__(parent)
         self.setupUi(self)
         self._cosmetic_patches = current
+
+        self.field_to_slider_mapping = {
+            "screen_brightness": self.screen_brightness_slider,
+            "screen_x_offset": self.screen_x_offset_slider,
+            "screen_y_offset": self.screen_y_offset_slider,
+            "screen_stretch": self.screen_stretch_slider,
+            "sfx_volume": self.sfx_volume_slider,
+            "music_volume": self.music_volume_slider,
+            "hud_alpha": self.hud_alpha_slider,
+            "helmet_alpha": self.helmet_alpha_slider,
+        }
+        self.field_to_check_mapping = {
+            "hud_lag": self.hud_lag_check,
+            "invert_y_axis": self.invert_y_axis_check,
+            "rumble": self.rumble_check,
+            "swap_beam_controls": self.swap_beam_controls_check,
+        }
+
+        for sound_mode in SoundMode:
+            self.sound_mode_combo.addItem(sound_mode.name, sound_mode)
 
         # Build dynamically preview color squares for suits
         suit_layouts = [ 
@@ -51,7 +83,6 @@ class PrimeCosmeticPatchesDialog(BaseCosmeticPatchesDialog, Ui_PrimeCosmeticPatc
 
         self.on_new_cosmetic_patches(current)
         self.connect_signals()
-
         self._update_color_squares()
 
     def connect_signals(self):
@@ -65,6 +96,13 @@ class PrimeCosmeticPatchesDialog(BaseCosmeticPatchesDialog, Ui_PrimeCosmeticPatc
         self.gravity_suit_rotation_field.valueChanged.connect(self._persist_suit_color_rotations)
         self.phazon_suit_rotation_field.valueChanged.connect(self._persist_suit_color_rotations)
         self.custom_hud_color_button.clicked.connect(self._open_color_picker)
+        self.sound_mode_combo.currentIndexChanged.connect(self._on_sound_mode_update)
+
+        for field_name, slider in self.field_to_slider_mapping.items():
+            slider.valueChanged.connect(partial(self._on_slider_update, slider, field_name))
+
+        for field_name, check in self.field_to_check_mapping.items():
+            check.stateChanged.connect(partial(self._on_check_update, check, field_name))
 
     def on_new_cosmetic_patches(self, patches: PrimeCosmeticPatches):
         self.qol_cosmetic_check.setChecked(patches.qol_cosmetic)
@@ -74,6 +112,25 @@ class PrimeCosmeticPatchesDialog(BaseCosmeticPatchesDialog, Ui_PrimeCosmeticPatc
         self.varia_suit_rotation_field.setValue(patches.suit_color_rotations[1])
         self.gravity_suit_rotation_field.setValue(patches.suit_color_rotations[2])
         self.phazon_suit_rotation_field.setValue(patches.suit_color_rotations[3])
+        self.on_new_user_preferences(patches.user_preferences)
+
+    def on_new_user_preferences(self, user_preferences: PrimeUserPreferences):
+        self.sound_mode_combo.setCurrentIndex(self.sound_mode_combo.findData(user_preferences.sound_mode))
+
+        for field in dataclasses.fields(user_preferences):
+            if field.name in self.field_to_slider_mapping:
+                slider = self.field_to_slider_mapping[field.name]
+                slider.setMinimum(field.metadata["min"])
+                slider.setMaximum(field.metadata["max"])
+                slider.setValue(getattr(user_preferences, field.name))
+
+                value_label: QLabel = getattr(self, f"{field.name}_value_label")
+                value_label.display_as_percentage = field.metadata["display_as_percentage"]
+                update_label_with_slider(value_label, slider)
+
+            elif field.name in self.field_to_check_mapping:
+                check = self.field_to_check_mapping[field.name]
+                check.setChecked(getattr(user_preferences, field.name))
 
     def _persist_option_then_notify(self, attribute_name: str):
         def persist(value: int):
@@ -131,6 +188,36 @@ class PrimeCosmeticPatchesDialog(BaseCosmeticPatchesDialog, Ui_PrimeCosmeticPatc
     @property
     def cosmetic_patches(self) -> PrimeCosmeticPatches:
         return self._cosmetic_patches
+
+    @property
+    def preferences(self) -> PrimeUserPreferences:
+        return self._cosmetic_patches.user_preferences
+
+    @preferences.setter
+    def preferences(self, value: PrimeUserPreferences):
+        self._cosmetic_patches = dataclasses.replace(
+            self._cosmetic_patches,
+            user_preferences=value,
+        )
+
+    def _on_sound_mode_update(self):
+        self.preferences = dataclasses.replace(
+            self.preferences,
+            sound_mode=self.sound_mode_combo.currentData()
+        )
+
+    def _on_slider_update(self, slider: QSlider, field_name: str, _):
+        self.preferences = dataclasses.replace(
+            self.preferences,
+            **{field_name: slider.value()}
+        )
+        update_label_with_slider(getattr(self, f"{field_name}_value_label"), slider)
+
+    def _on_check_update(self, check: QCheckBox, field_name: str, _):
+        self.preferences = dataclasses.replace(
+            self.preferences,
+            **{field_name: check.isChecked()}
+        )
 
     def reset(self):
         self.on_new_cosmetic_patches(PrimeCosmeticPatches())
---
+++ randovania/games/prime1/layout/prime_user_preferences.py
@@ -0,0 +1,37 @@
+import dataclasses
+from enum import IntEnum
+
+from randovania.bitpacking.json_dataclass import JsonDataclass
+
+def _int_field(default: int, min_value: int, max_value: int, display_as_percentage: bool = True):
+    return dataclasses.field(default=default, metadata={"min": min_value, "max": max_value,
+                                                        "display_as_percentage": display_as_percentage})
+
+class SoundMode(IntEnum):
+    MONO = 0
+    STEREO = 1
+    SURROUND = 2
+
+@dataclasses.dataclass(frozen=True)
+class PrimeUserPreferences(JsonDataclass):
+    sound_mode: SoundMode = SoundMode.STEREO
+    screen_brightness: int = _int_field(5, 2, 8, True)
+    screen_x_offset: int = _int_field(0, -30, 30, False)
+    screen_y_offset: int = _int_field(0, -30, 30, False)
+    screen_stretch: int = _int_field(0, -10, 10, False)
+    sfx_volume: int = _int_field(127, 0, 127, True)
+    music_volume: int = _int_field(127, 0, 127, True)
+    hud_alpha: int = _int_field(255, 0, 255, True)
+    helmet_alpha: int = _int_field(255, 0, 255, True)
+    hud_lag: bool = True
+    invert_y_axis: bool = False
+    rumble: bool = True
+    swap_beam_controls: bool = False
+
+    def __post_init__(self):
+        for field in dataclasses.fields(self):
+            value = getattr(self, field.name)
+            if "min" in field.metadata and value < field.metadata["min"]:
+                raise ValueError(f'Value {value} for field "{field.name}" is less than minimum {field.metadata["min"]}')
+            if "max" in field.metadata and value > field.metadata["max"]:
+                raise ValueError(f'Value {value} for field "{field.name}" is less than maximum {field.metadata["max"]}')
