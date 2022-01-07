f5869048303d24a52c04e52a0ab96f03f2db4a85
SolventMercury <antspiral3220@yahoo.com>
2022-01-06 18:55:28 -0700

    Code Cleanup Made usage of MusicMode enum more consistent Changed keys of MusicMode to strings rather than integers Removed default values for SM Patch Configuration Removed boiler-plate in SM Patch Configuration tab

--- randovania/games/super_metroid/gui/preset_settings/super_patches_tab.py
+++ randovania/games/super_metroid/gui/preset_settings/super_patches_tab.py
@@ -2,123 +2,74 @@ import dataclasses
 import functools
 
 from PySide2 import QtWidgets
-from PySide2.QtWidgets import QComboBox
+from PySide2.QtWidgets import QCheckBox
 
 from randovania.gui.generated.preset_patcher_super_patches_ui import Ui_PresetPatcherSuperPatches
 from randovania.gui.preset_settings.preset_tab import PresetTab
 from randovania.interface_common.preset_editor import PresetEditor
 from randovania.layout.preset import Preset
-from randovania.games.super_metroid.layout.super_metroid_patch_configuration import MusicMode
+from randovania.games.super_metroid.layout.super_metroid_patch_configuration import MusicMode, SuperPatchConfiguration
+
 
 class PresetSuperPatchConfiguration(PresetTab, Ui_PresetPatcherSuperPatches):
+    checkboxes = {}
+    radio_buttons = {}
+
     def __init__(self, editor: PresetEditor):
         super().__init__(editor)
         self.setupUi(self)
-        self.colorblind_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "colorblind_mode"
-        ))
-        self.instant_g4_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "instant_g4"
-        ))
-        self.max_ammo_display_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "max_ammo_display"
-        ))
-        self.aim_with_any_button_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "aim_with_any_button"
-        ))
-        self.fast_doors_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "fast_doors_and_elevators"
-        ))
-        self.backup_saves_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "backup_saves"
-        ))
-        self.better_decompression_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "better_decompression"
-        ))
-        self.skip_intro_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "skip_intro"
-        ))
-        self.mb_cutscene_tweaks_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "mother_brain_cutscene_edits"
-        ))
-        self.no_demo_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "no_demo"
-        ))
-        self.dachora_pit_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "dachora_pit"
-        ))
-        self.early_supers_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "early_supers_bridge"
-        ))
-        self.pre_hi_jump_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "pre_hi_jump"
-        ))
-        self.moat_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "moat"
-        ))
-        self.pre_spazer_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "pre_spazer"
-        ))
-        self.red_tower_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "red_tower"
-        ))
-        self.nova_boost_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "nova_boost_platform"
-        ))
-        self.save_refills_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "refill_before_save"
-        ))
-        self.respin_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "respin"
-        ))
-        self.cant_use_supers_on_red_doors_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "cant_use_supers_on_red_doors"
-        ))
-        self.cheap_charge_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "cheap_charge"
-        ))
-        self.speedkeep_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "speedkeep"
-        ))
-        self.isj_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "infinite_space_jump"
-        ))
-        self.mb_rainbow_beam_nerf_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "nerfed_rainbow_beam"
-        ))
-        self.fix_spacetime_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "fix_spacetime"
-        ))
-        self.fix_heat_echoes_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "fix_heat_echoes"
-        ))
-        self.fix_screw_attack_menu_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "fix_screw_attack_menu"
-        ))
-        self.no_gt_checkbox.stateChanged.connect(functools.partial(
-            self._on_patch_checkbox_changed, "no_gt_code"
-        ))
+        self.checkboxes = {
+            "colorblind_mode": self.colorblind_checkbox,
+            "instant_g4": self.instant_g4_checkbox,
+            "max_ammo_display": self.max_ammo_display_checkbox,
+            "aim_with_any_button": self.aim_with_any_button_checkbox,
+            "fast_doors_and_elevators": self.fast_doors_checkbox,
+            "backup_saves": self.backup_saves_checkbox,
+            "better_decompression": self.better_decompression_checkbox,
+            "skip_intro": self.skip_intro_checkbox,
+            "mother_brain_cutscene_edits": self.mb_cutscene_tweaks_checkbox,
+            "no_demo": self.no_demo_checkbox,
+            "dachora_pit": self.dachora_pit_checkbox,
+            "early_supers_bridge": self.early_supers_checkbox,
+            "pre_hi_jump": self.pre_hi_jump_checkbox,
+            "moat": self.moat_checkbox,
+            "pre_spazer": self.pre_spazer_checkbox,
+            "red_tower": self.red_tower_checkbox,
+            "nova_boost_platform": self.nova_boost_checkbox,
+            "refill_before_save": self.save_refills_checkbox,
+            "respin": self.respin_checkbox,
+            "cant_use_supers_on_red_doors": self.cant_use_supers_on_red_doors_checkbox,
+            "cheap_charge": self.cheap_charge_checkbox,
+            "speedkeep": self.speedkeep_checkbox,
+            "infinite_space_jump": self.isj_checkbox,
+            "nerfed_rainbow_beam": self.mb_rainbow_beam_nerf_checkbox,
+            "fix_spacetime": self.fix_spacetime_checkbox,
+            "fix_heat_echoes": self.fix_heat_echoes_checkbox,
+            "fix_screw_attack_menu": self.fix_screw_attack_menu_checkbox,
+            "no_gt_code": self.no_gt_checkbox,
+        }
+        self.radio_buttons = {
+            MusicMode.VANILLA: self.vanilla_music_option,
+            MusicMode.RANDOMIZED: self.random_music_option,
+            MusicMode.OFF: self.no_music_option,
+        }
+        for name, checkbox in self.checkboxes.items():
+            checkbox.stateChanged.connect(functools.partial(self._on_patch_checkbox_changed, name, checkbox))
+        for music_mode, radio_button in self.radio_buttons.items():
+            radio_button.toggled.connect(functools.partial(self._on_music_option_changed, music_mode))
+        self.update_controls(editor.configuration.patches)
 
-        self.vanilla_music_option.toggled.connect(functools.partial(
-            self._on_music_option_changed, MusicMode.VANILLA.value
-        ))
-        self.random_music_option.toggled.connect(functools.partial(
-            self._on_music_option_changed, MusicMode.RANDOMIZED.value
-        ))
-        self.no_music_option.toggled.connect(functools.partial(
-            self._on_music_option_changed, MusicMode.OFF.value
-        ))
     @property
     def uses_patches_tab(self) -> bool:
         return True
 
-    def _on_patch_checkbox_changed(self, field_name: str, value: bool):
+    def _on_patch_checkbox_changed(self, field_name: str, checkbox: QCheckBox, value: int):
         with self._editor as editor:
             patch_configuration = editor.configuration.patches
-            new_config = dataclasses.replace(patch_configuration, **{field_name: value})
+            new_config = dataclasses.replace(patch_configuration, **{field_name: checkbox.isChecked()})
             editor.set_configuration_field("patches", new_config)
 
-    def _on_music_option_changed(self, option: int, value: bool):
+    def _on_music_option_changed(self, option: MusicMode, value: bool, ):
         if value:
             with self._editor as editor:
                 patch_configuration = editor.configuration.patches
@@ -126,36 +77,10 @@ class PresetSuperPatchConfiguration(PresetTab, Ui_PresetPatcherSuperPatches):
                 editor.set_configuration_field("patches", new_config)
 
     def on_preset_changed(self, preset: Preset):
-        patch_configuration = preset.configuration.patches
+        self.update_controls(preset.configuration.patches)
 
-        self.colorblind_checkbox.setChecked(patch_configuration.colorblind_mode)
-        self.instant_g4_checkbox.setChecked(patch_configuration.instant_g4)
-        self.max_ammo_display_checkbox.setChecked(patch_configuration.max_ammo_display)
-        self.aim_with_any_button_checkbox.setChecked(patch_configuration.aim_with_any_button)
-        self.fast_doors_checkbox.setChecked(patch_configuration.fast_doors_and_elevators)
-        self.backup_saves_checkbox.setChecked(patch_configuration.backup_saves)
-        self.better_decompression_checkbox.setChecked(patch_configuration.better_decompression)
-        self.skip_intro_checkbox.setChecked(patch_configuration.skip_intro)
-        self.mb_cutscene_tweaks_checkbox.setChecked(patch_configuration.mother_brain_cutscene_edits)
-        self.no_demo_checkbox.setChecked(patch_configuration.no_demo)
-        self.dachora_pit_checkbox.setChecked(patch_configuration.dachora_pit)
-        self.early_supers_checkbox.setChecked(patch_configuration.early_supers_bridge)
-        self.pre_hi_jump_checkbox.setChecked(patch_configuration.pre_hi_jump)
-        self.moat_checkbox.setChecked(patch_configuration.moat)
-        self.pre_spazer_checkbox.setChecked(patch_configuration.pre_spazer)
-        self.red_tower_checkbox.setChecked(patch_configuration.red_tower)
-        self.nova_boost_checkbox.setChecked(patch_configuration.nova_boost_platform)
-        self.respin_checkbox.setChecked(patch_configuration.respin)
-        self.save_refills_checkbox.setChecked(patch_configuration.refill_before_save)
-        self.cant_use_supers_on_red_doors_checkbox.setChecked(patch_configuration.cant_use_supers_on_red_doors)
-        self.cheap_charge_checkbox.setChecked(patch_configuration.cheap_charge)
-        self.speedkeep_checkbox.setChecked(patch_configuration.speedkeep)
-        self.isj_checkbox.setChecked(patch_configuration.infinite_space_jump)
-        self.mb_rainbow_beam_nerf_checkbox.setChecked(patch_configuration.nerfed_rainbow_beam)
-        self.fix_spacetime_checkbox.setChecked(patch_configuration.fix_spacetime)
-        self.fix_heat_echoes_checkbox.setChecked(patch_configuration.fix_heat_echoes)
-        self.fix_screw_attack_menu_checkbox.setChecked(patch_configuration.fix_screw_attack_menu)
-        self.no_gt_checkbox.setChecked(patch_configuration.no_gt_code)
-
-        for radio_index, radio_button in enumerate([self.vanilla_music_option, self.random_music_option, self.no_music_option]):
-            radio_button.setChecked(radio_index == patch_configuration.music)
+    def update_controls(self, patch_configuration: SuperPatchConfiguration):
+        for name, checkbox in self.checkboxes.items():
+            checkbox.setChecked(getattr(patch_configuration, name))
+        for music_mode, radio_button in self.radio_buttons.items():
+            radio_button.setChecked(music_mode == patch_configuration.music)
