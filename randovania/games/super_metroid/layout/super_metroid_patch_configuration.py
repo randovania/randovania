5c1133eef49affe78160a246b6a6ab3a64e07f72
SolventMercury <antspiral3220@yahoo.com>
2022-01-09 14:37:34 -0700

    Remove cosmetic patches from Patch UI, presets, patcher tab, etc. Add UI file for cosmetic patch dialog

--- randovania/games/super_metroid/layout/super_metroid_patch_configuration.py
+++ randovania/games/super_metroid/layout/super_metroid_patch_configuration.py
@@ -1,30 +1,17 @@
 import dataclasses
 from typing import Iterator
-from enum import Enum
 
-from randovania.bitpacking.bitpacking import BitPackDataclass, BitPackEnum
+from randovania.bitpacking.bitpacking import BitPackDataclass
 from randovania.bitpacking.json_dataclass import JsonDataclass
-
-
-class MusicMode(BitPackEnum, Enum):
-    VANILLA = "vanilla_music"
-    RANDOMIZED = "random_music"
-    OFF = "no_music"
-
 
 @dataclasses.dataclass(frozen=True)
 class SuperPatchConfiguration(BitPackDataclass, JsonDataclass):
-    music: MusicMode
-    colorblind_mode: bool
     instant_g4: bool
-    max_ammo_display: bool
-    aim_with_any_button: bool
     fast_doors_and_elevators: bool
     backup_saves: bool
     better_decompression: bool
     skip_intro: bool
     mother_brain_cutscene_edits: bool
-    no_demo: bool
     dachora_pit: bool
     early_supers_bridge: bool
     pre_hi_jump: bool
