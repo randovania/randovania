132a371f7601220f4045b945feb2bbcd3d599dea
toasterparty <toasterparty@derpymail.org>
2022-01-10 03:25:16 -0800

    Set prime max capacity in a way that doesn't break multiworld (#2553)

--- randovania/games/prime1/patcher/randomprime_patcher.py
+++ randovania/games/prime1/patcher/randomprime_patcher.py
@@ -408,9 +408,9 @@ class RandomprimePatcher(Patcher):
 
                 "etankCapacity": configuration.energy_per_tank,
                 "itemMaxCapacity": {
-                    "Energy Tank": 200,
-                    "Power Bomb": 99,
-                    "Missile": 999,
+                    "Energy Tank": db.resource_database.get_item("EnergyTank").max_capacity,
+                    "Power Bomb": db.resource_database.get_item("PowerBomb").max_capacity,
+                    "Missile": db.resource_database.get_item("Missile").max_capacity,
                     "Unknown Item 1": db.resource_database.multiworld_magic_item.max_capacity,
                 },
 
