4339ec2333cef02de8bfbdd44bc1ffaab49cd017
toasterparty <toasterparty@derpymail.org>
2022-01-10 11:13:45 -0800

    Enforce required python version (linux)

--- tools/prepare_virtual_env.sh
+++ tools/prepare_virtual_env.sh
@@ -3,6 +3,8 @@
 set -e -x
 cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")/.."
 
+python3.9 tools/test_py_version.py
+
 python3.9 -m venv venv
 source venv/bin/activate
 python -m pip install --upgrade -r requirements-setuptools.txt
