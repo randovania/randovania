c6169e0c036f5912571b8e153f5f0fd9e847c5fc
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-01-23 23:31:23 +0200

    Test permalinks with invalid GeneratorParameters

--- test/layout/test_permalink.py
+++ test/layout/test_permalink.py
@@ -1,7 +1,11 @@
+import contextlib
 from unittest.mock import MagicMock, ANY
 
 import pytest
 
+from randovania.bitpacking import bitpacking
+from randovania.games.game import RandovaniaGame
+from randovania.layout import generator_parameters
 from randovania.layout.generator_parameters import GeneratorParameters
 from randovania.layout.permalink import Permalink, UnsupportedPermalink
 
@@ -28,16 +32,22 @@ def test_encode(fake_generator_parameters):
     assert encoded == "DX4wMTIzAx6sOvRw"
 
 
-def test_decode(mocker):
+@pytest.mark.parametrize("invalid", [False, True])
+def test_decode(mocker, invalid):
+    games = (RandovaniaGame.METROID_PRIME_ECHOES, RandovaniaGame.METROID_DREAD)
     mock_from_bytes: MagicMock = mocker.patch("randovania.layout.generator_parameters.GeneratorParameters.from_bytes",
                                               autospec=True)
+    parameters = mock_from_bytes.return_value
+    parameters.as_bytes = bitpacking.pack_results_and_bit_count(generator_parameters.encode_game_list(games))[0]
+    if invalid:
+        mock_from_bytes.side_effect = ValueError("Invalid Permalink")
 
     # This test should break whenever we change how permalinks are created
     # When this happens, we must bump the permalink version and change the tests
-    encoded = "DX4wMTIzAx6sOvRw"
+    encoded = "DQYwMTIzAkwMABlW"
 
     expected = Permalink(
-        parameters=mock_from_bytes.return_value,
+        parameters=parameters,
         seed_hash=None,
         randovania_version=b"0123",
     )
@@ -46,12 +56,21 @@ def test_decode(mocker):
     # assert expected.as_base64_str == ""
     # print(expected.as_base64_str)
 
+    if invalid:
+        expectation = pytest.raises(UnsupportedPermalink)
+    else:
+        expectation = contextlib.nullcontext()
+
     # Run
-    link = Permalink.from_str(encoded)
+    with expectation as exp:
+        link = Permalink.from_str(encoded)
 
     # Assert
-    assert link == expected
-    mock_from_bytes.assert_called_once_with(b"\xA0\xB0\xC0")
+    mock_from_bytes.assert_called_once_with(b'F\x00')
+    if invalid:
+        assert exp.value.games == games
+    else:
+        assert link == expected
 
 
 @pytest.mark.parametrize("invalid", [
