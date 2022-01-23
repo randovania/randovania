cc11f57e998fc6428bf38ed491be58eeb2b378e4
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-01-23 12:53:35 +0200

    Fix create_session_entry with layout

--- test/server/test_database.py
+++ test/server/test_database.py
@@ -1,5 +1,9 @@
+import pytest
 from peewee import SqliteDatabase
 
+from randovania.layout.layout_description import LayoutDescription
+from randovania.lib import construct_lib
+from randovania.network_common.binary_formats import BinaryGameSessionEntry
 from randovania.server import database
 
 
@@ -8,3 +12,38 @@ def test_init(tmpdir):
     with test_db.bind_ctx(database.all_classes):
         test_db.connect(reuse_if_open=True)
         test_db.create_tables(database.all_classes)
+
+
+@pytest.mark.parametrize("has_description", [False, True])
+def test_GameSession_create_session_entry(clean_database, has_description, test_files_dir):
+    # Setup
+    description = LayoutDescription.from_file(test_files_dir.joinpath("log_files", "seed_a.rdvgame"))
+    someone = database.User.create(name="Someone")
+    s = database.GameSession.create(name="Debug", num_teams=1, creator=someone)
+    game_details = None
+    if has_description:
+        s.layout_description = description
+        s.save()
+        game_details = {
+            'permalink': 'Df9uqXH9oVxqIzA1B9I6ucE8eXh5BvdEI4TRjHBxM_EDK2ssGHiv9nXsY_mv8F_cawoM6DaIHMAENhvQW-bMv8sAACRG',
+            'seed_hash': 'N2UXD7NB',
+            'spoiler': True,
+            'word_hash': 'Temple Bridge Quad',
+        }
+
+    # Run
+    session = database.GameSession.get_by_id(1)
+    result = session.create_session_entry()
+    readable_result = construct_lib.convert_to_raw_python(BinaryGameSessionEntry.parse(result))
+
+    # Assert
+    assert readable_result == {
+        'allowed_games': ['prime1', 'prime2'],
+        'game_details': game_details,
+        'generation_in_progress': None,
+        'id': 1,
+        'name': 'Debug',
+        'players': [],
+        'presets': [],
+        'state': 'setup',
+    }
