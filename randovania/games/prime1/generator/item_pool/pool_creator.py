fc27a222ee2b0d59b9e512ccdf6429a0ed47165b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-02-12 17:40:17 +0200

    Split Game help into multiple widgets
    This also relocates the ownership of these tabs to the GameData class.

--- randovania/games/prime1/generator/item_pool/pool_creator.py
+++ randovania/games/prime1/generator/item_pool/pool_creator.py
@@ -6,9 +6,11 @@ from randovania.games.prime1.generator.item_pool.artifacts import add_artifacts
 from randovania.games.prime1.layout.prime_configuration import PrimeConfiguration
 from randovania.generator.item_pool import PoolResults
 from randovania.generator.item_pool.pool_creator import _extend_pool_results
+from randovania.layout.base.base_configuration import BaseConfiguration
 
 
-def prime1_specific_pool(results: PoolResults, configuration: PrimeConfiguration, db: ResourceDatabase,
+def prime1_specific_pool(results: PoolResults, configuration: BaseConfiguration, db: ResourceDatabase,
                          patches: GamePatches, rng: Random):
+    assert isinstance(configuration, PrimeConfiguration)
     _extend_pool_results(results, add_artifacts(db, configuration.artifact_target,
                                                 configuration.artifact_minimum_progression))
---
+++ randovania/games/prime3/gui/corruption_help_widget.py
@@ -0,0 +1,15 @@
+from PySide2 import QtWidgets
+
+from randovania.games.game import RandovaniaGame
+from randovania.gui.generated.games_help_corruption_widget_ui import Ui_CorruptionHelpWidget
+from randovania.gui.generated.games_help_prime_widget_ui import Ui_PrimeHelpWidget
+from randovania.gui.lib import hints_text, faq_lib
+
+
+class CorruptionHelpWidget(QtWidgets.QTabWidget, Ui_CorruptionHelpWidget):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        game = RandovaniaGame.METROID_PRIME_CORRUPTION
+        hints_text.update_hints_text(game, self.hint_item_names_tree_widget)
