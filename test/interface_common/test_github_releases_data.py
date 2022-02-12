93df457f7f17ec630cc3ddbe740190344e1f43f2
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-02-12 20:34:57 +0200

    Get 100 releases to avoid pagination for now.

--- test/interface_common/test_github_releases_data.py
+++ test/interface_common/test_github_releases_data.py
@@ -1,25 +1,43 @@
-from unittest.mock import AsyncMock, MagicMock
+from unittest.mock import AsyncMock, MagicMock, call
 
 import aiohttp
-import pytest
 
 from randovania.interface_common import github_releases_data
 
 
 async def test_download_from_github_success(mocker):
     # Setup
+    page_size = 5
+    response_a = AsyncMock()
+    response_b = AsyncMock()
+    responses = [response_a, response_b]
+
     mock_get_response: MagicMock = mocker.patch.object(aiohttp.ClientSession, 'get')
-    mock_get_response.return_value.__aenter__.return_value.raise_for_status = MagicMock()
-    mock_get_response.return_value.__aenter__.return_value.json = AsyncMock()
+    mock_get_response.side_effect = responses
+    for r in responses:
+        r.__aenter__.return_value.raise_for_status = MagicMock()
+
+    response_a.__aenter__.return_value.json = AsyncMock(return_value=[1, 2, 3, 4, 5])
+    response_b.__aenter__.return_value.json = AsyncMock(return_value=[6, 7])
 
     # Run
-    returned_value = await github_releases_data._download_from_github()
+    returned_value = await github_releases_data._download_from_github(page_size=page_size)
 
     # Assert
-    mock_get_response.assert_called()
-    mock_get_response.return_value.__aenter__.return_value.raise_for_status.assert_called_once_with()
-    mock_get_response.return_value.__aenter__.return_value.json.assert_awaited()
-    assert returned_value == mock_get_response.return_value.__aenter__.return_value.json.return_value
+    mock_get_response.assert_has_calls([
+        call(
+            github_releases_data._RELEASES_URL,
+            params={"page": 1, "per_page": page_size},
+        ),
+        call(
+            github_releases_data._RELEASES_URL,
+            params={"page": 2, "per_page": page_size},
+        ),
+    ])
+    for r in responses:
+        r.__aenter__.return_value.raise_for_status.assert_called_once_with()
+        r.__aenter__.return_value.json.assert_awaited()
+    assert returned_value == [1, 2, 3, 4, 5, 6, 7]
 
 
 async def test_download_from_github_bad_response_is_caught(mocker):
@@ -34,7 +52,7 @@ async def test_download_from_github_bad_response_is_caught(mocker):
     # Assert
     mock_get_response.assert_called()
     mock_get_response.return_value.__aenter__.return_value.raise_for_status.assert_called()
-    assert returned_value == None
+    assert returned_value is None
 
 
 async def test_download_from_github_connection_failure_is_caught(mocker):
@@ -47,4 +65,4 @@ async def test_download_from_github_connection_failure_is_caught(mocker):
 
     # Assert
     mock_get_response.assert_called()
-    assert returned_value == None
+    assert returned_value is None
