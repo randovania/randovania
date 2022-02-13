9ba318cc67f0bc77d5b67e0bff6e9938e917ff85
jonloveslegos <jonloveslegos@gmail.com>
2022-02-13 14:31:33 -0500

    added options for progressive weapons and armor

---
+++ randovania/games/deltarune/item_database/progressive_items.py
@@ -0,0 +1,46 @@
+def gui_tuples():
+    return [
+        (
+            "Progressive Weapons",
+            (
+                "Spookysword",
+                "Brave Ax",
+                "Ragger",
+                "DaintyScarf",
+                "Devilsknife")
+        ),
+        (
+            "Progressive Armor",
+            (
+                "Amber Card",
+                "Dice Brace",
+                "White Ribbon",
+                "Amber Card",
+                "IronShackle",
+                "Jevilstail")
+        ),
+        (
+            "Progressive Custom Weapons",
+            (
+                "MechaSaber",
+                "AutoAxe",
+                "FiberScarf",
+                "BounceBlade",
+                "Ragger2",
+                "PuppetScarf",
+                "UltiBlade")
+        ),
+        (
+            "Progressive Custom Armor",
+            (
+                "Pink Ribbon",
+                "GlowWrist",
+                "Silver Card",
+                "GlowWrist",
+                "SpikeBand",
+                "ChainMail",
+                "B.ShotBowtie",
+                "Royal Pin",
+                "Dealmaker")
+        )
+    ]
