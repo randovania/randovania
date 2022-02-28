63ef96ef0e289101270d16bfe6c7e044cff33be3
SolventMercury <87032239+SolventMercury@users.noreply.github.com>
2022-02-27 23:45:09 -0700

    Removed the vestigial "Pickup Style" option from the preset settings, as it serves no purpose at the moment.

--- randovania/games/super_metroid/gui/preset_settings/__init__.py
+++ randovania/games/super_metroid/gui/preset_settings/__init__.py
@@ -13,11 +13,14 @@ def super_metroid_preset_tabs(editor: PresetEditor, window_manager: WindowManage
     from randovania.gui.preset_settings.metroid_item_pool_tab import MetroidPresetItemPool
     from randovania.games.super_metroid.gui.preset_settings.super_patches_tab import PresetSuperPatchConfiguration
 
+    itemPoolTab = MetroidPresetItemPool(editor)
+    itemPoolTab.pickup_style_widget.hide()
+
     return [
         PresetTrickLevel(editor, game_description, window_manager),
         PresetMetroidStartingArea(editor, game_description),
         PresetGeneration(editor, game_description),
         PresetLocationPool(editor, game_description),
-        MetroidPresetItemPool(editor),
+        itemPoolTab,
         PresetSuperPatchConfiguration(editor),
     ]
