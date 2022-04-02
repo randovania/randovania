3299bc3819c3cc523e7878b8cf571ed80355ab4f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-04-02 20:28:26 +0300

    Install all packages in prepare_virtual_env
    Fixes #2968.

--- tools/prepare_virtual_env.sh
+++ tools/prepare_virtual_env.sh
@@ -9,6 +9,6 @@ python3.9 -m venv venv
 source venv/bin/activate
 python -c "import sys; assert sys.version_info[0:2] == (3, 9), 'Python 3.9 required'"
 python -m pip install --upgrade -r requirements-setuptools.txt
-python -m pip install -e ".[gui]" -c requirements.txt
+python -m pip install pyqt-distutils -e ".[gui]" -c requirements.txt
 
 echo "Setup finished successfully."
