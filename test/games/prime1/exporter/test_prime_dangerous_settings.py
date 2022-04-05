7230d1959905323fe9cc078f9a127b5b88a80775
toasterparty <toasterparty@derpymail.org>
2022-04-05 16:49:03 -0700

    add test for dangerous settings

---
+++ test/games/prime1/exporter/test_prime_dangerous_settings.py
@@ -0,0 +1,21 @@
+import pytest
+
+from randovania.interface_common.players_configuration import PlayersConfiguration
+from randovania.layout.layout_description import LayoutDescription
+
+def test_dangerous_settings(test_files_dir, rdvgame_filename="prime1_crazy_seed.rdvgame"):
+    rdvgame = test_files_dir.joinpath("log_files", rdvgame_filename)
+    layout_description = LayoutDescription.from_file(rdvgame)    
+    players_config = PlayersConfiguration(0, {0: "Prime", 1: "Echoes"})
+    preset = layout_description.get_preset(players_config.player_index)
+    
+    print(preset.dangerous_settings())
+
+    assert preset.dangerous_settings() == [
+        'One-way anywhere elevators',
+        'Shuffled Item Position',
+        'Room Randomizer',
+        'Extra Superheated Rooms',
+        'Submerged Rooms',
+        'Dangerous Gravity Suit Logic',
+    ]
