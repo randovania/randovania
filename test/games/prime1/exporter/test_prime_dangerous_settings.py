760685106d8b6205879bc78f72d340687195fa57
toasterparty <toasterparty@derpymail.org>
2022-04-06 09:27:08 -0700

    cleanup

--- test/games/prime1/exporter/test_prime_dangerous_settings.py
+++ test/games/prime1/exporter/test_prime_dangerous_settings.py
@@ -6,8 +6,7 @@ from randovania.layout.layout_description import LayoutDescription
 def test_dangerous_settings(test_files_dir, rdvgame_filename="prime1_crazy_seed.rdvgame"):
     rdvgame = test_files_dir.joinpath("log_files", rdvgame_filename)
     layout_description = LayoutDescription.from_file(rdvgame)    
-    players_config = PlayersConfiguration(0, {0: "Prime", 1: "Echoes"})
-    preset = layout_description.get_preset(players_config.player_index)
+    preset = layout_description.get_preset(0)
 
     assert preset.dangerous_settings() == [
         'One-way anywhere elevators',
