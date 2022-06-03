fae29de8d78c80fa9db113929612a604868982c7
Thomas Ryan <thomas.ryan1@ontariotechu.net>
2022-06-03 15:41:57 -0400

    Added equipment to prime_items.py

--- randovania/games/prime1/patcher/prime_items.py
+++ randovania/games/prime1/patcher/prime_items.py
@@ -18,3 +18,30 @@ ARTIFACT_ITEMS = [
 ]
 ARTIFACT_NAMES = ["Artifact of " + item for item in ARTIFACT_ITEMS]
 ARTIFACT_MODEL = list(ARTIFACT_NAMES)
+
+EQUIPMENT = [
+    "Charge",
+    "Power",
+    "Wave",
+    "Ice",
+    "Plasma",
+    "Grapple",
+    "Combat",
+    "Scan",
+    "Thermal",
+    "X-Ray",
+    "SpaceJump",
+    "EnergyTank",
+    "MorphBall",
+    "Bombs",
+    "Boost",
+    "Spider",
+    "PowerSuit",
+    "VariaSuit",
+    "GravitySuit",
+    "PhazonSuit",
+    "Supers",
+    "Wavebuster",
+    "IceSpreader",
+    "Flamethrower",
+]
