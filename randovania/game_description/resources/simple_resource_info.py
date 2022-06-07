a0063a48e2686e9b5954f22fd669eec6666fe887
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-06-07 16:42:35 +0300

    Hash less fields for resources

--- randovania/game_description/resources/simple_resource_info.py
+++ randovania/game_description/resources/simple_resource_info.py
@@ -7,10 +7,10 @@ from randovania.game_description.resources.resource_type import ResourceType
 
 @dataclasses.dataclass(frozen=True, order=True, slots=True)
 class SimpleResourceInfo:
-    long_name: str
+    long_name: str = dataclasses.field(hash=False)
     short_name: str
     resource_type: ResourceType
-    extra: frozendict = dataclasses.field(default_factory=frozendict)
+    extra: frozendict = dataclasses.field(hash=False, default_factory=frozendict)
 
     def __str__(self):
         return self.long_name
