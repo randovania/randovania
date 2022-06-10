3e6b9f798e372844f7e1fa0218a33e3475dd5039
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-06-10 23:48:01 +0300

    Another attempt at fixing scope issues with Discord Auth.

---
+++ randovania/server/custom_discord_oauth.py
@@ -0,0 +1,20 @@
+import typing
+
+import flask_discord
+from flask_discord import types
+from requests_oauthlib import OAuth2Session
+
+
+class CustomDiscordOAuth2Session(flask_discord.DiscordOAuth2Session):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.scope = ["identify"]
+
+    def create_session(
+            self, scope: list = None, *, data: dict = None, prompt: bool = True,
+            permissions: typing.Union[types.Permissions, int] = 0, **params
+    ):
+        return super().create_session(self.scope, data=data, prompt=prompt, permissions=permissions, **params)
+
+    def _make_session(self, token: str = None, state: str = None, scope: list = None) -> OAuth2Session:
+        return super()._make_session(token=token, state=state, scope=self.scope)
