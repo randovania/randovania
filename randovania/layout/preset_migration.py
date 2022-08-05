b2a8cb96108ba6eda49ce59dcaf5639e208330b8
unknown <fantaselion@gmail.com>
2022-08-04 19:13:39 -0500

    Bumped PARAMETEREDITOR from 2.1.1 to 2.1.2 (previous version had the 'file writing' code commented out)

--- randovania/layout/preset_migration.py
+++ randovania/layout/preset_migration.py
@@ -6,7 +6,7 @@ from randovania.games.game import RandovaniaGame
 from randovania.layout.base.dock_rando_configuration import DockRandoMode, DockTypeState
 from randovania.lib import migration_lib
 
-CURRENT_VERSION = 35
+CURRENT_VERSION = 36
 
 
 def _migrate_v1(preset: dict) -> dict:
@@ -659,7 +659,7 @@ def _migrate_v34(preset: dict) -> dict:
     preset["configuration"].pop("multi_pickup_new_weighting")
 
     return preset
-<<<<<<< HEAD
+
 def _migrate_v35(preset: dict) -> dict:
     if preset["game"] == "prime1":
         preset["configuration"]["enemy_rando_range_scale_low"] = 1.0
@@ -674,9 +674,7 @@ def _migrate_v35(preset: dict) -> dict:
         preset["configuration"]["enemy_rando_range_knockback_high"] = 1.0
         preset["configuration"]["enemy_rando_diff_xyz"] = False
     return preset
-=======
 
->>>>>>> upstream/main
 
 _MIGRATIONS = {
     1: _migrate_v1,  # v1.1.1-247-gaf9e4a69
@@ -713,10 +711,7 @@ _MIGRATIONS = {
     32: _migrate_v32,
     33: _migrate_v33,
     34: _migrate_v34,
-<<<<<<< HEAD
     35: _migrate_v35,
-=======
->>>>>>> upstream/main
 }
 
 
