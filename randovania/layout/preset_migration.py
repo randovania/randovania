d97c68f78732b843c045384c060b2295ec59db10
unknown <fantaselion@gmail.com>
2022-08-05 01:11:15 -0500

    fixed mistake

--- randovania/layout/preset_migration.py
+++ randovania/layout/preset_migration.py
@@ -661,18 +661,9 @@ def _migrate_v34(preset: dict) -> dict:
     return preset
 
 def _migrate_v35(preset: dict) -> dict:
-    if preset["game"] == "prime1":
-        preset["configuration"]["enemy_rando_range_scale_low"] = 1.0
-        preset["configuration"]["enemy_rando_range_scale_high"] = 1.0
-        preset["configuration"]["enemy_rando_range_health_low"] = 1.0
-        preset["configuration"]["enemy_rando_range_health_high"] = 1.0
-        preset["configuration"]["enemy_rando_range_speed_low"] = 1.0
-        preset["configuration"]["enemy_rando_range_speed_high"] = 1.0
-        preset["configuration"]["enemy_rando_range_damage_low"] = 1.0
-        preset["configuration"]["enemy_rando_range_damage_high"] = 1.0
-        preset["configuration"]["enemy_rando_range_knockback_low"] = 1.0
-        preset["configuration"]["enemy_rando_range_knockback_high"] = 1.0
-        preset["configuration"]["enemy_rando_diff_xyz"] = False
+    if preset["game"] == "dread":
+        preset["configuration"]["linear_damage_runs"] = False
+        preset["configuration"]["linear_dps"] = 20
     return preset
 
 def _migrate_v36(preset: dict) -> dict:
