5a5e845d1bd906563ae6420b2755fd99c5d932bf
unknown <fantaselion@gmail.com>
2022-08-12 22:21:45 -0500

    pytest.mark.parametrize is actually being used now

--- test/games/prime1/layout/test_preset_describer.py
+++ test/games/prime1/layout/test_preset_describer.py
@@ -12,29 +12,38 @@ def test_prime_format_params(use_enemy_attribute_randomizer):
     # Setup
     preset = PresetManager(None).default_preset_for_game(RandovaniaGame.METROID_PRIME).get_preset()
     assert isinstance(preset.configuration, PrimeConfiguration)
-    configuration = dataclasses.replace(
-        preset.configuration,
-        enemy_attributes=EnemyAttributeRandomizer(
-            enemy_rando_range_scale_low=0.25,
-            enemy_rando_range_scale_high=5.25,
-            enemy_rando_range_health_low=2.25,
-            enemy_rando_range_health_high=8.23,
-            enemy_rando_range_speed_low=0.15,
-            enemy_rando_range_speed_high=7.25,
-            enemy_rando_range_damage_low=1.25,
-            enemy_rando_range_damage_high=100.25,
-            enemy_rando_range_knockback_low=15.5,
-            enemy_rando_range_knockback_high=200.35,
-            enemy_rando_diff_xyz=True,
-        ),
-        random_boss_sizes=True,
-        superheated_probability=531,
-        submerged_probability=287
-    )
+    if use_enemy_attribute_randomizer:
+        configuration = dataclasses.replace(
+            preset.configuration,
+            enemy_attributes=EnemyAttributeRandomizer(
+                enemy_rando_range_scale_low=0.25,
+                enemy_rando_range_scale_high=5.25,
+                enemy_rando_range_health_low=2.25,
+                enemy_rando_range_health_high=8.23,
+                enemy_rando_range_speed_low=0.15,
+                enemy_rando_range_speed_high=7.25,
+                enemy_rando_range_damage_low=1.25,
+                enemy_rando_range_damage_high=100.25,
+                enemy_rando_range_knockback_low=15.5,
+                enemy_rando_range_knockback_high=200.35,
+                enemy_rando_diff_xyz=True,
+            ),
+            random_boss_sizes=True,
+            superheated_probability=531,
+            submerged_probability=287
+        )
+    else:
+        configuration = dataclasses.replace(
+            preset.configuration,
+            enemy_attributes=None,
+            random_boss_sizes=True,
+            superheated_probability=531,
+            submerged_probability=287
+        )
 
     # Run
     result = RandovaniaGame.METROID_PRIME.data.layout.preset_describer.format_params(configuration)
-    assert result == {
+    expected = {
         "Logic Settings": [
             "All tricks disabled",
             "Dangerous Actions: Randomly"
@@ -63,7 +72,17 @@ def test_prime_format_params(use_enemy_attribute_randomizer):
             "53.1% chance of superheated, 28.7% chance of submerged",
             "Deterministic I. Drone RNG, Deterministic Maze RNG",
             "Competitive cutscene removal",
-            "Random Size within range 0.25 - 5.25, Random Health within range 2.25 - 8.23, Random Speed within range 0.15 - 7.25, Random Damage within range 1.25 - 100.25, Random Knockback within range 15.5 - 200.35, Enemies will be stretched randomly",
             "Allowed backwards: Frigate, Labs, Upper Mines"
         ]
     }
+    if use_enemy_attribute_randomizer:
+        expected["Game Changes"].insert(5, "Random Size within range 0.25 - 5.25, Random Health within range 2.25 - 8.23, Random Speed within range 0.15 - 7.25, Random Damage within range 1.25 - 100.25, Random Knockback within range 15.5 - 200.35, Enemies will be stretched randomly")
+    else:
+        expected["Game Changes"].insert(2, "Random Boss Sizes")
+    assert expected == result
+
+#expected = {
+#    # all that stuff in the dict
+#}
+#if use_enemy_attribute_randomizer:
+#    expected["Game Changes"].append("Random size within... etc")
