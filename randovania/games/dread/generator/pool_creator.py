ff35bb0de5a67228ed81e7d797d5e0acffbe3033
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-08-22 14:13:10 +0300

    Use NodeIdentifiers in dread pool creator

--- randovania/games/dread/generator/pool_creator.py
+++ randovania/games/dread/generator/pool_creator.py
@@ -4,8 +4,8 @@ from randovania.game_description.game_description import GameDescription
 from randovania.game_description.game_patches import GamePatches
 from randovania.game_description.resources.pickup_entry import PickupEntry
 from randovania.game_description.resources.pickup_index import PickupIndex
-from randovania.game_description.resources.resource_database import ResourceDatabase
 from randovania.game_description.resources.resource_info import ResourceCollection
+from randovania.game_description.world.node_identifier import NodeIdentifier
 from randovania.games.dread.layout.dread_configuration import DreadConfiguration
 from randovania.generator.item_pool import PoolResults
 from randovania.generator.item_pool.pickup_creator import create_dread_artifact
@@ -34,7 +34,7 @@ def artifact_pool(game: GameDescription, configuration: DreadConfiguration, rng:
     for key in starting_keys:
         initial_resources.add_resource_gain(key.progression)
 
-    locations: list[PickupIndex] = []
+    locations: list[NodeIdentifier] = []
     if config.prefer_emmi:
         locations.extend(_EMMI_INDICES)
     if config.prefer_major_bosses:
@@ -44,26 +44,30 @@ def artifact_pool(game: GameDescription, configuration: DreadConfiguration, rng:
 
     if rng is not None:
         rng.shuffle(locations)
-        new_assignment = {location: key for location, key in zip(locations, keys_to_shuffle)}
+        new_assignment = {
+            game.world_list.get_pickup_node(location).pickup_index: key
+            for location, key in zip(locations, keys_to_shuffle)
+        }
         item_pool = [key for key in keys_to_shuffle if key not in new_assignment.values()]
 
     return PoolResults(item_pool, new_assignment, initial_resources)
 
 
+_c = NodeIdentifier.create
 _EMMI_INDICES = [
-    PickupIndex(139),  # Artaria
-    PickupIndex(144),  # Cataris
-    PickupIndex(147),  # Dairon
-    PickupIndex(143),  # Ferenia
-    PickupIndex(146),  # Ghavoran
-    PickupIndex(137),  # Hanubia
+    _c("Artaria", "Central Unit Access", "Pickup (Spider Magnet)"),
+    _c("Cataris", "Central Unit Access", "Pickup (Morph Ball)"),
+    _c("Dairon", "Central Unit Access", "Pickup (Speed Booster)"),
+    _c("Ferenia", "Purple EMMI Arena", "Pickup (Wave Beam)"),
+    _c("Ghavoran", "Central Unit Access", "Pickup (Ice Missile)"),
+    _c("Hanubia", "Orange EMMI Introduction", "Pickup (Power Bomb)"),
 ]
 
 _BOSS_INDICES = [
-    PickupIndex(138),  # Corpius
-    PickupIndex(142),  # Escue
-    PickupIndex(145),  # Golzuna
-    PickupIndex(150),  # Drogyga
-    PickupIndex(149),  # Experiment Z-57
-    PickupIndex(148),  # Kraid
+    _c("Artaria", "Corpius Arena", "Pickup (Phantom Cloak)"),
+    _c("Ferenia", "Escue Arena", "Pickup (Storm Missile)"),
+    _c("Ghavoran", "Golzuna Arena", "Pickup (Cross Bomb)"),
+    _c("Burenia", "Drogyga Arena", "Pickup (Drogyga)"),
+    _c("Cataris", "Above Z-57 Fight", "Pickup (Z-57)"),
+    _c("Cataris", "Kraid Arena", "Pickup (Kraid)"),
 ]
