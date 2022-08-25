684c0159b6f0fc1750a2b20ff7af038e71ba1889
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-08-25 23:38:32 +0300

    Add names to categories

--- randovania/game_description/item/item_category.py
+++ randovania/game_description/item/item_category.py
@@ -11,6 +11,11 @@ class ItemCategory:
     is_major: bool
     is_key: bool = False
 
+    def __post_init__(self):
+        assert self.name, "Name must not be empty"
+        assert self.long_name, "Long name must not be empty"
+        assert len(self.hint_details) == 2, "Hint details must be 2 elements"
+
     @classmethod
     def from_json(cls, name: str, value: dict) -> "ItemCategory":
         return cls(
