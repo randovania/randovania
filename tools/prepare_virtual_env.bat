bfdeaa5748af68ece2f692c9431711abb6210d0d
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-08-30 13:07:16 +0300

    Use Python 3.11

--- tools/prepare_virtual_env.bat
+++ tools/prepare_virtual_env.bat
@@ -13,16 +13,16 @@ if NOT exist .git\ (
     exit
 )
 
-py -3.10 tools\test_py_version.py
+py -3.11 tools\test_py_version.py
 if NOT ["%errorlevel%"]==["0"] pause
 if NOT ["%errorlevel%"]==["0"] exit /b 0
 
-py -3.10 -m venv venv
+py -3.11 -m venv venv
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 call venv\scripts\activate
 
-python -c "import sys; assert sys.version_info[0:2] == (3, 10), 'Python 3.10 required'"
+python -c "import sys; assert sys.version_info[0:2] == (3, 11), 'Python 3.11 required'"
 python -c "import sys; assert sys.maxsize > 2**32, '64-bit python required'"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
--- tools/prepare_virtual_env.sh
+++ tools/prepare_virtual_env.sh
@@ -3,11 +3,11 @@
 set -e -x
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.."
 
-python3.10 tools/test_py_version.py
+python3.11 tools/test_py_version.py
 
-python3.10 -m venv venv
+python3.11 -m venv venv
 source venv/bin/activate
-python -c "import sys; assert sys.version_info[0:2] == (3, 10), 'Python 3.10 required'"
+python -c "import sys; assert sys.version_info[0:2] == (3, 11), 'Python 3.11 required'"
 python -m pip install --upgrade -r requirements-setuptools.txt
 python -m pip install pyqt-distutils -e ".[gui]" -c requirements.txt
 
