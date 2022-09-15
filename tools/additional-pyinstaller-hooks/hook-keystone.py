8599b794fda8fc6c442ca2c33d786fc618711487
Ethan Armbrust <ethanarm@buffalo.edu>
2022-09-15 18:52:44 -0400

    Include dynamic libs for keystone, fixes #3765

--- tools/additional-pyinstaller-hooks/hook-keystone.py
+++ tools/additional-pyinstaller-hooks/hook-keystone.py
@@ -1,3 +1,4 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_dynamic_libs
 
 datas = collect_data_files('keystone')
+binaries = collect_dynamic_libs('keystone')
