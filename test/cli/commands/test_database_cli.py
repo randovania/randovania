556ea83ec895e7dd81e825e81ede341f4dfebc3b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-09-16 12:04:02 +0300

    Fix tests updating item-database.json, causing race conditions

--- test/cli/commands/test_database_cli.py
+++ test/cli/commands/test_database_cli.py
@@ -55,11 +55,13 @@ def test_refresh_all_logic(check, mocker):
     from randovania.game_description import pretty_print
     from randovania.game_description import data_writer
     from randovania.game_description import integrity_check
+    from randovania.game_description import default_database
 
     mock_write_human_readable_game = mocker.patch.object(pretty_print, "write_human_readable_game")
     mock_write_as_split_files = mocker.patch.object(data_writer, "write_as_split_files")
     mock_find_database_errors = mocker.patch.object(integrity_check, "find_database_errors",
                                                     return_value=["An error"])
+    mock_write_item_database_for_game = mocker.patch.object(default_database, "write_item_database_for_game")
 
     # Run
     database.refresh_all_logic(args)
@@ -69,7 +71,9 @@ def test_refresh_all_logic(check, mocker):
         mock_find_database_errors.assert_has_calls([call(ANY) for _ in RandovaniaGame])
         mock_write_human_readable_game.assert_not_called()
         mock_write_as_split_files.assert_not_called()
+        mock_write_item_database_for_game.assert_not_called()
     else:
         mock_find_database_errors.assert_not_called()
         mock_write_human_readable_game.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
         mock_write_as_split_files.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
+        mock_write_item_database_for_game.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
