43476d5cba52098b759476a545aedda8c55ecb40
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-10-02 13:29:36 +0300

    Update py-cord

--- test/server/discord/test_database_command.py
+++ test/server/discord/test_database_command.py
@@ -27,11 +27,12 @@ async def test_add_commands():
 async def test_database_command():
     # Setup
     cog = DatabaseCommandCog({"guild": 1234}, MagicMock())
+    cog.database_inspect.cog = cog
     cog._select_split_world_view[RandovaniaGame.METROID_PRIME_CORRUPTION] = view = MagicMock()
     ctx = AsyncMock()
 
     # Run
-    await cog.database_inspect(cog, ctx, game=RandovaniaGame.METROID_PRIME_CORRUPTION)
+    await cog.database_inspect(ctx, game=RandovaniaGame.METROID_PRIME_CORRUPTION)
 
     # Assert
     ctx.respond.assert_awaited_once_with(
