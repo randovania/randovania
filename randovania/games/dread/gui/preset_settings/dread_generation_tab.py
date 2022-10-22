b8909b1a81c6064db7506d6800b23919652fbbdb
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2022-10-22 16:26:35 +0300

    Improve tests

--- randovania/games/dread/gui/preset_settings/dread_generation_tab.py
+++ randovania/games/dread/gui/preset_settings/dread_generation_tab.py
@@ -1,13 +1,12 @@
 from typing import Iterable
 
 from PySide6 import QtWidgets, QtCore
-from PySide6.QtWidgets import *
 
 from randovania.game_description.game_description import GameDescription
 from randovania.game_description.resources.resource_type import ResourceType
-from randovania.game_description.resources.trick_resource_info import TrickResourceInfo
-from randovania.gui.dialog.trick_details_popup import TrickDetailsPopup, ResourceDetailsPopup
-from randovania.gui.lib import common_qt_lib, signal_handling
+from randovania.games.dread.layout.dread_configuration import DreadConfiguration
+from randovania.gui.dialog.trick_details_popup import ResourceDetailsPopup
+from randovania.gui.lib import signal_handling
 from randovania.gui.lib.window_manager import WindowManager
 from randovania.gui.preset_settings.generation_tab import PresetGeneration
 from randovania.interface_common.preset_editor import PresetEditor
@@ -15,32 +14,35 @@ from randovania.layout.preset import Preset
 
 
 class PresetDreadGeneration(PresetGeneration):
-    highdanger_logic_check: QCheckBox
-    highdanger_logic_label: QLabel
+    highdanger_logic_check: QtWidgets.QCheckBox
+    highdanger_logic_label: QtWidgets.QLabel
 
     def __init__(self, editor: PresetEditor, game_description: GameDescription, window_manager: WindowManager):
         super().__init__(editor, game_description, window_manager)
 
         signal_handling.on_checked(self.highdanger_logic_check, self._on_highdanger_logic_check)
         self.highdanger_logic_label.linkActivated.connect(self._on_click_link_highdanger_logic_details)
-        
 
     def setupUi(self, obj):
         super().setupUi(obj)
 
-        self.highdanger_logic_check = QCheckBox(
-            "Allow Highly Dangerous Logic")
-        self.highdanger_logic_label = QLabel(
-            """<html><head/><body><p>Highly Dangerous Logic is a setting which enables dangerous actions that are not as obvious.</p><p>Using these dangerous actions could render
-            a seed unbeatable if the player were to save afterwards.</p><p><a href=\"resource-details://misc/highdanger\"><span style=\" text-decoration: underline;
-            color:#0000ff;\">Click here</span></a> to see which rooms are affected.</p></body></html>""")
+        self.highdanger_logic_check = QtWidgets.QCheckBox("Allow Highly Dangerous Logic", obj)
+        self.highdanger_logic_label = QtWidgets.QLabel(obj)
+        self.highdanger_logic_label.setText(
+            """<html><head/><body>
+            <p>Highly Dangerous Logic is a setting which enables dangerous actions that are not as obvious.</p>
+            <p>Using these dangerous actions could render a seed unbeatable if the player were to save afterwards.</p>
+            <p><a href=\"resource-details://misc/highdanger\"><span style=\" text-decoration: underline;
+            color:#0000ff;\">Click here</span></a> to see which rooms are affected.</p></body></html>""",
+        )
 
     @property
-    def game_specific_widgets(self) -> Iterable[QWidget] | None:
+    def game_specific_widgets(self) -> Iterable[QtWidgets.QWidget] | None:
         yield self.highdanger_logic_check
         yield self.highdanger_logic_label
 
     def on_preset_changed(self, preset: Preset):
+        assert isinstance(preset.configuration, DreadConfiguration)
         super().on_preset_changed(preset)
         self.highdanger_logic_check.setChecked(preset.configuration.allow_highly_dangerous_logic)
 
@@ -59,8 +61,7 @@ class PresetDreadGeneration(PresetGeneration):
             self.game_description.resource_database.get_by_type_and_index(ResourceType.MISC, "HighDanger"),
         ))
 
-    def _exec_trick_details(self, popup: TrickDetailsPopup):
+    def _exec_trick_details(self, popup: ResourceDetailsPopup):
         self._trick_details_popup = popup
         self._trick_details_popup.setWindowModality(QtCore.Qt.WindowModal)
         self._trick_details_popup.open()
-
--- test/gui/preset_settings/test_generation_tab.py
+++ test/gui/preset_settings/test_generation_tab.py
@@ -3,8 +3,7 @@ import uuid
 from unittest.mock import MagicMock
 
 import pytest
-from PySide6 import QtCore
-from PySide6.QtWidgets import *
+from PySide6 import QtCore, QtWidgets
 
 from randovania.game_description import default_database
 from randovania.games.cave_story.gui.preset_settings.cs_generation_tab import PresetCSGeneration
@@ -16,38 +15,37 @@ from randovania.interface_common.preset_editor import PresetEditor
 
 
 @pytest.mark.parametrize("game_data", [
-    (RandovaniaGame.METROID_DREAD, True, True, PresetDreadGeneration),
+    (RandovaniaGame.METROID_DREAD, True, False, PresetDreadGeneration),
     (RandovaniaGame.METROID_PRIME, True, True, PresetPrimeGeneration),
     (RandovaniaGame.METROID_PRIME_ECHOES, False, True, PresetGeneration),
     (RandovaniaGame.CAVE_STORY, True, False, PresetCSGeneration)
 ])
 def test_on_preset_changed(skip_qtbot, preset_manager, game_data):
     # Setup
-    game, has_specific_settings, has_min_logic, has_highdanger_logic, tab = game_data
+    game, has_specific_settings, has_min_logic, tab = game_data
 
     base = preset_manager.default_preset_for_game(game).get_preset()
     preset = dataclasses.replace(base, uuid=uuid.UUID('b41fde84-1f57-4b79-8cd6-3e5a78077fa6'))
     editor = PresetEditor(preset)
     window: PresetGeneration = tab(editor, default_database.game_description_for(game), MagicMock())
-    parent = QGroupBox()
+    parent = QtWidgets.QWidget()
     window.setParent(parent)
+    skip_qtbot.addWidget(window)
 
     # Run
     window.on_preset_changed(editor.create_custom_preset_with())
 
     # Assert
     assert window.trick_level_minimal_logic_check.isVisibleTo(parent) == has_min_logic
-    assert window.highdanger_logic_check.isVisibleTo(parent) == has_highdanger_logic
     assert window.game_specific_group.isVisibleTo(parent) == has_specific_settings
 
 
 def test_persist_local_first_progression(skip_qtbot, preset_manager):
-    parent = QGroupBox()
     game = RandovaniaGame.BLANK
 
     editor = MagicMock()
     window = PresetGeneration(editor, default_database.game_description_for(game), MagicMock())
-    window.setParent(parent)
+    skip_qtbot.addWidget(window)
 
     # Run
     skip_qtbot.mouseClick(window.local_first_progression_check, QtCore.Qt.LeftButton)
