d165f7087bf517266ee974104766047fad7aa250
dyceron <38679103+dyceron@users.noreply.github.com>
2022-12-15 16:12:35 -0500

    Update game page

--- randovania/games/samus_returns/gui/__init__.py
+++ randovania/games/samus_returns/gui/__init__.py
@@ -1,4 +1,4 @@
 from randovania.games.samus_returns.gui.msr_game_tab import MSRGameTabWidget
 from randovania.games.samus_returns.gui.dialog.cosmetic_patches_dialog import MSRCosmeticPatchesDialog
 from randovania.games.samus_returns.gui.dialog.game_export_dialog import MSRGameExportDialog
-from randovania.games.samus_returns.gui.preset_settings import preset_tabs
+from randovania.games.samus_returns.gui.preset_settings import msr_preset_tabs
