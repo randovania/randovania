8b6f18748a4c1e9c92b4f704c02b58be88395da0
Thanatos <thanatosstr89@gmail.com>
2023-02-15 20:06:37 +0100

    Rename "my" preset migration to v44 because v43 was taken in the meantime Use NodeIdentifier in new test of test_pickup_list.py

--- randovania/layout/preset_migration.py
+++ randovania/layout/preset_migration.py
@@ -720,16 +720,15 @@ def _migrate_v42(preset: dict) -> dict:
     return preset
 
 def _migrate_v43(preset: dict) -> dict:
+    preset["configuration"]["single_set_for_pickups_that_solve"] = False
+    preset["configuration"]["staggered_multi_pickup_placement"] = False
+    return preset
+
+def _migrate_v44(preset: dict) -> dict:
     for start_loc in preset["configuration"]["starting_location"]:
         area_identifier = AreaIdentifier(start_loc["world_name"], start_loc["area_name"])
         node_identifier = migration_data.get_new_start_loc_from_old_start_loc(preset["game"], area_identifier)
         start_loc["node_name"] = node_identifier.node_name
-    return preset
-
-
-def _migrate_v43(preset: dict) -> dict:
-    preset["configuration"]["single_set_for_pickups_that_solve"] = False
-    preset["configuration"]["staggered_multi_pickup_placement"] = False
     return preset
 
 
@@ -777,6 +776,7 @@ _MIGRATIONS = [
     _migrate_v41,
     _migrate_v42,
     _migrate_v43,
+    _migrate_v44,
 ]
 CURRENT_VERSION = migration_lib.get_version(_MIGRATIONS)
 
