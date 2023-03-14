ea13dc2649c378c5d733625f9dae96a1758d39ac
XenoWars <sonicunleashedwr@gmail.com>
2023-03-14 12:35:30 -0400

    Fix test?

--- test/gui/preset_settings/test_starting_area.py
+++ test/gui/preset_settings/test_starting_area.py
@@ -87,7 +87,7 @@ def test_quick_fill_save_station(skip_qtbot, preset_manager, game_enum: Randovan
     skip_qtbot.mouseClick(window.starting_area_quick_fill_save_station, QtCore.Qt.LeftButton)
 
     # Assert
-    assert editor.configuration.starting_location.locations == (window.game_description.starting_location,)
+    assert editor.configuration.starting_location.locations == (window.save_stations,)
 
 
 def test_quick_fill_cs_classic(skip_qtbot, preset_manager):
