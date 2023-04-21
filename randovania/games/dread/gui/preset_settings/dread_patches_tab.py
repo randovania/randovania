6f0652e8fe83895b7d37e2dbd5613d9160603af2
Arcanox <arcanox@arcanox.me>
2023-04-20 21:33:38 -0500

    When editing a preset that has RB damage set to vanilla, save it back as vanilla if the "alternate damage" checkbox is still checked

--- randovania/games/dread/gui/preset_settings/dread_patches_tab.py
+++ randovania/games/dread/gui/preset_settings/dread_patches_tab.py
@@ -21,6 +21,8 @@ _FIELDS = [
 class PresetDreadPatches(PresetTab, Ui_PresetDreadPatches):
     def __init__(self, editor: PresetEditor, game_description: GameDescription, window_manager: WindowManager):
         super().__init__(editor, game_description, window_manager)
+        config = typing.cast(DreadConfiguration, editor.configuration)
+        self._orig_rb_damage_mode: DreadRavenBeakDamageMode = config.consistent_raven_beak_damage_table
         self.setupUi(self)
 
         # Signals
@@ -47,14 +49,23 @@ class PresetDreadPatches(PresetTab, Ui_PresetDreadPatches):
         signal_handling.on_checked(check, persist)
 
     def _on_consistent_raven_beak_damage_table_changed(self, value: bool):
+        checked_value = (
+            DreadRavenBeakDamageMode.DISABLED
+            if self._orig_rb_damage_mode == DreadRavenBeakDamageMode.DISABLED
+            else DreadRavenBeakDamageMode.BUFF_FINAL
+        )
+
         with self._editor as editor:
             editor.set_configuration_field(
                 "consistent_raven_beak_damage_table",
-                DreadRavenBeakDamageMode.BUFF_FINAL if value else DreadRavenBeakDamageMode.NERF_ALL)
+                checked_value if value else DreadRavenBeakDamageMode.NERF_ALL)
 
     def on_preset_changed(self, preset: Preset):
         config = typing.cast(DreadConfiguration, preset.configuration)
+
         for f in _FIELDS:
             typing.cast(QtWidgets.QCheckBox, getattr(self, f"{f}_check")).setChecked(getattr(config, f))
+
+        self._orig_rb_damage_mode = config.consistent_raven_beak_damage_table
         self.consistent_raven_beak_damage_table_check.setChecked(
             not config.consistent_raven_beak_damage_table.is_default)
