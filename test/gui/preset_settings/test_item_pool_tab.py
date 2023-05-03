6e520eea5289f72309254aba3a3679b6ffb1d6db
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-05-03 23:15:52 +0300

    Change calculate_pool_pickup_count to return the count per category

--- test/gui/preset_settings/test_item_pool_tab.py
+++ test/gui/preset_settings/test_item_pool_tab.py
@@ -2,11 +2,13 @@ import dataclasses
 import uuid
 from unittest.mock import MagicMock
 
+import pytest
+
 from randovania.game_description import default_database
-from randovania.games.game import RandovaniaGame
 from randovania.gui.lib.signal_handling import set_combo_with_value
 from randovania.gui.preset_settings.item_pool_tab import PresetItemPool
 from randovania.interface_common.preset_editor import PresetEditor
+from randovania.layout.base.available_locations import RandomizationMode
 from randovania.layout.base.standard_pickup_state import StandardPickupState
 
 
@@ -34,3 +36,37 @@ def test_on_default_item_updated(skip_qtbot, echoes_game_description, preset_man
         num_included_in_starting_pickups=1,
         included_ammo=(50,)
     )
+
+
+@pytest.mark.parametrize("randomization_mode", RandomizationMode)
+def test_item_pool_count_label(skip_qtbot, blank_game_description, preset_manager,
+                               randomization_mode: RandomizationMode):
+    base = preset_manager.default_preset_for_game(blank_game_description.game).get_preset()
+    preset = dataclasses.replace(
+        base,
+        uuid=uuid.UUID('b41fde84-1f57-4b79-8cd6-3e5a78077fa6'),
+        configuration=dataclasses.replace(
+            base.configuration,
+            available_locations=dataclasses.replace(
+                base.configuration.available_locations,
+                randomization_mode=randomization_mode,
+            ),
+            standard_pickup_configuration=dataclasses.replace(
+                base.configuration.standard_pickup_configuration,
+                minimum_random_starting_pickups=1,
+            ),
+        ),
+    )
+    options = MagicMock()
+    editor = PresetEditor(preset, options)
+    window = PresetItemPool(editor, blank_game_description, MagicMock())
+
+    # Run
+    window.on_preset_changed(preset)
+
+    # Assert
+    message = "Items in pool: 5/6"
+    if randomization_mode is RandomizationMode.MAJOR_MINOR_SPLIT:
+        message += "<br />Major: 4/4 - Minor: 1/1 - Starting: 1"
+
+    assert window.item_pool_count_label.text() == message
