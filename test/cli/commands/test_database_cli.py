2e23309817a2761480ed3fbd03cfae7f90d9b73c
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-05-22 19:31:23 +0300

    Split refresh-all, improve tests to not decode/write dbs

--- test/cli/commands/test_database_cli.py
+++ test/cli/commands/test_database_cli.py
@@ -46,34 +46,72 @@ def test_list_paths_with_resource_logic(mock_load_game_description: MagicMock,
     )
 
 
-@pytest.mark.parametrize("check", [False, True])
-def test_refresh_all_logic(check, mocker):
+def test_write_game_descriptions(mocker):
+    from randovania.game_description import pretty_print
+    from randovania.game_description import data_writer
+    mock_write_human_readable_game: MagicMock = mocker.patch.object(pretty_print, "write_human_readable_game")
+    mock_write_game_description: MagicMock = mocker.patch.object(data_writer, "write_game_description")
+    mock_write_as_split_files: MagicMock = mocker.patch.object(data_writer, "write_as_split_files")
+
+    gds = {
+        g: MagicMock()
+        for g in [RandovaniaGame.BLANK, RandovaniaGame.METROID_PRIME_ECHOES]
+    }
+
+    # Run
+    database.write_game_descriptions(gds)
+
+    # Assert
+    mock_write_game_description.assert_has_calls([call(g) for g in gds.values()])
+    mock_write_human_readable_game.assert_has_calls([
+        call(gd, g.data_path.joinpath("json_data")) for g, gd in gds.items()
+    ])
+    mock_write_as_split_files.assert_has_calls([
+        call(mock_write_game_description.return_value, g.data_path.joinpath("json_data")) for g in gds.keys()
+    ])
+
+
+@pytest.mark.parametrize("check", [False])
+def test_refresh_game_description_logic(check, mocker):
     # Setup
     args = MagicMock()
     args.integrity_check = check
 
-    from randovania.game_description import pretty_print
-    from randovania.game_description import data_writer
     from randovania.game_description import integrity_check
     from randovania.game_description import default_database
 
-    mock_write_human_readable_game = mocker.patch.object(pretty_print, "write_human_readable_game")
-    mock_write_as_split_files = mocker.patch.object(data_writer, "write_as_split_files")
     mock_find_database_errors = mocker.patch.object(integrity_check, "find_database_errors",
                                                     return_value=["An error"])
-    mock_write_pickup_database_for_game = mocker.patch.object(default_database, "write_pickup_database_for_game")
+
+    mock_game_description_for: MagicMock = mocker.patch.object(default_database, "game_description_for")
+    mock_write_game_descriptions: MagicMock = mocker.patch.object(database, "write_game_descriptions")
 
     # Run
-    database.refresh_all_logic(args)
+    database.refresh_game_description_logic(args)
 
     # Assert
     if check:
-        mock_find_database_errors.assert_has_calls([call(ANY) for _ in RandovaniaGame])
-        mock_write_human_readable_game.assert_not_called()
-        mock_write_as_split_files.assert_not_called()
-        mock_write_pickup_database_for_game.assert_not_called()
+        mock_find_database_errors.assert_has_calls([
+            call(mock_game_description_for.return_value) for _ in RandovaniaGame
+        ])
+        mock_write_game_descriptions.assert_not_called()
     else:
         mock_find_database_errors.assert_not_called()
-        mock_write_human_readable_game.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
-        mock_write_as_split_files.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
-        mock_write_pickup_database_for_game.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
+        mock_write_game_descriptions.assert_called_once_with({
+            g: mock_game_description_for.return_value for g in RandovaniaGame
+        })
+
+
+def test_refresh_pickup_database_logic(mocker):
+    # Setup
+    args = MagicMock()
+
+    from randovania.game_description import default_database
+
+    mock_write_pickup_database_for_game = mocker.patch.object(default_database, "write_pickup_database_for_game")
+
+    # Run
+    database.refresh_pickup_database_logic(args)
+
+    # Assert
+    mock_write_pickup_database_for_game.assert_has_calls([call(ANY, ANY) for _ in RandovaniaGame])
