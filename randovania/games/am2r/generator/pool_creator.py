56a1527b73f3308dd45bc702b15828355cd764b5
Miepee <janbidler00@protonmail.com>
2023-06-08 00:21:36 +0200

    AM2R: Remove unused imports

--- randovania/games/am2r/generator/pool_creator.py
+++ randovania/games/am2r/generator/pool_creator.py
@@ -2,10 +2,6 @@ from random import Random
 
 from randovania.game_description.game_description import GameDescription
 from randovania.game_description.game_patches import GamePatches
-from randovania.game_description.pickup import pickup_category
-from randovania.game_description.resources.location_category import LocationCategory
-from randovania.game_description.resources.pickup_entry import PickupEntry, PickupModel, PickupGeneratorParams
-from randovania.game_description.resources.resource_database import ResourceDatabase
 from randovania.games.am2r.layout.am2r_configuration import AM2RConfiguration
 from randovania.generator.pickup_pool import PoolResults
 from randovania.layout.base.base_configuration import BaseConfiguration
