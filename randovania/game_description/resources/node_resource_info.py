0404246331d09ced0d1281a82e8710f39d809c71
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-06-28 20:00:51 +0300

    Revert "Cache NodeResourceInfo.from_node"
    This reverts commit d2397ef742c72e8b83b7681911b189e60ad65520.

--- randovania/game_description/resources/node_resource_info.py
+++ randovania/game_description/resources/node_resource_info.py
@@ -24,17 +24,12 @@ class NodeResourceInfo:
 
     @classmethod
     def from_node(cls, node: Node, context: NodeContext) -> NodeResourceInfo:
-        result = getattr(node, "cache_node_resource_info", None)
-        if result is None:
-            result = cls(
-                context.database.first_unused_resource_index() + node.node_index,
-                node.identifier,
-                node.name,
-                node.name,
-            )
-            object.__setattr__(node, "cache_node_resource_info", result)
-
-        return result
+        return cls(
+            context.database.first_unused_resource_index() + node.node_index,
+            node.identifier,
+            node.name,
+            node.name,
+        )
 
     @classmethod
     def from_identifier(cls, identifier: NodeIdentifier, context: NodeContext) -> NodeResourceInfo:
