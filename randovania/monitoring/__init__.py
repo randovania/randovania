93d0c177a2d36205a04fb92f10d7aca8222e019e
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-03 14:13:26 +0300

    Disable monitoring for source builds

--- randovania/monitoring/__init__.py
+++ randovania/monitoring/__init__.py
@@ -127,6 +127,10 @@ def _init(include_flask: bool, default_url: str, sampling_rate: float = 0.25, ex
 
 
 def client_init():
+    if not randovania.is_frozen():
+        # TODO: It'd be nice to catch these running from source, but only for unmodified main.
+        return
+
     _init(False, _CLIENT_DEFAULT_URL,
           exclude_server_name=True)
     sentry_sdk.set_tag("frozen", randovania.is_frozen())
