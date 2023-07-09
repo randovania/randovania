c1d60bd20d8ce230c80b6686a28e3daf7d12d9ec
duncathan <dunc@duncathan.com>
2023-07-08 20:54:32 -0600

    fixes echoes remote connector on nintendont

--- test/game_connection/connector/test_echoes_remote_connector.py
+++ test/game_connection/connector/test_echoes_remote_connector.py
@@ -75,7 +75,7 @@ async def test_get_inventory_valid(connector: EchoesRemoteConnector):
         op: struct.pack(">II", item.max_capacity, item.max_capacity)
         for op, item in zip(ops, connector.game.resource_database.item)
     }
-    connector.executor.perform_single_memory_operation.return_value = b"\x00\x00\x00\x00" * 32
+    connector.executor.perform_single_memory_operation.return_value = b"\x00\x00\x00\x00" * 16
     _override = {
         "ObjectCount": 0,
     }
