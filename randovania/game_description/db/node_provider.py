79a64777afa720ce94deeed086c20d6f0ede1294
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-07-11 17:24:27 +0300

    Add some of ruff's flake8-type-checking rules

--- randovania/game_description/db/node_provider.py
+++ randovania/game_description/db/node_provider.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 from collections.abc import Iterator
 from typing import TYPE_CHECKING
 
+from randovania.game_description.db.area_identifier import AreaIdentifier
+
 if TYPE_CHECKING:
     from randovania.game_description.db.area import Area
-    from randovania.game_description.db.area_identifier import AreaIdentifier
     from randovania.game_description.db.dock import DockWeakness
     from randovania.game_description.db.node import Node
     from randovania.game_description.db.node_identifier import NodeIdentifier
