ba9dc80ad3b45b5ed119b132ffcb7f2a847de3fe
Miepee <janbidler00@protonmail.com>
2023-07-23 18:16:12 +0200

    AM2R: Add interaction with patcher

--- randovania/games/am2r/exporter/game_exporter.py
+++ randovania/games/am2r/exporter/game_exporter.py
@@ -1,8 +1,16 @@
 from __future__ import annotations
 
 import dataclasses
+import json
+import os
+import platform
+import shutil
+import subprocess
+import sys
+import tempfile
 from typing import TYPE_CHECKING
 
+from randovania import get_data_path
 from randovania.exporter.game_exporter import GameExporter, GameExportParams
 
 if TYPE_CHECKING:
@@ -37,21 +45,75 @@ class AM2RGameExporter(GameExporter):
     def _do_export_game(self, patch_data: dict, export_params: AM2RGameExportParams,
                         progress_update: status_update_lib.ProgressUpdateCallable):
         pass
-        # TODO: WIP, needs to implement a bunch of prep work. current implementation is for testing purposes only.
-        # yams_path = os.fspath(get_data_path().joinpath("yams"))
-        # sys.path.append(yams_path)
+        # TODO: This looks *ugly* but I have no idea how to do it better. If I try to put this at top of the file,
+        # then ruff complains.
+        # Pythonnet needs the assembly folder to be added to PATH. AFAIK it must also be located in PATH before
+        # attempting to load the CLR
+        if "YAMS_LIB" not in sys.modules:
+            yams_path = os.fspath(get_data_path().joinpath("yams"))
+            sys.path.append(yams_path)
+            from pythonnet import load
+            # Don't try to load this multiple times
+            load("coreclr")
+            import clr
+            clr.AddReference("YAMS-LIB")
+        from YAMS_LIB import Patcher
 
-        # from pythonnet import load
+        # Copy to input dir to temp dir first to do operations there
+        progress_update("Copying to temporary path...", -1)
+        tempdir = tempfile.TemporaryDirectory()
+        shutil.copytree(export_params.input_path, tempdir.name, dirs_exist_ok=True)
 
-        # load("coreclr")
-        # import clr
+        # Get data.win path. Both of these *need* to be strings, as otherwise patcher won't accept them.
+        output_data_win_path: str = os.fspath(self._get_data_win_path(tempdir.name))
+        input_data_win_path: str = shutil.move(output_data_win_path, output_data_win_path + "_orig")
 
-        # clr.AddReference("YAMS-LIB")
-        # from YAMS_LIB import Patcher
-        # input_data_win_path = os.fspath(export_params.input_path.joinpath("assets", "game.unx_older"))
-        # output_data_win_path = os.fspath(export_params.output_path.joinpath("assets", "game.unx"))
-        # json_file = tempfile.NamedTemporaryFile(mode='w+', delete=False)
-        # json_file.write(json.dumps(patch_data))
-        # json_file.close()
+        # Temp write patch_data into json file for yams later
+        progress_update("Creating json file...", -1)
+        json_file = tempfile.NamedTemporaryFile(mode='w+', delete=False)
+        json_file.write(json.dumps(patch_data))
+        json_file.close()
 
-        # Patcher.Main(input_data_win_path, output_data_win_path, json_file.name)
+        # AM2RLauncher installations usually have a profile.xml file. For less confusion, remove it if it exists
+        if Path.exists(tempdir.joinpath("profile.xml")):
+            Path.unlink(tempdir.joinpath("profile.xml"))
+
+        # TODO: this is where we'd do some customization options like music shuffler or samus palettes
+
+        # Patch data.win
+        progress_update("Patching data file...", -1)
+        Patcher.Main(input_data_win_path, output_data_win_path, json_file.name)
+
+        # Move temp dir to output dir and get rid of it. Also delete original data.win
+        Path.unlink(input_data_win_path)
+        progress_update("Moving to output directory...", -1)
+        shutil.copytree(tempdir.name, export_params.output_path, dirs_exist_ok=True)
+        shutil.rmtree(tempdir.name)
+
+    def _get_data_win_path(self, folder: str) -> Path:
+        current_platform = platform.system()
+        if current_platform == "Windows":
+            return Path(folder) / "data.win"
+
+        elif current_platform == "Linux":
+            # Linux can have the game packed in an AppImage. If it exists, extract it first
+            # Also extraction for some reason only does it into CWD, so we temporarily change it
+            appimage = Path(folder) / "AM2R.AppImage"
+            if Path.exists(appimage):
+                cwd = Path.cwd()
+                os.chdir(folder)
+                subprocess.run([appimage, "--appimage-extract"])
+                os.chdir(cwd)
+                Path.unlink(appimage)
+                # shutil doesn't support moving a directory like this, so I copy + delete
+                shutil.copytree(Path(folder) / "squashfs-root", folder, dirs_exist_ok=True)
+                shutil.rmtree(Path(folder) / "squashfs-root")
+                return Path(folder) / "usr" / "bin" / "assets" / "game.unx"
+            else:
+                return Path(folder) / "assets" / "game.unx"
+
+        elif current_platform == "Darwin":
+            return Path(folder) / "AM2R.app" / "Contents" / "Resources" / "game.ios"
+
+        else:
+            raise ValueError(f"Unknown system: {platform.system()}")
