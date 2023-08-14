7610f96520325d17a9664bc232ab8eec5c69bc16
dyceron <38679103+dyceron@users.noreply.github.com>
2023-08-14 00:31:23 -0400

    More Prep/Improvements for patcher prep

--- randovania/games/samus_returns/exporter/patch_data_factory.py
+++ randovania/games/samus_returns/exporter/patch_data_factory.py
@@ -5,15 +5,28 @@ from typing import TYPE_CHECKING
 from randovania.exporter import pickup_exporter
 from randovania.exporter.patch_data_factory import BasePatchDataFactory
 from randovania.game_description.assignment import PickupTarget
+from randovania.game_description.resources.pickup_entry import PickupModel
 from randovania.games.game import RandovaniaGame
 from randovania.generator.pickup_pool import pickup_creator
 
 if TYPE_CHECKING:
+    from collections.abc import Iterator
+
     from randovania.exporter.pickup_exporter import ExportedPickupDetails
     from randovania.game_description.db.node import Node
     from randovania.game_description.db.node_identifier import NodeIdentifier
     from randovania.game_description.resources.item_resource_info import ItemResourceInfo
+    from randovania.game_description.resources.pickup_entry import ConditionalResources, PickupEntry
     from randovania.game_description.resources.resource_info import ResourceCollection
+
+_ALTERNATIVE_MODELS = {
+    PickupModel(RandovaniaGame.METROID_SAMUS_RETURNS, "Nothing"): ["itemsphere"],
+
+    PickupModel(RandovaniaGame.METROID_SAMUS_RETURNS, "PROGRESSIVE_BEAM"): [
+        "powerup_wavebeam", "powerup_spazerbeam", "powerup_plasmabeam"],
+
+    PickupModel(RandovaniaGame.METROID_SAMUS_RETURNS, "PROGRESSIVE_SUIT"): ["powerup_variasuit", "powerup_gravitysuit"],
+}
 
 
 def get_item_id_for_item(item: ItemResourceInfo) -> str:
@@ -23,6 +36,40 @@ def get_item_id_for_item(item: ItemResourceInfo) -> str:
         return item.extra["item_id"]
     except KeyError as e:
         raise KeyError(f"{item.long_name} has no item ID.") from e
+
+
+def convert_conditional_resource(res: ConditionalResources) -> Iterator[dict]:
+    if not res.resources:
+        yield {"item_id": "ITEM_NONE", "quantity": 0}
+        return
+
+    for resource in reversed(res.resources):
+        item_id = get_item_id_for_item(resource[0])
+        quantity = resource[1]
+
+        yield {"item_id": item_id, "quantity": quantity}
+
+
+def get_resources_for_details(pickup: PickupEntry, conditional_resources: list[ConditionalResources],
+                              other_player: bool) -> list[list[dict]]:
+    resources = [
+        list(convert_conditional_resource(conditional_resource))
+        for conditional_resource in conditional_resources
+    ]
+
+    # don't add more resources for multiworld items
+    if other_player:
+        return resources
+
+    if pickup.resource_lock is not None and not pickup.respects_lock and not pickup.unlocks_resource:
+        # Add the lock resource into the pickup in addition to the expansion's resources
+        assert len(resources) == 1
+        resources[0].append({
+            "item_id": get_item_id_for_item(pickup.resource_lock.locked_by),
+            "quantity": 1,
+        })
+
+    return resources
 
 
 class MSRPatchDataFactory(BasePatchDataFactory):
@@ -58,7 +105,7 @@ class MSRPatchDataFactory(BasePatchDataFactory):
         else:
             return {}
 
-    def _pickup_detail_for_target(self, detail: ExportedPickupDetails) -> dict | None:
+    def d_pickup_detail_for_target(self, detail: ExportedPickupDetails) -> dict | None:
 
         pickup_node = self.game.region_list.node_from_pickup_index(detail.index)
         pickup_type = pickup_node.extra.get("pickup_type", "actor")
@@ -74,7 +121,102 @@ class MSRPatchDataFactory(BasePatchDataFactory):
 
         return details
 
-    def _node_for(self, identifier:  NodeIdentifier) -> Node:
+    def _key_error_for_node(self, node: Node, err: KeyError):
+        return KeyError(f"{self.game.region_list.node_name(node, with_region=True)} has no extra {err}")
+
+    def _level_name_for(self, node: Node) -> str:
+        region = self.game.region_list.nodes_to_region(node)
+        return region.extra["scenario_id"]
+
+    def _teleporter_ref_for(self, node: Node, actor_key: str = "actor_name") -> dict:
+        try:
+            return {
+                "scenario": self._level_name_for(node),
+                # "layer": node.extra.get("actor_layer", "default"),
+                "actor": node.extra[actor_key],
+            }
+        except KeyError as e:
+            return {}
+            raise self._key_error_for_node(node, e)
+
+    def _callback_ref_for(self, node: Node) -> dict:
+        try:
+            return {
+                "scenario": self._level_name_for(node),
+                "function": node.extra["callback_function"],
+                "args": node.extra.get("callback_args", 0)
+            }
+        except KeyError as e:
+            return {}
+            raise KeyError(f"{node} has no extra {e}")
+
+    def _pickup_detail_for_target(self, detail: ExportedPickupDetails) -> dict | None:
+        alt_model = _ALTERNATIVE_MODELS.get(detail.model, [detail.model.name])
+        model_names = alt_model
+
+        if detail.other_player:
+            if model_names == ["offworld"]:
+                base_icon = "unknown"
+                model_names = ["itemsphere"]
+            else:
+                base_icon = detail.model.name
+
+            map_icon = {
+                "custom_icon": {
+                    "label": detail.name.upper(),
+                    "base_icon": base_icon
+                }
+            }
+        elif alt_model[0] == "itemsphere":
+            map_icon = {
+                "custom_icon": {
+                    "label": detail.original_pickup.name.upper(),
+                }
+            }
+        else:
+            map_icon = {
+                "icon_id": detail.model.name
+            }
+
+        resources = get_resources_for_details(detail.original_pickup, detail.conditional_resources, detail.other_player)
+
+        pickup_node = self.game.region_list.node_from_pickup_index(detail.index)
+        pickup_type = pickup_node.extra.get("pickup_type", "actor")
+
+        hud_text = detail.collection_text[0]
+        if len(set(detail.collection_text)) > 1:
+            hud_text = self.memo_data[detail.original_pickup.name]
+
+        details = {
+            # "pickup_type": pickup_type,
+            "caption": hud_text,
+            "item_id": resources[0][0]["item_id"],
+            "quantity": resources[0][0]["quantity"],
+        }
+
+        if pickup_type == "actor":
+            pickup_actor = self._teleporter_ref_for(pickup_node)
+
+            if "map_icon_actor" in pickup_node.extra:
+                map_icon.update({
+                    "original_actor": self._teleporter_ref_for(pickup_node, "map_icon_actor")
+                })
+            details.update({
+                "pickup_actor": pickup_actor,
+                "model": model_names[0],
+                # "map_icon": map_icon,
+            })
+        else:
+            details["pickup_lua_callback"] = self._callback_ref_for(pickup_node)
+            if pickup_type != "cutscene":
+                details.update({
+                    "pickup_actordef": pickup_node.extra["actor_def"],
+                    "pickup_string_key": pickup_node.extra["string_key"],
+                })
+
+        return details
+
+    def _node_for(self, identifier: NodeIdentifier) -> Node:
         return self.game.region_list.node_by_identifier(identifier)
 
     def create_data(self) -> dict:
--- randovania/games/samus_returns/gui/dialog/game_export_dialog.py
+++ randovania/games/samus_returns/gui/dialog/game_export_dialog.py
@@ -1,15 +1,24 @@
 from __future__ import annotations
 
+import json
 from typing import TYPE_CHECKING
 
+from PySide6.QtCore import QCoreApplication
+
 from randovania.gui.dialog.game_export_dialog import GameExportDialog
+from randovania.gui.generated.msr_game_export_dialog_ui import Ui_MSRGameExportDialog
 
 if TYPE_CHECKING:
     from randovania.exporter.game_exporter import GameExportParams
-    from randovania.interface_common.options import PerGameOptions
+    from randovania.games.game import RandovaniaGame
+    from randovania.interface_common.options import Options, PerGameOptions
 
 
-class MSRGameExportDialog(GameExportDialog):
+class MSRGameExportDialog(GameExportDialog, Ui_MSRGameExportDialog):
+    def __init__(self, options: Options, patch_data: dict, word_hash: str, spoiler: bool, games: list[RandovaniaGame]):
+        super().__init__(options, patch_data, word_hash, spoiler, games)
+        self.json_label.setText(QCoreApplication.translate("json_label", f"{json.dumps(patch_data, indent=2)}", None))
+
     def update_per_game_options(self, per_game: PerGameOptions) -> PerGameOptions:
         raise NotImplementedError
 
