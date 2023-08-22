59a6bcb31afdfe51238a5e36050e9d784c449aa1
Thanatos <thanatosstr89@gmail.com>
2023-08-22 22:08:20 +0200

    Fix wrong imports

--- randovania/games/dread/gui/dread_teleporter_details_tab.py
+++ randovania/games/dread/gui/dread_teleporter_details_tab.py
@@ -6,15 +6,15 @@ from randovania.games.dread.layout.dread_configuration import DreadConfiguration
 from randovania.gui.game_details.teleporter_details_tab import TeleporterDetailsTab
 
 if TYPE_CHECKING:
-    from logging.config import BaseConfigurator
 
     from randovania.game_description.game_patches import GamePatches
     from randovania.interface_common.players_configuration import PlayersConfiguration
+    from randovania.layout.base.base_configuration import BaseConfiguration
 
 
 class DreadTeleporterDetailsTab(TeleporterDetailsTab):
     @classmethod
-    def should_appear_for(cls, configuration: BaseConfigurator, all_patches: dict[int, GamePatches],
+    def should_appear_for(cls, configuration: BaseConfiguration, all_patches: dict[int, GamePatches],
                           players: PlayersConfiguration) -> bool:
         assert isinstance(configuration, DreadConfiguration)
         return not configuration.teleporters.is_vanilla
