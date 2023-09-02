d4b9cc8eaedbd162a786251dbd86a166a58b866a
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2023-09-02 22:21:04 +0300

    Move render-region-graph to development command

--- randovania/cli/development.py
+++ randovania/cli/development.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 from randovania.cli.commands.new_game import add_create_databases, add_new_game_command
 from randovania.cli.commands.refresh_presets import add_refresh_presets_command
+from randovania.cli.commands.render_regions import render_regions_graph
 
 if TYPE_CHECKING:
     from argparse import ArgumentParser
@@ -20,6 +21,7 @@ def create_subparsers(sub_parsers):
     add_refresh_presets_command(sub_parsers)
     add_new_game_command(sub_parsers)
     add_create_databases(sub_parsers)
+    render_regions_graph(sub_parsers)
 
     def check_command(args):
         if args.command is None:
