6153e441f7b685d85f937ab505ec66dd0e133080
UltiNaruto <zenaruto@ymail.com>
2023-09-04 13:55:36 +0200

    Planets (Zebeth) DB: First version

---
+++ randovania/games/metroid/generator/pool_creator.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from randovania.games.metroid.layout.metroid_configuration import MetroidConfiguration
+
+if TYPE_CHECKING:
+    from randovania.game_description.game_description import GameDescription
+    from randovania.generator.pickup_pool import PoolResults
+    from randovania.layout.base.base_configuration import BaseConfiguration
+
+
+def pool_creator(results: PoolResults, configuration: BaseConfiguration, game: GameDescription) -> None:
+    assert isinstance(configuration, MetroidConfiguration)
+
+    pass
---
+++ randovania/games/metroid/gui/dialog/game_export_dialog.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+import dataclasses
+from typing import TYPE_CHECKING
+
+from randovania.games.game import RandovaniaGame
+from randovania.games.metroid.exporter.game_exporter import MetroidGameExportParams
+from randovania.gui.dialog.game_export_dialog import GameExportDialog, spoiler_path_for
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from randovania.games.metroid.exporter.options import MetroidPerGameOptions
+
+
+class MetroidGameExportDialog(GameExportDialog):
+    """A window for asking the user for what is needed to export this specific game.
+
+    The provided implementation assumes you need an ISO/ROM file, and produces a new ISO/ROM file."""
+
+    @classmethod
+    def game_enum(cls):
+        return RandovaniaGame.METROID
+
+    @property
+    def input_file(self) -> Path:
+        raise NotImplementedError("This method hasn't been implemented yet")
+
+    @property
+    def output_file(self) -> Path:
+        raise NotImplementedError("This method hasn't been implemented yet")
+
+    @property
+    def auto_save_spoiler(self) -> bool:
+        raise NotImplementedError("This method hasn't been implemented yet")
+
+    def update_per_game_options(self, per_game: MetroidPerGameOptions) -> MetroidPerGameOptions:
+        return dataclasses.replace(
+            per_game,
+            input_path=self.input_file,
+            output_path=self.output_file,
+        )
+
+    def get_game_export_params(self) -> MetroidGameExportParams:
+        """Creates the GameExportParams for this specific game,
+        based on the data provided by the user in this window."""
+
+        spoiler_output = spoiler_path_for(self.auto_save_spoiler, self.output_file)
+
+        return MetroidGameExportParams(
+            spoiler_output=spoiler_output,
+            input_path=self.input_file,
+            output_path=self.output_file,
+        )
