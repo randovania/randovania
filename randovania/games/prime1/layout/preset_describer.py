25dc8975e05ea33f61838b18e06e9050474b0d36
toasterparty <toasterparty@derpymail.org>
2023-09-07 08:53:57 -0700

    don't describe default preset

--- randovania/games/prime1/layout/preset_describer.py
+++ randovania/games/prime1/layout/preset_describer.py
@@ -17,9 +17,9 @@ _PRIME1_CUTSCENE_MODE_DESCRIPTION = {
     LayoutCutsceneMode.MAJOR: "Major cutscene removal",
     LayoutCutsceneMode.MINOR: "Minor cutscene removal",
     LayoutCutsceneMode.COMPETITIVE: "Competitive cutscene removal",
-    LayoutCutsceneMode.SKIPPABLE: "Skippable cutscenes",
+    LayoutCutsceneMode.SKIPPABLE: None,
     LayoutCutsceneMode.SKIPPABLE_COMPETITIVE: "Competitive cutscenes",
-    LayoutCutsceneMode.ORIGINAL: None,
+    LayoutCutsceneMode.ORIGINAL: "Original cutscenes",
 }
 
 _PRIME1_PHAZON_SUIT_HINT = {
