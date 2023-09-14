47a141ae716e5ab22a32708111c6ac7bb5b7abdf
dyceron <38679103+dyceron@users.noreply.github.com>
2023-09-13 20:33:02 -0400

    More patcher support

--- randovania/games/samus_returns/gui/preset_settings/__init__.py
+++ randovania/games/samus_returns/gui/preset_settings/__init__.py
@@ -7,11 +7,10 @@ if TYPE_CHECKING:
     from randovania.interface_common.preset_editor import PresetEditor
 
 
-def preset_tabs(editor: PresetEditor, window_manager: WindowManager):
+def msr_preset_tabs(editor: PresetEditor, window_manager: WindowManager):
     from randovania.games.samus_returns.gui.preset_settings.msr_patches_tab import PresetMSRPatches
-    from randovania.gui.preset_settings.dock_rando_tab import PresetDockRando
-    from randovania.gui.preset_settings.item_pool_tab import PresetItemPool
     from randovania.gui.preset_settings.location_pool_tab import PresetLocationPool
+    from randovania.gui.preset_settings.metroid_item_pool_tab import MetroidPresetItemPool
     from randovania.gui.preset_settings.starting_area_tab import PresetMetroidStartingArea
     from randovania.gui.preset_settings.trick_level_tab import PresetTrickLevel
 
@@ -24,7 +23,6 @@ def preset_tabs(editor: PresetEditor, window_manager: WindowManager):
         PresetTrickLevel,
         PresetMetroidStartingArea,
         PresetLocationPool,
-        PresetItemPool,
+        MetroidPresetItemPool,
         PresetMSRPatches,
-        PresetDockRando,
     ]
