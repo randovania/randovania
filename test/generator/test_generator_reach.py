30a191fb7f3548719f3234c0b30c01823d5ca8e9
XenoWars <43147270+XenoWars@users.noreply.github.com>
2023-09-20 17:26:39 -0400

    Update test_generator_reach.py

--- test/generator/test_generator_reach.py
+++ test/generator/test_generator_reach.py
@@ -128,7 +128,7 @@ _include_tricks_for_game = {
     # Some items require shinesparking to reach in vanilla, which due to varying difficulty has been made into a trick
     RandovaniaGame.AM2R: {("Shinesparking", LayoutTrickLevel.ADVANCED)},
     # Same reasons as above, with some still trickless
-    RandovaniaGame.DREAD: {("Speedbooster", LayoutTrickLevel.BEGINNER)},
+    RandovaniaGame.METROID_DREAD: {("Speedbooster", LayoutTrickLevel.BEGINNER)},
     # Same reasons as above
     RandovaniaGame.SUPER_METROID: {("Shinespark", LayoutTrickLevel.BEGINNER)},
 }
