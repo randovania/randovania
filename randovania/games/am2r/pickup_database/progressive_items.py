11358946d50d90e44fd8ab4d052dda1665c8fe71
Miepee <janbidler00@protonmail.com>
2023-09-26 17:27:16 +0200

    Fix ProgressiveItemTuples

--- randovania/games/am2r/pickup_database/progressive_items.py
+++ randovania/games/am2r/pickup_database/progressive_items.py
@@ -3,10 +3,10 @@ from __future__ import annotations
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
-    from randovania.games.game import Progressive_item_tuples
+    from randovania.games.game import ProgressiveItemTuples
 
 
-def tuples() -> Progressive_item_tuples:
+def tuples() -> ProgressiveItemTuples:
     return [
         ("Progressive Suit", ("Varia Suit", "Gravity Suit")),
         ("Progressive Jump", ("Hi-Jump Boots", "Space Jump")),
