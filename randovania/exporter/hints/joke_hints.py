eb8ef9dd5a130fe946a23ba067003d13934d12bf
LatiusAuro <twiliteeveeauro@gmail.com>
2023-10-03 18:24:42 +0100

    Update joke_hints.py
    Added two extra joke hints - one which claims to offer the Metroid Suit if you reach somewhere (which has been redacted) without taking any damage, and one playing on how "ADAM" uses the 'But He Sounds Hamsome' trope towards Raven Beak.

--- randovania/exporter/hints/joke_hints.py
+++ randovania/exporter/hints/joke_hints.py
@@ -29,4 +29,6 @@ JOKE_HINTS = [
     "We now pledge sole allegiance to our liberator, the great one, Dark Samus.",
     "Defeat enemies with the Light Beam to earn Dark Ammo. Defeat enemies with the Dark Beam to earn Light Ammo.",
     "Your Friend Roster is currently empty.",
+    "It is said that reaching <Redacted> without taking any damage will unlock the Metroid Suit.",
+    "Raven Beak? I dont know him, but he sounds hamsome, wouldn't you agree Samus?",
 ]
