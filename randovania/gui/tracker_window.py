6e4093019bc45a7f77af89c1aee47a4eae55d887
Thanatos <thanatosstr89@gmail.com>
2023-10-10 08:30:37 +0200

    Fix map tracker with a single non-default starting location

--- randovania/gui/tracker_window.py
+++ randovania/gui/tracker_window.py
@@ -624,10 +624,14 @@ class TrackerWindow(QtWidgets.QMainWindow, Ui_TrackerWindow):
                     self, "Starting Location", "Select starting location", location_names, 0, False
                 )
                 node_location = node_locations[location_names.index(selected_name[0])]
+        elif len(self.game_configuration.starting_location.locations) == 1:
+            if node_location is None:
+                node_location = self.game_configuration.starting_location.locations[0]
+        else:
+            raise ValueError("Preset without a starting location.")
 
-            # TODO If there is no `default_node` anymore, what would be the replacement?
-            node = region_list.node_by_identifier(node_location)
-            self._initial_state.node = region_list.nodes_to_area(node).get_start_nodes()[0]
+        node = region_list.node_by_identifier(node_location)
+        self._initial_state.node = node
 
         def is_resource_node_present(node: Node, state: State):
             if node.is_resource_node:
