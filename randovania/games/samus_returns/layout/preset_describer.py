20bf5a9bb11d4688a5a718ec4d549530b1a63d59
dyceron <dycerontv@gmail.com>
2024-01-07 09:15:25 -0500

    Rename Nerf Supers for more clarity
    Also update UI

--- randovania/games/samus_returns/layout/preset_describer.py
+++ randovania/games/samus_returns/layout/preset_describer.py
@@ -104,14 +104,18 @@ class MSRPresetDescriber(GamePresetDescriber):
                 {
                     "Charge Door Buff": configuration.charge_door_buff,
                     "Beam Door Buff": configuration.beam_door_buff,
-                    "Nerfed Super Missiles": configuration.nerf_super_missiles,
+                    "Missile Door Buff": configuration.nerf_super_missiles,
                 },
                 {
                     "Open Area 3 Interior East Shortcut": configuration.area3_interior_shortcut_no_grapple,
-                    "Remove Area Exit Path Grapple Blocks": configuration.elevator_grapple_blocks,
-                    "Remove Surface Scan Pulse Crumble Blocks": configuration.surface_crumbles,
-                    "Remove Area 1 Chozo Seal Crumble Blocks": configuration.area1_crumbles,
-                    "Enabled Reverse Area 8": configuration.reverse_area8,
+                    "Remove Area Exit Grapple Blocks": configuration.elevator_grapple_blocks,
+                },
+                {
+                    "Change Surface Scan Pulse Crumble Blocks": configuration.surface_crumbles,
+                    "Change Area 1 Chozo Seal Crumble Blocks": configuration.area1_crumbles,
+                },
+                {
+                    "Enable Reverse Area 8": configuration.reverse_area8,
                 },
             ],
         }
