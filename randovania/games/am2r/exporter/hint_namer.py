5c2346dab3ebacdc0443c4a7276f71a50505a8e5
Miepee <janbidler00@protonmail.com>
2024-01-07 01:39:10 +0100

    AM2R: Hack around the fact that am2r does not support colored spoilers

--- randovania/games/am2r/exporter/hint_namer.py
+++ randovania/games/am2r/exporter/hint_namer.py
@@ -35,7 +35,8 @@ def _area_name(region_list: RegionList, pickup_node: PickupNode, hide_region: bo
 
 
 def colorize_text(color: AM2RColor, text: str, with_color: bool) -> str:
-    if with_color:
+    # FIXME: This is bad. We should instead change generic_credits, to allow coloring item+player
+    if with_color and False:
         return f"{color.value}{text}{AM2RColor.WHITE.value}"
     else:
         return text
