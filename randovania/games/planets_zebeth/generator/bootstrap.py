368ffaa8ad130ac1886ad479008e88866a1b45c5
UltiNaruto <zenaruto@ymail.com>
2024-01-11 12:00:28 +0100

    Planets (Zebeth): Added todo for bootstrap

--- randovania/games/planets_zebeth/generator/bootstrap.py
+++ randovania/games/planets_zebeth/generator/bootstrap.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 from randovania.resolver.bootstrap import Bootstrap
 
+# TODO: look at this later and figure out what to add here
+
 
 class PlanetsZebethBootstrap(Bootstrap):
     pass
