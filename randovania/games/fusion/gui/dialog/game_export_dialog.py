8646fce58fb9d58e0946c7c82c28d46c49a183ed
JeffGainsNGames <jeffgainsngames@gmail.com>
2024-02-05 07:47:01 -0500

    Add fusion files

---
+++ randovania/games/fusion/gui/dialog/game_export_dialog.py
@@ -0,0 +1,56 @@
+from __future__ import annotations
+
+import dataclasses
+from typing import TYPE_CHECKING
+
+from randovania.games.fusion.exporter.game_exporter import FusionGameExportParams
+from randovania.games.fusion.exporter.options import FusionPerGameOptions
+from randovania.games.game import RandovaniaGame
+from randovania.gui.dialog.game_export_dialog import GameExportDialog, spoiler_path_for
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from randovania.interface_common.options import PerGameOptions
+
+
+class FusionGameExportDialog(GameExportDialog):
+    """A window for asking the user for what is needed to export this specific game.
+
+    The provided implementation assumes you need an ISO/ROM file, and produces a new ISO/ROM file."""
+
+    @classmethod
+    def game_enum(cls) -> RandovaniaGame:
+        return RandovaniaGame.FUSION
+
+    @property
+    def input_file(self) -> Path:
+        raise NotImplementedError("This method hasn't been implemented yet")
+
+    @property
+    def output_file(self) -> Path:
+        raise NotImplementedError("This method hasn't been implemented yet")
+
+    @property
+    def auto_save_spoiler(self) -> bool:
+        raise NotImplementedError("This method hasn't been implemented yet")
+
+    def update_per_game_options(self, per_game: PerGameOptions) -> FusionPerGameOptions:
+        assert isinstance(per_game, FusionPerGameOptions)
+        return dataclasses.replace(
+            per_game,
+            input_path=self.input_file,
+            output_path=self.output_file,
+        )
+
+    def get_game_export_params(self) -> FusionGameExportParams:
+        """Creates the GameExportParams for this specific game,
+        based on the data provided by the user in this window."""
+
+        spoiler_output = spoiler_path_for(self.auto_save_spoiler, self.output_file)
+
+        return FusionGameExportParams(
+            spoiler_output=spoiler_output,
+            input_path=self.input_file,
+            output_path=self.output_file,
+        )
---
+++ randovania/games/fusion/layout/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from randovania.games.fusion.layout.blank_configuration import FusionConfiguration
+from randovania.games.fusion.layout.blank_cosmetic_patches import FusionCosmeticPatches
+
+__all__ = [
+    "FusionConfiguration",
+    "FusionCosmeticPatches",
+]
