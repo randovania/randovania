553d84fa34275810f9f4142207bf8486b1aca425
JeffGainsNGames <jeffgainsngames@gmail.com>
2024-03-08 17:12:40 -0500

    Add fusion goal and basic tests

--- randovania/games/fusion/generator/bootstrap.py
+++ randovania/games/fusion/generator/bootstrap.py
@@ -1,8 +1,71 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from randovania.game_description.db.pickup_node import PickupNode
+from randovania.games.fusion.generator.pool_creator import INFANT_METROID_CATEGORY
+from randovania.games.fusion.layout import FusionConfiguration
+from randovania.layout.exceptions import InvalidConfiguration
 from randovania.resolver.bootstrap import MetroidBootstrap
+
+if TYPE_CHECKING:
+    from random import Random
+
+    from randovania.game_description.game_description import GameDescription
+    from randovania.game_description.game_patches import GamePatches
+    from randovania.games.fusion.layout.fusion_configuration import FusionArtifactConfig
+    from randovania.generator.pickup_pool import PoolResults
+
+
+def all_metroid_locations(game: GameDescription, config: FusionArtifactConfig):
+    locations = []
+    for node in game.region_list.all_nodes:
+        if isinstance(node, PickupNode):
+            name = node.pickup_index
+            # Pickups guarded by bosses
+            if config.prefer_bosses and str(name) in _boss_items:
+                locations.append(node)
+
+    return locations
 
 
 class FusionBootstrap(MetroidBootstrap):
-    pass
-    # TODO
+    def assign_pool_results(self, rng: Random, patches: GamePatches, pool_results: PoolResults) -> GamePatches:
+        assert isinstance(patches.configuration, FusionConfiguration)
+        config = patches.configuration.artifacts
+
+        if config.prefer_anywhere:
+            return super().assign_pool_results(rng, patches, pool_results)
+
+        locations = all_metroid_locations(patches.game, config)
+        rng.shuffle(locations)
+
+        metroid_to_assign = [
+            pickup for pickup in list(pool_results.to_place) if pickup.pickup_category is INFANT_METROID_CATEGORY
+        ]
+
+        if len(metroid_to_assign) > len(locations):
+            raise InvalidConfiguration(
+                f"Has {len(metroid_to_assign)} Infant Metroids in the pool, but only {len(locations)} valid locations."
+            )
+
+        for metroid, location in zip(metroid_to_assign, locations, strict=False):
+            pool_results.to_place.remove(metroid)
+            pool_results.assignment[location.pickup_index] = metroid
+
+        return super().assign_pool_results(rng, patches, pool_results)
+
+
+_boss_items = [
+    "PickupIndex 100",
+    "PickupIndex 106",
+    "PickupIndex 114",
+    "PickupIndex 104",
+    "PickupIndex 115",
+    "PickupIndex 107",
+    "PickupIndex 110",
+    "PickupIndex 102",
+    "PickupIndex 109",
+    "PickupIndex 108",
+    "PickupIndex 111",
+]
---
+++ test/games/fusion/test_fusion.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from randovania.game_description import default_database
+from randovania.game_description.requirements.requirement_template import RequirementTemplate
+from randovania.game_description.requirements.resource_requirement import ResourceRequirement
+from randovania.games.game import RandovaniaGame
+
+resource_list = [
+    # Tricks
+    ("Jump Bombjump", "Can Use Bombs"),
+    ("Mid-Air Morph", "Morph Ball"),
+    ("Shinesparking Tricks", "SpeedBooster"),
+    ("Stand On Frozen Enemies", "Can Freeze Enemies"),
+    # Items
+    ("Morph Ball Bomb Data", "Morph Ball"),
+    ("Power Bomb Data", "Morph Ball"),
+]
+
+
+def does_requirement_contain_resource(requirement, resource, db):
+    if isinstance(requirement, RequirementTemplate):
+        if requirement.template_name == resource:
+            return True
+        template_req = requirement.template_requirement(db)
+        return does_requirement_contain_resource(template_req, resource, db)
+    if isinstance(requirement, ResourceRequirement):
+        if requirement.pretty_text == resource:
+            return True
+        return False
+    for subreq in requirement.items:
+        if does_requirement_contain_resource(subreq, resource, db):
+            return True
+    return False
+
+
+def test_all_tricks_should_have_proper_requirements():
+    game = default_database.game_description_for(RandovaniaGame.FUSION)
+    rl = game.region_list
+    db = game.resource_database
+    expected_dict = {}
+    database_dict = {}
+    for i in range(len(resource_list)):
+        database_dict[resource_list[i]] = []
+        expected_dict[resource_list[i]] = []
+
+    for node in rl.all_nodes:
+        for dest_node, requirements in rl.area_connections_from(node):
+            for required_resources in resource_list:
+                size = len(required_resources)
+                required_resource_collection = {}
+                for i in range(size):
+                    required_resource_collection[i] = False
+
+                for i in range(size):
+                    required_resource_collection[i] = does_requirement_contain_resource(
+                        requirements, required_resources[i], db
+                    )
+                    if not required_resource_collection[i]:
+                        break
+
+                if len(set(required_resource_collection.values())) > 1:
+                    database_dict[required_resources].append(
+                        (
+                            f"{node.name} ({node.identifier.area_name})",
+                            f"{dest_node.name} ({dest_node.identifier.area_name})",
+                        )
+                    )
+
+    assert database_dict == expected_dict
