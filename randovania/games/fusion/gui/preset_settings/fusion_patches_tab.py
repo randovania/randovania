6ea71e805f0368e997000cb1fae98c88929eead0
JeffGainsNGames <jeffgainsngames@gmail.com>
2024-03-10 17:12:15 -0400

    Fusion: Add instant transitions to patch options (#6124)
    * Fusion: Add instant transition settings
    
    * remember to track ui file
    
    * Add test for fusion patches
    
    * fix mypy errors in fusion patches
    
    * add missing comma from resolving conflicts
    
    * Test for enabling/disabling all checkboxes
    
    * test now checks that all ui elements are there with findChildren
    
    * Correctly set all checkboxes to enabled instead of just toggling

--- randovania/games/fusion/gui/preset_settings/fusion_patches_tab.py
+++ randovania/games/fusion/gui/preset_settings/fusion_patches_tab.py
@@ -1,41 +1,36 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import typing
 
 from PySide6 import QtWidgets
 
 from randovania.games.fusion.layout import FusionConfiguration
+from randovania.gui.generated.preset_fusion_patches_ui import Ui_PresetFusionPatches
+from randovania.gui.lib import signal_handling
 from randovania.gui.preset_settings.preset_tab import PresetTab
 
-if TYPE_CHECKING:
+if typing.TYPE_CHECKING:
     from randovania.game_description.game_description import GameDescription
     from randovania.gui.lib.window_manager import WindowManager
     from randovania.interface_common.preset_editor import PresetEditor
     from randovania.layout.preset import Preset
 
 
-# TODO
-class PresetFusionPatches(PresetTab):
+_FIELDS = [
+    "instant_transitions",
+]
+
+
+class PresetFusionPatches(PresetTab, Ui_PresetFusionPatches):
     def __init__(self, editor: PresetEditor, game_description: GameDescription, window_manager: WindowManager):
         super().__init__(editor, game_description, window_manager)
-
-        self.root_widget = QtWidgets.QWidget(self)
-        self.root_layout = QtWidgets.QVBoxLayout(self.root_widget)
-
-        self.include_extra_pickups_check = QtWidgets.QCheckBox(self.root_widget)
-        self.include_extra_pickups_check.setEnabled(True)
-        self.include_extra_pickups_check.setText("Include Extra Pickups")
-        self.root_layout.addWidget(self.include_extra_pickups_check)
-
-        self.include_extra_pickups_label = QtWidgets.QLabel(self.root_widget)
-        self.include_extra_pickups_label.setWordWrap(True)
-        self.include_extra_pickups_label.setText("Include some optional pickups.")
-        self.root_layout.addWidget(self.include_extra_pickups_label)
+        self.setupUi(self)
 
         self.setCentralWidget(self.root_widget)
 
         # Signals
-        self.include_extra_pickups_check.stateChanged.connect(self._persist_option_then_notify("include_extra_pickups"))
+        for f in _FIELDS:
+            self._add_persist_option(getattr(self, f"{f}_check"), f)
 
     @classmethod
     def tab_title(cls) -> str:
@@ -45,7 +40,15 @@ class PresetFusionPatches(PresetTab):
     def uses_patches_tab(cls) -> bool:
         return True
 
+    def _add_persist_option(self, check: QtWidgets.QCheckBox, attribute_name: str) -> None:
+        def persist(value: bool) -> None:
+            with self._editor as editor:
+                editor.set_configuration_field(attribute_name, value)
+
+        signal_handling.on_checked(check, persist)
+
     def on_preset_changed(self, preset: Preset) -> None:
         config = preset.configuration
         assert isinstance(config, FusionConfiguration)
-        self.include_extra_pickups_check.setChecked(config.include_extra_pickups)
+        for f in _FIELDS:
+            typing.cast(QtWidgets.QCheckBox, getattr(self, f"{f}_check")).setChecked(getattr(config, f))
