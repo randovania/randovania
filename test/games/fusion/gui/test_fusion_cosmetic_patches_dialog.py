e6cbb1722a02409acc8a0694ff603daa164ccaff
JeffGainsNGames <jeffgainsngames@gmail.com>
2024-03-11 12:22:45 -0400

    Actually track test file..

---
+++ test/games/fusion/gui/test_fusion_cosmetic_patches_dialog.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from PySide6 import QtCore
+
+from randovania.games.fusion.gui.dialog.cosmetic_patches_dialog import FusionCosmeticPatchesDialog
+from randovania.games.fusion.layout.fusion_cosmetic_patches import ColorSpace, FusionCosmeticPatches
+
+
+def test_suit_palette(skip_qtbot):
+    cosmetic_patches = FusionCosmeticPatches(enable_suit_palette=False)
+
+    dialog = FusionCosmeticPatchesDialog(None, cosmetic_patches)
+    skip_qtbot.addWidget(dialog)
+
+    skip_qtbot.mouseClick(dialog.suit_palette_check, QtCore.Qt.MouseButton.LeftButton)
+
+    assert dialog.cosmetic_patches == FusionCosmeticPatches(enable_suit_palette=True)
+
+
+def test_beam_palette(skip_qtbot):
+    cosmetic_patches = FusionCosmeticPatches(enable_beam_palette=False)
+
+    dialog = FusionCosmeticPatchesDialog(None, cosmetic_patches)
+    skip_qtbot.addWidget(dialog)
+
+    skip_qtbot.mouseClick(dialog.beam_palette_check, QtCore.Qt.MouseButton.LeftButton)
+
+    assert dialog.cosmetic_patches == FusionCosmeticPatches(enable_beam_palette=True)
+
+
+def test_enemy_palette(skip_qtbot):
+    cosmetic_patches = FusionCosmeticPatches(enable_enemy_palette=False)
+
+    dialog = FusionCosmeticPatchesDialog(None, cosmetic_patches)
+    skip_qtbot.addWidget(dialog)
+
+    skip_qtbot.mouseClick(dialog.enemy_palette_check, QtCore.Qt.MouseButton.LeftButton)
+
+    assert dialog.cosmetic_patches == FusionCosmeticPatches(enable_enemy_palette=True)
+
+
+def test_tileset_palette(skip_qtbot):
+    cosmetic_patches = FusionCosmeticPatches(enable_tileset_palette=False)
+
+    dialog = FusionCosmeticPatchesDialog(None, cosmetic_patches)
+    skip_qtbot.addWidget(dialog)
+
+    skip_qtbot.mouseClick(dialog.tileset_palette_check, QtCore.Qt.MouseButton.LeftButton)
+
+    assert dialog.cosmetic_patches == FusionCosmeticPatches(enable_tileset_palette=True)
+
+
+def test_color_space(skip_qtbot):
+    cosmetic_patches = FusionCosmeticPatches(color_space=ColorSpace.Oklab)
+
+    dialog = FusionCosmeticPatchesDialog(None, cosmetic_patches)
+    skip_qtbot.addWidget(dialog)
+
+    skip_qtbot.keyClicks(dialog.color_space_combo, "HSV")
+
+    assert dialog.cosmetic_patches == FusionCosmeticPatches(color_space=ColorSpace.HSV)
+
+    skip_qtbot.keyClicks(dialog.color_space_combo, "Oklab")
+
+    assert dialog.cosmetic_patches == FusionCosmeticPatches(color_space=ColorSpace.Oklab)
