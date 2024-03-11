4c731c32d7a1a329f692abcbbd23b11f6d38c6d2
JeffGainsNGames <jeffgainsngames@gmail.com>
2024-03-11 06:55:25 -0400

    Fusion: add palette randomizer

--- randovania/games/fusion/gui/dialog/cosmetic_patches_dialog.py
+++ randovania/games/fusion/gui/dialog/cosmetic_patches_dialog.py
@@ -1,38 +1,52 @@
 from __future__ import annotations
 
+import dataclasses
 from typing import TYPE_CHECKING
 
-from randovania.games.fusion.layout.fusion_cosmetic_patches import FusionCosmeticPatches
+from randovania.games.fusion.layout.fusion_cosmetic_patches import ColorSpace, FusionCosmeticPatches
 from randovania.gui.dialog.base_cosmetic_patches_dialog import BaseCosmeticPatchesDialog
 from randovania.gui.generated.fusion_cosmetic_patches_dialog_ui import Ui_FusionCosmeticPatchesDialog
+from randovania.gui.lib.signal_handling import set_combo_with_value
 
 if TYPE_CHECKING:
-    from PySide6 import QtWidgets
+    from PySide6.QtWidgets import QWidget
 
     from randovania.layout.base.cosmetic_patches import BaseCosmeticPatches
 
 
-# TODO
 class FusionCosmeticPatchesDialog(BaseCosmeticPatchesDialog, Ui_FusionCosmeticPatchesDialog):
     _cosmetic_patches: FusionCosmeticPatches
 
-    def __init__(self, parent: QtWidgets.QWidget | None, current: BaseCosmeticPatches):
+    def __init__(self, parent: QWidget | None, current: BaseCosmeticPatches):
         super().__init__(parent)
         self.setupUi(self)
 
         assert isinstance(current, FusionCosmeticPatches)
         self._cosmetic_patches = current
 
+        for color_space in ColorSpace:
+            self.color_space_combo.addItem(color_space.long_name, color_space)
+
         self.on_new_cosmetic_patches(current)
         self.connect_signals()
 
     def connect_signals(self) -> None:
         super().connect_signals()
-        # More signals here!
+
+        self._persist_check_field(self.suit_palette_check, "enabled")
+        self.color_space_combo.currentIndexChanged.connect(self._on_color_space_update)
+
+    def _on_color_space_update(self) -> None:
+        self._cosmetic_patches = dataclasses.replace(
+            self._cosmetic_patches, color_space=self.color_space_combo.currentData()
+        )
 
     def on_new_cosmetic_patches(self, patches: FusionCosmeticPatches) -> None:
-        # Update fields with the new values
-        pass
+        self.suit_palette_check.setChecked(patches.suit_shuffle.enabled)
+        self.beam_palette_check.setChecked(patches.beam_shuffle.enabled)
+        self.enemy_palette_check.setChecked(patches.enemy_shuffle.enabled)
+        self.tileset_palette_check.setChecked(patches.tileset_shuffle.enabled)
+        set_combo_with_value(self.color_space_combo, patches.color_space)
 
     @property
     def cosmetic_patches(self) -> FusionCosmeticPatches:
--- randovania/games/fusion/layout/fusion_cosmetic_patches.py
+++ randovania/games/fusion/layout/fusion_cosmetic_patches.py
@@ -1,14 +1,53 @@
 from __future__ import annotations
 
 import dataclasses
+from enum import Enum
 
+from randovania.bitpacking.json_dataclass import JsonDataclass
 from randovania.games.game import RandovaniaGame
 from randovania.layout.base.cosmetic_patches import BaseCosmeticPatches
+from randovania.lib import enum_lib
 
 
-# TODO
+class ColorSpace(Enum):
+    """Types of Color Spaces used by the palette shuffler"""
+
+    long_name: str
+
+    Oklab = "Oklab"
+    HSV = "HSV"
+
+
+enum_lib.add_long_name(
+    ColorSpace,
+    {
+        ColorSpace.Oklab: "Oklab",
+        ColorSpace.HSV: "HSV",
+    },
+)
+
+
+@dataclasses.dataclass(frozen=True)
+class PaletteShuffle(JsonDataclass):
+    enabled: bool
+    hue_min: int = dataclasses.field(metadata={"min": 0, "max": 360, "precision": 1})
+    hue_max: int = dataclasses.field(metadata={"min": 0, "max": 360, "precision": 1})
+
+
 @dataclasses.dataclass(frozen=True)
 class FusionCosmeticPatches(BaseCosmeticPatches):
+    suit_shuffle: PaletteShuffle = PaletteShuffle(False, 0, 360)
+    beam_shuffle: PaletteShuffle = PaletteShuffle(False, 0, 360)
+    enemy_shuffle: PaletteShuffle = PaletteShuffle(False, 0, 360)
+    tileset_shuffle: PaletteShuffle = PaletteShuffle(False, 0, 360)
+
+    symmetric: bool = False
+    color_space: ColorSpace = ColorSpace.Oklab
+
+    @classmethod
+    def default(cls) -> FusionCosmeticPatches:
+        return cls()
+
     @classmethod
     def game(cls) -> RandovaniaGame:
         return RandovaniaGame.FUSION
