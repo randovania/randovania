dc67f95cdeea7b02e8ff902689efdc60569cede0
JeffGainsNGames <jeffgainsngames@gmail.com>
2024-03-13 09:26:50 -0400

    Fusion: Add required and placed metroids to goal

--- test/games/fusion/generator/test_fusion_pool_creator.py
+++ test/games/fusion/generator/test_fusion_pool_creator.py
@@ -8,32 +8,49 @@ from randovania.generator.pickup_pool import PoolResults
 from randovania.layout.exceptions import InvalidConfiguration
 
 
-@pytest.mark.parametrize("metroid_count", [0, 1, 11, 20])
-def test_fusion_pool_creator(fusion_game_description, metroid_count):
+@pytest.mark.parametrize(
+    ("required_metroids", "placed_metroids"),
+    [
+        (0, 0),
+        (1, 1),
+        (11, 11),
+        (20, 20),
+    ],
+)
+def test_fusion_pool_creator(fusion_game_description, required_metroids, placed_metroids):
     db = fusion_game_description.resource_database
     # Run
-    results = artifact_pool(fusion_game_description, FusionArtifactConfig(True, True, metroid_count))
+    results = artifact_pool(
+        fusion_game_description, FusionArtifactConfig(True, True, required_metroids, placed_metroids)
+    )
 
     # Assert
     assert results == PoolResults(
-        [create_fusion_artifact(i, db) for i in range(metroid_count)],
+        [create_fusion_artifact(i, db) for i in range(required_metroids)],
         {},
-        [create_fusion_artifact(i, db) for i in range(metroid_count, 20)],
+        [create_fusion_artifact(i, db) for i in range(required_metroids, 20)],
     )
 
 
 @pytest.mark.parametrize(
-    ("bosses", "anywhere", "artifacts"),
+    ("bosses", "anywhere", "required_metroids", "placed_metroids"),
     [
-        (False, False, 1),
-        (True, False, 12),
-        (True, True, 21),
-        (False, True, 21),
+        (False, False, 1, 1),
+        (True, False, 12, 12),
+        (True, True, 21, 21),
+        (False, True, 21, 21),
     ],
 )
-def test_fusion_artifact_pool_should_throw_on_invalid_config(fusion_game_description, bosses, anywhere, artifacts):
+def test_fusion_artifact_pool_should_throw_on_invalid_config(
+    fusion_game_description, bosses, anywhere, required_metroids, placed_metroids
+):
     # Setup
-    configuration = FusionArtifactConfig(prefer_bosses=bosses, prefer_anywhere=anywhere, required_artifacts=artifacts)
+    configuration = FusionArtifactConfig(
+        prefer_bosses=bosses,
+        prefer_anywhere=anywhere,
+        required_artifacts=required_metroids,
+        placed_artifacts=placed_metroids,
+    )
 
     # Run
     with pytest.raises(InvalidConfiguration, match="More Infant Metroids than allowed!"):
