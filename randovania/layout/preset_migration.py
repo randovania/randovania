8bb786c33e2226ea3436aef1ea567e401295a06a
Mayberry <mayberryzoom@gmail.com>
2024-03-22 23:19:01 -0400

    Add preset migration for Floor Clip trick

--- randovania/layout/preset_migration.py
+++ randovania/layout/preset_migration.py
@@ -1051,6 +1051,22 @@ def _migrate_v73(preset: dict) -> dict:
     return preset
 
 
+def _migrate_v74(preset: dict) -> dict:
+    if preset["game"] == "dread":
+        difficultyLevels = ["beginner", "intermediate", "advanced", "expert", "hypermode"]
+
+        floorClips = [
+            difficultyLevels.index(v)
+            for k, v in preset["configuration"]["trick_level"]["specific_levels"].items()
+            if k in ["ADC", "SSC"]
+        ]
+
+        if floorClips:
+            preset["configuration"]["trick_level"]["specific_levels"]["FloorClip"] = difficultyLevels[min(floorClips)]
+
+        return preset
+
+
 _MIGRATIONS = [
     _migrate_v1,  # v1.1.1-247-gaf9e4a69
     _migrate_v2,  # v1.2.2-71-g0fbabe91
@@ -1125,6 +1141,7 @@ _MIGRATIONS = [
     _migrate_v71,
     _migrate_v72,
     _migrate_v73,
+    _migrate_v74,
 ]
 CURRENT_VERSION = migration_lib.get_version(_MIGRATIONS)
 
