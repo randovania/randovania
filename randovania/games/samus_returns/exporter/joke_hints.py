a1c04d989d2014a7953697d9ecfdf0abb0665fca
Miepee <janbidler00@protonmail.com>
2024-04-05 07:03:34 +0200

    Update joke_hints.py

--- randovania/games/samus_returns/exporter/joke_hints.py
+++ randovania/games/samus_returns/exporter/joke_hints.py
@@ -4,8 +4,8 @@ JOKE_HINTS = [
     "The Chozo have Sealed this hint away.",
     "Spider Boosting can quickly cover large distances.\nTake advantage whenever possible!",
     (
-        "The Spider Boost can be used while in Spider Ball form.\nIt will launch you forward,"
-        " damaging enemies you make contact with."
+        "The Spider Boost can be used while in Spider Ball form.\n"
+        "It will launch you forward, damaging enemies you make contact with."
     ),
     "Drag pins onto the map to mark points of interest.",
     "Samus, we cannot Return this hint to you.",
