c66427e2c12b82486c53316b7ca79d6406df0293
Miepee <janbidler00@protonmail.com>
2024-05-19 21:11:28 +0200

    fix test 2

--- test/games/am2r/layout/test_am2r_describer.py
+++ test/games/am2r/layout/test_am2r_describer.py
@@ -64,6 +64,7 @@ def test_am2r_format_params(artifacts) -> None:
         "Hints": ["DNA Hint: Area and room", "Ice Beam Hint: Area only"],
         "Item Pool": [
             f"Size: {118+artifacts.placed_artifacts} of 134",
+            "Vanilla starting items",
             "Excludes Varia Suit, Space Jump, Hi-Jump Boots, Gravity Suit",
             "Shuffles 2x Progressive Jump, 2x Progressive Suit",
         ],
