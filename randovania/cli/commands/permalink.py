520c0104d38c5a5d88f9e967afdf3f34ab055580
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-06-08 23:24:07 +0300

    Change CLI to allow better creating crossgame permalinks
    Game is now part of the the preset name, via <game name>/<preset name>.
    Add --file to specify files directly.
    Add --folder to use all presets in a folder.

--- randovania/cli/commands/permalink.py
+++ randovania/cli/commands/permalink.py
@@ -2,12 +2,19 @@ from __future__ import annotations
 
 import asyncio
 import random
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from randovania.games.game import RandovaniaGame
+from randovania.layout.versioned_preset import VersionedPreset
 
 if TYPE_CHECKING:
     from argparse import ArgumentParser
+
+
+def _error(msg: str) -> None:
+    print(msg)
+    raise SystemExit(1)
 
 
 def create_permalink(args):
@@ -15,17 +22,37 @@ def create_permalink(args):
     from randovania.layout.generator_parameters import GeneratorParameters
     from randovania.layout.permalink import Permalink
 
-    game: RandovaniaGame = RandovaniaGame(args.game)
     preset_manager = PresetManager(None)
     presets = []
-    for preset_name in args.preset_name:
+
+    for combined_name in args.name:
+        try:
+            game_value, preset_name = combined_name.split("/", 1)
+        except ValueError:
+            return _error(f"Name {combined_name} does not match format <Game Name>/<Preset Name>")
+
+        try:
+            game = RandovaniaGame(game_value)
+        except ValueError:
+            return _error(f"{game_value} is not a known RandovaniaGame")
+
         versioned = preset_manager.included_preset_with(game, preset_name)
         if versioned is None:
             options = [preset.name for preset in preset_manager.included_presets.values() if preset.game == game]
-            raise ValueError(
+            return _error(
                 f"Unknown included preset '{preset_name}' for game {game.long_name}. Valid options are: {options}"
             )
         presets.append(versioned.get_preset())
+
+    for preset_file in args.file:
+        versioned = VersionedPreset.from_file_sync(preset_file)
+        presets.append(versioned.get_preset())
+
+    folder: Path | None = args.folder
+    if folder is not None:
+        for preset_file in folder.glob(f"*.{VersionedPreset.file_extension()}"):
+            versioned = VersionedPreset.from_file_sync(preset_file)
+            presets.append(versioned.get_preset())
 
     seed = args.seed_number
     if seed is None:
@@ -54,14 +81,22 @@ def permalink_command(args):
 
 
 def add_permalink_arguments(parser: ArgumentParser):
-    parser.add_argument(
-        "--game",
-        choices=[game.value for game in RandovaniaGame],
-        required=True,
-        help="The name of the game of the preset to use.",
-    )
     parser.add_argument("--seed-number", type=int, help="The seed number. Defaults to 0.")
-    parser.add_argument("--preset-name", required=True, type=str, help="The name of the presets to use", nargs="+")
+    parser.add_argument(
+        "--name",
+        type=str,
+        action="append",
+        help="The name of the presets to use. Should be named <game>/<name>. Can be specified multiple times.",
+        default=[],
+    )
+    parser.add_argument(
+        "--file",
+        type=Path,
+        action="append",
+        help="The path of preset file to use. Can be specified multiple times.",
+        default=[],
+    )
+    parser.add_argument("--folder", type=Path, help="Use all presets in the folder")
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--race", default=False, action="store_true", help="Make a race permalink (without spoiler).")
     group.add_argument(
