c48d7caf3cd76476d45b5e3a8a097b44a7b0093d
dyceron <dycerontv@gmail.com>
2024-06-16 22:31:55 -0400

    Add multiworld rdvgame

--- test/games/test_log_file_export.py
+++ test/games/test_log_file_export.py
@@ -84,6 +84,7 @@ def pytest_generate_tests(metafunc: _pytest.python.Metafunc) -> None:
         # Cross Game Multis
         "multi-cs+dread+prime1+prime2.rdvgame",
         "multi-am2r+cs+dread+prime1+prime2.rdvgame",
+        "multi-am2r+cs+dread+prime1+prime2+msr.rdvgame",
         "prime1_and_2_multi.rdvgame",
         "cs_echoes_multi_1.rdvgame",
         "dread_prime1_multiworld.rdvgame",  # dread-prime1 multi
