103af6692bd878257855c020fdc7918a14ad22d5
Miepee <janbidler00@protonmail.com>
2024-06-24 13:25:25 +0200

    test chaos option shows up in describer

--- test/games/am2r/layout/test_am2r_describer.py
+++ test/games/am2r/layout/test_am2r_describer.py
@@ -13,24 +13,25 @@ from randovania.interface_common.preset_manager import PresetManager
 
 
 @pytest.mark.parametrize(
-    ("artifacts"),
+    ("artifacts", "darkness_chance"),
     [
-        AM2RArtifactConfig(True, True, False, 5, 5),
-        AM2RArtifactConfig(True, False, False, 46, 46),
-        AM2RArtifactConfig(False, True, False, 3, 6),
-        AM2RArtifactConfig(False, False, False, 0, 0),
-        AM2RArtifactConfig(False, False, True, 2, 5),
-        AM2RArtifactConfig(False, True, True, 10, 10),
-        AM2RArtifactConfig(True, False, True, 30, 40),
+        (AM2RArtifactConfig(True, True, False, 5, 5), 0),
+        (AM2RArtifactConfig(True, False, False, 46, 46), 50),
+        (AM2RArtifactConfig(False, True, False, 3, 6), 1000),
+        (AM2RArtifactConfig(False, False, False, 0, 0), 500),
+        (AM2RArtifactConfig(False, False, True, 2, 5), 0),
+        (AM2RArtifactConfig(False, True, True, 10, 10), 0),
+        (AM2RArtifactConfig(True, False, True, 30, 40), 0),
     ],
 )
-def test_am2r_format_params(artifacts) -> None:
+def test_am2r_format_params(artifacts: AM2RArtifactConfig, darkness_chance: int) -> None:
     # Setup
     preset = PresetManager(None).default_preset_for_game(RandovaniaGame.AM2R).get_preset()
     assert isinstance(preset.configuration, AM2RConfiguration)
     configuration = dataclasses.replace(
         preset.configuration,
         artifacts=artifacts,
+        darkness_chance=darkness_chance,
     )
 
     # Run
@@ -47,14 +48,19 @@ def test_am2r_format_params(artifacts) -> None:
     else:
         dna_where = "Kill the Queen"
 
+    game_changes = [
+        "Missiles need Launcher, Super Missiles need Launcher, Power Bombs need Launcher",
+        "Enable Septoggs, Add new Nest Pipes, Softlock block checks, Screw blocks near Pipes, "
+        "Industrial Complex Bomb Blocks",
+        "Skip gameplay cutscenes, Open Missile Doors with Supers",
+    ]
+
+    if darkness_chance != 0:
+        game_changes.append(f"{darkness_chance / 10:.1f}% chance of a room being dark")
+
     # Assert
     assert dict(result) == {
-        "Game Changes": [
-            "Missiles need Launcher, Super Missiles need Launcher, Power Bombs need Launcher",
-            "Enable Septoggs, Add new Nest Pipes, Softlock block checks, Screw blocks near Pipes, "
-            "Industrial Complex Bomb Blocks",
-            "Skip gameplay cutscenes, Open Missile Doors with Supers",
-        ],
+        "Game Changes": game_changes,
         "Gameplay": ["Starts at Main Caves - Landing Site"],
         "Goal": (
             [f"{artifacts.required_artifacts} Metroid DNA out of {artifacts.placed_artifacts}", dna_where]
