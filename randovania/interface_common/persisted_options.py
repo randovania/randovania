1ddd7f93c08f7b59536f33b27ae8c3ad8e7f3c4a
dyceron <dycerontv@gmail.com>
2024-07-04 08:25:38 -0400

    Enable room names on HUD by default

--- randovania/interface_common/persisted_options.py
+++ randovania/interface_common/persisted_options.py
@@ -186,6 +186,13 @@ def _msr_exheader_path(options: dict) -> dict:
     return options
 
 
+def _msr_room_names_visible(options: dict) -> dict:
+    if "game_samus_returns" in options:
+        options["game_samus_returns"]["cosmetic_patches"]["show_room_names"] = "Always"
+
+    return options
+
+
 _CONVERTER_FOR_VERSION = [
     None,
     None,
@@ -219,6 +226,7 @@ _CONVERTER_FOR_VERSION = [
     _dread_linux_ryujinx_path,  # added Dread Ryujinx export to Unix systems
     _msr_cosmetic_laser_color,  # remove the Grapple Laser color checkbox for MSR Cosmetics
     _msr_exheader_path,  # adds empty exheader path for MSR
+    _msr_room_names_visible, # forces room name display to be on by default
 ]
 _CURRENT_OPTIONS_FILE_VERSION = migration_lib.get_version(_CONVERTER_FOR_VERSION)
 
