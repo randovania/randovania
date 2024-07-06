c720e51d50746691e720eeb1b4ff540d029b0bde
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2024-07-06 18:50:13 +0300

    Support doing new patcher only for Echoes

--- randovania/games/prime2/exporter/game_exporter.py
+++ randovania/games/prime2/exporter/game_exporter.py
@@ -27,6 +27,129 @@ if TYPE_CHECKING:
     from randovania.exporter.patch_data_factory import PatcherDataMeta
 
 
+def extract_or_restore(
+    export_params: EchoesGameExportParams, backups_update: status_update_lib.ProgressUpdateCallable
+) -> Path:
+    contents_files_path = export_params.contents_files_path
+    backup_files_path = export_params.backup_files_path
+
+    if export_params.input_path is not None:
+        unpack_updaters = status_update_lib.split_progress_update(backups_update, 2)
+        shutil.rmtree(contents_files_path, ignore_errors=True)
+        shutil.rmtree(backup_files_path, ignore_errors=True)
+        iso_packager.unpack_iso(
+            iso=export_params.input_path,
+            game_files_path=contents_files_path,
+            progress_update=unpack_updaters[0],
+        )
+        claris_randomizer.create_pak_backups(contents_files_path, backup_files_path, unpack_updaters[1])
+    else:
+        try:
+            claris_randomizer.restore_pak_backups(contents_files_path, backup_files_path, backups_update)
+        except FileNotFoundError:
+            raise UnableToExportError(
+                "Your internal copy is missing files.\nPlease press 'Delete internal copy' and select a clean game ISO."
+            )
+
+    return contents_files_path
+
+
+def classic_export(
+    patch_data: dict,
+    export_params: EchoesGameExportParams,
+    progress_update: status_update_lib.ProgressUpdateCallable,
+    new_patcher: dict | None,
+) -> None:
+    # restore backups
+    # convert prime models
+    # claris patcher
+    # open-prime-rando
+    # menu mod
+    # nod
+    num_updaters = 3
+    if new_patcher is not None:
+        num_updaters += 1
+    if patch_data["menu_mod"]:
+        num_updaters += 1
+    if export_params.use_prime_models:
+        num_updaters += 1
+    updaters = status_update_lib.split_progress_update(progress_update, num_updaters)
+
+    contents_files_path = extract_or_restore(export_params, updaters.pop(0))
+
+    # Save patcher data
+    json_lib.write_path(
+        contents_files_path.joinpath("files", "patcher_data.json"),
+        patch_data,
+    )
+
+    # Apply patcher
+    banner_patcher.patch_game_name_and_id(contents_files_path, patch_data["banner_name"], patch_data["publisher_id"])
+    randomizer_data = copy.deepcopy(decode_randomizer_data())
+
+    if export_params.use_prime_models:
+        convert_update = updaters.pop(0)
+        from randovania.patching.prime import asset_conversion
+
+        assets_path = export_params.asset_cache_path
+        asset_conversion.convert_prime1_pickups(
+            export_params.prime_path,
+            contents_files_path,
+            assets_path,
+            patch_data,
+            randomizer_data,
+            convert_update,
+        )
+
+    copy_coin_chest(contents_files_path)
+
+    # Claris Rando
+    claris_update = updaters.pop(0)
+    adjust_model_name(patch_data, randomizer_data)
+
+    claris_randomizer.apply_patcher_file(contents_files_path, patch_data, randomizer_data, claris_update)
+
+    dol_patcher.apply_patches(
+        dol_file.DolFile(contents_files_path.joinpath("sys/main.dol")),
+        dol_patcher.EchoesDolPatchesData.from_json(patch_data["dol_patches"]),
+    )
+
+    # New Patcher
+    if new_patcher is not None:
+        opr_update = updaters.pop(0)
+
+        with monitoring.trace_block("open_prime_rando.echoes_patcher.patch_paks"):
+            import open_prime_rando.echoes_patcher
+
+            open_prime_rando.echoes_patcher.patch_paks(
+                PathFileProvider(contents_files_path), contents_files_path, new_patcher, opr_update
+            )
+
+    # Menu Mod
+    if patch_data["menu_mod"]:
+        menumod_update = updaters.pop(0)
+        claris_randomizer.add_menu_mod_to_files(contents_files_path, menumod_update)
+
+    # Change the color of the hud
+    hud_color = patch_data["specific_patches"]["hud_color"]
+    if hud_color:
+        hud_color = [
+            hud_color[0] / 255,
+            hud_color[1] / 255,
+            hud_color[2] / 255,
+        ]
+        ntwk_file = str(contents_files_path.joinpath("files", "Standard.ntwk"))
+        mp2hudcolor.mp2hudcolor_c(ntwk_file, ntwk_file, hud_color[0], hud_color[1], hud_color[2])  # RGB 0.0-1.0
+
+    # Pack ISO
+    nod_update = updaters.pop(0)
+    iso_packager.pack_iso(
+        iso=export_params.output_path,
+        game_files_path=contents_files_path,
+        progress_update=nod_update,
+    )
+
+
 class EchoesGameExporter(GameExporter[EchoesGameExportParams]):
     _busy: bool = False
 
@@ -70,120 +193,15 @@ class EchoesGameExporter(GameExporter[EchoesGameExportParams]):
         progress_update: status_update_lib.ProgressUpdateCallable,
         randovania_meta: PatcherDataMeta,
     ) -> None:
-        new_patcher = patch_data.pop("new_patcher", None)
-        monitoring.set_tag("echoes_new_patcher", new_patcher is not None)
+        new_patcher_only = patch_data.pop("new_patcher_only")
+        new_patcher: dict | None = patch_data.pop("new_patcher", None)
 
-        # restore backups
-        # convert prime models
-        # claris patcher
-        # open-prime-rando
-        # menu mod
-        # nod
-        num_updaters = 3
-        if new_patcher is not None:
-            num_updaters += 1
-        if patch_data["menu_mod"]:
-            num_updaters += 1
-        if export_params.use_prime_models:
-            num_updaters += 1
-        updaters = status_update_lib.split_progress_update(progress_update, num_updaters)
-
-        contents_files_path = export_params.contents_files_path
-        backup_files_path = export_params.backup_files_path
-
-        backups_update = updaters.pop(0)
-        if export_params.input_path is not None:
-            unpack_updaters = status_update_lib.split_progress_update(backups_update, 2)
-            shutil.rmtree(contents_files_path, ignore_errors=True)
-            shutil.rmtree(backup_files_path, ignore_errors=True)
-            iso_packager.unpack_iso(
-                iso=export_params.input_path,
-                game_files_path=contents_files_path,
-                progress_update=unpack_updaters[0],
-            )
-            claris_randomizer.create_pak_backups(contents_files_path, backup_files_path, unpack_updaters[1])
+        monitoring.set_tag("echoes_new_patcher", "only" if new_patcher else (new_patcher is not None))
+        if new_patcher_only:
+            assert new_patcher is not None
+            raise NotImplementedError
         else:
-            try:
-                claris_randomizer.restore_pak_backups(contents_files_path, backup_files_path, backups_update)
-            except FileNotFoundError:
-                raise UnableToExportError(
-                    "Your internal copy is missing files.\nPlease press 'Delete internal copy' and select "
-                    "a clean game ISO."
-                )
-
-        # Save patcher data
-        json_lib.write_path(
-            contents_files_path.joinpath("files", "patcher_data.json"),
-            patch_data,
-        )
-
-        # Apply patcher
-        banner_patcher.patch_game_name_and_id(
-            contents_files_path, patch_data["banner_name"], patch_data["publisher_id"]
-        )
-        randomizer_data = copy.deepcopy(decode_randomizer_data())
-
-        if export_params.use_prime_models:
-            convert_update = updaters.pop(0)
-            from randovania.patching.prime import asset_conversion
-
-            assets_path = export_params.asset_cache_path
-            asset_conversion.convert_prime1_pickups(
-                export_params.prime_path,
-                contents_files_path,
-                assets_path,
-                patch_data,
-                randomizer_data,
-                convert_update,
-            )
-
-        copy_coin_chest(contents_files_path)
-
-        # Claris Rando
-        claris_update = updaters.pop(0)
-        adjust_model_name(patch_data, randomizer_data)
-
-        claris_randomizer.apply_patcher_file(contents_files_path, patch_data, randomizer_data, claris_update)
-
-        dol_patcher.apply_patches(
-            dol_file.DolFile(contents_files_path.joinpath("sys/main.dol")),
-            dol_patcher.EchoesDolPatchesData.from_json(patch_data["dol_patches"]),
-        )
-
-        # New Patcher
-        if new_patcher is not None:
-            opr_update = updaters.pop(0)
-
-            with monitoring.trace_block("open_prime_rando.echoes_patcher.patch_paks"):
-                import open_prime_rando.echoes_patcher
-
-                open_prime_rando.echoes_patcher.patch_paks(
-                    PathFileProvider(contents_files_path), contents_files_path, new_patcher, opr_update
-                )
-
-        # Menu Mod
-        if patch_data["menu_mod"]:
-            menumod_update = updaters.pop(0)
-            claris_randomizer.add_menu_mod_to_files(contents_files_path, menumod_update)
-
-        # Change the color of the hud
-        hud_color = patch_data["specific_patches"]["hud_color"]
-        if hud_color:
-            hud_color = [
-                hud_color[0] / 255,
-                hud_color[1] / 255,
-                hud_color[2] / 255,
-            ]
-            ntwk_file = str(contents_files_path.joinpath("files", "Standard.ntwk"))
-            mp2hudcolor.mp2hudcolor_c(ntwk_file, ntwk_file, hud_color[0], hud_color[1], hud_color[2])  # RGB 0.0-1.0
-
-        # Pack ISO
-        nod_update = updaters.pop(0)
-        iso_packager.pack_iso(
-            iso=export_params.output_path,
-            game_files_path=contents_files_path,
-            progress_update=nod_update,
-        )
+            classic_export(patch_data, export_params, progress_update, new_patcher)
 
 
 def copy_coin_chest(contents_path: Path) -> None:
--- randovania/games/prime2/layout/echoes_configuration.py
+++ randovania/games/prime2/layout/echoes_configuration.py
@@ -47,6 +47,15 @@ class LayoutSafeZone(BitPackDataclass, JsonDataclass):
     )
 
 
+class EchoesNewPatcher(BitPackEnum, Enum):
+    DISABLED = "disabled"
+    BOTH = "both"
+    ONLY = "only"
+
+    def is_enabled(self) -> bool:
+        return self != EchoesNewPatcher.DISABLED
+
+
 @dataclasses.dataclass(frozen=True)
 class EchoesConfiguration(BaseConfiguration):
     teleporters: PrimeTrilogyTeleporterConfiguration
@@ -60,7 +69,7 @@ class EchoesConfiguration(BaseConfiguration):
     varia_suit_damage: float = dataclasses.field(metadata={"min": 0.1, "max": 60.0, "precision": 3.0})
     dark_suit_damage: float = dataclasses.field(metadata={"min": 0.0, "max": 60.0, "precision": 3.0})
     dangerous_energy_tank: bool
-    use_new_patcher: bool
+    use_new_patcher: EchoesNewPatcher
     inverted_mode: bool
     portal_rando: bool
 
@@ -97,5 +106,8 @@ class EchoesConfiguration(BaseConfiguration):
 
         if self.portal_rando:
             result.append("Portal Rando")
+
+        if self.use_new_patcher == EchoesNewPatcher.ONLY:
+            result.append("Using the new patcher exclusively")
 
         return result
