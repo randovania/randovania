9f46960156a6bb9bbb82180db40127778b40daa0
Miepee <janbidler00@protonmail.com>
2024-07-22 03:38:57 +0200

    add test rdvgame

--- test/games/test_log_file_export.py
+++ test/games/test_log_file_export.py
@@ -113,6 +113,7 @@ def pytest_generate_tests(metafunc: _pytest.python.Metafunc) -> None:
         # Prime 1
         "prime1_crazy_seed.rdvgame",  # chaos features
         "prime1_crazy_seed_one_way_door.rdvgame",  # same as above but 1-way doors
+        "prime1_refills.rdvgame",  # Refill items + custom artifact count
         # Samus Returns
         "samus_returns/starter_preset.rdvgame",  # starter preset
         "samus_returns/start_inventory.rdvgame",  # test for starting inventory and export ids
