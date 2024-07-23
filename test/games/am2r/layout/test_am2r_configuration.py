91984a3ac10f4ea6c3d25e09a26a06a06d893ae3
Miepee <janbidler00@protonmail.com>
2024-07-23 22:52:41 +0200

    addmore test

--- test/games/am2r/layout/test_am2r_configuration.py
+++ test/games/am2r/layout/test_am2r_configuration.py
@@ -1,12 +1,26 @@
 from __future__ import annotations
 
 import dataclasses
+from typing import TYPE_CHECKING
+
+import pytest
 
 from randovania.games.am2r.layout.am2r_configuration import AM2RArtifactConfig, AM2RConfiguration
 from randovania.games.game import RandovaniaGame
 
+if TYPE_CHECKING:
+    from randovania.interface_common.preset_manager import PresetManager
 
-def test_has_unsupported_features(preset_manager):
+
+@pytest.mark.parametrize(
+    ("flip_vertical", "flip_horizontal"),
+    [
+        (True, False),
+        (False, True),
+        (True, True),
+    ],
+)
+def test_has_unsupported_features(preset_manager: PresetManager, flip_vertical: bool, flip_horizontal: bool) -> None:
     preset = preset_manager.default_preset_for_game(RandovaniaGame.AM2R).get_preset()
     assert isinstance(preset.configuration, AM2RConfiguration)
 
@@ -21,10 +35,13 @@ def test_has_unsupported_features(preset_manager):
         darkness_max=0,
         submerged_water_chance=1000,
         submerged_lava_chance=1000,
+        horizontally_flip_gameplay=flip_horizontal,
+        vertically_flip_gameplay=flip_vertical,
     )
 
     assert configuration.unsupported_features() == [
         "The amount of required DNA cannot be higher than the total amount of placed DNA.",
+        "Gameplay is flipped horizontally or vertically.",
         "The minimum darkness value cannot be higher than the maximum darkness value.",
         (
             "The probability of a room being submerged in water and being submerged in "
