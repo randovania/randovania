24091c0fb9a537796a353012d4ebead95e4b8df1
Thanatos <thanatosstr89@gmail.com>
2024-10-09 20:28:45 +0200

    Remove kwargs and add parameters to init

--- randovania/games/dread/gui/dialog/dread_connector_prompt_dialog.py
+++ randovania/games/dread/gui/dialog/dread_connector_prompt_dialog.py
@@ -1,13 +1,36 @@
 from __future__ import annotations
 
-from typing import Any
+import typing
+from typing import TYPE_CHECKING
 
 from randovania.gui.dialog.connector_prompt_dialog import ConnectorPromptDialog
 
+if TYPE_CHECKING:
+    from PySide6 import QtWidgets
+
 
 class DreadConnectorPromptDialog(ConnectorPromptDialog):
-    def __init__(self, **kwargs: dict[str, Any]) -> None:
-        super().__init__(**kwargs)
+    def __init__(
+        self,
+        parent: QtWidgets.QWidget | None,
+        title: str,
+        description: str,
+        max_length: int | None = None,
+        is_password: bool = False,
+        initial_value: str | None = None,
+        is_modal: bool = False,
+        check_re: typing.Pattern | None = None,
+    ) -> None:
+        super().__init__(
+            parent=parent,
+            title=title,
+            description=description,
+            max_length=max_length,
+            initial_value=initial_value,
+            is_password=is_password,
+            is_modal=is_modal,
+            check_re=check_re,
+        )
 
         self.top_radio.setText("Ryujinx")
         self.top_label.setText("Connects to a Ryujinx running on this computer.")
