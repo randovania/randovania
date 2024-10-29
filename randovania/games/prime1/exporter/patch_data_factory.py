959f53b467982b0cae8f03e5eb4891e1436aaa84
ppfannes <shin.wolflord10@gmail.com>
2024-10-29 14:38:12 +0100

    Added Power Bomb Launcher and Unlimited Power Bombs as proper starting items
    Power Bomb Launcher and Unlimited Power Bombs can now be used as proper starting items. Graphical bug present when you start with either Unlimited Missiles or Unlimited Power Bombs and have requried mains turned off: the respective ammo display appears, despite not having picked up an expansion prior.

--- randovania/games/prime1/exporter/patch_data_factory.py
+++ randovania/games/prime1/exporter/patch_data_factory.py
@@ -48,6 +48,8 @@ _STARTING_ITEM_NAME_TO_INDEX = {
     "scanVisor": "Scan",
     "bombs": "Bombs",
     "powerBombs": "PowerBomb",
+    "powerBombLauncher": "MainPB",
+    "unlimitedPowerBombs": "UnlimitedPowerBombs",
     "flamethrower": "Flamethrower",
     "thermalVisor": "Thermal",
     "charge": "Charge",
@@ -127,7 +129,7 @@ def prime1_pickup_details_to_patcher(
                 count = quantity
                 max_count = 0
                 break
-            elif resource.extra.get("launcher_type"):
+            elif resource.extra.get("custom_value"):
                 pickup_type = resource.long_name
                 break
             # Regular items
@@ -135,7 +137,6 @@ def prime1_pickup_details_to_patcher(
                 pickup_type = resource.long_name
                 count = quantity
                 max_count = count
-                # break
 
     if (
         model["name"] == "Missile"
@@ -148,7 +149,7 @@ def prime1_pickup_details_to_patcher(
         name = name.replace("Missile Expansion", "Shiny Missile Expansion")
         original_model = model
 
-    # if (model["name"] == "UnlimitedMissiles"):
+    # TODO: Model for custom items.
 
     result = {
         "type": pickup_type,
@@ -671,18 +672,6 @@ class PrimePatchDataFactory(PatchDataFactory[PrimeConfiguration, PrimeCosmeticPa
         db = self.game
         namer = PrimeHintNamer(self.description.all_patches, self.players_config)
 
-        ammo_with_mains = [
-            ammo.name
-            for ammo, state in self.configuration.ammo_pickup_configuration.pickups_state.items()
-            if state.requires_main_item
-        ]
-        if ammo_with_mains:
-            raise ValueError(
-                "Preset has {} with required mains enabled. This is currently not supported.".format(
-                    " and ".join(ammo_with_mains)
-                )
-            )
-
         scan_visor = self.game.resource_database.get_item_by_name("Scan Visor")
         pickup_list = self.export_pickup_list()
         modal_hud_override = _create_locations_with_modal_hud_memo(pickup_list)
@@ -939,12 +928,14 @@ class PrimePatchDataFactory(PatchDataFactory[PrimeConfiguration, PrimeCosmeticPa
             for name, index in _STARTING_ITEM_NAME_TO_INDEX.items()
         }
 
-        # Check if mains are required. If not, force Missile Launcher to be a starting item. Right?
+        # Check if mains are required. If not, force Missile Launcher or Main Power Bombs to be a starting item.
         for ammo, state in self.configuration.ammo_pickup_configuration.pickups_state.items():
             if ammo.name == "Missile Expansion":
                 if not state.requires_main_item:
                     starting_items["missileLauncher"] = True
-                break
+            elif ammo.name == "Power Bomb Expansion":
+                if not state.requires_main_item:
+                    starting_items["powerBombLauncher"] = True
 
         if not self.configuration.legacy_mode:
             idrone_config = {
