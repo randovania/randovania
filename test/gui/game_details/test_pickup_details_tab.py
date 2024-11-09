003d58e687ff2ecc1d1c31aac54b456c4407add4
Miepee <janbidler00@protonmail.com>
2024-11-09 17:47:07 +0100

    maybe i shouldve moved these tests in another pr

--- test/gui/game_details/test_pickup_details_tab.py
+++ test/gui/game_details/test_pickup_details_tab.py
@@ -7,7 +7,9 @@ from randovania.layout.layout_description import LayoutDescription
 
 
 def test_search_pickup(skip_qtbot, test_files_dir):
-    layout = LayoutDescription.from_file(test_files_dir.joinpath("log_files", "dread_prime1_multiworld.rdvgame"))
+    layout = LayoutDescription.from_file(
+        test_files_dir.joinpath("log_files", "multis", "dread_prime1_multiworld.rdvgame")
+    )
 
     root = QtWidgets.QWidget()
     skip_qtbot.addWidget(root)
