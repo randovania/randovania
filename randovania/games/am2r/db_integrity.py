f6a47a91fb7d084c44dec6011bb510cc0b1ecb42
Miepee <janbidler00@protonmail.com>
2024-11-14 21:24:51 +0100

    oops

--- randovania/games/am2r/db_integrity.py
+++ randovania/games/am2r/db_integrity.py
@@ -1,15 +1,10 @@
-import typing
 from collections.abc import Iterator
 
-from randovania.game_description.db.node import NodeContext
 from randovania.game_description.game_description import GameDescription
-from randovania.game_description.game_patches import GamePatches
 from randovania.game_description.integrity_check import (
     check_for_items_to_be_replaced_by_templates,
     check_for_resources_to_use_together,
 )
-from randovania.game_description.resources.resource_collection import ResourceCollection
-from randovania.layout.base.base_configuration import BaseConfiguration
 
 # Short name resource -> recommended template name
 use_templates_over_items = {
@@ -37,12 +32,5 @@ combined_resources = {
 
 
 def find_am2r_db_errors(game: GameDescription) -> Iterator[str]:
-    context = NodeContext(
-        patches=GamePatches.create_from_game(game, 0, typing.cast(BaseConfiguration, None)),
-        current_resources=ResourceCollection.with_database(game.resource_database),
-        database=game.resource_database,
-        node_provider=game.region_list,
-    )
-
-    yield from check_for_items_to_be_replaced_by_templates(game, context, use_templates_over_items)
-    yield from check_for_resources_to_use_together(game, context, combined_resources)
+    yield from check_for_items_to_be_replaced_by_templates(game, use_templates_over_items)
+    yield from check_for_resources_to_use_together(game, combined_resources)
