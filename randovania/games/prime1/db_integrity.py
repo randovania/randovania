eb5666d32454cdeef3d38ecdc839e62735fad5fe
Miepee <janbidler00@protonmail.com>
2024-12-22 14:19:30 +0100

    Prime 1: Add Power bombs template

--- randovania/games/prime1/db_integrity.py
+++ randovania/games/prime1/db_integrity.py
@@ -11,6 +11,7 @@ use_templates_over_items = {
     "Boost": "Can Use Boost Ball",
     "Spider": "Can Use Spider Ball",
     "Bombs": "Can Use Bombs or Can Use Any Bombs",
+    "PowerBomb": "Can Use Power Bombs or Can Use Any Bombs",
 }
 
 # short name resource -> Tuple(short name resources)
@@ -18,6 +19,7 @@ combined_resources = {
     "Boost": ("MorphBall",),
     "Spider": ("MorphBall",),
     "Bombs": ("MorphBall",),
+    "PowerBomb": ("MorphBall",),
 }
 
 
