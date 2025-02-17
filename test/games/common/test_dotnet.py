212b4ce8280642dae8550ff6db87abecc9b26139
UltiNaruto <zenaruto@ymail.com>
2025-02-17 07:48:12 +0100

    Changed the tests for dotnet

--- test/games/common/test_dotnet.py
+++ test/games/common/test_dotnet.py
@@ -1,5 +1,29 @@
-from randovania.games.common.dotnet import is_dotnet_set_up
+from unittest.mock import MagicMock
+
+import pytest
+
+from randovania.exporter.game_exporter import GameExporter, GameExportParams
+from randovania.patching.patchers.exceptions import UnableToExportError
 
 
-def test_dotnet_setup():
-    is_dotnet_set_up()
+def test_dotnet_missing(mocker):
+    mocker.patch("subprocess.run", side_effect=FileNotFoundError)
+
+    exporter = GameExporter()
+
+    with pytest.raises(UnableToExportError):
+        exporter._do_export_game(
+            MagicMock(), GameExportParams(spoiler_output=None, input_path=None, output_path=None), MagicMock()
+        )
+
+
+def test_dotnet_non_zero_error_code(mocker):
+    dotnet_process = mocker.patch("subprocess.run")
+    dotnet_process.returncode = 1
+
+    exporter = GameExporter()
+
+    with pytest.raises(UnableToExportError):
+        exporter._do_export_game(
+            MagicMock(), GameExportParams(spoiler_output=None, input_path=None, output_path=None), MagicMock()
+        )
