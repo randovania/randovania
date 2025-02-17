4d95500e6784750f0adf986f2d91cf23da0e8839
UltiNaruto <zenaruto@ymail.com>
2025-02-17 10:45:35 +0100

    Added docstring to is_dotnet_set_up()

--- randovania/games/common/dotnet.py
+++ randovania/games/common/dotnet.py
@@ -10,6 +10,8 @@ class DotnetNotSetupException(UnableToExportError):
 
 
 def is_dotnet_set_up() -> None:
+    """Tests if dotnet is set up. Throws a DotnetNotSetupException exception if it's not the case."""
+
     try:
         dotnet_process = subprocess.run(["dotnet", "--info"], check=False)
         dotnet_ran_fine = dotnet_process.returncode == 0
