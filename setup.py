9ab0b27111d872d255f99f216d7bc61c6b253341
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-02-18 20:04:29 +0200

    Improve setup.py, add PySide as a build dependency, for uic

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
 from setuptools import setup
-from setuptools.command.build_py import build_py
-from wheel.bdist_wheel import bdist_wheel
+from setuptools.command.build import build
 
 try:
     from pyqt_distutils.build_ui import build_ui
@@ -10,22 +9,13 @@ except ModuleNotFoundError:
     build_ui = None
 
 
-class BuildPyCommand(build_py):
-    def run(self):
-        self.run_command("build_ui")
-        super().run()
-
-
-class BDistWheelCommand(bdist_wheel):
-    def run(self):
-        self.run_command("build_ui")
-        super().run()
+class CustomBuild(build):
+    sub_commands = [("build_ui", None), *build.sub_commands]
 
 
 setup(
     cmdclass={
         "build_ui": build_ui,
-        "build_py": BuildPyCommand,
-        "bdist_wheel": BDistWheelCommand,
+        "build": CustomBuild,
     },
 )
