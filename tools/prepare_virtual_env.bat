1a2ad7e60aec8c8c311538836fd3b088d0f17aa4
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-02-18 19:25:22 +0200

    Migrate to UV

--- tools/prepare_virtual_env.bat
+++ tools/prepare_virtual_env.bat
@@ -13,22 +13,15 @@ if NOT exist .git\ (
     exit
 )
 
-py -3.12 tools\check_py_version.py
-if NOT ["%errorlevel%"]==["0"] pause
-if NOT ["%errorlevel%"]==["0"] exit /b 0
+if [%1]!=["--all"] goto thin
+uv sync --frozen --all-extras
+goto final
 
-py -3.12 -m venv venv
-if %errorlevel% neq 0 exit /b %errorlevel%
+:thin
+uv sync --frozen --extra gui
 
-call venv\scripts\activate
-
-python -c "import sys; assert sys.version_info[0:2] == (3, 12), 'Python 3.12 required'"
-python -c "import sys; assert sys.maxsize > 2**32, '64-bit python required'"
-if %errorlevel% neq 0 exit /b %errorlevel%
-
-python -m pip install --upgrade -r requirements-setuptools.txt
-python -m pip install pyqt-distutils pre-commit -e ".[gui]" -c requirements.txt
-pre-commit install
+:final
+uvx pre-commit install
 
 echo Setup finished successfully.
 pause
--- tools/start_debug_client.sh
+++ tools/start_debug_client.sh
@@ -3,5 +3,4 @@
 set -e -x
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.."
 
-source venv/bin/activate
-python -m randovania --configuration tools/dev-server-configuration.json gui --preview main
+uv run randovania --configuration tools/dev-server-configuration.json gui --preview main
