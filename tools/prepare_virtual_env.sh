af02f144f5808ca61217ca2f94c0f78a9e816cda
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-02-18 19:25:22 +0200

    Migrate to UV

--- tools/prepare_virtual_env.sh
+++ tools/prepare_virtual_env.sh
@@ -14,13 +14,13 @@ if [ ! -d "./.git" ]; then
     exit
 fi
 
-python3.12 tools/check_py_version.py
+if [ "$1" == "--all" ]; then
+    extra_arg=(--all-extras)
+else
+    extra_arg=(--extra gui)
+fi
 
-python3.12 -m venv venv
-source venv/bin/activate
-python -c "import sys; assert sys.version_info[0:2] == (3, 12), 'Python 3.12 required'"
-python -m pip install --upgrade -r requirements-setuptools.txt
-python -m pip install pyqt-distutils pre-commit -e ".[gui]" -c requirements.txt
-pre-commit install
+uv sync --frozen "${extra_arg[@]}"
+uvx pre-commit install
 
 echo "Setup finished successfully."
--- tools/start_debug_client.bat
+++ tools/start_debug_client.bat
@@ -1,5 +1,4 @@
 cd /D "%~dp0"
 cd ..
 
-call venv\scripts\activate
-python -m randovania --configuration tools/dev-server-configuration.json gui --preview main
+uv run randovania --configuration tools/dev-server-configuration.json gui --preview main
