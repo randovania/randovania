17d7af5da35d8e48e325e775bf6e0bf0272a2ceb
JeffGainsNGames <jeffgainsngames@gmail.com>
2025-02-20 19:18:53 -0500

    fix distributor default precision

--- randovania/games/am2r/generator/hint_distributor.py
+++ randovania/games/am2r/generator/hint_distributor.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, override
 
+from randovania.game_description.hint import HintItemPrecision, HintLocationPrecision, PrecisionPair
 from randovania.generator.hint_distributor import HintDistributor
 
 if TYPE_CHECKING:
@@ -13,3 +14,8 @@ class AM2RHintDistributor(HintDistributor):
     @override
     def is_pickup_interesting(self, pickup: PickupEntry) -> bool:
         return not pickup.has_hint_feature("dna")
+
+    @override
+    @property
+    def default_precision_pair(self) -> PrecisionPair:
+        return PrecisionPair(HintLocationPrecision.REGION_ONLY, HintItemPrecision.DETAILED, True)
