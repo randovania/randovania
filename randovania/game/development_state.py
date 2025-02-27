da66a1652e6e1974fdc56a4dc2e6931a548e381a
Miepee <janbidler00@protonmail.com>
2025-02-27 19:14:51 +0100

    docstrings

--- randovania/game/development_state.py
+++ randovania/game/development_state.py
@@ -6,9 +6,14 @@ import randovania
 
 
 class DevelopmentState(Enum):
+    """Indicates in which builds of Randovania a game is available in."""
+
     STABLE = "stable"
+    """The game is available in stable builds, dev builds or when running RDV from source."""
     STAGING = "staging"
-    SOURCE = "source"
+    """The game is available in dev-builds or when running RDV from source."""
+    SOURCE_ONLY = "source"
+    """Indicates that the game is only available when running RDV from source."""
 
     @property
     def is_stable(self) -> bool:
