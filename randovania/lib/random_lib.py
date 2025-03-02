0d4daabb074398a7466d345033009013a7459a51
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-03-02 17:17:34 +0200

    Move select_weighted_action to random_lib

--- randovania/lib/random_lib.py
+++ randovania/lib/random_lib.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypeVar
+from typing import TYPE_CHECKING, TypeVar
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Iterable, Iterator, Mapping
@@ -21,11 +21,7 @@ def shuffle(rng: Random, x: Iterator[T]) -> list[T]:
     return result
 
 
-def iterate_with_weights(
-    items: Iterable[T],
-    item_weights: Mapping[T, float],
-    rng: Random,
-) -> Iterator[T]:
+def iterate_with_weights(rng: Random, items: Iterable[T], item_weights: Mapping[T, float]) -> Iterator[T]:
     """
     Iterates over the given list randomly, with each item having the probability listed in item_weigths
     :param items:
@@ -38,22 +34,34 @@ def iterate_with_weights(
     weights = [max(item_weights[action], 0) for action in item_list]
 
     while item_list and any(weight > 0 for weight in weights):
-        pickup_node = rng.choices(item_list, weights)[0]
+        item = rng.choices(item_list, weights)[0]
 
         # Remove the pickup_node from the potential list, along with its weight
-        index = item_list.index(pickup_node)
+        index = item_list.index(item)
         item_list.pop(index)
         weights.pop(index)
 
-        yield pickup_node
+        yield item
 
 
-def select_element_with_weight(weighted_items: Mapping[T, float], rng: Random) -> T:
-    return next(iterate_with_weights(items=list(weighted_items.keys()), item_weights=weighted_items, rng=rng))
+def select_element_with_weight(rng: Random, weighted_items: Mapping[T, float]) -> T:
+    """
+    Choose a random element, with each one having a custom weight.
+    :param rng: The random object to use.
+    :param weighted_items: The weights
+    :return: One of the keys in weighted_items
+    """
+    return next(iterate_with_weights(rng=rng, items=list(weighted_items.keys()), item_weights=weighted_items))
 
 
-def random_key(d: dict[T, Any], rng: Random) -> T:
-    return rng.choice(list(d.keys()))
+def select_element_with_weight_and_uniform_fallback(rng: Random, weighted_items: Mapping[T, float]) -> T:
+    """
+    Same as `select_element_with_weight`, but if all elements have weight 0 then one is selected uniformly.
+    """
+    try:
+        return select_element_with_weight(rng=rng, weighted_items=weighted_items)
+    except StopIteration:
+        return rng.choice(list(weighted_items.keys()))
 
 
 def create_weighted_list(
