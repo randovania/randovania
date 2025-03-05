b7c252844be6e7222630c114a97fdc82eaad1760
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-03-06 00:17:37 +0200

    Improve preset describer

--- randovania/games/factorio/layout/factorio_preset_describer.py
+++ randovania/games/factorio/layout/factorio_preset_describer.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from randovania.games.factorio.layout import FactorioConfiguration
 from randovania.layout.preset_describer import GamePresetDescriber
 
 if TYPE_CHECKING:
@@ -10,6 +11,12 @@ if TYPE_CHECKING:
 
 class FactorioPresetDescriber(GamePresetDescriber):
     def format_params(self, configuration: BaseConfiguration) -> dict[str, list[str]]:
+        assert isinstance(configuration, FactorioConfiguration)
         template_strings = super().format_params(configuration)
         template_strings.pop("Logic Settings")
+        template_strings.pop("Gameplay")
+
+        if configuration.full_tech_tree:
+            template_strings["Item Pool"].insert(0, "Uses Full Tech Tree")
+
         return template_strings
