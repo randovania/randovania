2793ad449db6d2ba55f1aee0a0744594e97ff944
duncathan <dunc@duncathan.com>
2025-03-11 13:40:21 -0600

    fix tests

--- test/gui/widgets/test_game_validator_widget.py
+++ test/gui/widgets/test_game_validator_widget.py
@@ -105,22 +105,22 @@ async def test_on_start_button_no_task(widget, mocker, cancel: bool, verbosity: 
         assert widget.start_button.text() == "Stop"
         assert widget.status_label.text() == "Running..."
         if verbosity:
-            write_to_log("> Starting Area/Spawn Point for []")
+            write_to_log("> Intro/Starting Area/Spawn Point for []")
             write_to_log(
-                "> Starting Area/Pickup (Weapon) [100/100 Energy] for "
+                "> Intro/Starting Area/Pickup (Weapon) [100/100 Energy] for "
                 "[action Major - World 0's Blue Key] ['N: Pickup (Weapon)', 'I: Blue Key']"
             )
             if verbosity > 1:
                 write_to_log(" # Satisfiable Actions")
-                write_to_log("  = Starting Area/Lock - Door to Boss Arena")
+                write_to_log("  = Intro/Starting Area/Lock - Door to Boss Arena")
             if verbosity > 2:
-                write_to_log(": Back-Only Lock Room/Door to Starting Area")
+                write_to_log(": Intro/Back-Only Lock Room/Door to Starting Area")
             write_to_log(
-                "> Starting Area/Spawn Point [100/100 Energy] for [action Some weird action] ['N: Spawn Point']"
+                "> Intro/Starting Area/Spawn Point [100/100 Energy] for [action Some weird action] ['N: Spawn Point']"
             )
-            write_to_log(" * Rollback weird action")
+            write_to_log(" * Rollback Intro/Starting Area/Spawn Point [action Some weird action]")
             write_to_log(
-                "> Back-Only Lock Room/Event - Key Switch 1 [100/100 Energy] for "
+                "> Intro/Back-Only Lock Room/Event - Key Switch 1 [100/100 Energy] for "
                 "[action Event - Key Switch 1] ['E: Key Switch 1']"
             )
         if cancel:
@@ -139,31 +139,35 @@ async def test_on_start_button_no_task(widget, mocker, cancel: bool, verbosity: 
 
     # Assert
     mock_run_validator.assert_awaited_once_with(ANY, verbosity, widget.layout_description)
-    assert widget.log_widget.topLevelItemCount() == (5 if verbosity else 0)
+    assert widget.log_widget.topLevelItemCount() == (1 if verbosity else 0)
 
     if verbosity:
-        assert not widget.log_widget.topLevelItem(0).isExpanded()
+        region_item = widget.log_widget.topLevelItem(0)
+        assert region_item.childCount() == 5
+        assert region_item.text(LABEL_IDS["Node"]) == "Intro"
 
-        pickup_action = widget.log_widget.topLevelItem(1)
+        assert not region_item.child(0).isExpanded()
+
+        pickup_action = region_item.child(1)
         assert pickup_action.text(LABEL_IDS["Type"]) == "Major"
         assert pickup_action.text(LABEL_IDS["Action"]) == "Blue Key"
 
-        other_action = widget.log_widget.topLevelItem(2)
+        other_action = region_item.child(2)
         assert other_action.text(LABEL_IDS["Type"]) == "Other"
         assert other_action.text(LABEL_IDS["Action"]) == "Some weird action"
 
-        rollback = widget.log_widget.topLevelItem(3)
-        assert rollback.text(LABEL_IDS["Node"]) == "Rollback weird action"
+        rollback = region_item.child(3)
+        assert rollback.text(LABEL_IDS["Node"]) == "Rollback Starting Area/Spawn Point"
         assert rollback.text(LABEL_IDS["Type"]) == "Rollback"
 
     if verbosity > 1:
         child = pickup_action.child(0)
         assert child.text(0) == "# Satisfiable Actions"
-        assert child.child(0).text(LABEL_IDS["Node"]) == "Starting Area/Lock - Door to Boss Arena"
+        assert child.child(0).text(LABEL_IDS["Node"]) == "Intro/Starting Area/Lock - Door to Boss Arena"
 
     if verbosity > 2:
         child = pickup_action.child(1)
-        assert child.text(LABEL_IDS["Node"]) == "Back-Only Lock Room/Door to Starting Area"
+        assert child.text(LABEL_IDS["Node"]) == "Intro/Back-Only Lock Room/Door to Starting Area"
 
     for column in (LABEL_IDS["Energy"], LABEL_IDS["Resources"]):
         assert widget.log_widget.isColumnHidden(column) == (verbosity < 2)
