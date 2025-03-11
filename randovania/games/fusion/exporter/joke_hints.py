286616ae9df2af61c9444dde15e8e9e351e9abcb
JeffGainsNGames <jeffgainsngames@gmail.com>
2025-03-11 17:34:04 -0400

    add joke hints

---
+++ randovania/games/fusion/exporter/joke_hints.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from randovania.exporter.hints.joke_hints import GENERIC_JOKE_HINTS
+
+FUSION_JOKE_HINTS = GENERIC_JOKE_HINTS + [
+    "Adam has not yet authorized the use of this hint.",
+    "Space Pirates, strangely, dislike theft.",
+    "We've been trying to contact you about your ship's extended warranty.",
+    "Yellow X are lemon flavoured.",
+    "Green X are lime flavoured.",
+    "Red X are strawberry flavoured.",
+    "Blue X are blueberry flavoured.",
+    "The Light Suit is fully resistant to the effects of Dark Aether.",
+    "I would tell you a hint, but Aran out of them.",
+]
