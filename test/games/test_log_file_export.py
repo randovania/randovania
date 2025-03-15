d26f3afad6db261e39ab994aa8746996cd4e87ba
JeffGainsNGames <jeffgainsngames@gmail.com>
2025-03-15 14:09:55 -0400

    add random starting tests for fusion

--- test/games/test_log_file_export.py
+++ test/games/test_log_file_export.py
@@ -108,6 +108,7 @@ def pytest_generate_tests(metafunc: _pytest.python.Metafunc) -> None:
         "fusion/starter_preset.rdvgame",
         "fusion/short_intro.rdvgame",
         "fusion/all_hidden_with_nothing.rdvgame",
+        "fusion/random_starting.rdvgame",
         # Dread
         "dread/starter_preset.rdvgame",  # starter preset
         "dread/crazy_settings.rdvgame",  # crazy settings
