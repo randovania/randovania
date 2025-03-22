4fe2c408f1fcaebb625d56539b0f3e50d1c0c577
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-03-22 13:36:52 +0200

    Add LayerFilteredGameDatabaseView.create_for_configuration

--- randovania/game_description/filtered_game_database_view.py
+++ randovania/game_description/filtered_game_database_view.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import typing
+from typing import Self, override
 
 from randovania.game_description.game_database_view import GameDatabaseView, GameDatabaseViewProxy
 
@@ -18,22 +19,22 @@ class LayerFilteredGameDatabaseView(GameDatabaseViewProxy):
         super().__init__(original)
         self.enabled_layers = enabled_layers
 
-    @typing.override
+    @override
     def node_iterator(self) -> Iterator[tuple[Region, Area, Node]]:
         for entry in super().node_iterator():
             if self.enabled_layers.intersection(entry[2].layers):
                 yield entry
 
-
-def filter_view_for_configuration(view: GameDatabaseView, configuration: BaseConfiguration) -> GameDatabaseView:
-    """
-    Creates a new GameDatabaseView that filters what nodes are visible based on what layers are
-    active in the given Configuration.
-    :param view: A view to wrap
-    :param configuration: The configuration to query for active layers.
-    :return: A new view with filters applied
-    """
-    return LayerFilteredGameDatabaseView(
-        view,
-        configuration.active_layers(),
-    )
+    @classmethod
+    def create_for_configuration(cls, view: GameDatabaseView, configuration: BaseConfiguration) -> Self:
+        """
+        Creates a new GameDatabaseView that filters what nodes are visible based on what layers are
+        active in the given Configuration.
+        :param view: A view to wrap
+        :param configuration: The configuration to query for active layers.
+        :return: A new view with filters applied
+        """
+        return cls(
+            view,
+            configuration.active_layers(),
+        )
