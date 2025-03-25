6040900776cca126d493a7524d9ac3f1e95a4a77
dyceron <dycerontv@gmail.com>
2025-03-25 18:44:37 -0400

    More test fixes

--- test/games/prime_hunters/generator/test_prime_hunters_pool_creator.py
+++ test/games/prime_hunters/generator/test_prime_hunters_pool_creator.py
@@ -11,20 +11,28 @@ from randovania.generator.pickup_pool.pickup_creator import create_generated_pic
 @pytest.mark.parametrize("placed_octoliths", [0, 1, 4, 8])
 def test_prime_hunters_pool_creator(prime_hunters_game_description, placed_octoliths):
     db = prime_hunters_game_description.resource_database
+    pdb = prime_hunters_game_description.get_pickup_database()
     # Run
     results = add_octoliths(prime_hunters_game_description, HuntersOctolithConfig(True, placed_octoliths))
 
     # Assert
     assert results == PoolResults(
-        [create_generated_pickup("Octolith", db, i=i + 1) for i in range(placed_octoliths)],
+        [create_generated_pickup("Octolith", db, pdb, i=i + 1) for i in range(placed_octoliths)],
         {},
-        [create_generated_pickup("Octolith", db, i=i + 1) for i in range(placed_octoliths, 8)],
+        [create_generated_pickup("Octolith", db, pdb, i=i + 1) for i in range(placed_octoliths, 8)],
     )
 
 
 @pytest.mark.parametrize("index", range(8))
-def test_create_octolith_id(index: int, prime_hunters_resource_database):
-    octolith = create_generated_pickup("Octolith", prime_hunters_resource_database, i=index + 1)
+def test_create_octolith_id(index: int, prime_hunters_game_description):
+    octolith = create_generated_pickup(
+        "Octolith",
+        prime_hunters_game_description.resource_database,
+        prime_hunters_game_description.get_pickup_database(),
+        i=index + 1,
+    )
 
     assert octolith.gui_category.name == "octolith"
-    assert octolith.progression == ((prime_hunters_resource_database.get_item(f"Octolith{index + 1}"), 1),)
+    assert octolith.progression == (
+        (prime_hunters_game_description.resource_database.get_item(f"Octolith{index + 1}"), 1),
+    )
