d1508db6ebb4badb5da8228b1fb601b32f5e5fe8
dyceron <dycerontv@gmail.com>
2025-03-25 17:53:46 -0400

    Fix pool creator for artifacts and octoliths

--- randovania/games/prime_hunters/generator/pool_creator.py
+++ randovania/games/prime_hunters/generator/pool_creator.py
@@ -11,6 +11,7 @@ from randovania.generator.pickup_pool.pickup_creator import create_generated_pic
 
 if TYPE_CHECKING:
     from randovania.game_description.game_description import GameDescription
+    from randovania.game_description.pickup.pickup_database import PickupDatabase
     from randovania.game_description.pickup.pickup_entry import PickupEntry
     from randovania.game_description.resources.resource_database import ResourceDatabase
     from randovania.layout.base.base_configuration import BaseConfiguration
@@ -20,7 +21,7 @@ def pool_creator(results: PoolResults, configuration: BaseConfiguration, game: G
     assert isinstance(configuration, HuntersConfiguration)
 
     # Add Alimbic Artifacts to the item pool
-    results.extend_with(add_alimbic_artifacts(game.resource_database))
+    results.extend_with(add_alimbic_artifacts(game.resource_database, game.get_pickup_database()))
 
     # Add Octoliths to the item pool
     results.extend_with(add_octoliths(game, configuration.octoliths))
@@ -28,6 +29,7 @@ def pool_creator(results: PoolResults, configuration: BaseConfiguration, game: G
 
 def add_alimbic_artifacts(
     resource_database: ResourceDatabase,
+    pickup_database: PickupDatabase,
 ) -> PoolResults:
     """
     :param resource_database:
@@ -39,7 +41,7 @@ def add_alimbic_artifacts(
         for artifact_type in ("Attameter", "Cartograph", "Binary Subscripture"):
             for i in range(2):
                 artifact = f"{region} {artifact_type} Artifact"
-                item_pool.append(create_generated_pickup(artifact, resource_database, i=i + 1))
+                item_pool.append(create_generated_pickup(artifact, resource_database, pickup_database, i=i + 1))
 
     return PoolResults(item_pool, {}, [])
 
@@ -53,7 +55,8 @@ def add_octoliths(
     :return:
     """
     octoliths: list[PickupEntry] = [
-        create_generated_pickup("Octolith", game.resource_database, i=i + 1) for i in range(8)
+        create_generated_pickup("Octolith", game.resource_database, game.get_pickup_database(), i=i + 1)
+        for i in range(8)
     ]
 
     octoliths_to_shuffle = octoliths[: config.placed_octoliths]
