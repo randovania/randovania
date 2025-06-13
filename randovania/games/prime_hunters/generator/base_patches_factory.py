18b69dc1a2088a71086e705107c89875def54d5d
dyceron <dycerontv@gmail.com>
2025-06-13 17:54:20 -0400

    Add portal shuffle

--- randovania/games/prime_hunters/generator/base_patches_factory.py
+++ randovania/games/prime_hunters/generator/base_patches_factory.py
@@ -6,11 +6,19 @@ from typing import TYPE_CHECKING
 from randovania.games.prime_hunters.layout import HuntersConfiguration
 from randovania.games.prime_hunters.layout.force_field_configuration import LayoutForceFieldRequirement
 from randovania.generator.base_patches_factory import BasePatchesFactory, MissingRng
+from randovania.generator.teleporter_distributor import (
+    get_dock_connections_assignment_for_teleporter,
+    get_teleporter_connections,
+)
 
 if TYPE_CHECKING:
+    from collections.abc import Iterable
     from random import Random
 
+    from randovania.game_description.db.dock_node import DockNode
+    from randovania.game_description.db.node import Node
     from randovania.game_description.game_description import GameDescription
+    from randovania.layout.base.base_configuration import BaseConfiguration
 
 
 class HuntersBasePatchesFactory(BasePatchesFactory[HuntersConfiguration]):
@@ -33,3 +41,13 @@ class HuntersBasePatchesFactory(BasePatchesFactory[HuntersConfiguration]):
         return {
             "force_fields": force_fields,
         }
+
+    def dock_connections_assignment(
+        self, configuration: BaseConfiguration, game: GameDescription, rng: Random
+    ) -> Iterable[tuple[DockNode, Node]]:
+        assert isinstance(configuration, HuntersConfiguration)
+        teleporter_connection = get_teleporter_connections(configuration.teleporters, game, rng)
+        dock_assignment = get_dock_connections_assignment_for_teleporter(
+            configuration.teleporters, game, teleporter_connection
+        )
+        yield from dock_assignment
