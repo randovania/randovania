b6a817d08ad6d8baacf7ac78649a0c5ab1a85bc6
Miepee <janbidler00@protonmail.com>
2025-10-05 16:43:52 +0200

    fix more stuff

--- test/gui/lib/test_foldable.py
+++ test/gui/lib/test_foldable.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from randovania.gui.lib.foldable import Foldable
+from randovania.gui.lib.custom_ui_widgets.foldable import Foldable
 
 
 def test_foldable_initial_state(skip_qtbot):
