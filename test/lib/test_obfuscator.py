8f78b2e71a2bf4eab5ab52edb4bb66a0c485d179
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-10-06 11:47:24 +0300

    Supply obfuscator secret via env var for the discord bot

--- test/lib/test_obfuscator.py
+++ test/lib/test_obfuscator.py
@@ -23,6 +23,22 @@ def test_round_trip_raw(obfuscator_test_secret: None) -> None:
     assert result == original
 
 
+def test_decode_raw(obfuscator_test_secret: None) -> None:
+    encrypted = "gAAAAABo44Ew2ZrUo7HzDQ3RowFJ8Npto8SetNJ2tBELbFkzl8UOnPw8yskWXHBXtbuLniLk8ZTzmomhavVCACj0MeNvHoNSLg=="
+    result = obfuscator.deobfuscate(encrypted)
+
+    assert result == b"abcfefaw512312t"
+
+
 def test_obfuscate_no_secret(obfuscator_no_secret: None) -> None:
     with pytest.raises(obfuscator.MissingSecret):
         obfuscator.obfuscate_json({"foo": 1234, "bar": ["a", "b", 5]})
+
+
+def test_obfuscate_env_secret(obfuscator_no_secret: None, monkeypatch: pytest.MonkeyPatch) -> None:
+    monkeypatch.setenv("OBFUSCATOR_SECRET", "cNGtDlTqCYF3BFCAQTaDSo5O7DQtzjsd3mS801MPM_M=")
+
+    encrypted = "gAAAAABo44Ew2ZrUo7HzDQ3RowFJ8Npto8SetNJ2tBELbFkzl8UOnPw8yskWXHBXtbuLniLk8ZTzmomhavVCACj0MeNvHoNSLg=="
+    result = obfuscator.deobfuscate(encrypted)
+
+    assert result == b"abcfefaw512312t"
