a1028ee55609c11e55e66dfca240f651ae60ac18
Logarhythmic <sehennicht@gmail.com>
2025-10-14 17:24:02 -0400

    removed `run` from start script

--- tools/start_dev_server.sh
+++ tools/start_dev_server.sh
@@ -4,4 +4,4 @@ set -e -x
 cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.."
 
 source .venv/bin/activate
-python -m run randovania --configuration tools/dev-server-configuration.json server run --mode dev
+python -m randovania --configuration tools/dev-server-configuration.json server run --mode dev
