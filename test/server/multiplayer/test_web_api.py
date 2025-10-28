f9d4a29f2d8f2ca2eae9011205fa7e4bdd6daeae
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-10-28 23:22:51 +0200

    Server: improve db tests

--- test/server/multiplayer/test_web_api.py
+++ test/server/multiplayer/test_web_api.py
@@ -1,40 +1,17 @@
 from __future__ import annotations
 
-import base64
 import datetime
-import json
-from typing import TYPE_CHECKING
 
 import pytest
-from fastapi.encoders import jsonable_encoder
-from playhouse.shortcuts import dict_to_model, model_to_dict
 
 from randovania.bitpacking import construct_pack
 from randovania.network_common.remote_inventory import RemoteInventory
 from randovania.network_common.session_visibility import MultiplayerSessionVisibility
 from randovania.server import database
 
-if TYPE_CHECKING:
-    from collections.abc import Callable
 
-    from test.server.conftest import RdvTestClient
-
-
-def _handle_db_nonsense(test_client: RdvTestClient, session_setup: Callable[[], database.MultiplayerSession]) -> None:
-    # The test client doesn't seem to play well with the database fixtures.
-    # The solution I've found is to do everything inside the test client's event loop, like this
-
-    @test_client.sa.app.post("/setup-db")
-    def setup_db() -> dict:
-        session = model_to_dict(session_setup(), backrefs=True)
-        return jsonable_encoder(session, custom_encoder={bytes: lambda v: base64.b64encode(v).decode("utf-8")})
-
-
-def test_admin_sessions(test_client, solo_two_world_session_setup) -> None:
-    _handle_db_nonsense(test_client, solo_two_world_session_setup)
-
+def test_admin_sessions(test_client, solo_two_world_session) -> None:
     # Run
-    test_client.post("/setup-db")
     result = test_client.get("/sessions")
 
     # Assert
@@ -45,21 +22,17 @@ def test_admin_sessions(test_client, solo_two_world_session_setup) -> None:
 
 def test_admin_sessions_paginated(test_client) -> None:
     # Setup
-    def setup() -> database.MultiplayerSession:
-        user = database.User.create(id=1234, name="The Name")
-        for i in range(50):
-            session = database.MultiplayerSession.create(
-                name=f"Debug{i}",
-                state=MultiplayerSessionVisibility.VISIBLE,
-                creator=user,
-                creation_date=datetime.datetime(2020, 5, 2, 10, 20, tzinfo=datetime.UTC),
-            )
-        return session
-
-    _handle_db_nonsense(test_client, setup)
+    user = database.User.create(id=1234, name="The Name", admin=True)
+    for i in range(50):
+        database.MultiplayerSession.create(
+            name=f"Debug{i}",
+            state=MultiplayerSessionVisibility.VISIBLE,
+            creator=user,
+            creation_date=datetime.datetime(2020, 5, 2, 10, 20, tzinfo=datetime.UTC),
+        )
+    test_client.set_logged_in_user(1234)
 
     # Run
-    test_client.post("/setup-db")
     result = test_client.get("/sessions?page=2")
 
     # Assert
@@ -71,12 +44,10 @@ def test_admin_sessions_paginated(test_client) -> None:
     assert "Page 2 of 3." in result.text
 
 
-def test_admin_session_missing(test_client, solo_two_world_session_setup) -> None:
+def test_admin_session_missing(test_client, solo_two_world_session) -> None:
     # Setup
-    _handle_db_nonsense(test_client, solo_two_world_session_setup)
 
     # Run
-    test_client.post("/setup-db")
     result = test_client.get("/session/2403")
 
     # Assert
@@ -84,22 +55,16 @@ def test_admin_session_missing(test_client, solo_two_world_session_setup) -> Non
     assert "Session not found" in result.text
 
 
-def test_admin_session_exists(test_client, solo_two_world_session_setup) -> None:
+def test_admin_session_exists(test_client, solo_two_world_session) -> None:
     # Setup
-    def setup() -> database.MultiplayerSession:
-        session = solo_two_world_session_setup()
-        assoc = database.WorldUserAssociation.get_by_instances(world=1, user=1234)
-        assoc.inventory = construct_pack.encode(
-            {"Charge": 1},
-            RemoteInventory,
-        )
-        assoc.save()
-        return session
-
-    _handle_db_nonsense(test_client, setup)
+    assoc = database.WorldUserAssociation.get_by_instances(world=1, user=1234)
+    assoc.inventory = construct_pack.encode(
+        {"Charge": 1},
+        RemoteInventory,
+    )
+    assoc.save()
 
     # Run
-    test_client.post("/setup-db")
     result = test_client.get("/session/1")
 
     # Assert
@@ -116,27 +81,19 @@ def test_admin_session_exists(test_client, solo_two_world_session_setup) -> None
     assert whitespace.join(entry2) in result.text
 
 
-def test_delete_session_get(test_client, solo_two_world_session_setup) -> None:
-    # Setup
-    _handle_db_nonsense(test_client, solo_two_world_session_setup)
-
+def test_delete_session_get(test_client, solo_two_world_session) -> None:
     # Run
-    session_dict = json.loads(test_client.post("/setup-db").text)
-    session_dict["layout_description_json"] = base64.b64decode(session_dict["layout_description_json"])
+    session = database.MultiplayerSession.get_by_id(1)
     result = test_client.get("/session/1/delete")
 
     # Assert
     assert '<button type="submit" formmethod="post">Confirm delete</button>' in result.text
     assert '<button type="submit" formaction="http://testserver/session/1">Cancel</button>' in result.text
-    assert database.MultiplayerSession.get_by_id(1) == dict_to_model(database.MultiplayerSession, session_dict)
+    assert database.MultiplayerSession.get_by_id(1) == session
 
 
-def test_toggle_admin_status_doesnt_exist(test_client, solo_two_world_session_setup) -> None:
-    # Setup
-    _handle_db_nonsense(test_client, solo_two_world_session_setup)
-
+def test_toggle_admin_status_doesnt_exist(test_client, solo_two_world_session) -> None:
     # Run
-    test_client.post("/setup-db")
     result = test_client.post("/session/9999/user/0/toggle_admin")
 
     assert result.status_code == 404
@@ -144,58 +101,36 @@ def test_toggle_admin_status_doesnt_exist(test_client, solo_two_world_session_se
 
 
 @pytest.mark.parametrize("initial_admin", [False, True])
-def test_toggle_admin_status(test_client, solo_two_world_session_setup, initial_admin) -> None:
+def test_toggle_admin_status(test_client, solo_two_world_session, initial_admin) -> None:
     # Setup
-    def setup() -> database.MultiplayerSession:
-        session = solo_two_world_session_setup()
-
-        member = database.MultiplayerMembership.get_by_ids(1234, 1)
-        member.admin = initial_admin
-        member.save()
-
-        return session
-
-    _handle_db_nonsense(test_client, setup)
+    membership = database.MultiplayerMembership.get_by_ids(1234, 1)
+    membership.admin = initial_admin
+    membership.save()
 
     # Run
-    test_client.post("/setup-db")
     result = test_client.post("/session/1/user/1234/toggle_admin")
 
     # Assert
-    @test_client.sa.app.get("/check-toggled")
-    def check_toggled():
-        membership = database.MultiplayerMembership.get_by_ids(1234, 1)
-        assert membership.admin is not initial_admin
-
-    @test_client.sa.app.get("/check-new-audit")
-    def check_new_audit():
-        session = database.MultiplayerSession.get_by_id(1)
-        audit_log = session.get_audit_log()
-        last_entry = audit_log.entries[-1]
-        assert f"Randovania Team made The Name{' not' if initial_admin else ''} an Admin" == last_entry.message
-
     assert (
         "Admin status of <code>The Name</code> toggled. <a href='http://testserver/session/1'>Return to session</a>"
         in result.text
     )
-    test_client.get("/check-toggled")
-    test_client.get("/check-new-audit")
+
+    # Fetch from db again
+    membership = database.MultiplayerMembership.get_by_ids(1234, 1)
+    assert membership.admin is not initial_admin
+
+    audit_log = solo_two_world_session.get_audit_log()
+    last_entry = audit_log.entries[-1]
+    assert f"Randovania Team made The Name{' not' if initial_admin else ''} an Admin" == last_entry.message
 
 
-def test_delete_session_post(test_client, solo_two_world_session_setup) -> None:
-    # Setup
-    _handle_db_nonsense(test_client, solo_two_world_session_setup)
-
-    @test_client.sa.app.get("/check-deleted")
-    def check_deleted():
-        database.MultiplayerSession.get_by_id(1)
-
+def test_delete_session_post(test_client, solo_two_world_session) -> None:
     # Run
-    test_client.post("/setup-db")
     result = test_client.post("/session/1/delete")
 
     # Assert
     assert "Session deleted. <a href='http://testserver/sessions'>Return to list</a>" in result.text
 
     with pytest.raises(database.MultiplayerSession.DoesNotExist):
-        test_client.get("/check-deleted")
+        database.MultiplayerSession.get_by_id(1)
