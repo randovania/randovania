afe2aa9b048d1dbb13d136465acc8aa20dc0fd70
UltiNaruto <zenaruto@ymail.com>
2025-11-21 20:59:48 +0100

    Planets (Zebeth): Make use of set_combo_with_value

--- randovania/games/planets_zebeth/gui/dialog/cosmetic_patches_dialog.py
+++ randovania/games/planets_zebeth/gui/dialog/cosmetic_patches_dialog.py
@@ -11,6 +11,7 @@ from randovania.games.planets_zebeth.layout.planets_zebeth_cosmetic_patches impo
     PlanetsZebethRoomGuiType,
 )
 from randovania.gui.dialog.base_cosmetic_patches_dialog import BaseCosmeticPatchesDialog
+from randovania.gui.lib.signal_handling import set_combo_with_value
 
 if TYPE_CHECKING:
     from PySide6 import QtWidgets
@@ -50,7 +51,7 @@ class PlanetsZebethCosmeticPatchesDialog(
 
     def on_new_cosmetic_patches(self, patches: PlanetsZebethCosmeticPatches) -> None:
         self.disable_low_health_beeping_check.setChecked(patches.disable_low_health_beeping)
-        self.room_name_dropdown.setCurrentIndex(PlanetsZebethRoomGuiType.get_index(patches.show_room_names))
+        set_combo_with_value(self.room_name_dropdown, patches.show_room_names)
         self.show_unexplored_map_check.setChecked(patches.show_unexplored_map)
         self.use_alternative_escape_theme_check.setChecked(patches.use_alternative_escape_theme)
         self.use_sm_boss_theme_check.setChecked(patches.use_sm_boss_theme)
