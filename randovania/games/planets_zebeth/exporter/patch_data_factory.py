e440225405947272a3b57910af13352f9955b544
UltiNaruto <zenaruto@ymail.com>
2025-11-21 22:33:45 +0100

    Planets (Zebeth): QoL patches support

--- randovania/games/planets_zebeth/exporter/patch_data_factory.py
+++ randovania/games/planets_zebeth/exporter/patch_data_factory.py
@@ -101,14 +101,16 @@ class PlanetsZebethPatchDataFactory(PatchDataFactory[PlanetsZebethConfiguration,
         }
 
     def _create_game_config_dict(self):
+        c = self.patches.configuration
+        assert isinstance(c, PlanetsZebethConfiguration)
         return {
             "starting_room": self._create_starting_location(),
             "seed_identifier": self._create_hash_dict(),
             "starting_items": self._create_starting_items_dict(),
             "starting_memo": self._create_starting_memo(),
-            "warp_to_start": False,
-            "open_missile_doors_with_one_missile": False,
-            "allow_downward_shots": False,
+            "warp_to_start": c.warp_to_start,
+            "open_missile_doors_with_one_missile": c.open_missile_doors_with_one_missile,
+            "allow_downward_shots": c.allow_downward_shots,
             "credits_string": self._credits_spoiler(),
         }
 
--- randovania/games/planets_zebeth/gui/preset_settings/planets_zebeth_patches_tab.py
+++ randovania/games/planets_zebeth/gui/preset_settings/planets_zebeth_patches_tab.py
@@ -1,12 +1,9 @@
 from __future__ import annotations
 
-import typing
 from typing import TYPE_CHECKING
 
-from PySide6 import QtWidgets
-
+from randovania.games.planets_zebeth.gui.generated.preset_planets_zebeth_patches_ui import Ui_PresetPlanetsZebethPatches
 from randovania.games.planets_zebeth.layout import PlanetsZebethConfiguration
-from randovania.gui.lib import signal_handling
 from randovania.gui.preset_settings.preset_tab import PresetTab
 
 if TYPE_CHECKING:
@@ -15,27 +12,21 @@ if TYPE_CHECKING:
     from randovania.interface_common.preset_editor import PresetEditor
     from randovania.layout.preset import Preset
 
-# TODO: add walljump, downward shooting and open missile doors with 1 missile patches
-_FIELDS = ["include_extra_pickups"]
+# TODO: add walljump patch
 
 
-class PresetPlanetsZebethPatches(PresetTab):
+class PresetPlanetsZebethPatches(PresetTab, Ui_PresetPlanetsZebethPatches):
     def __init__(self, editor: PresetEditor, game_description: GameDescription, window_manager: WindowManager):
         super().__init__(editor, game_description, window_manager)
 
-        self.root_widget = QtWidgets.QWidget(self)
-        self.root_layout = QtWidgets.QVBoxLayout(self.root_widget)
-
-        self.include_extra_pickups_check = QtWidgets.QCheckBox(self.root_widget)
-        self.include_extra_pickups_check.setEnabled(True)
-        self.include_extra_pickups_check.setText("Include Extra Pickups")
-        self.root_layout.addWidget(self.include_extra_pickups_check)
-
-        self.setCentralWidget(self.root_widget)
+        self.setupUi(self)
 
         # Signals
-        for f in _FIELDS:
-            self._add_persist_option(getattr(self, f"{f}_check"), f)
+        self.warp_to_start_check.stateChanged.connect(self._persist_option_then_notify("warp_to_start"))
+        self.open_missile_doors_with_one_missile_check.stateChanged.connect(
+            self._persist_option_then_notify("open_missile_doors_with_one_missile")
+        )
+        self.allow_downward_shots_check.stateChanged.connect(self._persist_option_then_notify("allow_downward_shots"))
 
     @classmethod
     def tab_title(cls) -> str:
@@ -45,15 +36,9 @@ class PresetPlanetsZebethPatches(PresetTab):
     def header_name(cls) -> str | None:
         return None
 
-    def _add_persist_option(self, check: QtWidgets.QCheckBox, attribute_name: str) -> None:
-        def persist(value: bool) -> None:
-            with self._editor as editor:
-                editor.set_configuration_field(attribute_name, value)
-
-        signal_handling.on_checked(check, persist)
-
     def on_preset_changed(self, preset: Preset) -> None:
         config = preset.configuration
         assert isinstance(config, PlanetsZebethConfiguration)
-        for f in _FIELDS:
-            typing.cast("QtWidgets.QCheckBox", getattr(self, f"{f}_check")).setChecked(getattr(config, f))
+        self.warp_to_start_check.setChecked(config.warp_to_start)
+        self.open_missile_doors_with_one_missile_check.setChecked(config.open_missile_doors_with_one_missile)
+        self.allow_downward_shots_check.setChecked(config.allow_downward_shots)
