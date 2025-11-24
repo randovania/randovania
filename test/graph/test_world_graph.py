034378597ef22e87f45364670b30274f786bf3c2
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-11-24 22:21:07 +0200

    Change HintNode tests to with WorldGraph

--- test/graph/test_world_graph.py
+++ test/graph/test_world_graph.py
@@ -1,6 +1,17 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+from unittest.mock import MagicMock
+
+import pytest
+
+from randovania.game_description.db.node import NodeContext
+from randovania.game_description.db.node_identifier import NodeIdentifier
+from randovania.game_description.resources.resource_collection import ResourceCollection
 from randovania.graph import world_graph
+
+if TYPE_CHECKING:
+    from randovania.game_description.resources.resource_info import ResourceInfo
 
 
 def test_create_graph(
@@ -110,3 +121,41 @@ def test_create_graph(
 #     assert result_2 == [
 #         (node_1, RequirementAnd([Requirement.trivial(), simple(NodeResourceInfo.from_node(node_2, context))])),
 #     ]
+
+
+@pytest.fixture(params=[False, True])
+def hint_node(request, blank_game_description, blank_world_graph):
+    has_translator = request.param
+    translator = blank_game_description.resource_database.get_item("BlueKey")
+
+    identifier = NodeIdentifier.create(
+        "Intro",
+        "Hint Room",
+        "Hint with Translator" if has_translator else "Hint no Translator",
+    )
+    node = blank_world_graph.node_identifier_to_node[identifier]
+
+    return has_translator, translator, node
+
+
+def test_hint_node_should_collect(hint_node, empty_patches, blank_world_graph):
+    # Setup
+    db = empty_patches.game.resource_database
+    has_translator, translator, node = hint_node
+    node_provider = MagicMock()
+
+    def ctx(*args: ResourceInfo):
+        resources = ResourceCollection.from_dict(db, dict.fromkeys(args, 1))
+        return NodeContext(empty_patches, resources, db, node_provider)
+
+    assert node.requirement_to_collect.satisfied(ctx(), 0) != has_translator
+    assert node.requirement_to_collect.satisfied(ctx(translator), 0)
+
+    assert not node.has_all_resources(ctx())
+    assert not node.has_all_resources(ctx(translator))
+
+    resource = blank_world_graph.resource_info_for_node(node)
+    assert node.has_all_resources(ctx(resource))
+    assert node.has_all_resources(ctx(resource, translator))
+
+    assert node.resource_gain == [(resource, 1)]
