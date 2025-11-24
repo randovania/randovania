2194fc0be8d7e0ea01c80a0c8b37f3e7e50d10e1
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-11-24 21:45:37 +0200

    Rename

--- randovania/resolver/resolver.py
+++ randovania/resolver/resolver.py
@@ -446,8 +446,8 @@ def setup_resolver(
 
     game.resource_database = bootstrap.patch_resource_database(game.resource_database, configuration)
 
-    game_or_graph, starting_state = bootstrap.logic_bootstrap_graph(configuration, game, patches)
-    logic = Logic(game_or_graph, configuration)
+    graph, starting_state = bootstrap.logic_bootstrap_graph(configuration, game, patches)
+    logic = Logic(graph, configuration)
     logic.record_paths = record_paths
 
     return starting_state, logic
