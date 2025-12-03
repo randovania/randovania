b8ba47a81af2d8c8ea6b5e09427a5771a5db298b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2025-12-03 19:11:14 +0200

    Mypy fix

--- randovania/interface_common/preset_manager.py
+++ randovania/interface_common/preset_manager.py
@@ -41,7 +41,7 @@ def _commit(message: str, file_path: Path, repository: Path, remove: bool):
             Path(r.index_path()).unlink()
             r.reset_index()
 
-    author = "randovania <nobody@example.com>"
+    author = b"randovania <nobody@example.com>"
     if remove:
         dulwich.porcelain.remove(repository, [file_path])
     else:
