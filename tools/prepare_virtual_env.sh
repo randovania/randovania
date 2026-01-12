29ea5a7b93f51d882a5155f2c8f886d35eb46f1f
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-01-13 00:17:52 +0200

    Fix prepare_virtual_env building too much
    - Don't create the venv to run that script
    - Properly handle the native mode flag not being set
    - Don't change the native mode if no argument was set

--- tools/prepare_virtual_env.sh
+++ tools/prepare_virtual_env.sh
@@ -6,4 +6,4 @@ cd -- "$( dirname -- "${BASH_SOURCE[0]}" )/.."
 if ! which uv > /dev/null; then
   curl -LsSf https://astral.sh/uv/install.sh | sh
 fi
-uv run tools/prepare_virtual_env.py "$@"
+uv run --no-project tools/prepare_virtual_env.py "$@"
