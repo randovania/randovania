1ecda543a2c85904c0e97f390f43e8db6448a2bb
StalledStorm <112809386+StalledStorm@users.noreply.github.com>
2026-02-16 20:28:44 +0000

    Revert "Merge remote-tracking branch 'upstream/main'"
    This reverts commit 3c2c8a4e9b6ba7b4244a75a5de2b7a383524c9f1, reversing
    changes made to 6cd8fd7d4d0b14122ed8746cf782080c6871752b.

--- randovania/gui/generated/preset_trick_level_ui.py
+++ randovania/gui/generated/preset_trick_level_ui.py
@@ -34,8 +34,8 @@ class Ui_PresetTrickLevel(object):
         self.verticalLayout.setContentsMargins(0, 0, 0, 0)
         self.trick_level_scroll = QScrollArea(self.centralWidget)
         self.trick_level_scroll.setObjectName(u"trick_level_scroll")
-        self.trick_level_scroll.setFrameShape(QFrame.Shape.NoFrame)
-        self.trick_level_scroll.setFrameShadow(QFrame.Shadow.Plain)
+        self.trick_level_scroll.setFrameShape(QFrame.NoFrame)
+        self.trick_level_scroll.setFrameShadow(QFrame.Plain)
         self.trick_level_scroll.setWidgetResizable(True)
         self.trick_level_scroll_contents = QWidget()
         self.trick_level_scroll_contents.setObjectName(u"trick_level_scroll_contents")
@@ -47,7 +47,7 @@ class Ui_PresetTrickLevel(object):
         self.trick_level_layout.setContentsMargins(4, 8, 4, 0)
         self.logic_description_label = QLabel(self.trick_level_scroll_contents)
         self.logic_description_label.setObjectName(u"logic_description_label")
-        self.logic_description_label.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.logic_description_label.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
         self.logic_description_label.setWordWrap(True)
 
         self.trick_level_layout.addWidget(self.logic_description_label)
@@ -66,7 +66,7 @@ class Ui_PresetTrickLevel(object):
 
         self.underwater_abuse_label = QLabel(self.trick_level_scroll_contents)
         self.underwater_abuse_label.setObjectName(u"underwater_abuse_label")
-        self.underwater_abuse_label.setAlignment(Qt.AlignmentFlag.AlignLeading|Qt.AlignmentFlag.AlignLeft|Qt.AlignmentFlag.AlignTop)
+        self.underwater_abuse_label.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignTop)
         self.underwater_abuse_label.setWordWrap(True)
 
         self.trick_level_layout.addWidget(self.underwater_abuse_label)
@@ -97,7 +97,7 @@ class Ui_PresetTrickLevel(object):
 
     def retranslateUi(self, PresetTrickLevel):
         PresetTrickLevel.setWindowTitle(QCoreApplication.translate("PresetTrickLevel", u"Trick Level", None))
-        self.logic_description_label.setText(QCoreApplication.translate("PresetTrickLevel", u"<html><head/><body><p>Randovania has rules in place which guarantee that your game is completable regardless of the modifications made to it. Here you can also configure what level of skill it expects you to have, allowing for even more varied games.</p><p>No matter your settings, it is always possible to softlock when you enter a room or area that you're unable to leave. For example: passing through a door with a different lock on the other side, or jumping down from a ledge that's too tall to jump back up again. These are commonly known as &quot;point of no returns,&quot; or PoNRs for short.</p><p>However, it may be necessary to pass through a point of no return if a required pickup is placed behind it. For this reason, it is recommended to save often, especially before passing through a door or obstacle you may not be able to come back through.</p></body></html>", None))
+        self.logic_description_label.setText(QCoreApplication.translate("PresetTrickLevel", u"<html><head/><body><p align=\"justify\">Randovania has rules in place which guarantees that the game is completable regardless of the modifications made to the game. Here you can also configure which kind of game knowledge or skill it expects you to have, allowing for even more varied games.</p><p align=\"justify\">No matter the level, it is always possible to softlock when you enter a room or area that you're unable to leave. For example, vanilla beam rooms without the necessary beam to escape, Dark World without Light Beam/Anihhilator Beam, Torvus Bog without Super Missile.</p><p align=\"justify\">However, it may be <span style=\" font-style:italic;\">necessary</span> to enter Dark World without a way to escape if that item is located in the Dark World.</p></body></html>", None))
         self.underwater_abuse_check.setText(QCoreApplication.translate("PresetTrickLevel", u"Allow Dangerous Gravity Suit Logic", None))
         self.underwater_abuse_label.setText(QCoreApplication.translate("PresetTrickLevel", u"<html><head/><body><p> Allows the generator to require tricks which are only possible prior to Gravity Suit being obtained. <a href=\"resource-details://misc/gravity\"><span style=\" text-decoration: underline; color:#0000ff;\">Click here</span></a> to see which rooms are affected.</p><p>Enabling this option adds these alternatives to logic. To prevent the creation of an uncompletable save file, it is advised that you not pick up Gravity Suit until these rooms are cleared. </p></body></html>", None))
         self.trick_level_help_label.setText(QCoreApplication.translate("PresetTrickLevel", u"<html><head/><body><p>If you want to tweak the knowledge or skill needed expected in a game, you can configure the level used for each of the tweaks listed below by moving the slider to the apropriate level.</p><p>Press the ? button to see which rooms use that trick on the selected level.</p></body></html>", None))
