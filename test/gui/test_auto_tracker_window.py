ad5645535d895f2a6fef8106b518b66f959b7b4b
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-03-06 00:48:51 +0200

    Fixes for new PySide6

--- test/gui/test_auto_tracker_window.py
+++ test/gui/test_auto_tracker_window.py
@@ -115,15 +115,20 @@ def test_update_sources_combo(skip_qtbot):
 
 
 @pytest.mark.parametrize("correct_source", [False, True])
-def test_on_game_state_updated(skip_qtbot, mocker, correct_source):
+def test_on_game_state_updated(skip_qtbot, correct_source):
     # Setup
     inventory = MagicMock()
     connection = MagicMock()
-    connector = connection.get_connector_for_builder.return_value
-    mocker.patch("randovania.gui.auto_tracker_window.AutoTrackerWindow.create_tracker")
+    connector = MagicMock()
+    # Return None during __init__ so create_tracker() exits early without crashing
+    connection.get_connector_for_builder.return_value = None
 
     # Run
     window = AutoTrackerWindow(connection, None, MagicMock())
+    # Now set up the real connector and reset call history from init
+    connection.get_connector_for_builder.return_value = connector
+    connection.get_connector_for_builder.reset_mock()
+    window.create_tracker = MagicMock()
     window.selected_builder = MagicMock()
     window.item_tracker = MagicMock()
     skip_qtbot.addWidget(window)
--- test/gui/test_main_window.py
+++ test/gui/test_main_window.py
@@ -15,7 +15,6 @@ from randovania.gui.lib.qt_network_client import QtNetworkClient
 from randovania.gui.main_window import MainWindow
 from randovania.gui.multiplayer_session_window import MultiplayerSessionWindow
 from randovania.gui.widgets.about_widget import AboutWidget
-from randovania.gui.widgets.changelog_widget import ChangeLogWidget
 from randovania.gui.widgets.dependencies_widget import DependenciesWidget
 from randovania.gui.widgets.randovania_help_widget import RandovaniaHelpWidget
 from randovania.gui.widgets.reporting_optout_widget import ReportingOptOutWidget
@@ -200,10 +199,9 @@ def test_on_menu_action_help(default_main_window, monkeypatch):
 
 
 @pytest.mark.parametrize("has_changelog", [False, True])
-def test_on_menu_action_changelog(default_main_window, monkeypatch, has_changelog):
+async def test_on_menu_action_changelog(default_main_window, monkeypatch, has_changelog):
     mock_show = MagicMock()
     monkeypatch.setattr(QtWidgets.QWidget, "show", mock_show)
-    monkeypatch.setattr(ChangeLogWidget, "setup_labels", MagicMock())
     if has_changelog:
         default_main_window.all_change_logs = {}
 
