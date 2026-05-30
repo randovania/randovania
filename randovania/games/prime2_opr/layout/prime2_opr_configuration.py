b38f59328350e2555c5e91b2953e37cf3d28d3e3
duncathan <dunc@duncathan.com>
2026-05-30 16:23:39 -0600

    configuration and cosmetic patches

--- randovania/games/prime2_opr/layout/prime2_opr_configuration.py
+++ randovania/games/prime2_opr/layout/prime2_opr_configuration.py
@@ -3,22 +3,46 @@ from __future__ import annotations
 import dataclasses
 
 from randovania.game.game_enum import RandovaniaGame
+from randovania.games.prime2.layout.beam_configuration import BeamConfiguration
+from randovania.games.prime2.layout.echoes_configuration import LayoutSafeZone, LayoutSkyTempleKeyMode
+from randovania.games.prime2.layout.echoes_teleporters import EchoesTeleporterConfiguration
+from randovania.games.prime2.layout.translator_configuration import TranslatorConfiguration
 from randovania.layout.base.base_configuration import BaseConfiguration
 
 
 @dataclasses.dataclass(frozen=True)
 class EchoesOPRConfiguration(BaseConfiguration):
-    # These fields aren't necessary for a new game: they're here to have example/test features
-    include_extra_pickups: bool
+    teleporters: EchoesTeleporterConfiguration
+    sky_temple_keys: LayoutSkyTempleKeyMode
+    translator_configuration: TranslatorConfiguration
+    beam_configuration: BeamConfiguration
+    energy_per_tank: int = dataclasses.field(metadata={"min": 1, "max": 1000, "precision": 1})
+    safe_zone: LayoutSafeZone
+
+    varia_suit_damage: float = dataclasses.field(metadata={"min": 0.1, "max": 60.0, "precision": 3.0})
+    dark_suit_damage: float = dataclasses.field(metadata={"min": 0.0, "max": 60.0, "precision": 3.0})
+    dangerous_energy_tank: bool
+
+    inverted_mode: bool
+
+    blue_save_doors: bool
 
     @classmethod
     def game_enum(cls) -> RandovaniaGame:
         return RandovaniaGame.METROID_PRIME_ECHOES_OPR
 
-    def active_layers(self) -> set[str]:
-        result = super().active_layers()
+    def dangerous_settings(self) -> list[str]:
+        result = super().dangerous_settings()
 
-        if self.include_extra_pickups:
-            result.add("extra_pickups")
+        if self.dangerous_energy_tank:
+            result.append("1 HP Mode")
+
+        return result
+
+    def unsupported_features(self) -> list[str]:
+        result = super().unsupported_features()
+
+        if self.inverted_mode:
+            result.append("Inverted Aether")
 
         return result
--- randovania/games/prime2_opr/layout/prime2_opr_cosmetic_patches.py
+++ randovania/games/prime2_opr/layout/prime2_opr_cosmetic_patches.py
@@ -3,11 +3,15 @@ from __future__ import annotations
 import dataclasses
 
 from randovania.game.game_enum import RandovaniaGame
-from randovania.layout.base.cosmetic_patches import BaseCosmeticPatches
+from randovania.games.prime2.layout.echoes_cosmetic_patches import BaseEchoesCosmeticPatches
 
 
 @dataclasses.dataclass(frozen=True)
-class EchoesOPRCosmeticPatches(BaseCosmeticPatches):
+class EchoesOPRCosmeticPatches(BaseEchoesCosmeticPatches):
+    @classmethod
+    def default(cls) -> EchoesOPRCosmeticPatches:
+        return cls()
+
     @classmethod
     def game(cls) -> RandovaniaGame:
         return RandovaniaGame.METROID_PRIME_ECHOES_OPR
