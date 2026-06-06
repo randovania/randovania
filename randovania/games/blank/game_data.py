6704c64611c70896b6120d86bdb7e12299f95a27
Henrique Gemignani Passos Lima <henrique@gemignani.org>
2026-06-06 20:03:02 +0300

    Add reject_undocumented_tricks_in_database to GameData

--- randovania/games/blank/game_data.py
+++ randovania/games/blank/game_data.py
@@ -120,6 +120,7 @@ game_data: randovania.game.data.GameData = randovania.game.data.GameData(
     patch_data_factory=_patch_data_factory,
     exporter=_exporter,
     test_data=_test_data,
+    reject_undocumented_tricks_in_database=True,
     multiple_start_nodes_per_area=True,
     defaults_available_in_game_sessions=randovania.is_dev_version(),
 )
