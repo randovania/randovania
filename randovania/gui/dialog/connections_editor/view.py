d7a6f593fedf20f5d960aa682f7c0973c97b1731
JustinDMS <90155396+JustinDMS@users.noreply.github.com>
2026-07-26 17:33:08 -0500

    Add custom delegate for drawing array requirements
    Mimics the styling used by `connections_visualizer.py` by emboldening comments with a green color

--- randovania/gui/dialog/connections_editor/view.py
+++ randovania/gui/dialog/connections_editor/view.py
@@ -1,9 +1,20 @@
 from typing import cast
 
-from PySide6.QtCore import QItemSelectionModel, QModelIndex, QObject, Signal
-from PySide6.QtWidgets import QAbstractItemView, QTreeView, QWidget
+from PySide6.QtCore import QItemSelectionModel, QModelIndex, QObject, QPersistentModelIndex, QSize, Qt, Signal
+from PySide6.QtGui import QAbstractTextDocumentLayout, QPainter, QPalette, QTextDocument
+from PySide6.QtWidgets import (
+    QAbstractItemView,
+    QApplication,
+    QStyle,
+    QStyledItemDelegate,
+    QStyleOptionViewItem,
+    QTreeView,
+    QWidget,
+)
 
-from .model import RequirementModel
+from randovania.game_description.requirements.array_base import RequirementArrayBase
+
+from .model import ROLE, RequirementModel
 from .path import Path
 
 
@@ -14,6 +25,7 @@ class RequirementView(QObject):
         super().__init__(parent)
 
         tree.setModel(model)
+        tree.setItemDelegate(RequirementDelegate(tree))
         tree.expandAll()
 
         tree.setSelectionMode(QAbstractItemView.SelectionMode.SingleSelection)
@@ -37,3 +49,72 @@ class RequirementView(QObject):
 
         self._tree.setFocus()
         self._tree.scrollTo(index, QAbstractItemView.ScrollHint.EnsureVisible)
+
+
+# The liberal use of ignore[attr-defined] is due to the lack of accessor functions - see details below
+# https://doc.qt.io/qtforpython-6/PySide6/QtWidgets/QStyleOption.html#PySide6.QtWidgets.QStyleOption
+class RequirementDelegate(QStyledItemDelegate):
+    """Renders Array comments in bold green"""
+
+    def _build_document(
+        self, option: QStyleOptionViewItem, index: QModelIndex | QPersistentModelIndex
+    ) -> QTextDocument | None:
+        requirement = index.data(ROLE)
+        if not isinstance(requirement, RequirementArrayBase) or requirement.comment is None:
+            return None
+
+        text: str = index.data(Qt.ItemDataRole.DisplayRole)
+        array_type, _, comment = text.partition("\n")
+
+        # Resolve text color for array type
+        # Without this, text color defaults to black with no regard for light/dark mode or selection state
+        color_group = (
+            QPalette.ColorGroup.Normal
+            if option.state & QStyle.StateFlag.State_Enabled  # type: ignore[attr-defined]
+            else QPalette.ColorGroup.Disabled
+        )
+        color_role = (
+            QPalette.ColorRole.HighlightedText
+            if option.state & QStyle.StateFlag.State_Selected  # type: ignore[attr-defined]
+            else QPalette.ColorRole.Text
+        )
+        text_color = option.palette.color(color_group, color_role)  # type: ignore[attr-defined]
+
+        html: str = (
+            f"<span style='color:{text_color.name()}'>{array_type}</span>"
+            f"<br><span style='font-weight: bold; color:green'>{comment}</span>"
+        )
+
+        document = QTextDocument()
+        document.setHtml(html)
+        document.setDefaultFont(option.font)  # type: ignore[attr-defined]
+        document.setTextWidth(option.rect.width())  # type: ignore[attr-defined]
+
+        return document
+
+    def paint(
+        self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex | QPersistentModelIndex
+    ) -> None:
+        document = self._build_document(option, index)
+        if document is None:
+            super().paint(painter, option, index)
+            return
+
+        _option = QStyleOptionViewItem(option)
+        self.initStyleOption(_option, index)
+        _option.text = ""  # type: ignore[attr-defined]
+
+        style: QStyle = _option.widget.style() if _option.widget else QApplication.style()  # type: ignore[attr-defined]
+        style.drawControl(QStyle.ControlElement.CE_ItemViewItem, _option, painter, _option.widget)  # type: ignore[attr-defined]
+
+        painter.save()
+        text_rect = style.subElementRect(QStyle.SubElement.SE_ItemViewItemText, _option, _option.widget)  # type: ignore[attr-defined]
+        painter.translate(text_rect.topLeft())
+        document.documentLayout().draw(painter, QAbstractTextDocumentLayout.PaintContext())
+        painter.restore()
+
+    def sizeHint(self, option: QStyleOptionViewItem, index: QModelIndex | QPersistentModelIndex) -> QSize:
+        document = self._build_document(option, index)
+        if document is None:
+            return super().sizeHint(option, index)
+        return document.size().toSize()
