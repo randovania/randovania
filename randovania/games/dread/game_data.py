d28a040170146dfe705e8066e93913976920ab7f
toasterparty <toasterparty@derpymail.org>
2026-08-03 14:08:13 -0700

    randovania development update-committed

--- randovania/games/dread/game_data.py
+++ randovania/games/dread/game_data.py
@@ -88,7 +88,7 @@ def _test_data() -> randovania.game.game_test_data.GameTestData:
     from randovania.layout.base.trick_level import LayoutTrickLevel
 
     return randovania.game.game_test_data.GameTestData(
-        expected_seed_hash="4AJTCCCQ",
+        expected_seed_hash="W5LMA3PN",
         # Some items require shinesparking to reach in vanilla,
         # which due to varying difficulty has been made into a trick
         database_collectable_include_tricks=(("Speedbooster", LayoutTrickLevel.BEGINNER),),
