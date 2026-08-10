b2e74056d989e4ca340a5acd458048c49d41f70d
Tyranisaur <t.w.skogen@gmail.com>
2026-08-10 19:37:49 +0200

    Update expected seed hash

--- randovania/games/prime1/game_data.py
+++ randovania/games/prime1/game_data.py
@@ -107,7 +107,7 @@ def _hash_words() -> list[str]:
 
 def _test_data() -> randovania.game.game_test_data.GameTestData:
     return randovania.game.game_test_data.GameTestData(
-        expected_seed_hash="LHK6D3PS",
+        expected_seed_hash="GFCNWA5G",
         database_collectable_ignore_events=("Event33",),
     )
 
