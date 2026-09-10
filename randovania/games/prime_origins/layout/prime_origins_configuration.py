17cbf41680ca1bc890cb1ecdf60acd6ae56f073e
Steven Franklin <contact@stevenfranklin.dev>
2026-09-09 19:42:16 -0500

    wip

--- randovania/games/prime_origins/layout/prime_origins_configuration.py
+++ randovania/games/prime_origins/layout/prime_origins_configuration.py
@@ -1,24 +1,26 @@
 from __future__ import annotations
 
 import dataclasses
+from enum import StrEnum
 
+from randovania.bitpacking.bitpacking import BitPackEnum
 from randovania.game.game_enum import RandovaniaGame
+from randovania.games.prime_origins.layout.artifact_mode import LayoutArtifactMode
 from randovania.layout.base.base_configuration import BaseConfiguration
+
+
+class InGameMode(BitPackEnum, StrEnum):
+    REMIX = "Remix"
+    CLASSIC = "Classic"
+    Any = "Any"
 
 
 @dataclasses.dataclass(frozen=True)
 class MPOConfiguration(BaseConfiguration):
-    # These fields aren't necessary for a new game: they're here to have example/test features
-    include_extra_pickups: bool
+    artifact_target: LayoutArtifactMode
+    artifact_minimum_progression: LayoutArtifactMode
+    game_mode: InGameMode
 
     @classmethod
     def game_enum(cls) -> RandovaniaGame:
         return RandovaniaGame.PRIME_ORIGINS
-
-    def active_layers(self) -> set[str]:
-        result = super().active_layers()
-
-        if self.include_extra_pickups:
-            result.add("extra_pickups")
-
-        return result
