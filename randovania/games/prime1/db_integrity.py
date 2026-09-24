b7f1e081477677dd231317d25117c7b81959ba43
Miepee <janbidler00@protonmail.com>
2026-09-24 13:51:00 +0200

    Prime 1: Add more missing internal coordinates

--- randovania/games/prime1/db_integrity.py
+++ randovania/games/prime1/db_integrity.py
@@ -23,6 +23,32 @@ combined_resources = {
 }
 
 
+def check_for_existing_world_coords(game: GameDescription) -> Iterator[str]:
+    for region, area, source_node in game.node_iterator():
+        # TODO: Impact crater and frigate aren't filled in yet hence ignored.
+        # Credits doesn't have coordinates.
+        if region.name in ["Impact Crater", "Frigate Orpheon", "End of Game"]:
+            continue
+        # These can spawn anywhere in the room and don't have set coordinates.
+        if "items_every_room" in source_node.layers:
+            continue
+        # Ignore automatically generated nodes
+        if source_node.is_derived_node:
+            continue
+
+        if "world_position" not in source_node.extra:
+            yield (
+                f"{source_node.full_name()} is missing the 'world_position' extra field which describes "
+                f"internal world coordinates (X, Y, Z). You can get them from PrimeWorldEditor."
+            )
+        elif len(source_node.extra["world_position"]) != 3:
+            yield (
+                f"The world_position of {source_node.full_name()} are incorrect. You need 3 entries"
+                f" for X, Y and Z respectively."
+            )
+
+
 def find_prime_db_errors(game: GameDescription) -> Iterator[str]:
     yield from check_for_items_to_be_replaced_by_templates(game, use_templates_over_items)
     yield from check_for_resources_to_use_together(game, combined_resources)
+    yield from check_for_existing_world_coords(game)
